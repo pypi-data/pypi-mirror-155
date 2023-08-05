0359d7075f55f50c9814bb71255fd71678ea43d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:18 +0000

    Add package notedrive/0.6.0/notedrive-0.6.0-py3.9.egg

---
+++ packages/notedrive/notedrive-0.6.0-py3.9.egg/notedrive/sqlalchemy/base.py
@@ -0,0 +1,66 @@
+import warnings
+
+from sqlalchemy import MetaData, Table, create_engine
+
+meta = MetaData()
+
+
+def create_engines(url, **kwargs):
+    return create_engine(url, **kwargs)
+
+
+# class BaseTable:
+#     def __init__(self, table_name, *args, **kwargs):
+#         self.table_name = table_name
+#         self.table: Table = None
+#         meta.create_all(engine)
+
+#     def insert(self, values, keys=None, *args, **kwargs):
+#         meta.create_all(engine)
+#         cols = [col.name for col in self.table.columns]
+#         if isinstance(values, dict):
+#             values = dict([(k, v) for k, v in values.items() if k in cols])
+#         elif isinstance(values, list):
+#             if isinstance(values[0], dict):
+#                 values = [dict([(k, v) for k, v in item.items() if k in cols]) for item in values]
+#             elif isinstance(values[0], list):
+#                 values = [dict(zip(keys, item)) for item in values]
+
+#         with warnings.catch_warnings():
+#             warnings.simplefilter("ignore")
+#             # code here...
+
+#             if str(engine.url).startswith('sqlite'):
+#                 ins = self.table.insert(values=values).prefix_with("OR IGNORE")
+#             else:
+#                 ins = self.table.insert(values=values).prefix_with("IGNORE")
+#             engine.execute(ins)
+
+
+class BaseTable:
+    def __init__(self, table_name, engine, *args, **kwargs):
+        self.table_name = table_name
+        self.table: Table = None
+        self.engine = engine
+
+    def create(self):
+        meta.create_all(self.engine)
+
+    def insert(self, values, keys=None, *args, **kwargs):
+        cols = [col.name for col in self.table.columns]
+        if isinstance(values, dict):
+            values = dict([(k, v) for k, v in values.items() if k in cols])
+        elif isinstance(values, list):
+            if isinstance(values[0], dict):
+                values = [dict([(k, v) for k, v in item.items() if k in cols]) for item in values]
+            elif isinstance(values[0], list):
+                values = [dict(zip(keys, item)) for item in values]
+
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            # code here...
+            if str(self.engine.url).startswith('sqlite'):
+                ins = self.table.insert(values=values).prefix_with("OR IGNORE")
+            else:
+                ins = self.table.insert(values=values).prefix_with("IGNORE")
+            self.engine.execute(ins)
