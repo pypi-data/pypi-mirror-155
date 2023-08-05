ede6c1711190568a17aa38a246d75627d79531fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:50 +0000

    Add package glqiwiapi/2.11/glQiwiApi-2.11.tar.gz

---
+++ packages/glqiwiapi/glQiwiApi-2.11.tar.gz/glQiwiApi-2.11/glQiwiApi/yoo_money/types/__init__.py
@@ -0,0 +1,17 @@
+from .types import (
+    AccountInfo,
+    IncomingTransaction,
+    Operation,
+    OperationDetails,
+    Payment,
+    RequestPaymentResponse,
+)
+
+__all__ = (
+    "AccountInfo",
+    "Operation",
+    "OperationDetails",
+    "RequestPaymentResponse",
+    "Payment",
+    "IncomingTransaction",
+)
