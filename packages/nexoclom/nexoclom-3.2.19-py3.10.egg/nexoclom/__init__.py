5f411b83888edfb42f0f271db58efc7f72ece4b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:26 +0000

    Add package nexoclom/3.2.19/nexoclom-3.2.19-py3.10.egg

---
+++ packages/nexoclom/nexoclom-3.2.19-py3.10.egg/nexoclom/__init__.py
@@ -0,0 +1,15 @@
+from nexoclom.modelcode.Input import Input
+from nexoclom.modelcode.Output import Output
+from nexoclom.modelcode.LOSResult import LOSResult
+from nexoclom.modelcode.ModelImage import ModelImage
+from nexoclom.utilities.configure import configure
+from nexoclom.solarsystem import SSObject
+
+
+__name__ = 'nexoclom'
+__author__ = 'Matthew Burger'
+__email__ = 'mburger@stsci.edu'
+__version__ = '3.2.19'
+__date__ = '2022-06-14'
+
+configure(verbose=False)
