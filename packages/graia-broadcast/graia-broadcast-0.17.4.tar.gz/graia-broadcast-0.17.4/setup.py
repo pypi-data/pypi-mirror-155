7cceb07842208663539d18fb9dc5a9b7ac0f9c81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:26 +0000

    Add package graia-broadcast/0.17.4/graia-broadcast-0.17.4.tar.gz

---
+++ packages/graia-broadcast/graia-broadcast-0.17.4.tar.gz/graia-broadcast-0.17.4/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['graia',
+ 'graia.broadcast',
+ 'graia.broadcast.builtin',
+ 'graia.broadcast.entities',
+ 'graia.broadcast.interfaces',
+ 'graia.broadcast.interrupt']
+
+package_data = \
+{'': ['*']}
+
+extras_require = \
+{':python_version < "3.9"': ['typing-extensions>=3.10.0']}
+
+setup_kwargs = {
+    'name': 'graia-broadcast',
+    'version': '0.17.4',
+    'description': 'a highly customizable, elegantly designed event system based on asyncio',
+    'long_description': None,
+    'author': 'GreyElaina',
+    'author_email': 'GreyElaina@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'extras_require': extras_require,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
