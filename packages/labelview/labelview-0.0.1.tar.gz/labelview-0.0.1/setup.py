9d9d963a056f1538bda5149dd04dbc52df2a7dd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:42 +0000

    Add package labelview/0.0.1/labelview-0.0.1.tar.gz

---
+++ packages/labelview/labelview-0.0.1.tar.gz/labelview-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='labelview',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='',
+    author='Meghdad Farahmand',
+    author_email='meghdad.farahmand@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
