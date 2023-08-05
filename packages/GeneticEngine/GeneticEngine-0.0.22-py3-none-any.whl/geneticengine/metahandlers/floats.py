cbd0925b35028bb92e4731da051ed1033d084188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:06 +0000

    Add package GeneticEngine/0.0.22/GeneticEngine-0.0.22-py3-none-any.whl

---
+++ packages/GeneticEngine/GeneticEngine-0.0.22-py3-none-any.whl/geneticengine/metahandlers/floats.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+from typing import Any
+from typing import Callable
+from typing import Dict
+from typing import Type
+from typing import TypeVar
+
+from geneticengine.core.grammar import Grammar
+from geneticengine.core.random.sources import Source
+from geneticengine.metahandlers.base import MetaHandlerGenerator
+
+min = TypeVar("min", covariant=True)
+max = TypeVar("max", covariant=True)
+
+
+class FloatRange(MetaHandlerGenerator):
+    """
+    FloatRange(a,b) restricts floats to be between a and b.
+    The range can be dynamically altered before the grammar extraction (Float.__annotations__["value"] = Annotated[float, FloatRange(c,d)].
+    """
+
+    def __init__(self, min, max):
+        self.min = min
+        self.max = max
+
+    def generate(
+        self,
+        r: Source,
+        g: Grammar,
+        rec,
+        new_symbol,
+        depth: int,
+        base_type,
+        context: dict[str, str],
+    ):
+        rec(r.random_float(self.min, self.max, str(base_type)))
+
+    def __class_getitem__(self, args):
+        return FloatRange(*args)
+
+    def __repr__(self):
+        return f"[{self.min}...{self.max}]"
