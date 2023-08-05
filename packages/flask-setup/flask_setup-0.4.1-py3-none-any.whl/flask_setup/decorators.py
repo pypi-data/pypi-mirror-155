cf86288f6126a974441578914dc5637be2b4bbd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:42 +0000

    Add package flask-setup/0.4.1/flask_setup-0.4.1-py3-none-any.whl

---
+++ packages/flask-setup/flask_setup-0.4.1-py3-none-any.whl/flask_setup/decorators.py
@@ -0,0 +1,15 @@
+import os, typer
+
+from functools import wraps
+
+def before_command(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if not os.environ.get('VIRTUAL_ENV'):
+            typer.echo('Please run this command in a virtual environment')
+            return
+        elif not os.path.isfile('.fs'):
+            typer.echo('Project not initialized')
+            return
+        return f(*args, **kwargs)
+    return decorated
