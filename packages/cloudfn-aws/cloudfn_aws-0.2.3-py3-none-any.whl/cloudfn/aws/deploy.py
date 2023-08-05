a7fd992dc2ff99eb9ef35e74d2aa0e323ee0b7f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:59 +0000

    Add package cloudfn-aws/0.2.3/cloudfn_aws-0.2.3-py3-none-any.whl

---
+++ packages/cloudfn-aws/cloudfn_aws-0.2.3-py3-none-any.whl/cloudfn/aws/deploy.py
@@ -0,0 +1,9 @@
+"""cloudfn-aws deploy"""
+
+def deploy_fn():
+	"""AWS Function Deployer"""
+	print('AWS Function Deploy')
+
+def deploy_fn_layer():
+	"""AWS Layer Deployer"""
+	print('AWS Layer Deploy')
