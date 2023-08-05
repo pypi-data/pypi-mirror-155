a244f65f70cfc6e8c86f79259265feac814a6733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:05:58 +0000

    Add package Face-Anti-Spoofing-FT/1.0/Face_Anti_Spoofing_FT-1.0.tar.gz

---
+++ packages/Face-Anti-Spoofing-FT/Face_Anti_Spoofing_FT-1.0.tar.gz/Face_Anti_Spoofing_FT-1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from inference_onnx import __version__
+
+setup(
+    name='Face_Anti_Spoofing_FT',
+    version=__version__,
+
+    url='',
+    author='Thong Le',
+    author_email='thonglv@ftech.ai',
+
+    py_modules=['inference_onnx'],
+)
