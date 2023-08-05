8e8f8518376fc63cb5c93ad610fdafc953f193b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:56 +0000

    Add package pye3dc/0.7.3/pye3dc-0.7.3.tar.gz

---
+++ packages/pye3dc/pye3dc-0.7.3.tar.gz/pye3dc-0.7.3/e3dc/__init__.py
@@ -0,0 +1,25 @@
+"""E3DC Library for Python.
+
+Python class to connect to an E3/DC system.
+Copyright 2017 Francesco Santini <francesco.santini@gmail.com>.
+Licensed under a MIT license. See LICENSE for details.
+"""
+
+from ._e3dc import E3DC, AuthenticationError, PollError
+from ._e3dc_rscp_local import CommunicationError, RSCPAuthenticationError
+from ._e3dc_rscp_web import RequestTimeoutError, SocketNotReady
+from ._rscpLib import FrameError
+from ._rscpLib import set_debug as set_rscp_debug
+
+__all__ = [
+    "E3DC",
+    "AuthenticationError",
+    "PollError",
+    "CommunicationError",
+    "RSCPAuthenticationError",
+    "RequestTimeoutError",
+    "SocketNotReady",
+    "FrameError",
+    "set_rscp_debug",
+]
+__version__ = "0.7.3"
