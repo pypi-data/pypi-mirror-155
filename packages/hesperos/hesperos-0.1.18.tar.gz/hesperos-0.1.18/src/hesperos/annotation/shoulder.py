8fa3977c415607181ada65e3edf8392b0653e563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:14 +0000

    Add package hesperos/0.1.18/hesperos-0.1.18.tar.gz

---
+++ packages/hesperos/hesperos-0.1.18.tar.gz/hesperos-0.1.18/src/hesperos/annotation/shoulder.py
@@ -0,0 +1,70 @@
+# ============ List of structure to annotate ============
+
+LIST_STRUCTURES = [
+    "BONES",
+    "MUSCLES",
+]
+
+DICT_SUB_STRUCTURES = {
+    "BONES":[
+        'Clavicle',
+        'Elbow',
+        'Glenoid',
+        'Humerus',
+        'Humeral Head',
+        'Scapula',
+        'Sternum',
+        'Vertebrae'],
+
+    "MUSCLES":[     
+        'Biceps',
+        'Coracobrachialis',
+        'Deltoid',
+        'Infraspinatus',
+        'Latissimus dorsi',
+        'Pectoralis',
+        'Rhomboid',
+        'Subscapularis ',
+        'Teres major',
+        'Trapezius',
+        'Triceps brachii',
+        'Upper Cuff Muscle']
+}
+
+DICT_SUB_SUB_STRUCTURES = {
+    "Scapula":[
+        'Acromion',
+        'Body',
+        'Coracoid process',
+        'Spine'],
+
+    "Humeral Head":[
+        'Diaphyse',
+        'Head',
+        'Trochiter',
+        'Trochin'],
+
+    "Upper Cuff Muscle":[
+        'Infra-spinatus',
+        'Sub-scapularis',
+        'Supra-spinatus',
+        'Terres minor'],
+
+    "Elbow":[
+        'Humeral pallet',
+        'Olecranon',
+        'Radius'],
+
+    "Deltoid":[
+        'Anterior',
+        'Lateral',
+        'Posterior'],
+
+    "Biceps":[
+        'Long',
+        'Short'],
+
+    "Pectoralis":[
+        'Major',
+        'Minor'],    
+}
