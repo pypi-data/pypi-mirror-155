25856a7b7a63646bbc3eccf00f4b7facf0ca01f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:22 +0000

    Add package gadgetron-integration-tests/0.0.3/gadgetron_integration_tests-0.0.3.tar.gz

---
+++ packages/gadgetron-integration-tests/gadgetron_integration_tests-0.0.3.tar.gz/gadgetron_integration_tests-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='gadgetron_integration_tests',
+    version='0.0.3',
+    url='https://github.com/gadgetron/gadgetron/tree/master/test/integration',
+    author='Gadgetron',
+    py_modules=['get_data', 'mem_watch', 'run_gadgetron_test', 'run_tests', 'stats_to_junit'],
+    install_requires=['h5py','ismrmrd'],
+    entry_points = {
+        "console_scripts": ['gadgetron_get_integration_test_data = get_data:main', 
+                            'gadgetron_run_integration_tests = run_tests:main']
+    },
+)
