18ee9fe878d100d54a832a2d01609f9d0d038ec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:45 +0000

    Add package pysolarfocus/0.0.3/pysolarfocus-0.0.3.tar.gz

---
+++ packages/pysolarfocus/pysolarfocus-0.0.3.tar.gz/pysolarfocus-0.0.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pysolarfocus']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pymodbus>=2.5.3,<3.0.0']
+
+setup_kwargs = {
+    'name': 'pysolarfocus',
+    'version': '0.0.3',
+    'description': 'Unofficial, local Solarfocus client',
+    'long_description': '# pysolarfocus',
+    'author': 'Jeroen Laverman',
+    'author_email': 'jjlaverman@web.de',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/lavermanjj/pysolarfocus',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
