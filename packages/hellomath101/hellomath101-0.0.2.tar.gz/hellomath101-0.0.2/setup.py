eb15b16a87ab12142e3349a60932c5894316341c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:52 +0000

    Add package hellomath101/0.0.2/hellomath101-0.0.2.tar.gz

---
+++ packages/hellomath101/hellomath101-0.0.2.tar.gz/hellomath101-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='hellomath101', # Package name for use on pip install
+    version='0.0.2',
+    license='MIT',
+    author="Tester",
+    author_email='email@example.com',
+    packages=find_packages('math1'),
+    package_dir={'': 'master_math'},
+    url='https://github.com/gmyrianthous/example-publish-pypi',
+    keywords='example project'
+)
