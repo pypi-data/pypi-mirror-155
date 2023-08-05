77b6b0b26b0e5f5514e83025ff66abfa0cfcc9ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:50 +0000

    Add package mob-scrapy-redis-sentinel/0.7/mob_scrapy_redis_sentinel-0.7.tar.gz

---
+++ packages/mob-scrapy-redis-sentinel/mob_scrapy_redis_sentinel-0.7.tar.gz/mob_scrapy_redis_sentinel-0.7/mob_scrapy_redis_sentinel/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+__original_author__ = "Rolando Espinoza"
+__author__ = "luzihang"
+__email__ = "luzihang@mob.com"
+__version__ = "0.5"
+
+from mob_tools.mobLog import MobLoguru
+from mob_tools.inner_ip import get_inner_ip
+import os
+
+# 环境常量
+DEV = "dev"
+PROD = "prod"
+
+ENV = os.getenv("ENV", DEV)
+
+if ENV == "prod":
+    mob_log = MobLoguru(deep=2, log_file='/data/logs/crawler/crawler.log.es')
+    inner_ip = os.getenv("LOCAL_IP", get_inner_ip())
+else:
+    mob_log = MobLoguru()
+    inner_ip = "127.0.0.1"
