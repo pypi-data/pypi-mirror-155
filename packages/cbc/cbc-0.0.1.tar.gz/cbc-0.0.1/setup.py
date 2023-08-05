715ac7c24821ff9776832522a40cec10be6632c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:20 +0000

    Add package cbc/0.0.1/cbc-0.0.1.tar.gz

---
+++ packages/cbc/cbc-0.0.1.tar.gz/cbc-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# Setting up
+setup(
+    name='cbc',
+    version='0.0.1',
+    author='nemo256 (Amine Neggazi)',
+    author_email='<neggazimedlamine@gmail.com>',
+    description='Count Blood Cells',
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/nemo256/cbc',
+    install_requires=['opencv-python', 'tensorflow', 'tensorflow_addons', 'scipy', 'numpy'],
+    keywords=['python', 'artificial intelligence', 'deep learning', 'blood cells', 'image segmentation', 'unet'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    scripts=['cbc']
+)
