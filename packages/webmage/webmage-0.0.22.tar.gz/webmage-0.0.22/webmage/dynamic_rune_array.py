84d5d9d7c47f51bd244dc8a29aacb927d85278c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:28 +0000

    Add package webmage/0.0.22/webmage-0.0.22.tar.gz

---
+++ packages/webmage/webmage-0.0.22.tar.gz/webmage-0.0.22/webmage/dynamic_rune_array.py
@@ -0,0 +1,24 @@
+from collections import Iterator
+
+class DynamicRuneArr(Iterator):
+    def __init__(self, items):
+        self._data = items
+        self._index = 0
+
+    def __getitem__(self, key):
+        if isinstance(key, str):
+            return [item.selenium_rune.get_attribute(key) for item in self._data]
+        elif isinstance(key, int):
+            return self._data[key]
+
+    def __next__(self):
+        if self._index < len(self._data):
+            value = self._data[self._index]
+            self._index += 1
+            return value
+        else:
+            self._index = 0
+            raise StopIteration
+            
+    def __len__(self):
+        return len(self._data)
