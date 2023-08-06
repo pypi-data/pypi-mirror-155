7bc3e82258659465022fdca1a89110671d1c13a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:56 +0000

    Add package django-large-image/0.8.0/django_large_image-0.8.0-py3-none-any.whl

---
+++ packages/django-large-image/django_large_image-0.8.0-py3-none-any.whl/django_large_image/rest/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa: F401
+from django_large_image.rest.standalone import ListColormapsView, ListTileSourcesView
+from django_large_image.rest.viewsets import (
+    LargeImageDetailMixin,
+    LargeImageFileDetailMixin,
+    LargeImageMixin,
+    LargeImageVSIFileDetailMixin,
+)
