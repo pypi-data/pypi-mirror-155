d4c0aae9e3c36ec4dd9b44134423bba0325d0d34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:22:22 +0000

    Add package polars-u64-idx/0.13.47/polars_u64_idx-0.13.47.tar.gz

---
+++ packages/polars-u64-idx/polars_u64_idx-0.13.47.tar.gz/polars_u64_idx-0.13.47/local_dependencies/polars-time/src/chunkedarray/mod.rs
@@ -0,0 +1,31 @@
+//! Traits and utilities for temporal data.
+#[cfg(feature = "dtype-date")]
+mod date;
+#[cfg(feature = "dtype-datetime")]
+mod datetime;
+#[cfg(feature = "dtype-duration")]
+mod duration;
+mod kernels;
+mod rolling_window;
+#[cfg(feature = "dtype-time")]
+mod time;
+pub mod utf8;
+
+use chrono::{NaiveDate, NaiveDateTime, NaiveTime};
+#[cfg(feature = "dtype-date")]
+pub use date::DateMethods;
+#[cfg(feature = "dtype-datetime")]
+pub use datetime::DatetimeMethods;
+#[cfg(feature = "dtype-duration")]
+pub use duration::DurationMethods;
+use kernels::*;
+use polars_arrow::utils::CustomIterTools;
+use polars_core::prelude::*;
+pub use rolling_window::*;
+#[cfg(feature = "dtype-time")]
+pub use time::TimeMethods;
+pub use utf8::Utf8Methods;
+
+pub fn unix_time() -> NaiveDateTime {
+    NaiveDateTime::from_timestamp(0, 0)
+}
