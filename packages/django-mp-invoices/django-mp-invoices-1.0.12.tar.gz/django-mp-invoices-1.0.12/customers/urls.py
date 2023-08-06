fca39b3f88a6bbc3789248ec86603fe8210b24cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:46 +0000

    Add package django-mp-invoices/1.0.12/django-mp-invoices-1.0.12.tar.gz

---
+++ packages/django-mp-invoices/django-mp-invoices-1.0.12.tar.gz/django-mp-invoices-1.0.12/customers/urls.py
@@ -0,0 +1,23 @@
+
+from django.urls import path, include
+
+from customers import views
+
+
+app_name = 'customers'
+
+
+urlpatterns = [
+
+    path('add/', views.add_customer, name='add'),
+
+    path('<int:customer_id>/details/', views.get_customer_detail,
+         name='detail'),
+
+    path('export/', views.export_customers, name='export')
+]
+
+
+app_urls = [
+    path('customers/', include((urlpatterns, app_name)))
+]
