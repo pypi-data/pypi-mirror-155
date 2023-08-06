c9060500ffc4ac06eae58924b2e0c793af4b1dbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:03 +0000

    Add package python-office/0.1.1/python-office-0.1.1.tar.gz

---
+++ packages/python-office/python-office-0.1.1.tar.gz/python-office-0.1.1/office/api/excel.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+#############################################
+# File Name: excel.py
+# Mail: 1957875073@qq.com
+# Created Time:  2022-4-25 10:17:34
+# Description: 有关 excel 的自动化操作
+#############################################
+from office.core.ExcelType import MainExcel
+
+mainExcel = MainExcel()
+
+
+def fake2excel(columns=['name'], rows=1, language='zh_CN', path='./fake2excel.xlsx'):
+    mainExcel.fake2excel(columns, rows, language, path)
