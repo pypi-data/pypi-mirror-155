4e55b259ba36ed6ebf235b83d39a49c410d09603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:49 +0000

    Add package zavod/0.2.1/zavod-0.2.1-py3-none-any.whl

---
+++ packages/zavod/zavod-0.2.1-py3-none-any.whl/zavod/util.py
@@ -0,0 +1,23 @@
+import os
+from typing import Optional, Union
+from normality import slugify
+
+PathLike = Union[str, os.PathLike[str]]
+
+
+def join_slug(
+    *parts: Optional[str],
+    prefix: Optional[str] = None,
+    sep: str = "-",
+    strict: bool = True
+) -> Optional[str]:
+    sections = [slugify(p, sep=sep) for p in parts]
+    if strict and None in sections:
+        return None
+    texts = [p for p in sections if p is not None]
+    if not len(texts):
+        return None
+    prefix = slugify(prefix, sep=sep)
+    if prefix is not None:
+        texts = [prefix, *texts]
+    return sep.join(texts)
