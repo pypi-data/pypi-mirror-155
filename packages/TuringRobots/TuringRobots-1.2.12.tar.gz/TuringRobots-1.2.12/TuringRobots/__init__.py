40afe50c1c02dfb7695be84842b6d7d8cd069adb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:17:00 +0000

    Add package TuringRobots/1.2.12/TuringRobots-1.2.12.tar.gz

---
+++ packages/TuringRobots/TuringRobots-1.2.12.tar.gz/TuringRobots-1.2.12/TuringRobots/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/6/18 11:34
+# @Author  : AI悦创
+# @FileName: __init__.py
+# @Software: PyCharm
+# @Blog    ：https://bornforthis.cn/
+from .TuringRobots import *
+from .req import *
