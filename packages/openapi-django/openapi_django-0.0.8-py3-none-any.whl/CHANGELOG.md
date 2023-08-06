7ad5ec4e01040c596dcb4f36a1b33f6ae621605f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:21 +0000

    Add package openapi-django/0.0.8/openapi_django-0.0.8-py3-none-any.whl

---
+++ packages/openapi-django/openapi_django-0.0.8-py3-none-any.whl/CHANGELOG.md
@@ -0,0 +1,8 @@
+# CHANGELOG
+## [Unreleased]
+
+---
+
+## [0.0.8] 2022-06-13
+### Featured
+- Base functional
---
+++ packages/openapi-django/openapi_django-0.0.8-py3-none-any.whl/openapi_django/openapi_utils/decorators.py
@@ -0,0 +1,15 @@
+def openapi(return_class=None, description=None, path_parameters=None, parameters=None, body=None):
+    def decorate(f):
+        def wrapper(*args, **kwargs):
+            if len(args) == 1:
+                if args[0] == 'openapi':
+                    return {
+                        'return_class': return_class,
+                        'description': description,
+                        'path_parameters': path_parameters,
+                        "parameters": parameters,
+                        "body": body
+                    }
+            return f(*args, **kwargs)
+        return wrapper
+    return decorate
