a03e2226b2efb8948c93efa680072ee4ffb67947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:48:33 +0000

    Add package nodegam/0.1.0/nodegam-0.1.0.tar.gz

---
+++ packages/nodegam/nodegam-0.1.0.tar.gz/nodegam-0.1.0/nodegam/__init__.py
@@ -0,0 +1,10 @@
+from . import nn_utils
+from . import odst
+from . import utils
+from . import data
+from . import trainer
+from . import recorder
+from . import arch
+from . import gams
+
+__version__ = '0.1.0'
