b9ceb6010ae9ab3a068e277cd69ea5a3e250bbbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:47 +0000

    Add package pycashier/0.3.5/pycashier-0.3.5-py3-none-any.whl

---
+++ packages/pycashier/pycashier-0.3.5-py3-none-any.whl/pycashier/pycashier.py
@@ -0,0 +1,86 @@
+import sys
+from pathlib import Path
+
+from .cli import get_args, sample_check
+from .cluster import cluster
+from .console import console
+from .extract import extract
+from .merge import merge
+from .read_filter import read_filter
+from .single_cell import single_cell
+
+
+def main():
+
+    cli_args = get_args()
+
+    console.rule()
+    console.rule("Barcode Extraction with CASHIER")
+    console.rule()
+
+    sourcedir = Path(cli_args["main"]["sourcedir"])
+
+    fastqs = [f for f in sourcedir.iterdir()]
+
+    if not fastqs:
+        console.print(f"Source dir: {sourcedir}, appears to be empty...")
+        console.print("Exiting.")
+        sys.exit(1)
+
+    Path(cli_args["main"]["pipelinedir"]).mkdir(exist_ok=True)
+
+    if cli_args["merge"]["merge"]:
+
+        merge(fastqs, sourcedir, cli_args)
+
+    Path(cli_args["main"]["outdir"]).mkdir(exist_ok=True)
+
+    if cli_args["single_cell"]:
+
+        single_cell(sourcedir, cli_args)
+
+    for f in fastqs:
+
+        ext = f.suffix
+
+        if ext != ".fastq":
+            print(
+                f"ERROR! There is a non fastq file in the provided fastq directory: {f}"
+            )
+            print("Exiting.")
+            sys.exit(1)
+
+    processed_samples = sample_check(sourcedir, fastqs, cli_args)
+
+    for fastq in fastqs:
+
+        sample = fastq.name.split(".")[0]
+
+        if sample in processed_samples:
+            console.print(f"Skipping Processing for [green]{sample}[/green]")
+            console.rule()
+            continue
+
+        with console.status(
+            f"Processing sample: [green]{sample}[/green]", spinner="dots12"
+        ):
+
+            extract(sample, fastq, **cli_args["main"], **cli_args["extract"])
+
+            cluster(sample, **cli_args["main"], **cli_args["cluster"])
+
+            read_filter(
+                sample,
+                **cli_args["main"],
+                **cli_args["filter"],
+                **cli_args["cluster"],
+            )
+
+        console.print(f"[green]{sample}[/green]: processing completed")
+        console.rule()
+
+    console.print("\n[green]FINISHED!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/pycashier/pycashier-0.3.5-py3-none-any.whl/pycashier/utils.py
@@ -0,0 +1,67 @@
+import sys
+
+from .console import console
+
+
+def get_fastqs(src):
+    fastqs = [f for f in src.iterdir()]
+    if not fastqs:
+        console.print(f"Source dir: {src}, appears to be empty...")
+        console.print("Exiting.")
+        sys.exit(1)
+
+    for f in fastqs:
+
+        if f.suffix not in [".fastq", ".fastq.gz"]:
+            print(
+                f"ERROR! There is a non fastq file in the provided fastq directory: {f}"
+            )
+            print("Exiting.")
+            sys.exit(1)
+
+    return fastqs
+
+
+def convert_to_csv(in_file, out_file):
+
+    for i, line in enumerate(in_file):
+
+        if not line.startswith("@") or in_file[i - 1].strip() == "+":
+            continue
+        seq_id = line.strip()
+        sequence = in_file[i + 1].strip()
+        out_file.write(f"{seq_id}\t{sequence}\n")
+
+
+def fastq_to_csv(in_file, out_file):
+
+    with open(in_file) as f_in:
+        with open(out_file, "w") as f_out:
+            convert_to_csv(f_in.readlines(), f_out)
+
+
+def extract_csv_column(csv_file, column):
+
+    ext = csv_file.suffix
+    tmp_out = csv_file.with_suffix(f".c{column}{ext}")
+    with open(csv_file, "r") as csv_in:
+        with open(tmp_out, "w") as csv_out:
+            for line in csv_in:
+                linesplit = line.split("\t")
+                csv_out.write(f"{linesplit[column-1]}")
+
+    return tmp_out
+
+
+def combine_outs(input, output):
+
+    files = {f.name.split(".")[0]: f for f in input.iterdir()}
+
+    console.print(f"Combing output files for {len(files)} samples.")
+
+    with output.open("w") as csv_out:
+        csv_out.write("sample\tsequence\tcount\n")
+        for sample, f in files.items():
+            with f.open("r") as csv_in:
+                for line in csv_in:
+                    csv_out.write(f"{sample}\t{line}")
