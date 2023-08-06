5d10c2f9c5605dcfcf1390b4b4d6921754abdea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:38 +0000

    Add package kong-data-plane/2.0.7/kong_data_plane-2.0.7-py3-none-any.whl

---
+++ packages/kong-data-plane/kong_data_plane-2.0.7-py3-none-any.whl/kong_data_plane/_jsii/__init__.py
@@ -0,0 +1,23 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk._jsii
+import constructs._jsii
+import kong_core._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "kong-data-plane", "2.0.7", __name__[0:-6], "kong-data-plane@2.0.7.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
