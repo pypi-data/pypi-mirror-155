d1c918d9da2d5c6aa48e0e4ab3dece99132495af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:56 +0000

    Add package cirq-google/0.15.0.dev20220616205840/cirq_google-0.15.0.dev20220616205840-py3-none-any.whl

---
+++ packages/cirq-google/cirq_google-0.15.0.dev20220616205840-py3-none-any.whl/cirq_google/engine/util.py
@@ -0,0 +1,63 @@
+# Copyright 2022 The Cirq Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import inspect
+from typing import Dict, Tuple, TypeVar
+
+from google.protobuf import any_pb2
+from google.protobuf.message import Message
+
+import cirq
+
+M = TypeVar('M', bound=Message)
+
+# Bundled Z phase errors in the format:
+#
+#   {gate_type: {angle_type: {qubit_pair: error}}}
+#
+# where gate_type is "syc" or "sqrt_iswap", angle_type is "zeta" or "gamma",
+# and "qubit_pair" is a tuple of qubits.
+ZPhaseDataType = Dict[str, Dict[str, Dict[Tuple[cirq.Qid, ...], float]]]
+
+
+def pack_any(message: Message) -> any_pb2.Any:
+    """Packs a message into an Any proto.
+
+    Returns the packed Any proto.
+    """
+    packed = any_pb2.Any()
+    packed.Pack(message)
+    return packed
+
+
+def unpack_any(message: any_pb2.Any, out: M) -> M:
+    message.Unpack(out)
+    return out
+
+
+def deprecated_gate_set_parameter(func):
+    """Decorates a function that takes a deprecated 'gate_set' parameter."""
+    signature = inspect.signature(func)
+    gate_set_param = signature.parameters['gate_set']
+    assert gate_set_param.default is None  # Must be optional and default to None.
+    idx = list(signature.parameters).index('gate_set')
+
+    decorator = cirq._compat.deprecated_parameter(
+        deadline='v0.15',
+        fix='Remove the gate_set parameter.',
+        parameter_desc='gate_set',
+        match=lambda args, kwargs: 'gate_set' in kwargs
+        or (gate_set_param.kind != inspect.Parameter.KEYWORD_ONLY and len(args) > idx),
+    )
+    return decorator(func)
