7a974eae86e5c41366cf94495e1a518a0bb3ae44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:31 +0000

    Add package dataobjtools/0.1.0/dataobjtools-0.1.0.tar.gz

---
+++ packages/dataobjtools/dataobjtools-0.1.0.tar.gz/dataobjtools-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dataobjtools']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'dataobjtools',
+    'version': '0.1.0',
+    'description': 'Data Object helpers for Dicts and Lists for searching, manipulating etc',
+    'long_description': None,
+    'author': 'pub12',
+    'author_email': 'pubudu79@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
