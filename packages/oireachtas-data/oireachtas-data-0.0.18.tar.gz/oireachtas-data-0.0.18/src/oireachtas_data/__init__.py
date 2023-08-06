b1df1c4e4be5194263db656bab042ced075f27af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:17:47 +0000

    Add package oireachtas-data/0.0.18/oireachtas-data-0.0.18.tar.gz

---
+++ packages/oireachtas-data/oireachtas-data-0.0.18.tar.gz/oireachtas-data-0.0.18/src/oireachtas_data/__init__.py
@@ -0,0 +1,12 @@
+import nltk
+
+
+try:
+    nltk.data.find('taggers/averaged_perceptron_tagger')
+except LookupError:
+    nltk.download('averaged_perceptron_tagger')
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
