1fd94bce6ae7a55032a50886e47f5cc24a55d513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:27 +0000

    Add package jiminy-py/1.7.13/jiminy_py-1.7.13-cp38-cp38-manylinux_2_28_x86_64.whl

---
+++ packages/jiminy-py/jiminy_py-1.7.13-cp38-cp38-manylinux_2_28_x86_64.whl/jiminy_py/core/cmake/jiminyConfig.cmake
@@ -0,0 +1,141 @@
+# Enable link rpath to find shared library dependencies at runtime
+set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+
+# Set Python find strategy to location by default
+if(CMAKE_VERSION VERSION_GREATER "3.15")
+    cmake_policy(SET CMP0094 NEW)
+endif()
+
+# Get Python executable
+if(DEFINED PYTHON_EXECUTABLE)
+    get_filename_component(_PYTHON_PATH "${PYTHON_EXECUTABLE}" DIRECTORY)
+    get_filename_component(_PYTHON_NAME "${PYTHON_EXECUTABLE}" NAME)
+    find_program(Python_EXECUTABLE "${_PYTHON_NAME}" PATHS "${_PYTHON_PATH}" NO_DEFAULT_PATH)
+else()
+    if(CMAKE_VERSION VERSION_LESS "3.12.4")
+        find_program(Python_EXECUTABLE "python${PYTHON_REQUIRED_VERSION}")
+    else()
+        if(CMAKE_VERSION VERSION_LESS "3.14")
+            set(Python_COMPONENTS_FIND Interpreter)
+        else()
+            set(Python_COMPONENTS_FIND Interpreter NumPy)
+        endif()
+        unset(Python_EXECUTABLE)
+        unset(Python_EXECUTABLE CACHE)
+        unset(_Python_EXECUTABLE)
+        unset(_Python_EXECUTABLE CACHE)
+        if(PYTHON_REQUIRED_VERSION)
+            find_package(Python ${PYTHON_REQUIRED_VERSION} EXACT COMPONENTS ${Python_COMPONENTS_FIND})
+        else()
+            find_package(Python COMPONENTS ${Python_COMPONENTS_FIND})
+        endif()
+    endif()
+endif()
+if(NOT Python_EXECUTABLE)
+    if (jiminy_FIND_REQUIRED)
+        message(FATAL_ERROR "Python executable not found, CMake will exit.")
+    else()
+        set(jiminy_FOUND FALSE)
+        return()
+    endif()
+endif()
+
+# On windows, pinocchio is not able to find boost components for some reasons
+if(WIN32)
+    find_package(Boost REQUIRED COMPONENTS chrono system filesystem serialization date_time thread atomic)
+endif()
+
+# Make sure the right version of pinocchio, and eigen are available
+find_package(pinocchio 2.6.7 EXACT REQUIRED NO_MODULE NO_CMAKE_SYSTEM_PATH)
+find_package(hpp-fcl 1.7.1 REQUIRED NO_MODULE NO_CMAKE_SYSTEM_PATH)
+find_package(Eigen3 3.3.0 REQUIRED NO_MODULE)
+
+# Make sure jiminy Python module is available
+execute_process(COMMAND "${Python_EXECUTABLE}" -c
+                "from importlib.util import find_spec; print(int(find_spec('jiminy_py') is not None), end='')"
+                OUTPUT_VARIABLE jiminy_FOUND)
+if(NOT jiminy_FOUND)
+    if (jiminy_FIND_REQUIRED)
+        message(FATAL_ERROR "`jiminy_py` Python module not found, CMake will exit.")
+    else()
+        return()
+    endif()
+endif()
+
+# Define Python_NumPy_INCLUDE_DIRS if necessary
+if (NOT Python_NumPy_INCLUDE_DIRS)
+    execute_process(
+        COMMAND "${Python_EXECUTABLE}" -c
+        "import numpy; print(numpy.get_include(), end='')\n"
+        OUTPUT_VARIABLE __numpy_path)
+    find_path(Python_NumPy_INCLUDE_DIRS numpy/arrayobject.h
+        HINTS "${__numpy_path}" "${Python_INCLUDE_DIRS}" NO_DEFAULT_PATH)
+endif()
+
+# Find jiminy libraries and headers.
+# Note that jiminy is compiled under C++17 using either old or new CXX11 ABI.
+# Make sure very project dependencies are compiled for the same CXX11 ABI
+# otherwise segfaults may occur. It should be fine for the standard library,
+# but not for precompiled boost libraries such as boost::filesystem.
+# https://gcc.gnu.org/onlinedocs/libstdc++/manual/abi.html
+execute_process(COMMAND "${Python_EXECUTABLE}" -c
+                "import jiminy_py; print(jiminy_py.get_include(), end='')"
+                OUTPUT_VARIABLE jiminy_INCLUDE_DIRS)
+execute_process(COMMAND "${Python_EXECUTABLE}" -c
+                "import jiminy_py; print(jiminy_py.get_libraries(), end='')"
+                OUTPUT_VARIABLE jiminy_LIBRARIES)
+
+# For now, there is only one libary (core), so just pick the most recent one
+list(GET jiminy_LIBRARIES 0 jiminy_CORE_LIBRARY)
+
+# Define compilation options and definitions
+set(jiminy_DEFINITIONS
+    BOOST_MPL_CFG_NO_PREPROCESSED_HEADERS=ON
+    BOOST_MPL_LIMIT_VECTOR_SIZE=30
+    BOOST_MPL_LIMIT_LIST_SIZE=30
+)
+if(WIN32)
+    list(APPEND jiminy_DEFINITIONS
+        WIN32 _USE_MATH_DEFINES NOMINMAX
+        EIGENPY_STATIC URDFDOM_STATIC HPP_FCL_STATIC PINOCCHIO_STATIC
+    )
+    set(jiminy_OPTIONS /EHsc /bigobj /Zc:__cplusplus /permissive- /wd4996 /wd4554 /wd4005)
+else()
+    execute_process(COMMAND readelf --version-info "${jiminy_CORE_LIBRARY}"
+                    COMMAND grep -c "Name: CXXABI_1.3.9\\\|Name: GLIBCXX_3.4.21"
+                    OUTPUT_STRIP_TRAILING_WHITESPACE
+                    OUTPUT_VARIABLE CHECK_NEW_CXX11_ABI)
+    list(APPEND jiminy_DEFINITIONS _GLIBCXX_USE_CXX11_ABI=$<BOOL:${CHECK_NEW_CXX11_ABI}>)
+    set(jiminy_OPTIONS -fPIC)
+endif()
+
+# Set architecture flags.
+list(APPEND jiminy_OPTIONS ${jiminy_OPTIONS} )
+if("" MATCHES "AVX[0-9]+")
+    list(APPEND jiminy_OPTIONS /wd9002 /fp:contract)
+    list(APPEND jiminy_DEFINITIONS __FMA__)
+endif()
+
+# Define imported target
+add_library(jiminy::core SHARED IMPORTED GLOBAL)
+set_target_properties(jiminy::core PROPERTIES
+    IMPORTED_LOCATION "${jiminy_CORE_LIBRARY}"
+    INTERFACE_INCLUDE_DIRECTORIES "${jiminy_INCLUDE_DIRS}"
+    INTERFACE_COMPILE_DEFINITIONS "${jiminy_DEFINITIONS}"
+    INTERFACE_COMPILE_OPTIONS "${jiminy_OPTIONS}"
+    INTERFACE_COMPILE_FEATURES cxx_std_17
+    CXX_STANDARD 17
+    CXX_STANDARD_REQUIRED YES
+    CXX_EXTENSIONS NO
+)
+
+if(WIN32)
+    get_filename_component(jiminy_LIBDIR "${jiminy_CORE_LIBRARY}" DIRECTORY)
+    get_filename_component(jiminy_LIBNAME "${jiminy_CORE_LIBRARY}" NAME_WE)
+    set_target_properties(jiminy::core PROPERTIES
+        IMPORTED_IMPLIB "${jiminy_LIBDIR}/${jiminy_LIBNAME}.lib"
+    )
+endif()
+
+# Display import is success
+message(STATUS "Found jiminy ('${jiminy_VERSION}'): ${jiminy_INCLUDE_DIRS} (${jiminy_CORE_LIBRARY})")
---
+++ packages/jiminy-py/jiminy_py-1.7.13-cp38-cp38-manylinux_2_28_x86_64.whl/jiminy_py/core/include/jiminy/core/Types.h
@@ -0,0 +1,280 @@
+///////////////////////////////////////////////////////////////////////////////
+/// \brief    Contains types used in Jiminy.
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef JIMINY_TYPES_H
+#define JIMINY_TYPES_H
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+#include "pinocchio/fwd.hpp"            // To avoid having to include it everywhere
+#include "pinocchio/multibody/fwd.hpp"  // `pinocchio::Model::...Index`
+#include "pinocchio/spatial/fwd.hpp"    // `Pinocchio::Force`, `Pinocchio::Motion`
+
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
+#include <Eigen/StdVector>
+
+#include <boost/variant.hpp>
+#include <boost/functional/hash.hpp>
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/hashed_index.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <boost/multi_index/member.hpp>
+#include <boost/multi_index/tag.hpp>
+
+
+// `pinocchio::container::aligned_vector`
+namespace pinocchio
+{
+    namespace container
+    {
+        template<typename T> struct aligned_vector;
+    }
+}
+
+
+namespace jiminy
+{
+    // ******************* General definitions *******************
+
+    // "Standard" types
+    using bool_t = bool;
+    using char_t = char;
+    using float32_t = float;
+    using float64_t = double;
+
+    template<typename K, typename M>
+    using static_map_t = std::vector<std::pair<K, M> >;
+
+    template<typename K, typename M>
+    using static_map_aligned_t = std::vector<std::pair<K, M>,
+        Eigen::aligned_allocator<std::pair<K, M> > >;
+
+    template<typename K, typename M>
+    using map_aligned_t = std::map<K, M, std::less<K>,
+        Eigen::aligned_allocator<std::pair<const K, M> > >;
+
+    template<typename M>
+    using vector_aligned_t = std::vector<M, Eigen::aligned_allocator<M> >;
+
+    // Eigen types
+    using matrixN_t = Eigen::Matrix<float64_t, Eigen::Dynamic, Eigen::Dynamic>;
+    using matrix2_t = Eigen::Matrix<float64_t, 2, 2>;
+    using matrix3_t = Eigen::Matrix<float64_t, 3, 3>;
+    using matrix6N_t = Eigen::Matrix<float64_t, 6, Eigen::Dynamic>;
+    using vectorN_t = Eigen::Matrix<float64_t, Eigen::Dynamic, 1>;
+    using vector2_t = Eigen::Matrix<float64_t, 2, 1>;
+    using vector3_t = Eigen::Matrix<float64_t, 3, 1>;
+    using vector6_t = Eigen::Matrix<float64_t, 6, 1>;
+
+    using quaternion_t = Eigen::Quaternion<float64_t>;
+
+    // Pinocchio types
+    using motionVector_t = pinocchio::container::aligned_vector<pinocchio::Motion>;
+    using forceVector_t = pinocchio::container::aligned_vector<pinocchio::Force>;
+    using jointIndex_t = pinocchio::JointIndex;
+    using frameIndex_t = pinocchio::FrameIndex;
+    using geomIndex_t = pinocchio::GeomIndex;
+    using pairIndex_t = pinocchio::PairIndex;
+
+    // *************** Constant of the universe ******************
+
+    // Define some aliases for convenience
+    float64_t const INF = std::numeric_limits<float64_t>::infinity();
+    float64_t const EPS = std::numeric_limits<float64_t>::epsilon();
+    float64_t const qNAN = std::numeric_limits<float64_t>::quiet_NaN();
+
+    // *************** Jiminy-specific definitions ***************
+
+    // Error codes
+    enum class hresult_t : int32_t
+    {
+        SUCCESS = 1,
+        ERROR_GENERIC = -1,
+        ERROR_BAD_INPUT = -2,
+        ERROR_INIT_FAILED = -3
+    };
+
+    // Pinocchio joint types
+    enum class joint_t : uint8_t
+    {
+        // CYLINDRICAL are not available so far
+
+        NONE = 0,
+        LINEAR = 1,
+        ROTARY = 2,
+        ROTARY_UNBOUNDED = 3,  // Must be treated separately because the position is encoded using [cos(theta), sin(theta)] instead of theta
+        PLANAR = 4,
+        TRANSLATION = 5,
+        SPHERICAL = 6,
+        FREE = 7
+    };
+
+    /* Ground profile signature.
+       Note that it is impossible to use function pointer since it does not support functors. */
+    using heightmapFunctor_t = std::function<std::pair<float64_t, vector3_t>(vector3_t const & /*pos*/)>;
+
+    // Flexible joints
+    struct flexibleJointData_t
+    {
+        std::string frameName;
+        vector3_t stiffness;
+        vector3_t damping;
+        vector3_t inertia;
+
+        flexibleJointData_t(void) :
+        frameName(),
+        stiffness(),
+        damping(),
+        inertia()
+        {
+            // Empty.
+        };
+
+        flexibleJointData_t(std::string const & frameNameIn,
+                            vector3_t   const & stiffnessIn,
+                            vector3_t   const & dampingIn,
+                            vector3_t   const & inertiaIn) :
+        frameName(frameNameIn),
+        stiffness(stiffnessIn),
+        damping(dampingIn),
+        inertia(inertiaIn)
+        {
+            // Empty.
+        };
+
+        inline bool_t operator==(flexibleJointData_t const & other) const
+        {
+            return (this->frameName == other.frameName
+                 && this->stiffness == other.stiffness
+                 && this->damping == other.damping
+                 && this->inertia == other.inertia);
+        };
+    };
+
+    using flexibilityConfig_t = std::vector<flexibleJointData_t>;
+
+    // Configuration/option holder
+    using configField_t = boost::make_recursive_variant<
+        bool_t, uint32_t, int32_t, float64_t, std::string, vectorN_t, matrixN_t, heightmapFunctor_t,
+        std::vector<std::string>, std::vector<vectorN_t>, std::vector<matrixN_t>,
+        flexibilityConfig_t, std::unordered_map<std::string, boost::recursive_variant_>
+    >::type;
+
+    using configHolder_t = std::unordered_map<std::string, configField_t>;
+
+    // Sensor data holder
+    struct sensorDataTypePair_t
+    {
+        // Disable the copy of the class
+        sensorDataTypePair_t(sensorDataTypePair_t const & sensorDataPairIn) = delete;
+        sensorDataTypePair_t & operator = (sensorDataTypePair_t const & other) = delete;
+
+        sensorDataTypePair_t(std::string                 const & nameIn,
+                             std::size_t                 const & idIn,
+                             Eigen::Ref<vectorN_t const> const & valueIn) :
+        name(nameIn),
+        idx(idIn),
+        value(valueIn)
+        {
+            // Empty on purpose.
+        };
+
+        ~sensorDataTypePair_t(void) = default;
+
+        sensorDataTypePair_t(sensorDataTypePair_t && other) :
+        name(other.name),
+        idx(other.idx),
+        value(other.value)
+        {
+            // Empty on purpose.
+        };
+
+        std::string name;
+        std::size_t idx;
+        Eigen::Ref<vectorN_t const> value;
+    };
+
+    using namespace boost::multi_index;
+    struct IndexByIdx {};
+    struct IndexByName {};
+    using sensorDataTypeMapImpl_t = multi_index_container<
+        sensorDataTypePair_t,
+        indexed_by<
+            ordered_unique<
+                tag<IndexByIdx>,
+                member<sensorDataTypePair_t, std::size_t, &sensorDataTypePair_t::idx>,
+                std::less<std::size_t> // Ordering by ascending order
+            >,
+            hashed_unique<
+                tag<IndexByName>,
+                member<sensorDataTypePair_t, std::string, &sensorDataTypePair_t::name>
+            >
+        >
+    >;
+    struct sensorDataTypeMap_t : public sensorDataTypeMapImpl_t
+    {
+    public:
+        sensorDataTypeMap_t(std::optional<std::reference_wrapper<matrixN_t const> > sharedData = std::nullopt) :
+        sensorDataTypeMapImpl_t(),
+        sharedData_(sharedData)
+        {
+            // Empty on purpose.
+        }
+
+        matrixN_t const & getAll(void) const
+        {
+            /* Use static shared buffer if no shared memory available.
+               It is useful if the sensors data is not contiguous in the first place,
+               which is likely to be the case when allocated from Python, or when
+               re-generating sensor data from log files. */
+            static matrixN_t sharedData;
+
+            if (sharedData_)
+            {
+                /* Return shared memory directly it is up to the sure to make sure
+                   that it is actually up-to-date. */
+                return sharedData_->get();
+            }
+            else
+            {
+                // Resize internal buffer if needed
+                sharedData.resize(static_cast<Eigen::Index>(size()), this->begin()->value.size());
+
+                // Set internal buffer by copying sensor data sequentially
+                for (auto const & sensor : *this)
+                {
+                    sharedData.row(static_cast<Eigen::Index>(sensor.idx)) = sensor.value;
+                }
+
+                return sharedData;
+            }
+        }
+
+    private:
+        std::optional<std::reference_wrapper<matrixN_t const> > sharedData_;
+    };
+
+    using sensorsDataMap_t = std::unordered_map<std::string, sensorDataTypeMap_t>;
+
+    // System force functors
+    using forceProfileFunctor_t = std::function<pinocchio::Force(float64_t const & /*t*/,
+                                                                 vectorN_t const & /*q*/,
+                                                                 vectorN_t const & /*v*/)>;
+    using forceCouplingFunctor_t = std::function<pinocchio::Force(float64_t const & /*t*/,
+                                                                  vectorN_t const & /*q_1*/,
+                                                                  vectorN_t const & /*v_1*/,
+                                                                  vectorN_t const & /*q_2*/,
+                                                                  vectorN_t const & /*v_2*/)>;
+
+    // System callback functor
+    using callbackFunctor_t = std::function<bool_t(float64_t const & /*t*/,
+                                                   vectorN_t const & /*q*/,
+                                                   vectorN_t const & /*v*/)>;
+}
+
+#endif  // JIMINY_TYPES_H
