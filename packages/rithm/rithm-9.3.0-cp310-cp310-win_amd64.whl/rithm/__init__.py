7ebf7449908fbaf2aeabac2dd13b3c200b11ced0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:35:12 +0000

    Add package rithm/9.3.0/rithm-9.3.0-cp310-cp310-win_amd64.whl

---
+++ packages/rithm/rithm-9.3.0-cp310-cp310-win_amd64.whl/rithm/__init__.py
@@ -0,0 +1,14 @@
+"""Arbitrary precision arithmetic."""
+
+__version__ = '9.3.0'
+
+try:
+    from ._crithm import (Endianness,
+                          Fraction,
+                          Int,
+                          TieBreaking)
+except ImportError:
+    from ._rithm import (Endianness,
+                         Fraction,
+                         Int,
+                         TieBreaking)
