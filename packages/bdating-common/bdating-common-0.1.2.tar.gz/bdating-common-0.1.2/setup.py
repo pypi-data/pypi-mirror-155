19d9464f2a58b1be1fc08055d49469382a3155bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:05 +0000

    Add package bdating-common/0.1.2/bdating-common-0.1.2.tar.gz

---
+++ packages/bdating-common/bdating-common-0.1.2.tar.gz/bdating-common-0.1.2/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+import bdating_common
+with open("README.md") as fp:
+  long_description = fp.read()
+
+setuptools.setup(
+    name="bdating-common",
+    version=bdating_common.__version__,
+
+    description="Bdating common libraries standard library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    author="Zac",
+    author_email="zac@bdating.io",
+    package_dir={"": "."},
+    packages=setuptools.find_packages(),
+    package_data={'': ['data/*', '*.cer']},
+    url='https://github.com/bdating-io/bdating-python-common',
+    install_requires=[
+        "boto3<2",
+        "click-log<0.4",
+        "click<8.0",
+        "dnspython==2.2.0",
+        "PyMySQL"
+    ],
+
+    python_requires=">=3.6",
+
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Utilities",
+        "Typing :: Typed",
+    ],
+    entry_points={
+        'console_scripts': [
+            'report-metrics=bdating_common.report_metrics:report',
+            'report-events=bdating_common.report_events:report'
+        ],
+    }
+)
