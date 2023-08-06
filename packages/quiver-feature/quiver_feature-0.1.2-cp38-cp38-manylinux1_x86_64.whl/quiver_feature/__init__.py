c70165a14a82bc175b46640bc44a18dd7d148e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:24 +0000

    Add package quiver-feature/0.1.2/quiver_feature-0.1.2-cp38-cp38-manylinux1_x86_64.whl

---
+++ packages/quiver-feature/quiver_feature-0.1.2-cp38-cp38-manylinux1_x86_64.whl/quiver_feature/__init__.py
@@ -0,0 +1,11 @@
+import torch
+from . import multiprocessing
+from .dist_tensor_rpc import DistTensorRPC
+from .common import Range, TensorEndPoint
+from .dist_tensor_pgas import DistTensor as DistTensorPGAS
+from .dist_helper import DistHelper
+from .local_tensor_pgas import LocalTensorPGAS
+from .tensor_loader import shared_load
+from qvf import PipeParam, DistTensorServer
+__all__ = ["DistTensorRPC", "DistTensorPGAS", "LocalTensorPGAS" , "Range", "TensorEndPoint", "DistHelper",
+           'shared_load', "PipeParam", "DistTensorServer"]
