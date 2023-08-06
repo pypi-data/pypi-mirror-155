a6cadccec03ad46763d8fbe7b3d57dd7a92d9786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:28 +0000

    Add package masonite-modules/0.0.2/masonite_modules-0.0.2-py3-none-any.whl

---
+++ packages/masonite-modules/masonite_modules-0.0.2-py3-none-any.whl/masonite_modules/routes/route.py
@@ -0,0 +1,8 @@
+from src.masonite_modules.masonite_module import MasoniteModule
+
+ROUTES = []
+
+modules = MasoniteModule.get_modules()
+
+for module in modules:
+    ROUTES += getattr(module, "ROUTES")
