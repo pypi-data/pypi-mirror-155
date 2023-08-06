1aa9a0b605774ca87906ac62c07fef14d0030d89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:20:44 +0000

    Add package discord-class-commands/1.1.0/discord_class_commands-1.1.0-py3-none-any.whl

---
+++ packages/discord-class-commands/discord_class_commands-1.1.0-py3-none-any.whl/discord/ext/class_commands/__init__.py
@@ -0,0 +1,14 @@
+"""
+discord.ext.class_commands
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+An extension module to facilitate class-based creation of application commands.
+
+:copyright: (c) 2022-present Dolfies
+:license: MIT, see LICENSE for more details.
+"""
+
+__version__ = '1.1.0'
+
+from .commands import *
+from .option import *
