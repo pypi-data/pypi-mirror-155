0a3338bb945cfa067e6e28ebd604b3d32ef84e4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:53:17 +0000

    Add package Supermarq/0.0.4/SupermarQ-0.0.4-py3-none-any.whl

---
+++ packages/Supermarq/SupermarQ-0.0.4-py3-none-any.whl/supermarq/__init__.py
@@ -0,0 +1,31 @@
+from . import benchmark
+from . import converters
+from . import features
+from . import plotting
+from . import simulation
+from . import stabilizers
+from .benchmarks import bit_code
+from .benchmarks import ghz
+from .benchmarks import hamiltonian_simulation
+from .benchmarks import mermin_bell
+from .benchmarks import phase_code
+from .benchmarks import qaoa_fermionic_swap_proxy
+from .benchmarks import qaoa_vanilla_proxy
+from .benchmarks import vqe_proxy
+
+__all__ = [
+    "benchmark",
+    "bit_code",
+    "converters",
+    "features",
+    "ghz",
+    "hamiltonian_simulation",
+    "mermin_bell",
+    "phase_code",
+    "plotting",
+    "qaoa_fermionic_swap_proxy",
+    "qaoa_vanilla_proxy",
+    "simulation",
+    "stabilizers",
+    "vqe_proxy",
+]
