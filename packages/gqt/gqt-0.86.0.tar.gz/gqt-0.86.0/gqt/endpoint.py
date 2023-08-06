4be9bc8ed4bc6f7c2bf5d1a73bc9b855e2f391ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:34 +0000

    Add package gqt/0.86.0/gqt-0.86.0.tar.gz

---
+++ packages/gqt/gqt-0.86.0.tar.gz/gqt-0.86.0/gqt/endpoint.py
@@ -0,0 +1,38 @@
+import sys
+
+import requests
+from graphql import get_introspection_query
+
+
+def fetch_schema(endpoint, headers, verify):
+    response = post(endpoint,
+                    {"query": get_introspection_query()},
+                    headers,
+                    verify)
+    response = response.json()
+
+    if 'errors' in response:
+        sys.exit(response['errors'])
+
+    return response['data']
+
+
+def post(endpoint, query, headers, verify):
+    response = requests.post(endpoint,
+                             json=query,
+                             headers=headers,
+                             verify=verify)
+
+    if response.status_code != 200:
+        response.raise_for_status()
+
+    return response
+
+
+def create_query(query, variables):
+    query = {"query": query}
+
+    if variables:
+        query['variables'] = variables
+
+    return query
