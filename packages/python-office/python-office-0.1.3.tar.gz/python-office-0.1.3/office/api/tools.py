abcc991fb1e59f4279f8bea8d71fd1ed54998e7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:57 +0000

    Add package python-office/0.1.3/python-office-0.1.3.tar.gz

---
+++ packages/python-office/python-office-0.1.3.tar.gz/python-office-0.1.3/office/api/tools.py
@@ -0,0 +1,24 @@
+from office.core.ToolsType import MainTools
+
+mainTools = MainTools()
+
+
+def transtools(to_lang, content):
+    mainTools.transtools(to_lang, content)
+
+
+def qrcodetools(url):
+    mainTools.qrcodetools(url)
+
+
+def passwordtools(len=8):
+    mainTools.passwordtools(len)
+
+
+def weather():
+    mainTools.weather()
+
+
+# 通过url，获取ip地址
+def url2ip(url):
+    mainTools.url2ip(url)
