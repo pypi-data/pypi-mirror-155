e59623fc4273b7a5e3848798032f47e38072afd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:31:39 +0000

    Add package archiverr/0.0.12/archiverr-0.0.12.tar.gz

---
+++ packages/archiverr/archiverr-0.0.12.tar.gz/archiverr-0.0.12/src/FolderName.py
@@ -0,0 +1,13 @@
+'''
+AUTEUR : Théo Hurlimann
+LIEU : CFPT Informatique Genève
+DATE : avril 2022
+PROJET: ARCHIVER
+VERSION : 1.0
+FICHIER : FolderName.py
+    Classe FolderName :
+        Classe enfant de ClassIdName
+'''
+from src.ClassIdName import ClassIdName
+class FolderName(ClassIdName):
+    pass
