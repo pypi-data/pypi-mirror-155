ee188a01dd6a9a904907173e7e19174addfcad6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:39 +0000

    Add package red2df/1.0.2/red2df-1.0.2.tar.gz

---
+++ packages/red2df/red2df-1.0.2.tar.gz/red2df-1.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='red2df',
+    version='1.0.2',
+    packages=['red2df'],
+    description='Create a Pandas dataframe with your Redshift data',
+    author='Hyunie',
+    author_email='kyunghyun7843@gmail.com',
+    url='https://github.com/Gyeong-Hyeon/pilot_works/tree/main/redshift_conn',
+    license='MIT',
+    python_requires='>=3',
+    install_requires=['pandas','psycopg2']
+)
