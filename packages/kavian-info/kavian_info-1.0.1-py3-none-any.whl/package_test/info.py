c8b9a679dfd802e62d0b7dd4e6c34d1f59a86cfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:32:05 +0000

    Add package kavian-info/1.0.1/kavian_info-1.0.1-py3-none-any.whl

---
+++ packages/kavian-info/kavian_info-1.0.1-py3-none-any.whl/package_test/info.py
@@ -0,0 +1,34 @@
+from colorama import Fore
+
+
+def kavian_info() -> None:
+    print(Fore.GREEN + 'Hello, My name is Kavian and I\'m currently 26 years old')
+
+
+def greet(msg: str = None) -> None:
+    if msg is None:
+        msg = input()
+    match msg.lower().split():
+        case ['hello' | 'salam', str(name)]:
+            print(Fore.BLUE, "Hello", name, "Welcome")
+        case name, int(age) if int(age) < 10:
+            print(f"{Fore.BLUE}happy birthday {name} you are {age} years old and little")
+        case name, int(age) if int(age) > 10:
+            print(f"{Fore.BLUE}happy birthday {name} you are {age} years old and big")
+        case [str('bye')]:
+            print(Fore.BLUE, "Bye")
+        case [name]:
+            print("Welcome", name)
+        case ['hello', *_, 'bye']:
+            print("Why are you leaving so soon")
+        case ['salam', _, _, name]:
+            print('salam bar to ey', name)
+        case ['bye', *extra, 'smth']:
+            print(Fore.RED + 'bye', extra)
+        case _:
+            print(Fore.RED + "fuck you")
+
+greet('Salam Kiara')
+greet('bye')
+if __name__ == '__main__':
+    kavian_info()
