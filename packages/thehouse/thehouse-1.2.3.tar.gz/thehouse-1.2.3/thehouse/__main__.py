e73413bab38041431ddac4360cfaa241514066de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:56 +0000

    Add package thehouse/1.2.3/thehouse-1.2.3.tar.gz

---
+++ packages/thehouse/thehouse-1.2.3.tar.gz/thehouse-1.2.3/thehouse/__main__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+from play import play
+
+if __name__ == "__main__":
+    play()
---
+++ packages/thehouse/thehouse-1.2.3.tar.gz/thehouse-1.2.3/thehouse/rooms/hallway.py
@@ -0,0 +1,6 @@
+from helpers import print_pause
+
+
+class Hallway:
+    def center(self):
+        print_pause("You are in the hallway.")
