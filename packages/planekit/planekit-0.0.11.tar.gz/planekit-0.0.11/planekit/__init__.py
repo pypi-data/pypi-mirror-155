abedc7a65f6b29c732b787975b173d75ee287cbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:05:23 +0000

    Add package planekit/0.0.11/planekit-0.0.11.tar.gz

---
+++ packages/planekit/planekit-0.0.11.tar.gz/planekit-0.0.11/planekit/__init__.py
@@ -0,0 +1,8 @@
+from .main import print_hi
+from pymavlink import mavutil
+def a(str):
+    print("hi")
+    master = mavutil.mavlink_connection(str)
+    # Wait a heartbeat before sending commands
+    master.wait_heartbeat()
+    # Choose a mode
