2b0196c021af983a204e57522c4aefb856f2d879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:35:20 +0000

    Add package classlogging/1.0.1/classlogging-1.0.1-py3-none-any.whl

---
+++ packages/classlogging/classlogging-1.0.1-py3-none-any.whl/classlogging/facility.py
@@ -0,0 +1,32 @@
+"""Facility classes"""
+
+import logging
+import typing as t
+from functools import lru_cache
+
+from .constants import DEFAULT_BASE_LOGGER
+from .extensions import Logger
+
+__all__ = [
+    "LoggerMixin",
+    "LoggerProperty",
+]
+
+
+class LoggerProperty:
+    """Class-level logger property"""
+
+    @staticmethod
+    @lru_cache(1)
+    def _prepare(caller_type: type) -> Logger:
+        logger_name: str = ".".join((DEFAULT_BASE_LOGGER, caller_type.__module__, caller_type.__name__))
+        return t.cast(Logger, logging.getLogger(logger_name))
+
+    def __get__(self, caller_instance: t.Any, caller_type: type) -> Logger:
+        return self._prepare(caller_type)
+
+
+class LoggerMixin:
+    """Add `logger` property"""
+
+    logger = LoggerProperty()
