7619481430960e4d6b1273ab114f95771bb0912a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:23 +0000

    Add package nosorog/0.2.0/nosorog-0.2.0-py3.9.egg

---
+++ packages/nosorog/nosorog-0.2.0-py3.9.egg/nosorog/__version__.py
@@ -0,0 +1,8 @@
+__title__ = "nosorog"
+__description__ = "An assertive security library."
+__url__ = "https://nosorog.readthedocs.io"
+__version__ = "0.2.0"
+__author__ = "Vyacheslav Lobanov"
+__author_email__ = "bartoldo@bk.ru"
+__license__ = "Apache 2.0"
+__copyright__ = "Copyright 2022 Vyacheslav Lobanov"
