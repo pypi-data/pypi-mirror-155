2a83cfc6ed553781fdbfe328259880e1c18d8266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:55 +0000

    Add package pysourcesinfo/0.3.0/pysourcesinfo-0.3.0.tar.gz

---
+++ packages/pysourcesinfo/pysourcesinfo-0.3.0.tar.gz/pysourcesinfo-0.3.0/tests/test_pysourcesinfo.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `pysourcesinfo` package."""
+
+
+import unittest
+
+from pysourcesinfo import pysourcesinfo
+
+
+class TestPysourcesinfo(unittest.TestCase):
+    """Tests for `pysourcesinfo` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
