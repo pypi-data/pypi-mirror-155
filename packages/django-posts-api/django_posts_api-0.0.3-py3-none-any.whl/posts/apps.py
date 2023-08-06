2bff673d60b69eb54a03b26a2dec870ed37bb499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:27 +0000

    Add package django-posts-api/0.0.3/django_posts_api-0.0.3-py3-none-any.whl

---
+++ packages/django-posts-api/django_posts_api-0.0.3-py3-none-any.whl/posts/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PostsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ai_view_poc.packages.django_posts_api.src.posts'
