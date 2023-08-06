e4779d821456dc662a7b688380c5aac3e76af52a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:37 +0000

    Add package curses-playlist/0.2.0/curses_playlist-0.2.0-py3-none-any.whl

---
+++ packages/curses-playlist/curses_playlist-0.2.0-py3-none-any.whl/curses_playlist/plist.py
@@ -0,0 +1,23 @@
+import os
+
+import click
+
+from curses_playlist.controller import PlistController
+
+
+@click.command
+@click.option(
+    "--playlist", "-p", help="specify location to write playlist to", default="curses_playlist.m3u"
+)
+@click.option(
+    "--working-directory", "-w", help="Where to look for video files", default="."
+)
+def main(playlist: str, working_directory: str):
+    if working_directory != ".":
+        os.chdir(working_directory)
+
+    PlistController(playlist)
+
+
+if __name__ == "__main__":
+    main()
