f2f584f385012dea8dfad3dda8a21e59ab3f1773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:25 +0000

    Add package integration-testing-environment-dev/0.0.1/integration_testing_environment_dev-0.0.1.tar.gz

---
+++ packages/integration-testing-environment-dev/integration_testing_environment_dev-0.0.1.tar.gz/integration_testing_environment_dev-0.0.1/integration_testing_environment/start_ite_option.py
@@ -0,0 +1,10 @@
+from integration_testing_environment.intergration_testing_environment_ui.extend_je_editor_ui import ITEUI
+
+
+def start_ite_as_cli():
+    ITEUI().start_editor()
+
+
+def start_ite_with_ui():
+    pass
+
