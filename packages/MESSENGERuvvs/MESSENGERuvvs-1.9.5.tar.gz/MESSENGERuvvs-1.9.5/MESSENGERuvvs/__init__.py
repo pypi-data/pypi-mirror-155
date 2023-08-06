e3341624856bf75f1764de76cb0c94df6366a6fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:44 +0000

    Add package MESSENGERuvvs/1.9.5/MESSENGERuvvs-1.9.5.tar.gz

---
+++ packages/MESSENGERuvvs/MESSENGERuvvs-1.9.5.tar.gz/MESSENGERuvvs-1.9.5/MESSENGERuvvs/__init__.py
@@ -0,0 +1,10 @@
+from MESSENGERuvvs.database_setup import messengerdb_connect
+from MESSENGERuvvs.MESSENGERdata import MESSENGERdata
+from .initialize_MESSENGERdata import initialize_MESSENGERdata
+
+
+name = 'MESSENGERuvvs'
+__author__ = 'Matthew Burger'
+__email__ = 'mburger@stsci.edu'
+__version__ = '1.9.5'
+__date__ = '2022-06-14'
