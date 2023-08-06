b551ad4021b49c60403ca1310d508231449e6f1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:32 +0000

    Add package authx/0.5.0/authx-0.5.0-py3-none-any.whl

---
+++ packages/authx/authx-0.5.0-py3-none-any.whl/authx/core/user.py
@@ -0,0 +1,37 @@
+class User:
+    """
+    Setup the user object, this is called by the authx framework, you can override this method to setup your own user object
+    """
+
+    def __init__(self, data=None):
+        """
+        Initialize the user object
+
+        Args:
+            data (dict): A dictionary containing the user's data
+        """
+        self.data = data
+        if data is None:
+            self.is_authenticated = False
+            self.is_admin = False
+            self.id = None
+            self.username = None
+        else:
+            self.is_authenticated = True
+            self.is_admin = "admin" in self.data.get("permissions")
+            self.id = int(self.data.get("id"))
+            self.username = self.data.get("username")
+
+    @classmethod
+    async def create(cls, token: str, backend):
+        """
+        Create a user object from a token
+
+        Args:
+            token (str): The token to create the user object from
+            backend (authx): The backend to use to create the user object
+        Returns:
+            User: The user object
+        """
+        data = await backend.decode_token(token)
+        return cls(data)
