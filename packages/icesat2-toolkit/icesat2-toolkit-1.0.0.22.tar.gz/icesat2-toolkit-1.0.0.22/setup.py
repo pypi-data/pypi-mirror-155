ba58db36301c9bd54d321290a7f64155c653a3ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:58 +0000

    Add package icesat2-toolkit/1.0.0.22/icesat2-toolkit-1.0.0.22.tar.gz

---
+++ packages/icesat2-toolkit/icesat2-toolkit-1.0.0.22.tar.gz/icesat2-toolkit-1.0.0.22/setup.py
@@ -0,0 +1,79 @@
+import os
+import sys
+import logging
+import subprocess
+from setuptools import setup, find_packages
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+log = logging.getLogger()
+
+# package description and keywords
+description = ('Python tools for obtaining and working with elevation data '
+    'from the NASA ICESat-2 mission')
+keywords = 'ICESat-2 laser altimetry, ATLAS, surface elevation and change'
+# get long_description from README.rst
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+long_description_content_type = "text/x-rst"
+
+# get install requirements
+with open('requirements.txt') as fh:
+    install_requires = [line.split().pop(0) for line in fh.read().splitlines()]
+
+# get version
+with open('version.txt') as fh:
+    version = fh.read()
+
+# list of all scripts to be included with package
+scripts=[os.path.join('scripts',f) for f in os.listdir('scripts') if f.endswith('.py')]
+
+# run cmd from the command line
+def check_output(cmd):
+    return subprocess.check_output(cmd).decode('utf')
+
+# check if GDAL is installed
+gdal_output = [None] * 4
+try:
+    for i, flag in enumerate(("--cflags", "--libs", "--datadir", "--version")):
+        gdal_output[i] = check_output(['gdal-config', flag]).strip()
+except:
+    log.warning('Failed to get options via gdal-config')
+else:
+    log.info("GDAL version from via gdal-config: {0}".format(gdal_output[3]))
+# if setting GDAL version from via gdal-config
+if gdal_output[3]:
+    # add version information to gdal in install_requires
+    gdal_index = install_requires.index('gdal')
+    install_requires[gdal_index] = 'gdal=={0}'.format(gdal_output[3])
+elif any(install_requires):
+    # gdal version not found
+    gdal_index = install_requires.index('gdal')
+    install_requires.pop(gdal_index)
+
+setup(
+    name='icesat2-toolkit',
+    version=version,
+    description=description,
+    long_description=long_description,
+    long_description_content_type=long_description_content_type,
+    url='https://github.com/tsutterley/read-ICESat-2',
+    author='Tyler Sutterley',
+    author_email='tsutterl@uw.edu',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Physics',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    keywords=keywords,
+    packages=find_packages(),
+    install_requires=install_requires,
+    scripts=scripts,
+    include_package_data=True,
+)
