27096eca73771b0e0967a2fa2f4262d7d1587bfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:42 +0000

    Add package plasmidrender/1.0.5/plasmidrender-1.0.5.tar.gz

---
+++ packages/plasmidrender/plasmidrender-1.0.5.tar.gz/plasmidrender-1.0.5/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="plasmidrender",
+    version="1.0.5",
+    author="strelka",
+    author_email="irohaprg@gmail.com",
+    description="CLI tool to illustrate plasmids with a structure described by json.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/strelka145/Plasmid_render",
+    install_requires=[
+        "playwright",
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+            'console_scripts':[
+                'plasmidrender = plasmidrender.main:draw',
+            ],
+        },
+)
