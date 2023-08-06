46ee22d0ab713a5b76cedec588727f84dcc69a39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:46 +0000

    Add package multibot/1.5.12/multibot-1.5.12-py3-none-any.whl

---
+++ packages/multibot/multibot-1.5.12-py3-none-any.whl/multibot/models/enums.py
@@ -0,0 +1,20 @@
+__all__ = ['Action', 'Platform']
+
+from enum import auto
+
+from flanautils import FlanaEnum
+
+
+class Action(FlanaEnum):
+    AUTO_WEATHER_CHART = auto()
+    MESSAGE_DELETED = auto()
+
+
+class Platform(FlanaEnum):
+    DISCORD = auto()
+    TELEGRAM = auto()
+    TWITCH = auto()
+
+    @property
+    def name(self):
+        return super().name.title()
