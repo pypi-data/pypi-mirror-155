1d722733030d4ed99ca365da2d0e58ebb0d153a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:17:19 +0000

    Add package clip-client/0.4.19.dev4/clip-client-0.4.19.dev4.tar.gz

---
+++ packages/clip-client/clip-client-0.4.19.dev4.tar.gz/clip-client-0.4.19.dev4/clip_client/__init__.py
@@ -0,0 +1,10 @@
+__version__ = '0.4.19.dev4'
+
+import os
+
+from clip_client.client import Client
+
+if 'NO_VERSION_CHECK' not in os.environ:
+    from clip_server.helper import is_latest_version
+
+    is_latest_version(github_repo='clip-as-service')
