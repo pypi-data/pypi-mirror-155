9b3a138db9890867d4aacd68f2777431cd4b19f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:03 +0000

    Add package TZnew/0.0.1/TZnew-0.0.1.tar.gz

---
+++ packages/TZnew/TZnew-0.0.1.tar.gz/TZnew-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='TZnew',
+      version='0.0.1',
+      description='abc',
+      packages=['TZnew'],
+      author_email='anariem@mail.ru',
+      zip_safe=False)
