c6fc800b2a8c83d42b3dfea9a66759954116e4cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:29 +0000

    Add package fbpcp/0.2.11/fbpcp-0.2.11-py3-none-any.whl

---
+++ packages/fbpcp/fbpcp-0.2.11-py3-none-any.whl/onedocker/common/env.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# This is the repository path that OneDocker downloads binaries from
+ONEDOCKER_REPOSITORY_PATH = "ONEDOCKER_REPOSITORY_PATH"
+
+# This is the local path that the binaries reside
+ONEDOCKER_EXE_PATH = "ONEDOCKER_EXE_PATH"
+
+# This is the path user specified to upload the core dump file to
+CORE_DUMP_REPOSITORY_PATH = "CORE_DUMP_REPOSITORY_PATH"
