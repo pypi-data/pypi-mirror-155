e672edaf529bf389f4914cb0b323dc9a1fe1488a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:35:29 +0000

    Add package XCurveLearn/0.0.2/XCurveLearn-0.0.2.tar.gz

---
+++ packages/XCurveLearn/XCurveLearn-0.0.2.tar.gz/XCurveLearn-0.0.2/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+import os
+
+if os.path.exists('README.md'):
+    with open('README.md', encoding='utf-8') as fp:
+        long_description = fp.read()
+else:
+    long_description = ' '
+if os.path.exists('requirements.txt'):
+    with open('requirements.txt', encoding='utf-8') as fp:
+        install_requires = fp.read()
+        install_requires = install_requires.split('\n')
+else:
+    install_requires = []
+
+setuptools.setup(
+    name="XCurveLearn",                     # This is the name of the package
+    version="0.0.2",                      # The initial release version
+    author="qqgroup",                     # Full name of the author
+    author_email='  ',
+    description="machine learning package",
+    url='https://gitee.com/Statusrank/xcurve-opt',
+    long_description=long_description,      # Long description read from the the readme file
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),    # List of all python modules to be installed
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                      # Information to filter the project on PyPi website
+    python_requires='>=3.7',                # Minimum version requirement of the package
+    py_modules=["XCurveLearn"],             # Name of the python package
+    package_dir={'XCurveLearn':'XCurveLearn'},
+    install_requires=install_requires       # Install other dependencies if any
+)
