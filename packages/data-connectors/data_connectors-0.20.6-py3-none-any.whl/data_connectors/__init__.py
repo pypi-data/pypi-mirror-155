4d7c1729f3f5966db7600523ad8122315ca49af4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:32 +0000

    Add package data-connectors/0.20.6/data_connectors-0.20.6-py3-none-any.whl

---
+++ packages/data-connectors/data_connectors-0.20.6-py3-none-any.whl/data_connectors/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.20.3'
+
+from data_connectors.gsheets import Gsheets
+from data_connectors.mssql import SQLServer
+from data_connectors.bigquery import BigQuery
+from data_connectors.calendar import Calendar
