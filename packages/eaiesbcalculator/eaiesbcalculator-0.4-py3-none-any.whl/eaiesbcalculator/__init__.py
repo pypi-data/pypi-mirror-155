0f2f972494a416275c71c4a1cbf43305ee7999b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:18 +0000

    Add package eaiesbcalculator/0.4/eaiesbcalculator-0.4-py3-none-any.whl

---
+++ packages/eaiesbcalculator/eaiesbcalculator-0.4-py3-none-any.whl/eaiesbcalculator/__init__.py
@@ -0,0 +1,29 @@
+import math
+
+
+def add_num(a,b):
+    return a+b
+
+def subtract_num(a,b):
+    return a-b
+
+def multiply_num(a,b):
+    return a*b
+
+def divide_num(a,b):
+    return a/b
+
+def simpleInterest(p,n,r):
+    return ((p*n*r)/100)
+
+def compoundInterest(p,n,r):
+    return (p * math.pow(1.0+r/100.0,n) - p)
+
+def concat(a,b):
+    return(str(a)+str(b))
+
+def remainder(a,b):
+    return a%b
+
+def power(a,b):
+    return math.pow(a,b)
