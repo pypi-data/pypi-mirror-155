286e98b67c0161c50cb487cfc73d5891ebf045e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:50:52 +0000

    Add package datazen/3.0.6/datazen-3.0.6.tar.gz

---
+++ packages/datazen/datazen-3.0.6.tar.gz/datazen-3.0.6/datazen/configs.py
@@ -0,0 +1,26 @@
+"""
+datazen - Top-level APIs for loading and interacting with configuration data.
+"""
+
+# built-in
+from typing import List
+
+# third-party
+from vcorelib.io.types import LoadResult
+
+# internal
+from datazen.load import DEFAULT_LOADS, LoadedFiles, load_dir
+
+
+def load(
+    directories: List[str],
+    variable_data: dict = None,
+    loads: LoadedFiles = DEFAULT_LOADS,
+) -> LoadResult:
+    """Load configuration data from a list of directories."""
+
+    result: dict = {}
+    errors = 0
+    for directory in directories:
+        errors += int(not load_dir(directory, result, variable_data, loads)[1])
+    return LoadResult(result, errors == 0)
