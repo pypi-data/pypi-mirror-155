f271897546f1310ef16accd45a1d3fcb30bfef92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:50 +0000

    Add package pyxem/0.14.2/pyxem-0.14.2-py3-none-any.whl

---
+++ packages/pyxem/pyxem-0.14.2-py3-none-any.whl/pyxem/release_info.py
@@ -0,0 +1,43 @@
+name = "pyxem"
+version = "0.14.2"
+author = "Duncan Johnstone, Phillip Crout, Magnus Nord"
+copyright = "Copyright 2016-2022, The pyxem developers"
+# Contributors listed by original commiter, maintainers, then other
+# within groups organised by  number of commits.
+# See: https://github.com/pyxem/pyxem/graphs/contributors
+credits = [
+    "Duncan Johnstone",
+    "Phillip Crout",
+    "Magnus Nord",
+    "Carter Francis",
+    "Joonatan Laulainen",
+    "Simon Høgås",
+    "Eirik Opheim",
+    "Ben Martineau",
+    "Tina Bergh",
+    "Eric Prestat",
+    "Niels Cautaerts",
+    "Stef Smeets",
+    "Håkon Wiik Ånes",
+    "Andrew Ross",
+    "Joseph Broussard",
+    "Sean Collins",
+    "Tom Furnival",
+    "Daen Jannis",
+    "Ida Hjorth",
+    "Mohsen Danaie",
+    "Andrew Herzing",
+    "Endre Jacobsen",
+    "Tim Poon",
+    "Tiarnan Doherty",
+    "Shuoyuan Huang",
+    "Jedrzej Morzy",
+    "Affan Iqbal",
+    "Tomas Ostasevicius",
+    "Matt von Lany",
+    "Rob Tovey",
+]
+license = "GPLv3"
+maintainer = "Duncan Johnstone, Phillip Crout, Magnus Nord, Carter Francis"
+email = "pyxem.team@gmail.com"
+status = "development"
