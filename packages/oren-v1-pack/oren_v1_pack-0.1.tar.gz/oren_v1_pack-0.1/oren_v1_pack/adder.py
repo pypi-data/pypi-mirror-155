076bcf40bf51712be9175565d494799df2879e4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:05:18 +0000

    Add package oren-v1-pack/0.1/oren_v1_pack-0.1.tar.gz

---
+++ packages/oren-v1-pack/oren_v1_pack-0.1.tar.gz/oren_v1_pack-0.1/oren_v1_pack/adder.py
@@ -0,0 +1,6 @@
+class plus():
+    def __init__(self,plus=0):
+        self.res = 4+plus
+        
+    def add(self,plus):
+        self.res = self.res + plus
---
+++ packages/oren-v1-pack/oren_v1_pack-0.1.tar.gz/oren_v1_pack-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='oren_v1_pack',
+      version='0.1',
+      description='oren_first_pack',
+      packages=['oren_v1_pack'],
+      zip_safe=False)
