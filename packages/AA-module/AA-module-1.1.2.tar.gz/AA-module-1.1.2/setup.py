07375749e2ebff07974b712af04a4ae603ecab2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:32:53 +0000

    Add package AA-module/1.1.2/AA-module-1.1.2.tar.gz

---
+++ packages/AA-module/AA-module-1.1.2.tar.gz/AA-module-1.1.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='AA-module',
+    version='1.1.2',
+    license='MIT',
+    author="Andreas Bigom, Michael Harborg, Oliver Elmgren & Marcus Presutti",
+    author_email='andreasbigom@gmail.com',
+    long_description = "This module can be used to perform, evaluate and analyse Conventional Archetypal Analysis, Ordinal Archetypal Analysis and Response Bias Ordinal Archetypal Analysis. The module is developed by Andreas Bigom, Michael Harborg, Marcus Presutti and Oliver Elmgren in a collaboration between the Technical University of Denmark and Copenhagen Business School in order to enable students to analyse human questionnaire data in an effective and meaningful way.",
+    description = "Module for performing various archetypal analyses.",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/anden1305/AAM-Module',
+    keywords='Archetypal Analysis',
+    python_requires = ">=3.10.0",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          'pandas',
+          'numpy',
+          'matplotlib',
+          'torch',
+          'scipy',
+          'sklearn',
+    ],
+)
---
+++ packages/AA-module/AA-module-1.1.2.tar.gz/AA-module-1.1.2/src/AAM/__init__.py
@@ -0,0 +1,3 @@
+from AAM import AA_class
+
+AA = AA_class.AA()
