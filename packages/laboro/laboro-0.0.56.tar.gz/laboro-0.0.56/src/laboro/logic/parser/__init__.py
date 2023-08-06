02f1bf5bfce950a64a02d5446404e059bce48f2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:25 +0000

    Add package laboro/0.0.56/laboro-0.0.56.tar.gz

---
+++ packages/laboro/laboro-0.0.56.tar.gz/laboro-0.0.56/src/laboro/logic/parser/__init__.py
@@ -0,0 +1,20 @@
+import logging
+from ast import literal_eval
+from asteval import Interpreter
+
+
+class Parser:
+  @staticmethod
+  def eval(expression):
+    try:
+      return Interpreter().eval(expression)
+    except SyntaxError as err:
+      logging.critical(f"Invalid syntax for expression: {expression}")
+      raise err
+
+  @staticmethod
+  def literal_eval(expression):
+    try:
+      return literal_eval(expression)
+    except (ValueError, SyntaxError):
+      return expression
