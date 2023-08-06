5f9f6e32546d4d21cd6ab28791a4634818aebdb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:16:10 +0000

    Add package Anar/0.0.1.1.1.1.1.3/Anar-0.0.1.1.1.1.1.3.tar.gz

---
+++ packages/Anar/Anar-0.0.1.1.1.1.1.3.tar.gz/Anar-0.0.1.1.1.1.1.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(version='0.0.1.1.1.1.1.3',
+      name='Anar',
+      description='abc',
+      packages=['Anar'],
+      author_email='anariem@mail.ru',
+      zip_safe=False)
