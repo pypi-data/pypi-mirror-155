45325beb7d0f09fc9f0d8f25db38efa7d2aa67e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:49 +0000

    Add package json2ast/1.0.1/json2ast-1.0.1.tar.gz

---
+++ packages/json2ast/json2ast-1.0.1.tar.gz/json2ast-1.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="json2ast",
+    version="1.0.1",
+    author="Adam Nichols",
+    description="The opposite of ast2json",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/HacMan137/json2ast",
+    project_urls={
+        "Bug tracker": "https://github.com/HacMan137/json2ast/issues"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    package_dir={"": "src"},
+    python_requires=">=3.10",
+)
