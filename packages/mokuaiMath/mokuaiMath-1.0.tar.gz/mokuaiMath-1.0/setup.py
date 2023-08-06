e874cf5a615e0c9d316006cc04c6ca107844ec08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:52 +0000

    Add package mokuaiMath/1.0/mokuaiMath-1.0.tar.gz

---
+++ packages/mokuaiMath/mokuaiMath-1.0.tar.gz/mokuaiMath-1.0/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+
+name='mokuaiMath',	# 对外我们模块的名字
+
+version='1.0', # 版本号
+
+description='这是第一个对外发布的模块，测试哦',	#描述
+
+author='lele', # 作者
+
+author_email='LL080300@163.com',
+
+py_modules=['mokuaiMath.demo1','mokuaiMath.demo2'] # 要发布的模块
+
+)
