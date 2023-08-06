b76eab2379b7cf95c1848179e1493706ed22bbb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:44 +0000

    Add package redshiftquery/0.0.6/redshiftquery-0.0.6-py3-none-any.whl

---
+++ packages/redshiftquery/redshiftquery-0.0.6-py3-none-any.whl/redshiftquery/GetTermsNoWinter.sql
@@ -0,0 +1,7 @@
+select time_term_year_ld, min(class_start_day_key) as startdate
+from tableau.enroll_drop_extract_tbl 
+where class_start_day_key > (select min(class_start_day_key) from tableau.enroll_drop_extract_tbl where time_term_year_ld = {term}) + 25
+ and class_start_day_key <= {date}
+ and time_term_year_ld not like 'Wint%'
+group by time_term_year_ld
+order by min(class_start_day_key)
