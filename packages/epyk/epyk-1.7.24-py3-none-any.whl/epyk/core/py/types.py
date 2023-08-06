611ab988747e62576d557cb542c3c5c146110e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:28 +0000

    Add package epyk/1.7.24/epyk-1.7.24-py3-none-any.whl

---
+++ packages/epyk/epyk-1.7.24-py3-none-any.whl/epyk/core/py/types.py
@@ -0,0 +1,9 @@
+
+from typing import Optional, Union, List
+from epyk.core.py import primitives
+
+
+PROFILE_TYPE = Optional[Union[bool, dict]]
+HELPER_TYPE = Union[primitives.HtmlModel, str]
+SIZE_TYPE = Union[tuple, int, str]
+JS_FUNCS_TYPES = Union[List[Union[str, primitives.JsDataModel]], str]
