2e47c5de21e0d3c0e439723855087a152cfc3c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:09 +0000

    Add package wangchohcao/1.0/wangchohcao-1.0.tar.gz

---
+++ packages/wangchohcao/wangchohcao-1.0.tar.gz/wangchohcao-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='wangchohcao',
+    version='1.0',
+    description='这是第一个对外发布的模块，测试哦',
+    author='gaoqi',
+    author_email='gaoqi110@163.com',
+    py_modules=['wangchaochao.demo1','wangchaochao.demo2'])
+
+
