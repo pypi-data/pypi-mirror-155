04ae839e25e0670604ed359a0663df6749de030a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:32:10 +0000

    Add package pyicon/0.2.0/pyicon-0.2.0-py3-none-any.whl

---
+++ packages/pyicon/pyicon-0.2.0-py3-none-any.whl/icon.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import click
+from PIL import Image
+from PIL import ImageOps
+
+
+@click.command()
+@click.option('--gray', '-g', help="Generate gray image.",
+              is_flag=True)
+@click.argument("FILENAME")
+@click.argument("SIZE", nargs=-1)
+def cli(filename, size, gray):
+    if not size:
+        size = (16, 32, 48, 128)
+    else:
+        size = [int(x) for x in size]
+
+    src_image = Image.open(filename, "r")
+
+    for size in size:
+        icon = src_image.resize((size, size), Image.LANCZOS)
+        icon.save("icon{size}.png".format(size=size))
+        if gray:
+            grayscale_icon = ImageOps.grayscale(icon)
+            grayscale_icon.save("icon{size}-disable.png".format(size=size))
+
+
+if __name__ == "__main__":
+    cli()
