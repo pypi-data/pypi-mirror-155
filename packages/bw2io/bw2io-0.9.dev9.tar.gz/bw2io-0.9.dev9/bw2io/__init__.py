d136fd2cac52b24a382fee6c16c8ff025da4f40b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:32:19 +0000

    Add package bw2io/0.9.dev9/bw2io-0.9.dev9.tar.gz

---
+++ packages/bw2io/bw2io-0.9.dev9.tar.gz/bw2io-0.9.dev9/bw2io/__init__.py
@@ -0,0 +1,167 @@
+__all__ = [
+    "activity_hash",
+    "add_ecoinvent_33_biosphere_flows",
+    "add_ecoinvent_34_biosphere_flows",
+    "add_ecoinvent_35_biosphere_flows",
+    "add_ecoinvent_36_biosphere_flows",
+    "add_ecoinvent_37_biosphere_flows",
+    "add_ecoinvent_38_biosphere_flows",
+    "add_example_database",
+    "backup_data_directory",
+    "backup_project_directory",
+    "BW2Package",
+    "bw2setup",
+    "ChemIDPlus",
+    "create_core_migrations",
+    "create_default_biosphere3",
+    "create_default_lcia_methods",
+    "CSVImporter",
+    "CSVLCIAImporter",
+    "DatabaseSelectionToGEXF",
+    "DatabaseToGEXF",
+    "Ecospold1LCIAImporter",
+    "es2_activity_hash",
+    "ExcelImporter",
+    "ExcelLCIAImporter",
+    "get_csv_example_filepath",
+    "get_xlsx_example_filepath",
+    "lci_matrices_to_excel",
+    "lci_matrices_to_matlab",
+    "load_json_data_file",
+    "Migration",
+    "migrations",
+    "MultiOutputEcospold1Importer",
+    "normalize_units",
+    "restore_project_directory",
+    "SimaProCSVImporter",
+    "SimaProLCIACSVImporter",
+    "SingleOutputEcospold1Importer",
+    "SingleOutputEcospold2Importer",
+    "useeio11",
+    "unlinked_data",
+    "UnlinkedData",
+]
+
+from .version import version as __version__
+
+from .chemidplus import ChemIDPlus
+from .package import BW2Package
+from .export import (
+    DatabaseToGEXF,
+    DatabaseSelectionToGEXF,
+    keyword_to_gephi_graph,
+    lci_matrices_to_excel,
+    lci_matrices_to_matlab,
+)
+from .backup import (
+    backup_data_directory,
+    backup_project_directory,
+    restore_project_directory,
+)
+from .data import (
+    add_ecoinvent_33_biosphere_flows,
+    add_ecoinvent_34_biosphere_flows,
+    add_ecoinvent_35_biosphere_flows,
+    add_ecoinvent_36_biosphere_flows,
+    add_ecoinvent_37_biosphere_flows,
+    add_ecoinvent_38_biosphere_flows,
+    add_example_database,
+    get_csv_example_filepath,
+    get_xlsx_example_filepath,
+)
+from .migrations import migrations, Migration, create_core_migrations
+from .importers import (
+    CSVImporter,
+    CSVLCIAImporter,
+    Ecospold1LCIAImporter,
+    ExcelImporter,
+    ExcelLCIAImporter,
+    MultiOutputEcospold1Importer,
+    SimaProCSVImporter,
+    SimaProLCIACSVImporter,
+    SingleOutputEcospold1Importer,
+    SingleOutputEcospold2Importer,
+    Exiobase3MonetaryImporter,
+    Exiobase3HybridImporter,
+)
+from .units import normalize_units
+from .unlinked_data import unlinked_data, UnlinkedData
+from .utils import activity_hash, es2_activity_hash, load_json_data_file
+
+from bw2data import config, databases
+
+config.metadata.extend(
+    [
+        migrations,
+        unlinked_data,
+    ]
+)
+
+
+def create_default_biosphere3(overwrite=False):
+    from .importers import Ecospold2BiosphereImporter
+
+    eb = Ecospold2BiosphereImporter()
+    eb.apply_strategies()
+    eb.write_database(overwrite=overwrite)
+
+
+def create_default_lcia_methods(overwrite=False, rationalize_method_names=False):
+    from .importers import EcoinventLCIAImporter
+
+    ei = EcoinventLCIAImporter()
+    if rationalize_method_names:
+        ei.add_rationalize_method_names_strategy()
+    ei.apply_strategies()
+    ei.write_methods(overwrite=overwrite)
+
+
+def bw2setup():
+    if "biosphere3" in databases:
+        print("Biosphere database already present!!! No setup is needed")
+        return
+    print("Creating default biosphere\n")
+    create_default_biosphere3()
+    print("Creating default LCIA methods\n")
+    create_default_lcia_methods()
+    print("Creating core data migrations\n")
+    create_core_migrations()
+
+
+def useeio11(name="US EEIO 1.1"):
+    URL = "https://www.lcacommons.gov/lca-collaboration/ws/public/download/json/repository_US_Environmental_Protection_Agency@USEEIO"
+
+    if "US EEIO 1.1" in databases:
+        print("US EEIO 1.1 already present")
+        return
+
+    from .importers.json_ld import JSONLDImporter
+    from .importers.json_ld_lcia import JSONLDLCIAImporter
+    from pathlib import Path
+    import tempfile
+    import urllib
+    import zipfile
+
+    with tempfile.TemporaryDirectory() as td:
+        dp = Path(td)
+        print("Downloading US EEIO 1.1")
+        urllib.request.urlretrieve(URL, dp / "USEEIO11.zip")
+
+        print("Unzipping file")
+        with zipfile.ZipFile(dp / "USEEIO11.zip", "r") as zip_ref:
+            zip_ref.extractall(dp)
+
+        (dp / "USEEIO11.zip").unlink()
+
+        print("Importing data")
+        j = JSONLDImporter(dp, name)
+        j.apply_strategies(no_warning=True)
+        j.merge_biosphere_flows()
+        assert j.all_linked
+        j.write_database()
+
+        l = JSONLDLCIAImporter(dp)
+        l.apply_strategies()
+        l.match_biosphere_by_id(name)
+        assert l.all_linked
+        l.write_methods()
---
+++ packages/bw2io/bw2io-0.9.dev9.tar.gz/bw2io-0.9.dev9/bw2io/extractors/exiobase.py
@@ -0,0 +1,110 @@
+import csv
+import os
+import re
+from pathlib import Path
+
+from tqdm import tqdm
+
+
+def remove_numerics(string):
+    """Transform names like 'Tobacco products (16)' into 'Tobacco products'"""
+    return re.sub(r" \(\d\d\)$", "", string)
+
+
+class Exiobase3MonetaryDataExtractor(object):
+    @classmethod
+    def _check_dir(cls, path):
+        # Note: this assumes industry by industry
+        assert os.path.isdir(path), "Must supply path to EXIOBASE data folder"
+        assert "x.txt" in os.listdir(path), "Directory path must include Exiobase files"
+
+    @classmethod
+    def _get_production_volumes(cls, dirpath):
+        with open(dirpath / "x.txt") as csvfile:
+            reader = csv.DictReader(csvfile, delimiter="\t")
+            data = {
+                (row["sector"], row["region"]): float(row["indout"]) for row in reader
+            }
+        return data
+
+    @classmethod
+    def _get_unit_data(cls, dirpath):
+        lookup = {"M.EUR": "million €"}
+
+        with open(dirpath / "unit.txt") as csvfile:
+            reader = csv.DictReader(csvfile, delimiter="\t")
+            data = {
+                (row["sector"], row["region"]): lookup[row["unit"]] for row in reader
+            }
+        return data
+
+    @classmethod
+    def get_flows(cls, dirpath):
+        dirpath = Path(dirpath)
+
+        with open(dirpath / "satellite" / "unit.txt") as csvfile:
+            reader = csv.reader(csvfile, delimiter="\t")
+            next(reader)
+            data = {o[0]: o[1] for o in reader}
+        return data
+
+    @classmethod
+    def get_products(cls, dirpath):
+        dirpath = Path(dirpath)
+
+        cls._check_dir(dirpath)
+        units = cls._get_unit_data(dirpath)
+        volumes = cls._get_production_volumes(dirpath)
+        return [
+            {
+                "name": key[0],
+                "location": key[1],
+                "unit": units[key],
+                "production volume": volumes[key],
+            }
+            for key in units
+        ]
+
+    @classmethod
+    def get_technosphere_iterator(
+        cls, dirpath, num_products, ignore_small_balancing_corrections=True
+    ):
+        dirpath = Path(dirpath)
+
+        with open(dirpath / "A.txt") as f:
+            reader = csv.reader(f, delimiter="\t")
+            locations = next(reader)[2:]
+            names = [remove_numerics(o) for o in next(reader)[2:]]
+
+            for line in tqdm(reader):
+                inpt = (remove_numerics(line[1]), line[0])
+                for index, elem in enumerate(line[2:]):
+                    if elem and float(elem) != 0:
+                        if (
+                            ignore_small_balancing_corrections
+                            and abs(float(elem)) < 1e-15
+                        ):
+                            continue
+                        else:
+                            yield (inpt, (names[index], locations[index]), float(elem))
+
+    @classmethod
+    def get_biosphere_iterator(cls, dirpath, ignore_small_balancing_corrections=True):
+        dirpath = Path(dirpath)
+
+        with open(dirpath / "satellite" / "S.txt") as f:
+            reader = csv.reader(f, delimiter="\t")
+            locations = next(reader)[1:]
+            names = [remove_numerics(o) for o in next(reader)[1:]]
+
+            for line in tqdm(reader):
+                flow = line[0]
+                for index, elem in enumerate(line[1:]):
+                    if elem and float(elem) != 0:
+                        if (
+                            ignore_small_balancing_corrections
+                            and abs(float(elem)) < 1e-15
+                        ):
+                            continue
+                        else:
+                            yield (flow, (names[index], locations[index]), float(elem))
