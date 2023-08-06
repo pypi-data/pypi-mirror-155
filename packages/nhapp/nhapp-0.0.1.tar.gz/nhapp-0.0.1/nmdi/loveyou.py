116c19f9fc20609c3d254a090656a7335bfcd862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:19 +0000

    Add package nhapp/0.0.1/nhapp-0.0.1.tar.gz

---
+++ packages/nhapp/nhapp-0.0.1.tar.gz/nhapp-0.0.1/nmdi/loveyou.py
@@ -0,0 +1,13 @@
+import youtube_dl
+def download_ytvid_as_mp3():
+    video_url = all_song_URL
+    video_info = youtube_dl.YoutubeDL().extract_info(url = video_url,download=False)
+    filename = f"{video_info['title']}.mp3"
+    options={
+        'format':'bestaudio/best',
+        'keepvideo':False,
+        'outtmpl':filename,
+    }
+    with youtube_dl.YoutubeDL(options) as ydl:
+        ydl.download([video_info['webpage_url']])
+    print("Download complete... {}".format(filename))
