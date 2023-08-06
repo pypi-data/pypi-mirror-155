7fd481fa42e3fe6cbf02ef5899dc8b9b4edd0b38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:01 +0000

    Add package jonschema/0.1.1/jonschema-0.1.1-py3-none-any.whl

---
+++ packages/jonschema/jonschema-0.1.1-py3-none-any.whl/jonschema/__init__.py
@@ -0,0 +1,16 @@
+from .scripts.JON import JONDefaultSchema
+from .scripts.JON_sup import String, Number, Boolean, Date, File, Enum, ChosenType, Object, Array, Clone
+
+__all__ = [
+    'JONDefaultSchema',
+    'String',
+    'Number',
+    'Boolean',
+    'Date',
+    'File',
+    'Enum',
+    'ChosenType',
+    'Object',
+    'Array',
+    'Clone',
+]
