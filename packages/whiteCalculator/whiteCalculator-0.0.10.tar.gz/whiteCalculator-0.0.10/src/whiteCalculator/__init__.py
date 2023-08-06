0093cd51d866f6c1378abaa0ad8d00da94a24ee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:11 +0000

    Add package whiteCalculator/0.0.10/whiteCalculator-0.0.10.tar.gz

---
+++ packages/whiteCalculator/whiteCalculator-0.0.10.tar.gz/whiteCalculator-0.0.10/src/whiteCalculator/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+whiteCalculator library
+-------------------------
+
+This is a proof of concept of a whiteCalculator library, written in Python.
+
+"""
+
+__title__ = "whiteCalculator"
+__version__ = "0.0.8"
+__build__ = 0x000007
+__author__ = "WhiteNight"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2021 WhiteNight"
+
+
+from .calculator import Calculator
+from .Formula import Formula
