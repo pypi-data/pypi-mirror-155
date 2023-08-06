f2e23da08142ec85e34ad71745612c8307aab428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:21 +0000

    Add package python-object-detector/0.1.1/python_object_detector-0.1.1-py3-none-any.whl

---
+++ packages/python-object-detector/python_object_detector-0.1.1-py3-none-any.whl/object_detector/scan_file.py
@@ -0,0 +1,15 @@
+from object_detector import scan_buffer
+
+
+def scan_file(path):
+    """Scans image file with object detector.
+
+    :param path: Path to image file.
+    :return: Detection result.
+    """
+    with open(path, "rb") as file_in:
+        buffer = file_in.read()
+
+    with open(path + ".out", "w") as file_out:
+        out = scan_buffer(buffer)
+        file_out.write(out)
