778a6be69824b1189a56d62f11af473602d10829
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:55 +0000

    Add package scrapy-poet/0.4.0/scrapy-poet-0.4.0.tar.gz

---
+++ packages/scrapy-poet/scrapy-poet-0.4.0.tar.gz/scrapy-poet-0.4.0/scrapy_poet/__init__.py
@@ -0,0 +1,7 @@
+from .api import DummyResponse, callback_for
+from .middleware import InjectionMiddleware
+from .page_input_providers import (
+    CacheDataProviderMixin,
+    HttpResponseProvider,
+    PageObjectInputProvider,
+)
