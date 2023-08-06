d053ebc39128c2044c3e72687a24a3473b3ab954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:26 +0000

    Add package bentley-ottmann/7.3.0/bentley_ottmann-7.3.0-py3-none-any.whl

---
+++ packages/bentley-ottmann/bentley_ottmann-7.3.0-py3-none-any.whl/bentley_ottmann/core/event.py
@@ -0,0 +1,192 @@
+from abc import (ABC,
+                 abstractmethod)
+from reprlib import recursive_repr
+from typing import (Dict,
+                    List,
+                    Optional,
+                    Sequence,
+                    Set,
+                    Tuple)
+
+from ground.base import Relation
+from ground.hints import (Point,
+                          Segment)
+from reprit.base import generate_repr
+
+from .utils import (classify_overlap,
+                    to_sorted_pair)
+
+
+class Event(ABC):
+    __slots__ = ()
+
+    is_left = False
+    left = None  # type: Optional['LeftEvent']
+    right = None  # type: Optional['RightEvent']
+
+    @property
+    @abstractmethod
+    def end(self) -> Point:
+        """Returns end of the event."""
+
+    @property
+    @abstractmethod
+    def original_end(self) -> Point:
+        """Returns original end of the event."""
+
+    @property
+    @abstractmethod
+    def original_start(self) -> Point:
+        """Returns original start of the segment."""
+
+    @abstractmethod
+    def register_tangent(self, tangent: 'Event') -> None:
+        """Registers new tangent to the event"""
+
+    @property
+    @abstractmethod
+    def segments_ids(self) -> Set[int]:
+        """Returns segments ids of the event."""
+
+    @property
+    @abstractmethod
+    def start(self) -> Point:
+        """Returns start of the event."""
+
+    @property
+    @abstractmethod
+    def tangents(self) -> Sequence['Event']:
+        """Returns tangents of the event."""
+
+
+class LeftEvent(Event):
+    @classmethod
+    def from_segment(cls, segment: Segment, segment_id: int) -> 'LeftEvent':
+        start, end = to_sorted_pair(segment.start, segment.end)
+        result = LeftEvent(start, None, start, {start: {end: {segment_id}}})
+        result.right = RightEvent(end, result, end)
+        return result
+
+    is_left = True
+
+    @property
+    def end(self) -> Point:
+        return self.right.start
+
+    @property
+    def original_start(self) -> Point:
+        return self._original_start
+
+    @property
+    def original_end(self) -> Point:
+        return self.right.original_start
+
+    @property
+    def segments_ids(self) -> Set[int]:
+        return self.parts_ids[self.start][self.end]
+
+    @property
+    def start(self) -> Point:
+        return self._start
+
+    @property
+    def tangents(self) -> Sequence[Event]:
+        return self._tangents
+
+    __slots__ = ('parts_ids', 'right', '_original_start', '_relations_mask',
+                 '_start', '_tangents')
+
+    def __init__(self,
+                 start: Point,
+                 right: Optional['RightEvent'],
+                 original_start: Point,
+                 parts_ids: Dict[Point, Dict[Point, Set[int]]]) -> None:
+        self.right, self.parts_ids, self._original_start, self._start = (
+            right, parts_ids, original_start, start
+        )
+        self._relations_mask = 0
+        self._tangents = []  # type: List[Event]
+
+    __repr__ = recursive_repr()(generate_repr(__init__))
+
+    def divide(self, point: Point) -> Tuple['RightEvent', 'LeftEvent']:
+        """Divides the event at given break point and returns tail."""
+        segments_ids = self.segments_ids
+        (self.parts_ids.setdefault(self.start, {})
+         .setdefault(point, set()).update(segments_ids))
+        (self.parts_ids.setdefault(point, {})
+         .setdefault(self.end, set()).update(segments_ids))
+        point_to_end_event = self.right.left = LeftEvent(
+                point, self.right, self.original_start, self.parts_ids
+        )
+        point_to_start_event = self.right = RightEvent(point, self,
+                                                       self.original_end)
+        return point_to_start_event, point_to_end_event
+
+    def has_only_relations(self, *relations: Relation) -> bool:
+        mask = self._relations_mask
+        for relation in relations:
+            mask &= ~(1 << relation)
+        return not mask
+
+    def merge_with(self, other: 'LeftEvent') -> None:
+        assert self.start == other.start and self.end == other.end
+        full_relation = classify_overlap(
+                other.original_start, other.original_end,
+                self.original_start, self.original_end
+        )
+        self.register_relation(full_relation)
+        other.register_relation(full_relation.complement)
+        start, end = self.start, self.end
+        self.parts_ids[start][end] = other.parts_ids[start][end] = (
+                self.parts_ids[start][end] | other.parts_ids[start][end]
+        )
+
+    def register_tangent(self, tangent: Event) -> None:
+        assert self.start == tangent.start
+        self._tangents.append(tangent)
+
+    def register_relation(self, relation: Relation) -> None:
+        self._relations_mask |= 1 << relation
+
+
+class RightEvent(Event):
+    @property
+    def end(self) -> Point:
+        return self.left.start
+
+    @property
+    def original_end(self) -> Point:
+        return self.left.original_start
+
+    @property
+    def original_start(self) -> Point:
+        return self._original_start
+
+    @property
+    def segments_ids(self) -> Set[int]:
+        return self.left.segments_ids
+
+    @property
+    def start(self) -> Point:
+        return self._start
+
+    @property
+    def tangents(self) -> Sequence[Event]:
+        return self._tangents
+
+    __slots__ = 'left', '_original_start', '_start', '_tangents'
+
+    def __init__(self,
+                 start: Point,
+                 left: Optional[LeftEvent],
+                 original_start: Point) -> None:
+        self.left, self._original_start, self._start = (left, original_start,
+                                                        start)
+        self._tangents = []  # type: List[Event]
+
+    __repr__ = recursive_repr()(generate_repr(__init__))
+
+    def register_tangent(self, tangent: 'Event') -> None:
+        assert self.start == tangent.start
+        self._tangents.append(tangent)
---
+++ packages/bentley-ottmann/bentley_ottmann-7.3.0-py3-none-any.whl/bentley_ottmann/core/utils.py
@@ -0,0 +1,51 @@
+from typing import (Hashable,
+                    Iterable,
+                    Tuple,
+                    TypeVar)
+
+from ground.base import Relation
+from ground.hints import Point
+
+_T = TypeVar('_T')
+
+
+def all_unique(values: Iterable[Hashable]) -> bool:
+    seen = set()
+    seen_add = seen.add
+    for value in values:
+        if value in seen:
+            return False
+        else:
+            seen_add(value)
+    return True
+
+
+def classify_overlap(test_start: Point,
+                     test_end: Point,
+                     goal_start: Point,
+                     goal_end: Point) -> Relation:
+    assert test_start < test_end
+    assert goal_start < goal_end
+    if test_start == goal_start:
+        return (Relation.COMPONENT
+                if test_end < goal_end
+                else (Relation.COMPOSITE
+                      if goal_end < test_end
+                      else Relation.EQUAL))
+    elif test_end == goal_end:
+        return (Relation.COMPOSITE
+                if test_start < goal_start
+                else Relation.COMPONENT)
+    elif goal_start < test_start < goal_end:
+        return (Relation.COMPONENT
+                if test_end < goal_end
+                else Relation.OVERLAP)
+    else:
+        assert test_start < goal_start < test_end
+        return (Relation.COMPOSITE
+                if goal_end < test_end
+                else Relation.OVERLAP)
+
+
+def to_sorted_pair(start: _T, end: _T) -> Tuple[_T, _T]:
+    return (start, end) if start < end else (end, start)
