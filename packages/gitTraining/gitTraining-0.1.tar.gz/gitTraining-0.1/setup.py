fa79cb50e7542966d38c5cfe621fa60aa44e48b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:31:02 +0000

    Add package gitTraining/0.1/gitTraining-0.1.tar.gz

---
+++ packages/gitTraining/gitTraining-0.1.tar.gz/gitTraining-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="gitTraining",
+    version="0.1",
+    license="BSD License",
+    author="Christian Kindermann",
+    author_email="chris.kind.man@gmail.com",
+    py_modules=["hiccupy"],
+    # packages=find_packages("src"),
+    # package_dir={"": "src"},
+    url="https://github.com/ckindermann/gitTraining",
+    keywords="example project",
+    # install_requires=[
+    #    "",
+    # ],
+)
