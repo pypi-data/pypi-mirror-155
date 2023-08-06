d9804eccfeaf6009eb299324203c5be4702ede24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:46 +0000

    Add package GaiaXPy/1.1.3/GaiaXPy-1.1.3.tar.gz

---
+++ packages/GaiaXPy/GaiaXPy-1.1.3.tar.gz/GaiaXPy-1.1.3/gaiaxpy/__init__.py
@@ -0,0 +1,9 @@
+from .calibrator.calibrator import calibrate
+from .converter.converter import convert
+from .core import pwl_to_wl, wl_to_pwl, pwl_range, wl_range
+from .error_correction import apply_error_correction
+from .generator.generator import generate
+from .generator.photometric_system import PhotometricSystem
+from .plotter.plot_spectra import plot_spectra
+
+__version__ = '1.1.3'
