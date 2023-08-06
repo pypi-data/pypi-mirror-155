d442690b233c769c8217d064de11043079add79d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:11 +0000

    Add package djangoCRUDframework/0.2.4/djangoCRUDframework-0.2.4.tar.gz

---
+++ packages/djangoCRUDframework/djangoCRUDframework-0.2.4.tar.gz/djangoCRUDframework-0.2.4/crud_framework/views/oauth.py
@@ -0,0 +1,12 @@
+from oauth2_provider.views.mixins import ProtectedResourceMixin, OAuthLibMixin
+from .base import method_decorator, csrf_exempt, view_catch_error, BaseCrudView, BaseView
+
+
+@method_decorator([csrf_exempt, view_catch_error], name='dispatch')
+class OauthCrudView(ProtectedResourceMixin, OAuthLibMixin, BaseCrudView):
+    pass
+
+
+@method_decorator([csrf_exempt, view_catch_error], name='dispatch')
+class OauthFunctionalView(ProtectedResourceMixin, OAuthLibMixin, BaseView):
+    pass
