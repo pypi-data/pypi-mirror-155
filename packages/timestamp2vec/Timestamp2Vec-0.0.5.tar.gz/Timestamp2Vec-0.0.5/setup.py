124776d627f7b293b77d98b6e3b0aa6c1dc19bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:30 +0000

    Add package timestamp2vec/0.0.5/Timestamp2Vec-0.0.5.tar.gz

---
+++ packages/timestamp2vec/Timestamp2Vec-0.0.5.tar.gz/Timestamp2Vec-0.0.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.5'
+DESCRIPTION = 'Vectorizing Timestamps by means of a Variational Autoencoder'
+
+# Setting up
+setup(
+    name="Timestamp2Vec",
+    version=VERSION,
+    author="Gideon Rouwendaal",
+    author_email="<gideon.rouwendaal@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['keras', 'matplotlib', 'numpy', 'tensorflow', 'pathlib', 'seaborn', 'pandas', 'datetime'],
+    keywords=['python', 'vector', 'time', 'timestamps'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
