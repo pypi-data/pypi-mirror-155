d8b47098608f033b3b634be16625814e34e53f6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:12 +0000

    Add package ape-bsc/0.3.0a0/ape-bsc-0.3.0a0.tar.gz

---
+++ packages/ape-bsc/ape-bsc-0.3.0a0.tar.gz/ape-bsc-0.3.0a0/tests/test_integration.py
@@ -0,0 +1,33 @@
+EXPECTED_OUTPUT = """
+arbitrum
+├── mainnet
+│   ├── alchemy
+│   └── geth  (default)
+├── testnet
+│   ├── alchemy  (default)
+│   └── geth
+└── local  (default)
+    └── test  (default)
+""".strip()
+
+
+def assert_rich_text(actual: str, expected: str):
+    """
+    The output from `rich` causes a bunch of extra spaces to
+    appear at the end of each line. For easier testing, we remove those here.
+    """
+    actual = f"arbitrum{actual.split('arbitrum')[-1]}"
+    expected = expected.strip()
+    lines = actual.split("\n")
+    new_lines = []
+    for line in lines:
+        if line:
+            new_lines.append(line.rstrip())
+
+    actual = "\n".join(new_lines)
+    assert actual == expected
+
+
+def test_networks(runner, cli):
+    result = runner.invoke(cli, ["networks", "list"])
+    assert_rich_text(result.output, EXPECTED_OUTPUT)
