54f3276fc4fa1a63b095469ecdc6fb9dc5e79cf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:12 +0000

    Add package upbge-stubs/0.3.1.27.dev2499488706/upbge-stubs-0.3.1.27.dev2499488706.tar.gz

---
+++ packages/upbge-stubs/upbge-stubs-0.3.1.27.dev2499488706.tar.gz/upbge-stubs-0.3.1.27.dev2499488706/bake_target_items/__init__.pyi
@@ -0,0 +1,21 @@
+"""
+
+
+Bake Target Items
+^^^^^^^^^^^^^^^^^
+
+:IMAGE_TEXTURES:  
+  Image Textures.
+
+  Bake to image data-blocks associated with active image texture nodes in materials.
+
+:VERTEX_COLORS:   
+  Color Attributes.
+
+  Bake to active color attribute layer on meshes.
+
+.. _rna-enum-bake-target-items:
+
+"""
+
+import typing
