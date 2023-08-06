efc2e6786e1ec18dec7b3b2ab53adb0ed6a60fb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:45 +0000

    Add package beaker-py/1.5.0/beaker-py-1.5.0.tar.gz

---
+++ packages/beaker-py/beaker-py-1.5.0.tar.gz/beaker-py-1.5.0/beaker/data_model/experiment.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+from typing import Optional, Tuple
+
+from pydantic import Field
+
+from .account import Account
+from .base import BaseModel
+from .job import Job
+from .workspace import WorkspaceRef
+
+
+class Experiment(BaseModel):
+    id: str
+    name: Optional[str] = None
+    full_name: Optional[str] = None
+    description: Optional[str] = None
+    owner: Account
+    author: Account
+    created: datetime
+    workspace_ref: WorkspaceRef
+    jobs: Tuple[Job, ...] = Field(default_factory=tuple)
+
+    @property
+    def display_name(self) -> str:
+        return self.name if self.name is not None else self.id
+
+    @property
+    def workspace(self) -> WorkspaceRef:
+        return self.workspace_ref
+
+
+class Task(BaseModel):
+    id: str
+    name: Optional[str] = None
+    experiment_id: str
+    owner: Account
+    author: Account
+    created: datetime
+    schedulable: bool = False
+    jobs: Tuple[Job, ...] = Field(default_factory=tuple)
+
+    @property
+    def display_name(self) -> str:
+        return self.name if self.name is not None else self.id
+
+    @property
+    def latest_job(self) -> Optional[Job]:
+        if not self.jobs:
+            return None
+        return sorted(self.jobs, key=lambda job: job.status.created)[-1]
+
+
+class ExperimentsPage(BaseModel):
+    data: Tuple[Experiment, ...]
+    next_cursor: Optional[str] = None
+
+
+class ExperimentPatch(BaseModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
