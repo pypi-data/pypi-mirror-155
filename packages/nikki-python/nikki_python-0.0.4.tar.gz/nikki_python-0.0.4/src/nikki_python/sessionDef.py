e5aa6263c5cac65198148573274e3cf8dbeeed8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:40 +0000

    Add package nikki-python/0.0.4/nikki_python-0.0.4.tar.gz

---
+++ packages/nikki-python/nikki_python-0.0.4.tar.gz/nikki_python-0.0.4/src/nikki_python/sessionDef.py
@@ -0,0 +1,11 @@
+
+
+from sourceInfo import sourceInfo
+from srvCommon import servMsgType
+
+class serviceJoinInfo:
+    sessionID =''
+    userID = ""
+    srv = sourceInfo()
+    wsAddr = '' 
+    type = servMsgType.service.value
