bef66554cfd6aebedf993e9617c50a1dbfdae9ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:21:19 +0000

    Add package ai-core-sdk/1.13.3/ai_core_sdk-1.13.3-py3-none-any.whl

---
+++ packages/ai-core-sdk/ai_core_sdk-1.13.3-py3-none-any.whl/ai_core_sdk/models/application_source.py
@@ -0,0 +1,30 @@
+from typing import Dict
+
+
+class ApplicationSource:
+    """The ApplicationSource object defines the application source.
+
+        :param repourl: URL of the repository, defaults to None
+        :type repourl: str, optional
+        :param path: path within the repository, defaults to None
+        :type path: str, optional
+        :param revision: revision number of the application, defaults to None
+        :type revision: str, optional
+        """
+
+    def __init__(self, repourl: str = None, path: str = None, revision: str = None, **kwargs):
+        self.repourl: str = repourl
+        self.path: str = path
+        self.revision: str = revision
+
+    @staticmethod
+    def from_dict(application_source_dict: Dict[str, str]):
+        """Returns a :class:`ai_core_sdk.models.application_source.ApplicationSource` object, created from the values in
+        the dict provided as parameter
+
+        :param application_source_dict: Dict which includes the necessary values to create the object
+        :type application_source_dict: Dict[str, Any]
+        :return: An object, created from the values provided
+        :rtype: class:`ai_core_sdk.models.application_source.ApplicationSource`
+        """
+        return ApplicationSource(**application_source_dict)
