2d58c6f6cca07a1a62071c61b34c9d7c98df287a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:36 +0000

    Add package piction/0.0/piction-0.0.tar.gz

---
+++ packages/piction/piction-0.0.tar.gz/piction-0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+	name="piction",
+	version="0.0",
+	description="お絵描き関数変換アプリ",
+	auther="hackathonNIT",
+	url="https://github.com/hackathonNIT/piction",
+	packages=["piction"],
+)
