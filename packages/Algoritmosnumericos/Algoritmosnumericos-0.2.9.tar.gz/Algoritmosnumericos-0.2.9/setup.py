a477be8954b2adf75ce57f907de628313e2cabfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:56 +0000

    Add package Algoritmosnumericos/0.2.9/Algoritmosnumericos-0.2.9.tar.gz

---
+++ packages/Algoritmosnumericos/Algoritmosnumericos-0.2.9.tar.gz/Algoritmosnumericos-0.2.9/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 30 22:42:52 2022
+
+@author: Asus
+"""
+
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='Algoritmosnumericos',
+    version='0.2.9',
+    description='maths algorithms and util funtion for plot numerical methods',
+    long_description=open('README.txt').read()+'\n\n' +
+    open('CHANGELOG.txt').read(),
+    url='https://github.com/Ratabart666',
+    author='Thomas Gomez',
+    author_email='t.gomezs2@uniandes.edu.co',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='numerical methods',
+    packages=find_packages(),
+    install_requires=['numpy', 'scipy', 'statistic','prettytable','matplotlib']
+)
