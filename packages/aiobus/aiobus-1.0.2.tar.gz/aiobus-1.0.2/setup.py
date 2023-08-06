282d9e1480117e01a302ab598ee4be2caa9b8040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:52 +0000

    Add package aiobus/1.0.2/aiobus-1.0.2.tar.gz

---
+++ packages/aiobus/aiobus-1.0.2.tar.gz/aiobus-1.0.2/setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup, find_packages
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# The text of the README file
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
+
+
+setup(
+    name="aiobus",
+    version="1.0.2",
+    description="An event-bus application layer, supports redis",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Sirius-social/aiobus",
+    author="Networks Synergy",
+    author_email="support@socialsirius.com",
+    license="Apache License",
+    maintainer=', '.join(('Pavel Minenkov <minikspb@gmail.com>',)),
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+    ],
+    packages=[],
+    python_requires='>=3.5',
+    include_package_data=True,
+    install_requires=[
+        'aioredis>=1.3.1'
+    ]
+)
