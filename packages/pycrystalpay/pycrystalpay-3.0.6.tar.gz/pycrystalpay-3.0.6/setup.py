d7d300c32bfee825cf5e80dc9f2e54bffa34f560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:52 +0000

    Add package pycrystalpay/3.0.6/pycrystalpay-3.0.6.tar.gz

---
+++ packages/pycrystalpay/pycrystalpay-3.0.6.tar.gz/pycrystalpay-3.0.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='pycrystalpay',
+      version='3.0.6',
+      description='Lib for crystalpy.',
+      packages=['pycrystalpay'],
+      install_requires=['aiohttp==3.8.1'],
+      url="https://github.com/shirosandev/pyCrystalPay",
+      long_description='doc - https://github.com/shirosandev/pyCrystalPay',
+      author_email='seraphineru@mail.ru',
+      zip_safe=False)
