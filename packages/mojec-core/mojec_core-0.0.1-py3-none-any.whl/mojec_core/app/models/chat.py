7a512c4b75500913c18c5b216a9497cf1e6a31a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:47 +0000

    Add package mojec-core/0.0.1/mojec_core-0.0.1-py3-none-any.whl

---
+++ packages/mojec-core/mojec_core-0.0.1-py3-none-any.whl/mojec_core/app/models/chat.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+from .base import BaseModelAbstract
+from .user import User
+
+
+class Conversation(BaseModelAbstract, models.Model):
+    participants = models.ManyToManyField(
+        User, related_name="conversations"
+    )
+
+    class Meta:
+        db_table = 'Conversations'
+
+
+class Message(BaseModelAbstract, models.Model):
+    conversation = models.ForeignKey(Conversation, models.DO_NOTHING,
+                                     blank=True, null=True,
+                                     related_name="messages")
+    sender = models.ForeignKey(User, models.SET_NULL, related_name="messages",
+                               blank=True, null=True)
+    message = models.TextField(blank=False, null=False)
+    type = models.CharField(max_length=30, blank=False, null=False)
+
+    class Meta:
+        db_table = 'Messages'
