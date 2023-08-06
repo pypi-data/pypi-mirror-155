024e6840045baf012aba958baa0109f74bf20a95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:51 +0000

    Add package pyxem/0.14.2/pyxem-0.14.2.tar.gz

---
+++ packages/pyxem/pyxem-0.14.2.tar.gz/pyxem-0.14.2/pyxem/signals/correlation2d.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016-2022 The pyXem developers
+#
+# This file is part of pyXem.
+#
+# pyXem is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pyXem is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pyXem.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+from hyperspy.signals import Signal2D
+from hyperspy._signals.lazy import LazySignal
+
+from pyxem.utils.correlation_utils import corr_to_power
+from pyxem.signals.common_diffraction import CommonDiffraction
+
+
+class Correlation2D(Signal2D, CommonDiffraction):
+    """Signal class for Correlation data after applying some correlation along some axis.
+    """
+    _signal_type = "correlation"
+
+    def get_angular_power(self, inplace=False, **kwargs):
+        """Returns the power spectrum of the angular auto-correlation function
+        in the form of a Signal2D class.
+
+        This gives the fourier decomposition of the radial correlation. Due to
+        nyquist sampling the number of fourier coefficients will be equal to the
+        angular range.
+
+        Parameters
+        ----------
+        mask: Numpy array or Signal2D
+            A bool mask of values to ignore of shape equal to the signal shape.  If the mask
+            is a BaseSignal than it is iterated with the polar signal
+         normalize: bool
+             Normalize the radial correlation by the average value at some radius.
+        inplace: bool
+            From hyperspy.signal.map(). inplace=True means the signal is
+            overwritten.
+
+        Returns
+        -------
+        power: Signal2D
+            The power spectrum of the Signal2D
+        """
+        power = self.map(corr_to_power, inplace=inplace, **kwargs)
+
+        s = self if inplace else power
+
+        s.set_signal_type("power")
+        fourier_axis = s.axes_manager.signal_axes[0]
+
+        fourier_axis.name = "Fourier Coefficient"
+        fourier_axis.units = "a.u"
+        fourier_axis.scale = 1
+        return power
+
+    def get_summed_angular_power(self, inplace=False, **kwargs):
+        """Returns the power spectrum of the summed angular auto-correlation function
+        over all real space positions.  Averages the angular correlation.
+
+        Parameters
+        ----------
+        inplace: bool
+            From hyperspy.signal.map(). inplace=True means the signal is
+            overwritten.
+
+        Returns
+        -------
+        power: Power2D
+            The power spectrum of summed angular correlation
+        """
+        power = self.nansum().map(corr_to_power, inplace=inplace, **kwargs)
+
+        s = self if inplace else power
+
+        s.set_signal_type("power")
+        fourier_axis = s.axes_manager.signal_axes[0]
+
+        fourier_axis.name = "Fourier Coefficient"
+        fourier_axis.units = "a.u"
+        fourier_axis.scale = 1
+        return power
+
+
+class LazyCorrelation2D(LazySignal, Correlation2D):
+
+    _lazy = True
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
