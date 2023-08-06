1c98b2735a962a9aefff46fe0cd51cf95fc2653f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:45 +0000

    Add package yungestWeb/0.0.4/yungestWeb-0.0.4.tar.gz

---
+++ packages/yungestWeb/yungestWeb-0.0.4.tar.gz/yungestWeb-0.0.4/yungestWeb/__init__.py
@@ -0,0 +1,6 @@
+from yungestWeb.app import App 
+from yungestWeb.request import Request
+from yungestWeb.router import Path
+from yungestWeb.response import *
+from yungestWeb.reader_file import render_html
+from wsgiref.simple_server import make_server
