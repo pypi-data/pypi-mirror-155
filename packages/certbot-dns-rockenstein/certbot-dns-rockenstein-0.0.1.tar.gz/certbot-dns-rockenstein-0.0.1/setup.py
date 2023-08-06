c2272e044e60133eac368510829bf5e310fdaefc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:17:48 +0000

    Add package certbot-dns-rockenstein/0.0.1/certbot-dns-rockenstein-0.0.1.tar.gz

---
+++ packages/certbot-dns-rockenstein/certbot-dns-rockenstein-0.0.1.tar.gz/certbot-dns-rockenstein-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='certbot-dns-rockenstein',
+    version='0.0.1',
+    package='certbot_dns_rockenstein.py',
+    description="rockenstein DNS Authenticator plugin for Certbot",
+    url='https://www.rockenstein.de',
+    author="Frank Mueller",
+    author_email='fm@rockenstein.de',
+    install_requires=[
+        'certbot',
+        'requests'
+    ],
+    entry_points={
+        'certbot.plugins': [
+            'certbot-dns-rockenstein = certbot_dns_rockenstein:Authenticator'
+        ],
+    },
+    packages=find_packages(exclude=("tests", "etc", "logs",)),
+)
