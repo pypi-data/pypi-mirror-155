438bcc49a956b2d27345a57427fc5fff360b0d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:08:39 +0000

    Add package nodejs-bin/16.15.1a1/nodejs_bin-16.15.1a1-py3-none-win32.whl

---
+++ packages/nodejs-bin/nodejs_bin-16.15.1a1-py3-none-win32.whl/nodejs/npm.py
@@ -0,0 +1,10 @@
+import os, sys, subprocess
+def run(args):
+    return subprocess.call([
+        os.path.join(os.path.dirname(__file__), "npm.cmd"),
+        *args
+    ])
+def main():
+    sys.exit(run(sys.argv[1:]))
+if __name__ == '__main__':
+    main()
