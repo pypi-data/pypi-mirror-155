131d65f182d6b5f1a4368a8a6a15d960005acdc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:06 +0000

    Add package esi-leap/0.1.0/esi-leap-0.1.0.tar.gz

---
+++ packages/esi-leap/esi-leap-0.1.0.tar.gz/esi-leap-0.1.0/esi_leap/db/migration.py
@@ -0,0 +1,33 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_config import cfg
+from stevedore import driver
+
+_IMPL = None
+
+
+def get_backend():
+    global _IMPL
+    if not _IMPL:
+        cfg.CONF.import_opt('backend', 'oslo_db.options', group='database')
+        _IMPL = driver.DriverManager("esi_leap.database.migration_backend",
+                                     cfg.CONF.database.backend).driver
+    return _IMPL
+
+
+def version():
+    return get_backend().version()
+
+
+def create_schema():
+    return get_backend().create_schema()
