d72b934626ed6d1d9d681aabca2b9867c86eb448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:43 +0000

    Add package cvpr-2022-itt-pkg/0.0.5/cvpr_2022_itt_pkg-0.0.5.tar.gz

---
+++ packages/cvpr-2022-itt-pkg/cvpr_2022_itt_pkg-0.0.5.tar.gz/cvpr_2022_itt_pkg-0.0.5/prob/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='prob',
+    packages=find_packages(),
+    version='0.1.0',
+    description='My first Python library',
+    author='Nick Chimitt',
+    license='MIT',
+)
