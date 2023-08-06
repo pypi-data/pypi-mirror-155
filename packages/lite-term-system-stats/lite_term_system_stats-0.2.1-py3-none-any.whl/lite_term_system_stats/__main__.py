b63551646e603ef7fe2b95b2947c249e6980d73a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:13 +0000

    Add package lite-term-system-stats/0.2.1/lite_term_system_stats-0.2.1-py3-none-any.whl

---
+++ packages/lite-term-system-stats/lite_term_system_stats-0.2.1-py3-none-any.whl/lite_term_system_stats/__main__.py
@@ -0,0 +1,24 @@
+import psutil
+from colorama import Fore, Style
+
+def get_cpu_load() -> int:
+    return int(psutil.cpu_percent())
+
+
+def get_total_ram() -> int:
+    return int(psutil.virtual_memory().total / 1024 / 1024)
+
+
+def get_ram_load() -> int:
+    return int(psutil.virtual_memory().percent)
+
+
+#def get_cpu
+def pprint() -> None:
+    print(f'{Fore.GREEN}cpu{Style.RESET_ALL}: {get_cpu_load()},\n' 
+          f'{Fore.GREEN}total_ram{Style.RESET_ALL}: {get_total_ram()},\n'
+          f'{Fore.GREEN}load_ram{Style.RESET_ALL}: {get_ram_load()}%'
+    )
+
+if __name__ == '__main__':
+    pprint()
