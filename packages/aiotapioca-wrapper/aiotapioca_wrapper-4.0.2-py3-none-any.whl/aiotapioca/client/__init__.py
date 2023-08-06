b941244a2059b889d7fc8fe1ededf5475aaff6b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:45 +0000

    Add package aiotapioca-wrapper/4.0.2/aiotapioca_wrapper-4.0.2-py3-none-any.whl

---
+++ packages/aiotapioca-wrapper/aiotapioca_wrapper-4.0.2-py3-none-any.whl/aiotapioca/client/__init__.py
@@ -0,0 +1,15 @@
+from .client import (
+    TapiocaClient,
+    TapiocaClientExecutor,
+    TapiocaClientResource,
+    TapiocaClientResponse,
+)
+from .process_data import ProcessData
+
+__all__ = (
+    "TapiocaClient",
+    "TapiocaClientExecutor",
+    "TapiocaClientResource",
+    "TapiocaClientResponse",
+    "ProcessData",
+)
