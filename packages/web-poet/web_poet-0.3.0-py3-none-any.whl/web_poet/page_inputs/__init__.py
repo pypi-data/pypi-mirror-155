695383ac8319e56ed0aab134b89ceaac670281da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:39 +0000

    Add package web-poet/0.3.0/web_poet-0.3.0-py3-none-any.whl

---
+++ packages/web-poet/web_poet-0.3.0-py3-none-any.whl/web_poet/page_inputs/__init__.py
@@ -0,0 +1,13 @@
+from .browser import BrowserHtml
+from .client import HttpClient
+from .http import (
+    HttpRequest,
+    HttpRequestBody,
+    HttpRequestHeaders,
+    HttpResponse,
+    HttpResponseBody,
+    HttpResponseHeaders,
+    RequestUrl,
+    ResponseUrl,
+)
+from .page_params import PageParams
---
+++ packages/web-poet/web_poet-0.3.0-py3-none-any.whl/web_poet/utils.py
@@ -0,0 +1,49 @@
+import weakref
+from collections.abc import Iterable
+from functools import wraps
+from typing import Any, List, Optional
+
+
+def memoizemethod_noargs(method):
+    """Decorator to cache the result of a method (without arguments) using a
+    weak reference to its object
+    """
+    cache = weakref.WeakKeyDictionary()
+
+    @wraps(method)
+    def new_method(self, *args, **kwargs):
+        if self not in cache:
+            cache[self] = method(self, *args, **kwargs)
+        return cache[self]
+
+    return new_method
+
+
+def as_list(value: Optional[Any]) -> List[Any]:
+    """Normalizes the value input as a list.
+
+    >>> as_list(None)
+    []
+    >>> as_list("foo")
+    ['foo']
+    >>> as_list(123)
+    [123]
+    >>> as_list(["foo", "bar", 123])
+    ['foo', 'bar', 123]
+    >>> as_list(("foo", "bar", 123))
+    ['foo', 'bar', 123]
+    >>> as_list(range(5))
+    [0, 1, 2, 3, 4]
+    >>> def gen():
+    ...     yield 1
+    ...     yield 2
+    >>> as_list(gen())
+    [1, 2]
+    """
+    if value is None:
+        return []
+    if isinstance(value, str):
+        return [value]
+    if not isinstance(value, Iterable):
+        return [value]
+    return list(value)
