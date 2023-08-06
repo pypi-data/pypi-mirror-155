49cc55117d4a1da9a9ceebcd1c6432b7a6decb2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:00 +0000

    Add package lolcatfigletgnuplotprint/0.0.8/lolcatfigletgnuplotprint-0.0.8.tar.gz

---
+++ packages/lolcatfigletgnuplotprint/lolcatfigletgnuplotprint-0.0.8.tar.gz/lolcatfigletgnuplotprint-0.0.8/src/lolcatfigletgnuplotprint/utils/CommandlinePrinter.py
@@ -0,0 +1,83 @@
+import sh
+import sys
+
+
+class CommandlinePrinter:
+    ___buffer = ""
+    ___output_to_buffer_mode = False
+
+    ___bcolours = {
+        "white": "\033[37m",
+        "aqua": "\033[36m",
+        "magenta": "\033[35m",
+        "blue": "\033[34m",
+        "yellow": "\033[33m",
+        "green": "\033[32m",
+        "orange": "\033[33m",
+        "red": "\033[31m",
+        "grey": "\033[30;1m",
+        "black": "\033[30m",
+        "default": "\033[39m",
+        "unkown": "\033[30;1m",
+        "END_COLOUR": "\033[39m",
+        "START_BOLD_STYLE": "\033[1m",
+        "START_UNDERLINE_STYLE": "\033[4m",
+        "END_STYLE": "\033[0m",
+    }
+
+    def clear_screen(self):
+        self.print(sh.clear())
+
+    def get_colour(self, colour):
+        """
+        Get the escape code sequence for a colour
+        """
+        return self.___bcolours.get(colour, self.___bcolours["END_COLOUR"])
+
+    def wrap_text_with_colour(self, text: str, colour: str):
+        return f"{self.get_colour(colour)}{text}{self.get_colour('END_COLOUR')}"
+
+    def print(self, text, inline=False, colour=None, is_bold=False, is_underlined=False, end="\n"):
+
+        if colour is None:
+            colour = self.get_colour("END_COLOUR")
+
+        if inline:
+            end = ""
+
+        if is_bold:
+            text = f"{self.get_colour('START_BOLD_STYLE')}{text}{self.get_colour('END_STYLE')}"
+
+        if is_underlined:
+            text = f"{self.get_colour('START_UNDERLINE_STYLE')}{text}{self.get_colour('END_STYLE')}"
+
+        self.___write(f"{self.get_colour(colour)}{text}{self.get_colour('END_COLOUR')}{end}")
+
+    def print_by_hilighting_char(self, text, character, colour, bgColour="default"):
+        bgColour = self.get_colour(bgColour)
+
+        lines = text.split("\n")
+        for line in lines:
+            lineParts = line.split(character)
+            i = 0
+            c = len(lineParts)
+            for linePart in lineParts:
+                i = i + 1
+                self.___write(bgColour + linePart + self.get_colour("END_COLOUR"))
+                if i < c:
+                    self.___write(self.get_colour(colour) + character + self.get_colour("END_COLOUR"))
+            self.___write("\n")
+
+    def set_output_to_buffer_mode(self, is_enabled: bool):
+        self.___output_to_buffer_mode = is_enabled
+
+    def flush_buffer(self):
+        buffer = self.___buffer
+        self.___buffer = ""
+        return buffer
+
+    def ___write(self, data: str):
+        self.___buffer += data
+
+        if not self.___output_to_buffer_mode:
+            sys.stdout.write(data)
