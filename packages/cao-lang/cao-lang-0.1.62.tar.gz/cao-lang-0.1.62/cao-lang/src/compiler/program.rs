c839ba855aa5870f7057413ddc1e70134c7f5c7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:33 +0000

    Add package cao-lang/0.1.62/cao-lang-0.1.62.tar.gz

---
+++ packages/cao-lang/cao-lang-0.1.62.tar.gz/cao-lang-0.1.62/cao-lang/src/compiler/program.rs
@@ -0,0 +1,134 @@
+//! The public representation of a program
+//!
+
+use crate::compiler::Lane;
+use crate::prelude::CompilationErrorPayload;
+use smallvec::SmallVec;
+use std::borrow::Cow;
+use std::collections::hash_map::DefaultHasher;
+use std::collections::BTreeMap;
+use std::hash::Hasher;
+use thiserror::Error;
+
+use super::compiled_lane::CompiledLane;
+
+#[derive(Debug, Clone, Error)]
+pub enum IntoStreamError {
+    #[error("Main function by name {0} was not found")]
+    MainFnNotFound(String),
+    #[error("{0:?} is not a valid name")]
+    BadName(String),
+}
+
+pub type CaoProgram<'a> = Module<'a>;
+pub type CaoIdentifier<'a> = Cow<'a, str>;
+
+#[derive(Debug, Clone, Default)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct Module<'a> {
+    pub submodules: BTreeMap<CaoIdentifier<'a>, Module<'a>>,
+    pub lanes: BTreeMap<CaoIdentifier<'a>, Lane>,
+}
+
+impl<'a> Module<'a> {
+    /// flatten this program into a lane stream
+    // TODO: return an iterator???
+    pub(crate) fn into_ir_stream(mut self) -> Result<Vec<CompiledLane>, CompilationErrorPayload> {
+        // the first lane is special
+        //
+        let first_fn = self
+            .lanes
+            .remove("main")
+            .ok_or(CompilationErrorPayload::NoMain)?;
+
+        let first_fn = lane_to_compiled_lane(&first_fn, &["main"]);
+        let mut result = vec![first_fn];
+        result.reserve(self.lanes.len() * self.submodules.len() * 2); // just some dumb heuristic
+
+        let mut namespace = SmallVec::<[_; 16]>::new();
+
+        // flatten modules' functions
+        flatten_module(&self, &mut namespace, &mut result)?;
+
+        Ok(result)
+    }
+
+    /// Hash the keys in the program.
+    ///
+    /// Keys = lanes, submodules, card names.
+    pub fn get_keys_hash(&self) -> u64 {
+        let mut hasher = DefaultHasher::new();
+        hash_module(&mut hasher, self);
+        hasher.finish()
+    }
+}
+
+fn hash_module(hasher: &mut impl Hasher, module: &Module) {
+    for (name, lane) in module.lanes.iter() {
+        hasher.write(name.as_ref().as_bytes());
+        hash_lane(hasher, lane);
+    }
+    for (name, submodule) in module.submodules.iter() {
+        hasher.write(name.as_ref().as_bytes());
+        hash_module(hasher, submodule);
+    }
+}
+
+fn hash_lane(hasher: &mut impl Hasher, lane: &Lane) {
+    for card in lane.cards.iter() {
+        hasher.write(card.name().as_bytes());
+    }
+}
+
+fn flatten_module<'a>(
+    module: &'a Module,
+    namespace: &mut SmallVec<[&'a str; 16]>,
+    out: &mut Vec<CompiledLane>,
+) -> Result<(), CompilationErrorPayload> {
+    for (name, submod) in module.submodules.iter() {
+        namespace.push(name.as_ref());
+        flatten_module(submod, namespace, out)?;
+        namespace.pop();
+    }
+    if out.capacity() - out.len() < module.lanes.len() {
+        out.reserve(module.lanes.len() - (out.capacity() - out.len()));
+    }
+    for (name, lane) in module.lanes.iter() {
+        if !is_name_valid(name.as_ref()) {
+            return Err(CompilationErrorPayload::BadLaneName(name.to_string()));
+        }
+        namespace.push(name.as_ref());
+        out.push(lane_to_compiled_lane(lane, namespace));
+        namespace.pop();
+    }
+    Ok(())
+}
+
+fn lane_to_compiled_lane(lane: &Lane, namespace: &[&str]) -> CompiledLane {
+    assert!(
+        !namespace.is_empty(),
+        "Assume that lane name is the last entry in namespace"
+    );
+
+    let mut cl = CompiledLane {
+        name: flatten_name(namespace),
+        arguments: lane.arguments.clone(),
+        cards: lane.cards.clone(),
+        ..Default::default()
+    };
+    cl.namespace.extend(
+        namespace
+            .iter()
+            .take(namespace.len() - 1)
+            .map(|x| x.to_string()),
+    );
+    cl
+}
+
+fn is_name_valid(name: &str) -> bool {
+    !name.contains(|c: char| !c.is_alphanumeric() && c != '_') && !name.is_empty()
+}
+
+fn flatten_name(namespace: &[&str]) -> String {
+    namespace.join(".")
+}
---
+++ packages/cao-lang/cao-lang-0.1.62.tar.gz/cao-lang-0.1.62/py/tests/test_cao_lang.py
@@ -0,0 +1,88 @@
+import pytest
+import cao_lang as caoc
+
+
+def test_compile_and_run():
+    """
+    Test if we can take a simple program and parse, compile and run it without error
+    """
+
+    PROGRAM_YAML = """
+lanes:
+    main: 
+        cards:
+            - ty: ScalarInt
+              val: 5
+            - ty: ScalarInt
+              val: 5
+            - ty: Add
+            - ty: Jump
+              val: "foo.bar"
+submodules:
+    foo:
+        submodules: {}
+        lanes:
+            bar:
+                cards:
+                    - ty: ScalarInt
+                      val: 42
+
+"""
+
+    program = caoc.CompilationUnit.from_yaml(PROGRAM_YAML)
+    options = caoc.CompilationOptions()
+
+    program = caoc.compile(program, options)
+
+    caoc.run(program)
+
+
+def test_get_version():
+    v = caoc.native_version()
+    assert isinstance(v, str)
+
+
+def test_json():
+    PROGRAM_JSON = """
+    {
+        "lanes": {
+            "main": {
+                "cards": [
+                    { "ty": "Jump", "val": "foo.bar" }
+                ]
+            }
+        },
+        "submodules": {
+            "foo": {
+                "submodules": {},
+                "lanes": {
+                    "bar": {
+                        "cards": [
+                            { "ty": "Noop" }
+                        ]
+                    }
+                }
+            }
+        }
+    }
+    """
+    program = caoc.CompilationUnit.from_json(PROGRAM_JSON)
+    options = caoc.CompilationOptions()
+
+    program = caoc.compile(program, options)
+
+    caoc.run(program)
+
+
+def test_bad_json_is_value_error():
+    PROGRAM_JSON = """
+    {
+        "lanes": {
+            "main": {
+                "cards": [ {} ]
+            }
+        }
+    }
+    """
+    with pytest.raises(ValueError):
+        caoc.CompilationUnit.from_json(PROGRAM_JSON)
