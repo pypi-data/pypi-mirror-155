497342dae806e19433c5fe3b0af2a6ec9729dbf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:49 +0000

    Add package tools-lmself/0.0.4/tools-lmself-0.0.4.tar.gz

---
+++ packages/tools-lmself/tools-lmself-0.0.4.tar.gz/tools-lmself-0.0.4/kits/math.py
@@ -0,0 +1,20 @@
+def add(i, y):
+    """
+    显示两个值相加
+    :param i: 第一个
+    :param y: 第二个
+    :return:
+    """
+    print(str(i) + " + " + str(y) + " = " + str((i + y)))
+
+
+def sub(i, y):
+    print(str(i) + " - " + str(y) + " = " + str((i - y)))
+
+
+def mul(i, y):
+    print(str(i) + " * " + str(y) + " = " + str((i * y)))
+
+
+def div(i, y):
+    print(str(i) + " / " + str(y) + " = " + str((i / y)))
