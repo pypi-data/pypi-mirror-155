299c93bf8b3be1039875b38b0e3de88f9f89bc98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:56 +0000

    Add package disposable-phonebook/0.2.0/disposable-phonebook-0.2.0.tar.gz

---
+++ packages/disposable-phonebook/disposable-phonebook-0.2.0.tar.gz/disposable-phonebook-0.2.0/README.md
@@ -0,0 +1,41 @@
+# disposable-phonebook
+
+Collect "disposable" phone numbers from various internet sites that offer
+"receive free SMS to an anonymous number" functionality.
+
+```bash
+$ dphonebook scrape
+[{"number": "+16462879071", "area": "New York", "provider": "receive-smss.com", "last_message": 1652437175, "last_checked": 1652438795},
+{"number": "+16466623058", "area": "New York", "provider": "receive-smss.com", "last_message": 1652437776, "last_checked": 1652438796}]
+```
+
+## Quickstart
+
+```bash
+$ pip3 install disposable-phonebook
+$ dphonebook scrape
+```
+
+## Development
+
+Requires Python 3.10
+
+```bash
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements-dev.txt
+pip install --editable .
+dphonebook --help
+```
+
+- Install [Editorconfig](https://marketplace.visualstudio.com/items?itemName=EditorConfig.EditorConfig) extension
+- Install [pre-commit hooks](https://pre-commit.com/#install)
+
+
+## Contribution
+
+Want to add or fix something? Send a pull request.
+
+## License
+
+Apache2 license (see: [LICENSE](LICENSE))
---
+++ packages/disposable-phonebook/disposable-phonebook-0.2.0.tar.gz/disposable-phonebook-0.2.0/dphonebook/cli.py
@@ -0,0 +1,87 @@
+import logging
+import sys
+from pathlib import Path
+
+import click
+import yaml
+
+from dphonebook.lib.progress import Progress
+from dphonebook.lib.writer.json_writer import JsonWriter
+from dphonebook.lib.writer.stdout_writer import StdoutWriter
+from dphonebook.phonebook import Phonebook
+
+
+@click.group()
+def main():
+    pass
+
+
+def logger_factory():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    streamHandler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    streamHandler.setFormatter(formatter)
+    streamHandler.setLevel(logging.INFO)
+    logger.addHandler(streamHandler)
+    return logger
+
+
+def load_config(config_file: str, logger: logging.Logger):
+
+    try:
+        with open(config_file, 'r') as stream:
+            return yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        logging.error('Unable to load YAML config file %s: %s', config_file, exc)
+        sys.exit(1)
+    except FileNotFoundError:
+        logging.error('Unable to find config file %s', config_file)
+        sys.exit(1)
+
+
+def writer_factory(config: dict):
+    writer_type = config.get('writer', {}).get('type', 'StdoutWriter')
+
+    # Temp, later: replace with more dynamic/intelligent loader
+    if writer_type == 'StdoutWriter':
+        return StdoutWriter({})
+    if writer_type == 'JsonWriter':
+        return JsonWriter(config['writer']['args'])
+    raise Exception('Unknown writer type')
+
+
+def phonebook_factory(config_file) -> Phonebook:
+    logger = logger_factory()
+    config = load_config(config_file, logger)
+    return Phonebook(
+        logger=logger,
+        config=config,
+        result_writer=writer_factory(config)
+    )
+
+
+@main.command()
+@click.option('--config-file', default=Path.joinpath(Path(__file__).parent.absolute(), 'disposable-phonebook.yml'), help='Config file location')
+def list(config_file: str):
+    phonebook = phonebook_factory(config_file)
+    phonebook.load_providers()
+    for provider in phonebook.providers:
+        click.echo(provider.domain())
+
+
+@main.command()
+@click.option('--config-file', default=Path.joinpath(Path(__file__).parent.absolute(), 'disposable-phonebook.yml'), help='Config file location')
+def scrape(config_file: str):
+    phonebook = phonebook_factory(config_file)
+    phonebook.load_providers()
+
+    progress = Progress(phonebook)
+    progress.monitor()
+
+    phonebook.scrape()
+    progress.close()
+
+
+if __name__ == '__main__':
+    main()
