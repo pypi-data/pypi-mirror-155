bf0da8923779494b4e79bf73166ec58b8eed94f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:58 +0000

    Add package mch-vitals/0.1.0/mch_vitals-0.1.0.tar.gz

---
+++ packages/mch-vitals/mch_vitals-0.1.0.tar.gz/mch_vitals-0.1.0/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mch_vitals', 'mch_vitals.MTTS_CAN.code']
+
+package_data = \
+{'': ['*'],
+ 'mch_vitals': ['MTTS_CAN/*',
+                'MTTS_CAN/.git/*',
+                'MTTS_CAN/.git/hooks/*',
+                'MTTS_CAN/.git/info/*',
+                'MTTS_CAN/.git/logs/*',
+                'MTTS_CAN/.git/logs/refs/heads/*',
+                'MTTS_CAN/.git/logs/refs/remotes/origin/*',
+                'MTTS_CAN/.git/objects/pack/*',
+                'MTTS_CAN/.git/refs/heads/*',
+                'MTTS_CAN/.git/refs/remotes/origin/*']}
+
+setup_kwargs = {
+    'name': 'mch-vitals',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Ibrahim Animashaun',
+    'author_email': 'iaanimashaun@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
