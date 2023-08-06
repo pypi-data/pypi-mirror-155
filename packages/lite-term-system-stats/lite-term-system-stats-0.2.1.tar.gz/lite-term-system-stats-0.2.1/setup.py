104fd10b6366173efdce13e541f26da1c09dbf84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:13 +0000

    Add package lite-term-system-stats/0.2.1/lite-term-system-stats-0.2.1.tar.gz

---
+++ packages/lite-term-system-stats/lite-term-system-stats-0.2.1.tar.gz/lite-term-system-stats-0.2.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['lite_term_system_stats']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['colorama>=0.4.5,<0.5.0', 'psutil>=5.9.1,<6.0.0']
+
+setup_kwargs = {
+    'name': 'lite-term-system-stats',
+    'version': '0.2.1',
+    'description': '',
+    'long_description': None,
+    'author': None,
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
