5b4e296d27c40c5789eb2daddcc1db1f5dcddecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:22 +0000

    Add package yankee/0.1.6/yankee-0.1.6.tar.gz

---
+++ packages/yankee/yankee-0.1.6.tar.gz/yankee-0.1.6/yankee/xml/schema/schema.py
@@ -0,0 +1,19 @@
+from yankee.base import schema
+from yankee.base.deserializer import Deserializer
+
+from .fields import List
+from .key import XmlMixin
+
+class Deserializer(XmlMixin, Deserializer):
+    pass
+
+class Schema(XmlMixin, schema.Schema):
+    pass
+
+
+class PolymorphicSchema(XmlMixin, schema.PolymorphicSchema):
+    pass
+
+
+class ZipSchema(XmlMixin, schema.ZipSchema):
+    _list_field = List
