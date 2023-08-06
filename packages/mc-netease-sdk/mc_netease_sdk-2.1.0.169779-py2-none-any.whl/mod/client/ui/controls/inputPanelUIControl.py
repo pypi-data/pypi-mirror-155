37f3c6ebc0db61cc2709ffc0c3bbc1c859db68c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:47 +0000

    Add package mc-netease-sdk/2.1.0.169779/mc_netease_sdk-2.1.0.169779-py2-none-any.whl

---
+++ packages/mc-netease-sdk/mc_netease_sdk-2.1.0.169779-py2-none-any.whl/mod/client/ui/controls/inputPanelUIControl.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from mod.client.ui.controls.baseUIControl import BaseUIControl
+
+class InputPanelUIControl(BaseUIControl):
+    def SetIsModal(self, isModal):
+        # type: (bool) -> bool
+        """
+        设置当前面板是否为模态框
+        """
+        pass
+
+    def GetIsModal(self):
+        # type: () -> bool
+        """
+        判断当前面板是否为模态框
+        """
+        pass
+
