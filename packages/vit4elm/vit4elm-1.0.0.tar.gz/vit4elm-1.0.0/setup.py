8b00d13a7f05b133150dff457d41b36a4a6216ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:07 +0000

    Add package vit4elm/1.0.0/vit4elm-1.0.0.tar.gz

---
+++ packages/vit4elm/vit4elm-1.0.0.tar.gz/vit4elm-1.0.0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vit4elm",
+    version="1.0.0",
+    author="Adam Sabra",
+    description="Vision Transformers for Exotic Lattice Models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'scikit-learn',
+        'numpy',
+        'matplotlib',
+        'pandas',
+        'transformers',
+        'datasets',
+        'pytorch',
+        'pillow'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.8"
+)
