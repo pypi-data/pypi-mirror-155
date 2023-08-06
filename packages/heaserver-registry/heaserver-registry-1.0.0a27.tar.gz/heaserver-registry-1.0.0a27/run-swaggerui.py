e000a7e3270dcb9363b109c8a31cf144d0a8b5b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:41 +0000

    Add package heaserver-registry/1.0.0a27/heaserver-registry-1.0.0a27.tar.gz

---
+++ packages/heaserver-registry/heaserver-registry-1.0.0a27.tar.gz/heaserver-registry-1.0.0a27/run-swaggerui.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from heaserver.registry import service
+from heaserver.service.testcase import swaggerui
+from heaserver.service.wstl import builder_factory
+from integrationtests.heaserver.registryintegrationtest.componenttestcase import db_store
+from aiohttp.web import get, delete, post, put, view
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+if __name__ == '__main__':
+    swaggerui.run(project_slug='heaserver-registry', desktop_objects=db_store,
+                  wstl_builder_factory=builder_factory(service.__package__),
+                  routes=[(get, '/components/{id}', service.get_component),
+                          (get, '/components/byname/{name}', service.get_component_by_name),
+                          (get,
+                           '/components/bytype/{type}/byfilesystemtype/{filesystemtype}/byfilesystemname/{filesystemname}',
+                           service.get_component_by_type),
+                          (get, '/components/', service.get_all_components),
+                          (post, '/components', service.post_component),
+                          (put, '/components/{id}', service.put_component),
+                          (delete, '/components/{id}', service.delete_component),
+                          (get, '/properties/{id}', service.get_property),
+                          (get, '/properties/byname/{name}', service.get_property_by_name),
+                          (get, '/properties/', service.get_all_properties),
+                          (post, '/properties', service.post_property),
+                          (put, '/properties/{id}', service.put_property),
+                          (delete, '/properties/{id}', service.delete_property)
+                          ])
