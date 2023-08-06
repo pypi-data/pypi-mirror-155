bce1359cfb6273ea0a6762ac9263712e77040604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:22 +0000

    Add package hym16125/1.0/hym16125-1.0.tar.gz

---
+++ packages/hym16125/hym16125-1.0.tar.gz/hym16125-1.0/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+
+name='hym16125',	# 对外我们模块的名字
+
+version='1.0', # 版本号
+
+description='这是第一个对外发布的模块，测试哦',	#描述
+
+author='gaoqi', # 作者
+
+author_email='gaoqi110@163.com',
+
+py_modules=['hym16125.demo1','hym16125.demo2'] # 要发布的模块
+
+)
