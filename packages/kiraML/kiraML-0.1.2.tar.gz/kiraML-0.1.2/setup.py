ed40e55895ec7d92100e48cc1e7da223e249625e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:22 +0000

    Add package kiraML/0.1.2/kiraML-0.1.2.tar.gz

---
+++ packages/kiraML/kiraML-0.1.2.tar.gz/kiraML-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+import sys
+sys.path.append('kiraML')
+from _version import __version__ 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "kiraML/README.md").read_text()
+
+setup(
+    name='kiraML',
+    packages=find_packages(include=['kiraML']),
+    version=__version__,
+    description='The KiraML library',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    author='Kira Learning',
+    license='MIT',
+    install_requires=['scikit-learn', 'matplotlib'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+)
