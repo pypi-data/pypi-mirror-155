94ec28ae37a66b8d0cb6705588f4f412a1ddd6c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:57:14 +0000

    Add package geniusbot/2.0.8/geniusbot-2.0.8-py2.py3-none-any.whl

---
+++ packages/geniusbot/geniusbot-2.0.8-py2.py3-none-any.whl/chatterbot/__init__.py
@@ -0,0 +1,13 @@
+"""
+ChatterBot is a machine learning, conversational dialog engine. Forked from gunthercoxpip
+"""
+from .chatterbot import ChatBot
+
+
+__all__ = (
+    'ChatBot',
+)
+__version__ = '1.1.2'
+__author__ = 'Gunther Cox'
+__email__ = 'gunthercx@gmail.com'
+__url__ = 'https://github.com/gunthercox/chatterbot-corpus'
