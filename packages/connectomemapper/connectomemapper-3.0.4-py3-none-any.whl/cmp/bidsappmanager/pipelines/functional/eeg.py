6dd39fa2172b028ff08648de6962353f011c2c74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:23 +0000

    Add package connectomemapper/3.0.4/connectomemapper-3.0.4-py3-none-any.whl

---
+++ packages/connectomemapper/connectomemapper-3.0.4-py3-none-any.whl/cmp/bidsappmanager/pipelines/functional/eeg.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2009-2022, Ecole Polytechnique Federale de Lausanne (EPFL) and
+# Hospital Center and University of Lausanne (UNIL-CHUV), Switzerland, and CMP3 contributors
+# All rights reserved.
+#
+#  This software is distributed under the open-source license Modified BSD.
+
+""" EEG pipeline Class definition
+"""
