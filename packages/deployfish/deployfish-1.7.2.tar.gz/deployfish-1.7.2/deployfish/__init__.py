8b56976b01d71f296c5e0c087285942faea519ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:57 +0000

    Add package deployfish/1.7.2/deployfish-1.7.2.tar.gz

---
+++ packages/deployfish/deployfish-1.7.2.tar.gz/deployfish-1.7.2/deployfish/__init__.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+from jinja2 import FileSystemLoader, Environment
+
+__version__ = "1.7.2"
+
+TEMPLATE_PATHS = [
+    Path(__file__).parent / 'cli' / 'templates'
+]
+
+jinja_env = Environment(loader=FileSystemLoader([str(p) for p in TEMPLATE_PATHS]))
