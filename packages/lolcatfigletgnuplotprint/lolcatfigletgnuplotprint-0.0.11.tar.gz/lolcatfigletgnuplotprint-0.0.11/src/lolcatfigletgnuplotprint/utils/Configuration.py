d377b08202266663d7252f2a3928cad3c1ada479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:46 +0000

    Add package lolcatfigletgnuplotprint/0.0.11/lolcatfigletgnuplotprint-0.0.11.tar.gz

---
+++ packages/lolcatfigletgnuplotprint/lolcatfigletgnuplotprint-0.0.11.tar.gz/lolcatfigletgnuplotprint-0.0.11/src/lolcatfigletgnuplotprint/utils/Configuration.py
@@ -0,0 +1,5 @@
+from .data_structures import make_dotdict
+
+Configuration = make_dotdict(
+    {"view": {"left_margin_fill": "    "}, "plotter": {"show_ip_address": False, "show_scope_extra_info_line": False}}
+)
