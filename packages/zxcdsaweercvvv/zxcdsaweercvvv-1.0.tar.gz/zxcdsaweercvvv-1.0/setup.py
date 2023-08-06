73f60031de705de8725243368155e4cb60de80aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:45 +0000

    Add package zxcdsaweercvvv/1.0/zxcdsaweercvvv-1.0.tar.gz

---
+++ packages/zxcdsaweercvvv/zxcdsaweercvvv-1.0.tar.gz/zxcdsaweercvvv-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='zxcdsaweercvvv',  # 对外我们模块的名字
+    version='1.0',  # 版本号
+    description='这是第一个对外发布的模块，测试哦',  # 描述
+    author='gaoqi',  # 作者
+    author_email='213@163.com',
+    py_modules=['zxcdsaweercvvv.demo1', 'zxcdsaweercvvv.demo2']  # 要发布的模块
+)
