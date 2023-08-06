0e7e57013c3fdb6da8ca1491300c5142f69d8db3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:57 +0000

    Add package gerapy-patch/0.0.2/gerapy_patch-0.0.2.tar.gz

---
+++ packages/gerapy-patch/gerapy_patch-0.0.2.tar.gz/gerapy_patch-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from os.path import dirname, join
+import io,os
+from pkg_resources import parse_version
+from setuptools import setup, find_packages, __version__ as setuptools_version
+import sys
+
+
+
+setup(
+    name='gerapy_patch',
+    version="0.0.2",
+    url='https://scrapy.org',
+    description="fix gerapy build egg can't pack cfg/xlsx/txt etc type files",
+    author='buliqioqiolibusdo',
+    author_email = 'dingyeran@163.com',
+    license='BSD',
+    packages=find_packages(exclude=('gereapy_patch-0.0.1.dist-info')),
+    python_requires='>=3.6',
+)
