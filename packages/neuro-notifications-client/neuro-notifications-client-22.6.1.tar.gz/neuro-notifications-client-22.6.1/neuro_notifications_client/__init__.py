f3050409e19059436b2423dcda29fc53c6419c42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:45:07 +0000

    Add package neuro-notifications-client/22.6.1/neuro-notifications-client-22.6.1.tar.gz

---
+++ packages/neuro-notifications-client/neuro-notifications-client-22.6.1.tar.gz/neuro-notifications-client-22.6.1/neuro_notifications_client/__init__.py
@@ -0,0 +1,23 @@
+from .client import Client
+from .notifications import (
+    CreditsWillRunOutSoon,
+    JobCannotStartLackResources,
+    JobCannotStartNoCredits,
+    JobCannotStartQuotaReached,
+    JobTransition,
+    QuotaResourceType,
+    QuotaWillBeReachedSoon,
+    Welcome,
+)
+
+__all__ = [
+    "Client",
+    "JobCannotStartLackResources",
+    "JobCannotStartQuotaReached",
+    "JobCannotStartNoCredits",
+    "JobTransition",
+    "QuotaWillBeReachedSoon",
+    "QuotaResourceType",
+    "CreditsWillRunOutSoon",
+    "Welcome",
+]
