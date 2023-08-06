3576fc897e470adb7267f4123a8e468ecb9bd496
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:06 +0000

    Add package cvdatasets/0.11.0/cvdatasets-0.11.0.tar.gz

---
+++ packages/cvdatasets/cvdatasets-0.11.0.tar.gz/cvdatasets-0.11.0/cvdatasets/dataset/mixins/bounding_box/__init__.py
@@ -0,0 +1,4 @@
+from cvdatasets.dataset.mixins.bounding_box.base import BBCropMixin
+from cvdatasets.dataset.mixins.bounding_box.base import BBoxMixin
+from cvdatasets.dataset.mixins.bounding_box.bbox import BoundingBox
+from cvdatasets.dataset.mixins.bounding_box.multi_box import MultiBoxMixin
