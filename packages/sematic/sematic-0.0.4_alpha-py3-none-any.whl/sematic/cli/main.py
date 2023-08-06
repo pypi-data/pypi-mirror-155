85be675342bc5c0fe67eab1824eeb327db939ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:32 +0000

    Add package sematic/0.0.4a0/sematic-0.0.4_alpha-py3-none-any.whl

---
+++ packages/sematic/sematic-0.0.4_alpha-py3-none-any.whl/sematic/cli/main.py
@@ -0,0 +1,9 @@
+# Sematic
+from sematic.cli.cli import cli
+import sematic.cli.start  # noqa: F401
+import sematic.cli.stop  # noqa: F401
+import sematic.cli.run  # noqa: F401
+
+
+if __name__ == "__main__":
+    cli()
