c618849e9e244d52fa28569750c2e14f1c2f5a2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:30 +0000

    Add package hx-testpackage27/0.1/hx-testpackage27-0.1.tar.gz

---
+++ packages/hx-testpackage27/hx-testpackage27-0.1.tar.gz/hx-testpackage27-0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+from setuptools import setup, find_packages
+setup(
+    name='hx-testpackage27',
+    version='0.1',
+    author='Tector Pro',
+    description='Testing the efficientcy of Inspector',
+    packages=find_packages(),
+    python_requires="==2.7",
+)
