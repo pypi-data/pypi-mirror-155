5b799958d2d79c907f5aa6c2382b129407b01df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:50 +0000

    Add package moretest/0.1.0/moretest-0.1.0.tar.gz

---
+++ packages/moretest/moretest-0.1.0.tar.gz/moretest-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['moretest']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['APPLICATION-NAME = entry:main']}
+
+setup_kwargs = {
+    'name': 'moretest',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'chrbrauer',
+    'author_email': 'christoph@5xbrauer.de',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
