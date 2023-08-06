048cb861d3cdfa2f59028e07539ba6a1d3135d28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:52 +0000

    Add package pears/0.1.3/pears-0.1.3-py3-none-any.whl

---
+++ packages/pears/pears-0.1.3-py3-none-any.whl/pears/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.1.3"
+__uri__ = "http://github.com/al-jshen/pears"
+__author__ = "Jeff Shen"
+__email__ = "jshen2014@hotmail.com"
+__license__ = "MIT OR Apache-2.0"
+
+from .pears import pears
+
+__all__ = ["pears"]
