9f79776856990641ab6e5ea958d89c838e07bdce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:54 +0000

    Add package testgen/0.3/testgen-0.3.tar.gz

---
+++ packages/testgen/testgen-0.3.tar.gz/testgen-0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("README.md", mode="r", encoding="utf8") as f:
+    README = f.read()
+
+with open("example.py", mode="r", encoding="utf8") as f:
+    EXAMPLE = f'```python\n{f.read()}```'
+
+with open("requirements.txt", mode="r", encoding="utf8") as f:
+    DEPENDENCIES = f.read().splitlines()
+
+setup(
+    name='testgen',
+    version='0.3',
+    description='Generate random reproducible competitive programming tests',
+    author='Alon Krymgand',
+    long_description=README + '\n' + EXAMPLE,
+    long_description_content_type='text/markdown',
+    url='https://github.com/RealA10N/testgen',
+    py_modules=['testgen'],
+    install_requires=DEPENDENCIES,
+)
