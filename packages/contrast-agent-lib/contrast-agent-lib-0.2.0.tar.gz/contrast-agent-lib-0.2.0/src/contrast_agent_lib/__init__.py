3141af5917b244bf5b11b79b30153dad1140887b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:36 +0000

    Add package contrast-agent-lib/0.2.0/contrast-agent-lib-0.2.0.tar.gz

---
+++ packages/contrast-agent-lib/contrast-agent-lib-0.2.0.tar.gz/contrast-agent-lib-0.2.0/src/contrast_agent_lib/__init__.py
@@ -0,0 +1,13 @@
+import ctypes
+
+from sys import platform
+from os import path
+
+# TODO: PYT-2369 Wheel distributions will remove the need for this logic entirely
+lib_name = "libcontrast_c"
+if path.exists("/etc/alpine-release"):
+    lib_name += "_musl"
+lib_ext = ".dylib" if platform.startswith("darwin") else ".so"
+lib_path = "".join([path.dirname(__file__), "/libs/", lib_name, lib_ext])
+
+lib_contrast = ctypes.cdll.LoadLibrary(lib_path)
