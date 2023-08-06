ad365db3198af1b89bdf46de9b61e26d12a80c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:56 +0000

    Add package collective.playlist/1.0.0/collective.playlist-1.0.0-py2.py3-none-any.whl

---
+++ packages/collective.playlist/collective.playlist-1.0.0-py2.py3-none-any.whl/collective/playlist/content/playlist.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from collective.playlist import _
+from plone.app.textfield import RichText
+from plone.dexterity.content import Container
+from plone.supermodel import model
+from zope.interface import implementer
+
+
+class IPlaylist(model.Schema):
+    """Marker interfce and Dexterity Python Schema for Playlist"""
+
+    copyright = RichText(title=_("Copyright"), default="", required=False)
+
+
+@implementer(IPlaylist)
+class Playlist(Container):
+    """ """
