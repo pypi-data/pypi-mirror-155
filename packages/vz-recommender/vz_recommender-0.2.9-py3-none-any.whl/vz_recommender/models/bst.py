0f678ab085083b94a8a024bb6bcabecfa47e66d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:58:13 +0000

    Add package vz-recommender/0.2.9/vz_recommender-0.2.9-py3-none-any.whl

---
+++ packages/vz-recommender/vz_recommender-0.2.9-py3-none-any.whl/vz_recommender/models/bst.py
@@ -0,0 +1,80 @@
+from typing import *
+
+import torch
+
+from .bottom import BSTBottom
+from transformers import AutoModel, AutoTokenizer, AutoConfig, DistilBertTokenizer, DistilBertModel
+
+
+class BST(BSTBottom):
+    def __init__(self, deep_dims, seq_dim, seq_embed_dim, deep_embed_dims, seq_hidden_size,
+                 num_wide=0, num_shared=0, nlp_dim=0, nlp_embed_dim=768, context_head_kwargs=None, sequence_transformer_kwargs=None,
+                 item_embedding_weight=None, shared_embeddings_weight=None):
+        super().__init__(deep_dims, seq_dim, seq_embed_dim, deep_embed_dims, seq_hidden_size,
+                         num_wide, num_shared, nlp_dim, nlp_embed_dim, context_head_kwargs, sequence_transformer_kwargs,
+                         item_embedding_weight, shared_embeddings_weight)
+        self.dense3 = torch.nn.Linear(seq_embed_dim, seq_dim)
+
+    def forward(self, deep_in, seq_in, vl_in, wide_in=None, shared_in=None, search_in=None):
+        """
+        Args:
+            deep_in: list, a list of Tensor of shape [batch_size, deep_dims].
+            seq_in: Tensor, shape [batch_size, seq_len].
+            vl_in: Tensor, shape [batch_size].
+            wide_in: list, a list of Tensor of shape [batch_size, num_wide].
+            shared_in: list, a list of Tensor of shape [batch_size, num_shared] (default=None).
+            search_in: tensor, Tensor of shape [batch_size, 1] (default=None).
+
+        Return:
+            out: Tensor, shape [batch_size, seq_dim].
+            user_out: Tensor, shape [batch_size, seq_embed_dim].
+        """
+        ctx_out = self.context_head(deep_in=deep_in, wide_in=wide_in, shared_in=shared_in, search_in=search_in)
+        seq_out = self.sequence_transformer(seq_in=seq_in, vl_in=vl_in)
+        outs = torch.cat([seq_out, ctx_out], dim=1)
+        outs = self.dense1(outs)
+        outs = self.act1(outs)
+        outs = self.dense2(outs)
+        user_out = self.act2(outs)
+        outs = self.dense3(user_out)
+        return (outs, user_out)
+
+
+class BSTBERT(BSTBottom):
+    def __init__(self, deep_dims, seq_dim, seq_embed_dim, deep_embed_dims, seq_hidden_size, tokenizer_path, nlp_encoder_path, 
+                 num_wide=0, num_shared=0, nlp_dim=0, nlp_embed_dim=768, context_head_kwargs=None, sequence_transformer_kwargs=None,
+                 item_embedding_weight=None, shared_embeddings_weight=None):
+        super().__init__(deep_dims, seq_dim, seq_embed_dim, deep_embed_dims, seq_hidden_size,
+                         num_wide, num_shared, nlp_dim, nlp_embed_dim, context_head_kwargs, sequence_transformer_kwargs,
+                         item_embedding_weight, shared_embeddings_weight)
+        self.tokenizer = DistilBertTokenizer.from_pretrained(tokenizer_path)
+        self.nlp_encoder = DistilBertModel.from_pretrained(nlp_encoder_path)
+        # self.nlp_device = "cuda:0" if torch.cuda.is_available() else "cpu"
+        self.dense3 = torch.nn.Linear(seq_embed_dim, seq_dim)
+        # self.nlp_encoder.to(self.nlp_device)
+
+    def forward(self, deep_in, seq_in, vl_in, wide_in=None, shared_in=None, search_in=None):
+        """
+        Args:
+            deep_in: list, a list of Tensor of shape [batch_size, deep_dims].
+            seq_in: Tensor, shape [batch_size, seq_len].
+            vl_in: Tensor, shape [batch_size].
+            wide_in: list, a list of Tensor of shape [batch_size, num_wide].
+            shared_in: list, a list of Tensor of shape [batch_size, num_shared] (default=None).
+            search_in: tensor, Tensor of shape [batch_size, 1] (default=None).
+
+        Return:
+            out: Tensor, shape [batch_size, seq_dim].
+            user_out: Tensor, shape [batch_size, seq_embed_dim].
+        """
+        encoded_search = self.tokenizer(list(search_in), return_tensors='pt', truncation=True, padding=True, max_length=512)
+        search_in = self.nlp_encoder(**encoded_search).last_hidden_state[:,0,:].to(dtype=torch.float32)
+        ctx_out = self.context_head(deep_in=deep_in, wide_in=wide_in, shared_in=shared_in, search_in=search_in)
+        seq_out = self.sequence_transformer(seq_in=seq_in, vl_in=vl_in)
+        outs = torch.cat([seq_out, ctx_out], dim=1)
+        outs = self.dense1(outs)
+        outs = self.act1(outs)
+        outs = self.dense2(outs)
+        user_out = self.act2(outs)
+        outs = self.dense3(user_out)
+        return (outs, user_out)
