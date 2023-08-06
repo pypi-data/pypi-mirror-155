ea083257cd4324248a9db214eabc10b6d329881b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:51 +0000

    Add package rec-pangu/0.0.8/rec_pangu-0.0.8-py2.py3-none-any.whl

---
+++ packages/rec-pangu/rec_pangu-0.0.8-py2.py3-none-any.whl/rec_pangu/dataset/__init__.py
@@ -0,0 +1,10 @@
+# -*- ecoding: utf-8 -*-
+# @ModuleName: __init__
+# @Copyright: Deep_Wisdom 
+# @Author: wk
+# @Email: wangkai@fuzhi.ai
+# @Time: 2022/1/20 8:21 下午
+
+from .base_dataset import BaseDataset
+from .process_data import get_dataloader
+from .multi_task_dataset import MultiTaskDataset
