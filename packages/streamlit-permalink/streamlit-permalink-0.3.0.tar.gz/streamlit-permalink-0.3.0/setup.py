77823ee138ea9e74cc0a970fc0fb2f54058d16c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:39 +0000

    Add package streamlit-permalink/0.3.0/streamlit-permalink-0.3.0.tar.gz

---
+++ packages/streamlit-permalink/streamlit-permalink-0.3.0.tar.gz/streamlit-permalink-0.3.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from pathlib import Path
+
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+setup(
+    name='streamlit-permalink',
+    version='0.3.0',
+    description='Effortless permalinks in Streamlit apps.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Franciszek Piszcz',
+    author_email='franciszek.piszcz@rtbhouse.com',
+    url='https://github.com/franekp/streamlit-permalink',
+    py_modules=['streamlit_permalink'],
+    install_requires=[
+        'streamlit >= 1.4.0',
+    ],
+)
