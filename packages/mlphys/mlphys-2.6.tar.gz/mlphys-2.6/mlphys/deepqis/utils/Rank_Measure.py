629841ac151c8a8570672d898e5f285558cb3725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:56:51 +0000

    Add package mlphys/2.6/mlphys-2.6.tar.gz

---
+++ packages/mlphys/mlphys-2.6.tar.gz/mlphys-2.6/mlphys/deepqis/utils/Rank_Measure.py
@@ -0,0 +1,11 @@
+"""
+author: Sanjaya Lohani
+email: slohani@mlphys_nightly.com
+Licence: Apache-2.0
+"""
+import tensorflow as tf
+
+
+def rank(dm_tesnor):
+    ranks = tf.linalg.matrix_rank(dm_tesnor, tol=1e-5)
+    return ranks.numpy()
