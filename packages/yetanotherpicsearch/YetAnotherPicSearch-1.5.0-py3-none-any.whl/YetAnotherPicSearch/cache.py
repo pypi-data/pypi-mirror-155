4f20c66a1c9659d9746757f5bb6ecf8c15f56d2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:35:25 +0000

    Add package yetanotherpicsearch/1.5.0/YetAnotherPicSearch-1.5.0-py3-none-any.whl

---
+++ packages/yetanotherpicsearch/YetAnotherPicSearch-1.5.0-py3-none-any.whl/YetAnotherPicSearch/cache.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+from diskcache import Cache
+
+from .config import config
+from .result import Result
+
+
+def upsert_cache(cache: Cache, image_md5: str, mode: str, result: Result) -> None:
+    cache.set(f"{image_md5}_{mode}", result, expire=config.cache_expire * 24 * 60 * 60)
+
+
+def exist_in_cache(cache: Cache, image_md5: str, mode: str) -> Optional[Result]:
+    result: Optional[Result] = None
+    cache_result: Result = cache.get(f"{image_md5}_{mode}")
+    if cache_result:
+        cache.touch(f"{image_md5}_{mode}", expire=config.cache_expire * 24 * 60 * 60)
+        result = cache_result
+    return result
