2cb4b9c3e2af7a6d2d36ac2fd56567170c760506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:38 +0000

    Add package zcc-helper/3.0rc1/zcc_helper-3.0rc1-py3-none-any.whl

---
+++ packages/zcc-helper/zcc_helper-3.0rc1-py3-none-any.whl/zcc/description.py
@@ -0,0 +1,16 @@
+'''ZCC Zimi Controller Description Class'''
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+
+@dataclass
+class ControlPointDescription:
+    '''Data class to store ControlPoint description.'''
+
+    brand: str = None
+    product: str = None
+    mac: str = None
+    host: str = None
+    port: int = None
+    available_tcps: int = None
