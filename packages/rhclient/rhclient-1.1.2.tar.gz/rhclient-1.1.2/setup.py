e27cab3a368070c79733fb937f27c8362dd41188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:36 +0000

    Add package rhclient/1.1.2/rhclient-1.1.2.tar.gz

---
+++ packages/rhclient/rhclient-1.1.2.tar.gz/rhclient-1.1.2/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# Setup file for rest harness
+import setuptools
+from setuptools import setup
+
+setup(name='rhclient',
+      version='1.1.2',
+      description='Python REST Service Test utility',
+      author='Robbie Reed',
+      author_email='robbiereed@psg-inc.net',
+      url='https://www.restharness.com',
+      package_dir={"": "src"},
+      packages=setuptools.find_packages(where="src"),
+      python_requires=">=3.6",
+      include_package_data=True,
+      install_requires=['requests']
+     )
