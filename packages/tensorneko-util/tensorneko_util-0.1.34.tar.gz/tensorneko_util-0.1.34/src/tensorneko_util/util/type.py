4d01cc0a07ea24317969149615efadcbeb66d5cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:55 +0000

    Add package tensorneko-util/0.1.34/tensorneko_util-0.1.34.tar.gz

---
+++ packages/tensorneko-util/tensorneko_util-0.1.34.tar.gz/tensorneko_util-0.1.34/src/tensorneko_util/util/type.py
@@ -0,0 +1,12 @@
+from enum import Enum
+from typing import TypeVar
+
+# Generic types
+T = TypeVar('T')  # Any type
+R = TypeVar('R')  # Return type
+P = TypeVar("P", int, float, str, bool)  # Primitive type
+E = TypeVar("E", bound=Enum)
+
+T1 = TypeVar("T1")
+T2 = TypeVar("T2")
+T3 = TypeVar("T3")
