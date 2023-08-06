42439a1d4694ecb2c0a323f5a3cf16c6edf4a43f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:30:57 +0000

    Add package piperider-nightly/0.2.0.dev20220619/piperider_nightly-0.2.0.dev20220619-py3-none-any.whl

---
+++ packages/piperider-nightly/piperider_nightly-0.2.0.dev20220619-py3-none-any.whl/piperider_cli/__init__.py
@@ -0,0 +1,52 @@
+import logging
+import os
+import sys
+
+
+def create_logger(name) -> logging.Logger:
+    log_level = logging.WARNING
+    if os.environ.get('PIPERIDER_LOG_LEVEL') == 'DEBUG':
+        log_level = logging.DEBUG
+    if os.environ.get('PIPERIDER_LOG_LEVEL') == 'INFO':
+        log_level = logging.INFO
+
+    log = logging.getLogger(name)
+    log.setLevel(log_level)
+    handler = logging.StreamHandler(sys.stderr)
+    handler.setLevel(log_level)
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    log.addHandler(handler)
+    return log
+
+
+def get_assertion_dir():
+    return os.environ.get('PIPERIDER_CUSTOM_ASSERTIONS_PATH', '')
+
+
+def set_assertion_dir(dir):
+    snap_dir = os.path.join(os.path.normpath(dir), '__snapshots__')
+    if not os.path.exists(snap_dir):
+        os.mkdir(snap_dir)
+    os.environ['PIPERIDER_CUSTOM_ASSERTIONS_PATH'] = snap_dir
+
+
+def get_version():
+    version_file = os.path.normpath(os.path.join(os.path.dirname(__file__), 'VERSION'))
+    with open(version_file) as fh:
+        version = fh.read().strip()
+        return version
+
+
+def clone_directory(src, dst):
+    if sys.version_info >= (3, 8):
+        # dirs_exist_ok only available after 3.8
+        import shutil
+        shutil.copytree(src, dst, dirs_exist_ok=True)
+    else:
+        from distutils.dir_util import copy_tree
+        copy_tree(src, dst)
+
+
+__version__ = get_version()
