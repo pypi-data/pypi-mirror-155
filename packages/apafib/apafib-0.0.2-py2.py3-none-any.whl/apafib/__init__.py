80491a8050bf5352a9ecea952131cc88a6fbfd94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:35:42 +0000

    Add package apafib/0.0.2/apafib-0.0.2-py2.py3-none-any.whl

---
+++ packages/apafib/apafib-0.0.2-py2.py3-none-any.whl/apafib/__init__.py
@@ -0,0 +1,22 @@
+"""
+.. module:: __init__.py
+
+__init__.py
+*************
+
+:Description: __init__.py
+
+
+:Authors: bejar
+    
+
+:Version: 
+
+:Created on: 06/05/2022 10:53 
+
+"""
+
+from apafib.apafib import fetch_apa_data
+
+
+__all__ = ['fetch_apa_data']
