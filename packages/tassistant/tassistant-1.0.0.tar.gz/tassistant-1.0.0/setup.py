56476e79142b37b66a78c3a0ee04b4cbb60eeb7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:43 +0000

    Add package tassistant/1.0.0/tassistant-1.0.0.tar.gz

---
+++ packages/tassistant/tassistant-1.0.0.tar.gz/tassistant-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name="tassistant",
+    version="1.0.0",
+    description="Helper for turtle module",
+    packages=['tassistant'],
+    author_email='aroslavusin11@gmail.com',
+    zip_safe=False)
---
+++ packages/tassistant/tassistant-1.0.0.tar.gz/tassistant-1.0.0/tassistant/utilities.py
@@ -0,0 +1,6 @@
+from turtle import penup, goto, pendown
+
+def goto_(x, y):
+	penup()
+	goto(x, y)
+	pendown()
