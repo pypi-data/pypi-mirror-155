389955ef1c0c79b9ac63bac12142b0afae32c2db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:53:17 +0000

    Add package cscli/0.6.0/cscli-0.6.0-py2.py3-none-any.whl

---
+++ packages/cscli/cscli-0.6.0-py2.py3-none-any.whl/cscli/cli.py
@@ -0,0 +1,118 @@
+import json
+import os
+import sys
+
+import click
+import yaml
+
+from cscli import CloudSigmaClient, __description__, __version__
+
+CONTEXT_SETTINGS = dict(auto_envvar_prefix="CSCLI")
+
+
+class Environment:
+    def __init__(self):
+        self.verbose = False
+        self.compact = False
+        self.fmt = "json"
+        self.api = None
+
+    def log(self, msg, *args):
+        """Logs a message to stderr."""
+        if self.verbose:
+            if args:
+                msg %= args
+            click.echo(msg, file=sys.stderr)
+
+    def output(self, item, status=True):
+        if self.compact:
+            json_indent = None
+            json_separators = [",", ":"]
+        else:
+            json_indent = 2
+            json_separators = [", ", ": "]
+        if self.fmt == "yaml":
+            out = yaml.dump(item)
+        else:
+            out = json.dumps(
+                dict(status=status, result=item),
+                indent=json_indent,
+                separators=json_separators,
+            )
+        click.echo(out)
+
+    def error(self, message):
+        self.output(message, False)
+        sys.exit()
+
+    def confirm(self, resource, action, label, force):
+        if not force:
+            if not click.confirm(
+                f"Confirm {action} of {label} {resource['name']} {resource['uuid']}"
+            ):
+                self.output(f"{action} averted")
+                sys.exit()
+
+
+pass_environment = click.make_pass_decorator(Environment, ensure=True)
+cmd_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), "commands"))
+
+
+class ComplexCLI(click.MultiCommand):
+    def list_commands(self, ctx):
+        rv = []
+        for filename in os.listdir(cmd_folder):
+            if filename.endswith(".py") and filename.startswith("cmd_"):
+                rv.append(filename[4:-3])
+        rv.sort()
+        return rv
+
+    def get_command(self, ctx, name):
+        try:
+            mod = __import__(f"cscli.commands.cmd_{name}", None, None, ["cli"])
+        except ImportError:
+            return
+        return mod.cli
+
+
+@click.command("cscli", cls=ComplexCLI, context_settings=CONTEXT_SETTINGS)
+@click.version_option(message=f"{__package__} v{__version__} -- {__description__}")
+@click.option(
+    "-u", "--username", type=str, help="override env var CLOUDSIGMA_USERNAME]"
+)
+@click.option(
+    "-p", "--password", type=str, help="override env var CLOUDSIGMA_PASSWORD]"
+)
+@click.option("-r", "--region", type=str, help="override env var CLOUDSIGMA_REGION]")
+@click.option(
+    "-d", "--debug", is_flag=True, help="output full stacktrace on runtime error"
+)
+@click.option("-v", "--verbose", is_flag=True, help="Enables verbose mode.")
+@click.option("-c", "--compact", is_flag=True, help="Output Compact JSON")
+@click.option("-y", "--yaml", is_flag=True, help="Output YAML")
+@click.option("-j", "--json", is_flag=True, help="Output JSON")
+@pass_environment
+def cli(ctx, region, username, password, debug, verbose, compact, yaml, json):
+    """CLI for the CloudSigma API
+
+    create, modify, operate, and destroy resources on cloudsigma
+    """
+
+    def exception_handler(
+        exception_type, exception, traceback, debug_hook=sys.excepthook
+    ):
+        if debug:
+            debug_hook(exception_type, exception, traceback)
+        else:
+            ctx.error(f"{exception_type.__name__}: {exception}")
+
+    sys.excepthook = exception_handler
+
+    ctx.verbose = verbose
+    ctx.compact = compact
+    if yaml:
+        ctx.fmt = "yaml"
+    if json:
+        ctx.fmt = "json"
+
+    ctx.api = CloudSigmaClient(region, username, password)
---
+++ packages/cscli/cscli-0.6.0-py2.py3-none-any.whl/cscli/commands/cmd_list.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+import click
+
+from cscli.cli import pass_environment
+from cscli.error import ParameterError
+
+
+@click.command("list", short_help="list resources by type")
+@click.argument(
+    "resource",
+    type=click.Choice(
+        ["servers", "drives", "libdrives", "vlans", "ips", "subscriptions", "capabilities"]
+    ),
+    metavar="RESOURCE_TYPE",
+    required=False,
+    default=None,
+)
+@click.option(
+    "fmt",
+    "--detail",
+    flag_value="detail",
+    help="detailed JSON output (default)",
+)
+@click.option(
+    "fmt",
+    "--json",
+    flag_value="detail",
+    hidden=True,
+    help="detailed JSON output (default)",
+)
+@click.option("fmt", "--uuid", flag_value="uuid", help="output resource UUIDs")
+@click.option(
+    "fmt",
+    "--brief",
+    flag_value="brief",
+    help="abbreviated JSON output",
+)
+@click.option(
+    "fmt",
+    "--text",
+    flag_value="text",
+    help="text output",
+)
+@click.option('-f', '--filter', '_filter', type=str, multiple=True)
+@pass_environment
+def cli(ctx, resource, fmt, _filter):
+    """list resources: servers drives libdrives ips venvs capabilities subscriptions"""
+    list_map = {
+        "servers": ctx.api.list_servers,
+        "drives": ctx.api.list_drives,
+        "libdrives": ctx.api.list_libdrives,
+        "vlans": ctx.api.list_vlans,
+        "ips": ctx.api.list_ips,
+        "subscriptions": ctx.api.list_subscriptions,
+        "capabilities": ctx.api.list_capabilities,
+        None: ctx.api.list_all,
+    }
+
+    if resource == "capabilities" and fmt not in [None, "detail"]:
+        raise ParameterError(f"{resource} cannot be formatted as {fmt}")
+
+    if resource == "subscriptions" and fmt not in [None, "uuid", "detail"]:
+        raise ParameterError(f"{resource} cannot be formatted as {fmt}")
+
+    _filter = list(_filter)
+
+    ret = list_map[resource](fmt, _filter)
+    if fmt == "text":
+        click.echo("=" * 79)
+        for name, data in ret.items():
+            first = True
+            for result in data:
+                for uuid, lines in result.items():
+                    if first:
+                        first = False
+                    else:
+                        click.echo("-" * 79)
+                    click.echo(f"{name[:-1]} {uuid}")
+                    for line in lines:
+                        click.echo("   " + line)
+            click.echo("=" * 79)
+    elif fmt == "uuid":
+        uuids = {}
+        for name, data in ret.items():
+            uuids[name] = [u for u in [list(d.keys())[0] for d in data] if u]
+        ctx.output(uuids)
+    else:
+        ctx.output(ret)
