11a13c32af2a35e8645f83878fb08d7400098e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:11 +0000

    Add package envoy.code.check/0.2.2/envoy.code.check-0.2.2-py3-none-any.whl

---
+++ packages/envoy.code.check/envoy.code.check-0.2.2-py3-none-any.whl/envoy/code/check/typing.py
@@ -0,0 +1,47 @@
+
+from typing import Dict, List, Optional, Tuple, TypedDict
+
+
+ProblemDict = Dict[str, List[str]]
+
+YapfProblemTuple = Tuple[str, List[str]]
+YapfResultTuple = Tuple[str, str, bool]
+YapfCheckResultTuple = Tuple[str, YapfResultTuple]
+
+
+class BaseExtensionMetadataDict(TypedDict):
+    categories: List[str]
+    security_posture: str
+    status: str
+
+
+class ExtensionMetadataDict(BaseExtensionMetadataDict, total=False):
+    undocumented: bool
+    type_urls: List[str]
+
+
+ExtensionsMetadataDict = Dict[str, ExtensionMetadataDict]
+ExtensionsSchemaCategoriesList = List[str]
+ExtensionsSchemaBuiltinList = List[str]
+ExtensionsSchemaSecurityPosturesList = List[str]
+ExtensionsSchemaStatusValuesList = List[str]
+
+
+class ExtensionSecurityPostureMetadataDict(TypedDict):
+    name: str
+    description: str
+
+
+class ExtensionStatusMetadataDict(TypedDict):
+    name: str
+    description: Optional[str]
+
+
+class ExtensionsSchemaDict(TypedDict):
+    builtin: ExtensionsSchemaBuiltinList
+    security_postures: List[ExtensionSecurityPostureMetadataDict]
+    categories: ExtensionsSchemaCategoriesList
+    status_values: List[ExtensionStatusMetadataDict]
+
+
+ConfiguredExtensionsDict = Dict[str, str]
