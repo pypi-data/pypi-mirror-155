d6f645d5c41623d85f1f9585f0d7064bdb5e8a5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:21:30 +0000

    Add package flask-ory-auth/0.1.6/flask-ory-auth-0.1.6.tar.gz

---
+++ packages/flask-ory-auth/flask-ory-auth-0.1.6.tar.gz/flask-ory-auth-0.1.6/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['flask_ory_auth',
+ 'flask_ory_auth.hydra',
+ 'flask_ory_auth.keto',
+ 'flask_ory_auth.kratos']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Flask>=2.1.2,<3.0.0',
+ 'requests-oauthlib>=1.3.1,<2.0.0',
+ 'requests>=2.28.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'flask-ory-auth',
+    'version': '0.1.6',
+    'description': 'Useful middlewares and clients to use Ory products in flask App',
+    'long_description': None,
+    'author': 'Andrew Minkin',
+    'author_email': 'minkin.andrew@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7',
+}
+
+
+setup(**setup_kwargs)
