692a3bdc776f90da7bc11a8228886d545324c215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:23 +0000

    Add package pycosie/0.2.1/pycosie-0.2.1.tar.gz

---
+++ packages/pycosie/pycosie-0.2.1.tar.gz/pycosie-0.2.1/setup.py
@@ -0,0 +1,35 @@
+import sys
+from setuptools import setup, find_packages
+
+sys.path.insert(0,"pycosie")
+
+desc = "Anaylsis tools to help with TD (and hopefully other simulations...)"
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pycosie",
+    description=desc,
+    long_description=long_description,
+    version="0.2.1",
+    packages=find_packages(),
+    author="Samir Kusmic",
+    project_url={
+        "Pycosie":"https://github.com/kusmic/pycosie",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+    ],
+    license="GNU General Public License v2 (GPLv2)",
+    install_requires=[
+        "numpy",
+        "scipy",
+        "yt",
+        "caesar",
+        "astropy",
+        "h5py"
+    ]
+)
