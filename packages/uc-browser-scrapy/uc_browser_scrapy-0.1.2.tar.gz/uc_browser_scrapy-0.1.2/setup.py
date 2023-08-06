30b77cf828b26f5b07e1c642c009168c64c7b3b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:41 +0000

    Add package uc-browser-scrapy/0.1.2/uc_browser_scrapy-0.1.2.tar.gz

---
+++ packages/uc-browser-scrapy/uc_browser_scrapy-0.1.2.tar.gz/uc_browser_scrapy-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['uc_browser_scrapy']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Scrapy>=2.6.1,<3.0.0', 'uc-browser>=0.2.4,<0.3.0']
+
+setup_kwargs = {
+    'name': 'uc-browser-scrapy',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'Thiago Oliveira',
+    'author_email': 'thiceconelo@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
