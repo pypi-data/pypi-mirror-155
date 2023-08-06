3c898145eeff6bafcfd73973d8120fe44de5b65e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:17:48 +0000

    Add package LztApi/1.0/LztApi-1.0.tar.gz

---
+++ packages/LztApi/LztApi-1.0.tar.gz/LztApi-1.0/LztApi/__init__.py
@@ -0,0 +1,1 @@
+from .LolzApi import LolzApi
---
+++ packages/LztApi/LztApi-1.0.tar.gz/LztApi-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='LztApi',
+      version='1.0',
+      description='LolzTeam Api',
+      packages=['LztApi'],
+      author_email='coder@derkown.ru',
+      zip_safe=False)
