e0a0766f2d872c32c52cda53283a5a06c17f3a2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:19:58 +0000

    Add package mailersend/0.3.2/mailersend-0.3.2.tar.gz

---
+++ packages/mailersend/mailersend-0.3.2.tar.gz/mailersend-0.3.2/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mailersend',
+ 'mailersend.activity',
+ 'mailersend.analytics',
+ 'mailersend.base',
+ 'mailersend.domains',
+ 'mailersend.emails',
+ 'mailersend.inbound_routing',
+ 'mailersend.messages',
+ 'mailersend.recipients',
+ 'mailersend.scheduled_messages',
+ 'mailersend.sms_activity',
+ 'mailersend.sms_sending',
+ 'mailersend.templates',
+ 'mailersend.tokens',
+ 'mailersend.utils',
+ 'mailersend.webhooks']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pre-commit>=2.12.1,<3.0.0', 'requests>=2.25.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'mailersend',
+    'version': '0.3.2',
+    'description': 'The official MailerSend Python SDK',
+    'long_description': None,
+    'author': 'Igor Hrček',
+    'author_email': 'igor@mailerlite.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6.1,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
