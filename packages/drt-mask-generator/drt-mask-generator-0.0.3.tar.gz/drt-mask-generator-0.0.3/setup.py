c21b43784520f7b7cfc0cf97d777ac0f2f48edf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:45 +0000

    Add package drt-mask-generator/0.0.3/drt-mask-generator-0.0.3.tar.gz

---
+++ packages/drt-mask-generator/drt-mask-generator-0.0.3.tar.gz/drt-mask-generator-0.0.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="drt-mask-generator",
+    version="0.0.3",
+    author="Dmitry Trokhachev",
+    author_email="dimiaa573@gmail.com",
+    description="Tool for mask creating",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dimiaa/drt-mask-generator",
+    project_urls={
+        "Bug Tracker": "https://github.com/dimiaa/drt-mask-generator/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    install_requires=[
+       'PySide2',
+       'shiboken2',
+    ],
+    python_requires=">=3.6,<3.11",
+)
