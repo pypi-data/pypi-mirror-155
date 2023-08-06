1e247ffc7a3bedb6ac9eddfc0d26280593ec5182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:50 +0000

    Add package ML360-imgprepro-DP/0.0.1/ML360_imgprepro_DP-0.0.1.tar.gz

---
+++ packages/ML360-imgprepro-DP/ML360_imgprepro_DP-0.0.1.tar.gz/ML360_imgprepro_DP-0.0.1/ML360_imgprepro_DP/__init__.py
@@ -0,0 +1,4 @@
+#from ML360_NLP.ML360_NLP import ML360_NLP
+#from ML360_NLP.ml360_models import MlModles,MeanEmbeddingVectorizer
+
+from ML360_imgprepro_DP.ML360_imgprepro_DP import image_preprcessing
