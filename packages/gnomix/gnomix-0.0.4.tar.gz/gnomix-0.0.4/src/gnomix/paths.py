cfe64fdf19e961597e5fe820fe2dd9440bd45c45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:01 +0000

    Add package gnomix/0.0.4/gnomix-0.0.4.tar.gz

---
+++ packages/gnomix/gnomix-0.0.4.tar.gz/gnomix-0.0.4/src/gnomix/paths.py
@@ -0,0 +1,9 @@
+import pathlib
+
+DATA_FOLDER = pathlib.Path("./data/")
+
+TEST_DATA_FOLDER = DATA_FOLDER / "test"
+
+DEMO_DATA_FOLDER = DATA_FOLDER / "demo"
+
+GENETIC_MAP_FOLDER = DATA_FOLDER / "genetic_maps"
