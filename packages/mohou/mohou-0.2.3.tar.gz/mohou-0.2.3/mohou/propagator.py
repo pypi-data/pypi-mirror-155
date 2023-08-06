a4c5b05e22386defabdea1fc88a4f2eb002b91b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:53 +0000

    Add package mohou/0.2.3/mohou-0.2.3.tar.gz

---
+++ packages/mohou/mohou-0.2.3.tar.gz/mohou-0.2.3/mohou/propagator.py
@@ -0,0 +1,77 @@
+from typing import List, Optional
+
+import numpy as np
+import torch
+
+from mohou.constant import CONTINUE_FLAG_VALUE
+from mohou.encoding_rule import EncodingRule
+from mohou.model import LSTM
+from mohou.types import ElementDict, TerminateFlag
+
+
+class Propagator:
+    lstm: LSTM
+    encoding_rule: EncodingRule
+    fed_state_list: List[np.ndarray]  # eatch state is equipped with flag
+    n_init_duplicate: int
+    is_initialized: bool
+    time_invariant_input: Optional[np.ndarray] = None
+
+    def __init__(self, lstm: LSTM, encoding_rule: EncodingRule, n_init_duplicate: int = 0):
+        self.lstm = lstm
+        self.encoding_rule = encoding_rule
+        self.fed_state_list = []
+        self.n_init_duplicate = n_init_duplicate
+        self.is_initialized = False
+
+        require_time_invaliant_input = lstm.config.n_time_invariant_input > 0
+
+        if not require_time_invaliant_input:  # auto set
+            self.time_invariant_input = np.empty((0,))
+
+    def set_time_invariant_input(self, value: np.ndarray) -> None:
+        assert value.ndim == 1
+        assert self.lstm.config.n_time_invariant_input == len(value)
+        self.time_invariant_input = value
+
+    def feed(self, elem_dict: ElementDict):
+        self._feed(elem_dict)
+        if not self.is_initialized:
+            for _ in range(self.n_init_duplicate):
+                self._feed(elem_dict)
+
+        if not self.is_initialized:
+            self.is_initialized = True
+
+    def _feed(self, elem_dict: ElementDict):
+        if TerminateFlag not in elem_dict:
+            elem_dict[TerminateFlag] = TerminateFlag.from_bool(False)
+        state_with_flag = self.encoding_rule.apply(elem_dict)
+        self.fed_state_list.append(state_with_flag)
+
+    def predict(self, n_prop: int) -> List[ElementDict]:
+        pred_state_list = self._predict(n_prop)
+        elem_dict_list = []
+        for pred_state in pred_state_list:
+            elem_dict = self.encoding_rule.inverse_apply(pred_state)
+            elem_dict_list.append(elem_dict)
+        return elem_dict_list
+
+    def _predict(self, n_prop: int, force_continue: bool = False) -> List[np.ndarray]:
+        pred_state_list: List[np.ndarray] = []
+
+        assert self.time_invariant_input is not None, "forgot setting time_invariant_input ??"
+        ti_input_torch = torch.from_numpy(self.time_invariant_input).float().unsqueeze(dim=0)
+
+        for i in range(n_prop):
+            states = np.vstack(self.fed_state_list + pred_state_list)
+            states_torch = torch.from_numpy(states).float().unsqueeze(dim=0)
+            state_pred_torch: torch.Tensor = self.lstm(states_torch, ti_input_torch)[:, -1]
+            state_pred = state_pred_torch.squeeze().detach().numpy()
+
+            if force_continue:
+                state_pred[-1] = CONTINUE_FLAG_VALUE
+
+            pred_state_list.append(state_pred)
+
+        return pred_state_list
---
+++ packages/mohou/mohou-0.2.3.tar.gz/mohou-0.2.3/mohou/script/visualize_autoencoder_result.py
@@ -0,0 +1,15 @@
+import argparse
+
+from mohou.default import auto_detect_autoencoder_type
+from mohou.script_utils import visualize_image_reconstruction
+from mohou.setting import setting
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-pn", type=str, default=setting.primary_project_name, help="project name")
+    parser.add_argument("-n", type=int, default=5, help="number of visualization")
+    args = parser.parse_args()
+    project_name = args.pn
+    n_vis = args.n
+    ae_type = auto_detect_autoencoder_type(project_name)
+    visualize_image_reconstruction(project_name, n_vis, ae_type=ae_type)
