2fc5309afd1488db46560c13c48aff19001b6103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:55 +0000

    Add package cleanout/1.0/cleanout-1.0.tar.gz

---
+++ packages/cleanout/cleanout-1.0.tar.gz/cleanout-1.0/cleanout/__init__.py
@@ -0,0 +1,3 @@
+# hi, bro ;)
+def clean(end: str = ''):
+    print('\033[H\033[J', end=end)
---
+++ packages/cleanout/cleanout-1.0.tar.gz/cleanout-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='cleanout',
+    version='1.0',
+    description='Library for clean screen of console',
+    packages=['cleanout'],
+    author_email='cklfgads@knowledgemd.com',
+    install_requires=[]
+)
