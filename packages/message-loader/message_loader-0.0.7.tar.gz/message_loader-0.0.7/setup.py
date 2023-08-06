ea89a4f60ad87d585b2fcf05f73730c4ed7ed36c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:19:47 +0000

    Add package message-loader/0.0.7/message_loader-0.0.7.tar.gz

---
+++ packages/message-loader/message_loader-0.0.7.tar.gz/message_loader-0.0.7/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+# Setting up
+setup(
+    name="message_loader",
+    version='0.0.7',
+    author="Wxlbr73 (Will Ward)",
+    # author_email="<me@gmail.com>",
+    description='Basic Terminal Loading Message',
+    long_description_content_type="text/markdown",
+    long_description='Basic Loading Message Outputed to the Terminal with Animation',
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'threading', 'loading'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
+
+# py setup.py sdist bdist_wheel
+# twine upload dist/* --verbose
+# Wxlbr73
+# TwitchFan101
