545ed75f577996e5ae81dcf451f55f8ec2b2b6f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:38 +0000

    Add package mpraf/0.0.0.5/mpraf-0.0.0.5.tar.gz

---
+++ packages/mpraf/mpraf-0.0.0.5.tar.gz/mpraf-0.0.0.5/README.md
@@ -0,0 +1,16 @@
+# MPRAF
+
+MPRAF(Multi-layer Puzzle Recommendation Algorithm Frame) is a frame used for hybrid recommendation algorithms. Everyone can build their own hybrid algorithms with the architecture given by this frame to fix or solve specific shortcomings.
+
+This is only a preview version, later stable version is coming soon.
+
+## Input Layer
+
+In this layer, read the file at first, and transform them into the specific format that we need. The MPRAF provides the solutions for reading and processing data.
+
+## Embedding Layer
+
+## Puzzle Model
+
+## Output Layer
+
---
+++ packages/mpraf/mpraf-0.0.0.5.tar.gz/mpraf-0.0.0.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+# Package meta-data.
+NAME = 'mpraf'
+DESCRIPTION = 'MPRAF: Multi-layer Puzzle Recommendation Algorithm Frame'
+URL = 'http://zhihengyang.com'
+EMAIL = 'zach-young@outlook.com'
+AUTHOR = 'Zhiheng Yang 杨志恒'
+REQUIRES_PYTHON = '>=3.9.0'
+VERSION = '0.0.0.5'
+packages=['mpraf', "mpraf.algs", "mpraf.utility"],
+
+# What packages are required for this module to be executed?
+REQUIRED = []
+
+# Setting.
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(),
+    install_requires=REQUIRED,
+    license="MIT"
+)
