f61ded5723a3c5d148e4903ab52394f6730ac060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:30 +0000

    Add package uc-browser-scrapy/0.1.3/uc_browser_scrapy-0.1.3.tar.gz

---
+++ packages/uc-browser-scrapy/uc_browser_scrapy-0.1.3.tar.gz/uc_browser_scrapy-0.1.3/uc_browser_scrapy/middlewares.py
@@ -0,0 +1,83 @@
+import struct
+from importlib import import_module
+
+from scrapy import signals
+from scrapy.exceptions import NotConfigured
+from scrapy.http import HtmlResponse
+
+from uc_browser.browser_v2 import BrowserV2
+#import undetected_chromedriver as uc
+
+
+from selenium.webdriver import Chrome
+from selenium.webdriver import ChromeOptions
+from selenium.common.exceptions import TimeoutException, WebDriverException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+from .http import SeleniumBrowserUcRequest
+
+
+class SeleniumBrowserUcMiddleware:
+    """Scrapy middleware handling the requests using selenium"""
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        """Initialize the middleware with the crawler settings"""
+        driver = BrowserV2()
+
+        middleware = cls(
+            driver=driver
+        )
+
+        crawler.signals.connect(middleware.spider_closed, signals.spider_closed)
+
+        return middleware
+
+    def process_request(self, request, spider):
+        """Process a request using the selenium driver if applicable"""
+
+        if not isinstance(request, SeleniumRequest):
+            return None
+
+        self.driver.get(request.url)
+
+        for cookie_name, cookie_value in request.cookies.items():
+            self.driver.add_cookie(
+                {
+                    'name': cookie_name,
+                    'value': cookie_value
+                }
+            )
+
+        if request.wait_until:
+            WebDriverWait(self.driver, request.wait_time).until(
+                request.wait_until
+            )
+
+        if request.screenshot:
+            request.meta['screenshot'] = self.driver.get_screenshot_as_png()
+
+        if request.script:
+            self.driver.execute_script(request.script)
+
+        body = str.encode(self.driver.page_source)
+
+        # Expose the driver via the "meta" attribute
+        request.meta.update({'driver': self.driver})
+
+        return HtmlResponse(
+            self.driver.current_url,
+            body=body,
+            encoding='utf-8',
+            request=request
+        )
+
+    def spider_closed(self):
+        """Shutdown the driver when spider is closed"""
+
+        self.driver.quit()
