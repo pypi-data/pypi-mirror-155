d06bca9c59566401f080ccf2b45b7246f8f848ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:17:24 +0000

    Add package ib-syncevent/0.0.32/ib_syncevent-0.0.32-py3-none-any.whl

---
+++ packages/ib-syncevent/ib_syncevent-0.0.32-py3-none-any.whl/ib_syncevent/utilities/IBEvents.py
@@ -0,0 +1,8 @@
+from typing import Callable
+
+
+class IBEvents():
+    def __init__(self):
+        self.historical_bars_event: Callable or None = None
+        self.order_status: Callable or None = None
+        self.open_order: Callable or None = None
