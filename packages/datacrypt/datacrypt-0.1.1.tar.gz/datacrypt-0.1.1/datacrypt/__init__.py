e0c5f614d7976852e774e437c9435a1d6c181a3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:31:29 +0000

    Add package datacrypt/0.1.1/datacrypt-0.1.1.tar.gz

---
+++ packages/datacrypt/datacrypt-0.1.1.tar.gz/datacrypt-0.1.1/datacrypt/__init__.py
@@ -0,0 +1,11 @@
+# Files
+from . import array
+from . import number
+
+# Folders
+
+def files():
+    print(2)
+    
+def folders():
+    print(0)
---
+++ packages/datacrypt/datacrypt-0.1.1.tar.gz/datacrypt-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='datacrypt',
+      version='0.1.1',
+      description='datacrypt Package',
+      packages=['datacrypt'],
+      install_requires=["numpy", "pandas", "matplotlib"],
+      zip_safe=False,
+      )
