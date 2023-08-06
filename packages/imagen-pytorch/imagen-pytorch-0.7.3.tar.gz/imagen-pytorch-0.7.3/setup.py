ef9bc0affa9ace2513b266805de62782088984f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:21:53 +0000

    Add package imagen-pytorch/0.7.3/imagen-pytorch-0.7.3.tar.gz

---
+++ packages/imagen-pytorch/imagen-pytorch-0.7.3.tar.gz/imagen-pytorch-0.7.3/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+exec(open('imagen_pytorch/version.py').read())
+
+setup(
+  name = 'imagen-pytorch',
+  packages = find_packages(exclude=[]),
+  version = __version__,
+  license='MIT',
+  description = 'Imagen - unprecedented photorealism × deep level of language understanding',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/lucidrains/imagen-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'transformers',
+    'text-to-image',
+    'denoising-diffusion'
+  ],
+  install_requires=[
+    'click',
+    'einops>=0.4',
+    'einops-exts',
+    'numpy',
+    'packaging',
+    'pillow',
+    'pydantic',
+    'pytorch-warmup',
+    'resize-right',
+    'sentencepiece',
+    'torch>=1.6',
+    'torchvision',
+    'transformers',
+    'tqdm'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
