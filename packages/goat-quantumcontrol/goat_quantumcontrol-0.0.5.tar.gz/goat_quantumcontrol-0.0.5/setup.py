260c929b2f413e0d594b82ef3544acd64cc1c531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:14 +0000

    Add package goat-quantumcontrol/0.0.5/goat_quantumcontrol-0.0.5.tar.gz

---
+++ packages/goat-quantumcontrol/goat_quantumcontrol-0.0.5.tar.gz/goat_quantumcontrol-0.0.5/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+VERSION = '0.0.5'
+DESCRIPTION = 'Python implementation of the quantum optimal control GOAT-algorithm'
+# read the contents of the README file
+this_directory = Path(__file__).parent
+LONG_DESCRIPTION = (this_directory / "README.md").read_text()
+
+# Setting up
+setup(
+    name="goat_quantumcontrol",
+    version=VERSION,
+    author="Markus Plautz",
+    author_email="<markus.plautz@gmx.at>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    url='https://github.com/pmarkus-github/goat-quantumcontrol',
+    packages=find_packages(),
+    install_requires=['numpy', 'scipy', 'matplotlib'],
+    keywords=['python', 'quantum-control', 'goat', 'quantum-gates', 'quantum-computer'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
