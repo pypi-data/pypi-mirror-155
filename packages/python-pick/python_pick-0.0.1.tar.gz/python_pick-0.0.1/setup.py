cd8aa28cd9a2e31fea9ced3905981744c51c7fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:53:23 +0000

    Add package python-pick/0.0.1/python_pick-0.0.1.tar.gz

---
+++ packages/python-pick/python_pick-0.0.1.tar.gz/python_pick-0.0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python_pick",
+    version="0.0.1",
+    author="Coder10",
+    author_email="coder10@pikit.com",
+    description="Python pick, pick module on py code without use terminal !",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TheGamer548/Pikkit",
+    project_urls={
+        "Source-code": "https://github.com/TheGamer548/Pikkit",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"https://github.com/TheGamer548/Pikkit": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
