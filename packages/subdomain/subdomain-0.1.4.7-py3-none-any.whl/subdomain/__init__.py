9b976ca0dee8fd8c0d8ae93a26004d9341ac3339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:34:45 +0000

    Add package subdomain/0.1.4.7/subdomain-0.1.4.7-py3-none-any.whl

---
+++ packages/subdomain/subdomain-0.1.4.7-py3-none-any.whl/subdomain/__init__.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+Version: 0.1
+Autor: zmf96
+Email: zmf96@qq.com
+Date: 2022-02-21 08:45:43
+LastEditors: zmf96
+LastEditTime: 2022-05-06 12:17:52
+FilePath: /subdomain/__init__.py
+Description:
+"""
+
+from cp_common.base import Plugin
+
+from .subdomain import SubDomain
+
+__all__ = ["PluginClass", "SubDomain"]
+
+
+class PluginClass(Plugin):
+    usage = [
+        {
+            "name": "host_list",
+            "type": "List[str]",
+            "usage": '域名列表，eg: ["baidu.com","bing.com"]',
+        }
+    ]
+    plugin_name = "pysubdomain"
+    author = "example"
+    version = "0.1.4.7"
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def run(self):
+        self.logger.info(self.kwargs)
+        for host in self.kwargs["host_list"]:
+            sd = SubDomain({"deep": 1, "domain": host, "dictname": "default.txt"})
+            sd.run()
+            for dom, ips in sd.results.keys():
+                self.results.append({"domain": {"domain": dom, "ips": ips}})
+            sd.loop.close()
