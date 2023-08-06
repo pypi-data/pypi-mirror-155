f57a7a3cde29704103a81ec7859572ee5682a047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:06 +0000

    Add package elma-api-lib/1.0.0/elma-api-lib-1.0.0.tar.gz

---
+++ packages/elma-api-lib/elma-api-lib-1.0.0.tar.gz/elma-api-lib-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='elma-api-lib',
+    version='1.0.0',
+    packages=['elma', 'elma.models', 'elma.models.auth', 'elma.models.data', 'elma.models.data.util',
+              'elma.models.start_process'],
+    url='',
+    license='',
+    author='Evgeny Kuzin',
+    author_email='evgenykuzin21@gmail.com',
+    description='elma api web client'
+)
