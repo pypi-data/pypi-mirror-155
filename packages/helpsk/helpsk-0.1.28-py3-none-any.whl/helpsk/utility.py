b379eef24e5e2b5d78fffc78b9e102f209f38e3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:31 +0000

    Add package helpsk/0.1.28/helpsk-0.1.28-py3-none-any.whl

---
+++ packages/helpsk/helpsk-0.1.28-py3-none-any.whl/helpsk/utility.py
@@ -0,0 +1,100 @@
+"""A collection of system and environment related helper functions.
+"""
+import inspect
+import warnings
+import sys
+import os
+import pickle
+from contextlib import contextmanager, redirect_stdout
+
+
+def read_pickle(path):
+    """
+    Simple helper function that read's from a pickled object.
+
+    Args:
+        path:
+            File path where the pickled object will be stored.
+    """
+    with open(path, 'rb') as handle:
+        unpickled_object = pickle.load(handle)
+    return unpickled_object
+
+
+def to_pickle(obj, path):
+    """
+    Simple helper function that saves a pickled object.
+
+    Args:
+        obj:
+            the object to save
+        path:
+            File path where the pickled object will be read from.
+    """
+    with open(path, 'wb') as handle:
+        pickle.dump(obj, handle)
+
+
+@contextmanager
+def redirect_stdout_to_file(file, mode='w'):
+    """ Helper context manager that opens a file and redirects standard output to that file.
+
+    Example:
+
+    ```
+    with redirect_stdout_to_file(file_name):
+        print_dataframe(dataframe)
+    ```
+
+    Args:
+        file:
+            the name and path of the file to open (argument is passed to `open()`)
+        mode:
+            the mode of the file e.g. `w` (argument is passed to `open()`)
+    """
+    with open(file, mode) as handle:
+        with redirect_stdout(handle):
+            yield
+
+
+@contextmanager
+def suppress_stdout():
+    """Suppress Output
+
+
+    ```
+    print("Now you see it")
+    with suppress_stdout():
+        print("Now you don't")
+    ```
+
+    code from: https://stackoverflow.com/questions/2125702/how-to-suppress-console-output-in-python
+    """
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:
+            yield
+        finally:
+            sys.stdout = old_stdout
+
+
+@contextmanager
+def suppress_warnings():
+    """Simple Wrapper around warnings.catch_warnings()"""
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        yield
+
+
+def is_debugging():
+    """
+    Returns:
+        Returns True if the environment is currently debugging (e.g. set a breakpoint in PyCharm),
+        False if not
+    """
+    for frame in inspect.stack():
+        if frame[1].endswith("pydevd.py"):
+            return True
+
+    return False
