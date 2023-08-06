6ab190a56bc725a0bb09adfe957919fd8099ea23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:44 +0000

    Add package xdbx/1.0.12/xdbx-1.0.12-py3-none-any.whl

---
+++ packages/xdbx/xdbx-1.0.12-py3-none-any.whl/xdbx/__init__.py
@@ -0,0 +1,12 @@
+# #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time : 2021/12/29 11:21
+# @Author : BruceLong
+# @FileName: __init__.py
+# @Email   : 18656170559@163.com
+# @Software: PyCharm
+# @Blog ：http://www.cnblogs.com/yunlongaimeng/
+from .x_mysql import x_mysql
+from .x_sqlserver import x_mssql
+
+__all__ = ['x_mysql', 'x_mssql']
