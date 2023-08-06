fc5d2ad97e246c5e0a775e64401f57bd0bf971f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:40 +0000

    Add package notetiktok/0.1.2/notetiktok-0.1.2.tar.gz

---
+++ packages/notetiktok/notetiktok-0.1.2.tar.gz/notetiktok-0.1.2/notetiktok/server/data_service.py
@@ -0,0 +1,53 @@
+from fastapi import APIRouter
+from notebuild.tool.fastapi import add_api_routes, api_route
+from notetiktok.data import ResourceType, resource_db
+from notetiktok.data.core import (add_favorite, add_follow, add_image,
+                                  add_video, favorite_db, follow_db)
+
+
+class ResourceService(APIRouter):
+    def __init__(self, prefix='/resource', *args, **kwargs):
+        super(ResourceService, self).__init__(prefix=prefix, *args, **kwargs)
+        add_api_routes(self)
+
+    @api_route('/get', description="get value")
+    def get_resource(self, page_no: int = 1, page_size: int = 10):
+        return resource_db.get_resource(start=page_size * (page_no - 1),
+                                        stop=page_size * page_no,
+                                        resource_type=ResourceType.VIDEO)
+
+    @api_route('/add/video', description="add video")
+    def add_video(self, *args, **kwargs):
+        add_video(*args, **kwargs)
+
+    @api_route('/add/image', description="add image")
+    def add_image(self, *args, **kwargs):
+        add_image(*args, **kwargs)
+
+
+class FavoriteService(APIRouter):
+    def __init__(self, prefix='/favorite', *args, **kwargs):
+        super(FavoriteService, self).__init__(prefix=prefix, *args, **kwargs)
+        add_api_routes(self)
+
+    @api_route('/gets', description="get value")
+    def get_favorite(self, *args, **kwargs):
+        return favorite_db.get_favor(*args, **kwargs)
+
+    # @api_route('/add', description="add video")
+    # def add_favorite(self, *args, **kwargs):
+    #     add_favorite(*args, **kwargs)
+
+
+class FollowService(APIRouter):
+    def __init__(self, prefix='/follow', *args, **kwargs):
+        super(FollowService, self).__init__(prefix=prefix, *args, **kwargs)
+        add_api_routes(self)
+
+    # @api_route('/get', description="get value")
+    # def get_follow(self, *args, **kwargs):
+    #     return follow_db.get_following(*args, **kwargs)
+
+    # @api_route('/add', description="add video")
+    # def add_follow(self, *args, **kwargs):
+    #     add_follow(*args, **kwargs)
