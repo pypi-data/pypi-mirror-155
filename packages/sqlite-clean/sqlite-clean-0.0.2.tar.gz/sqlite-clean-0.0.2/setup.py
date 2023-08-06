ae19044ad1a78947248dc8d707814cbe17a20699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:10 +0000

    Add package sqlite-clean/0.0.2/sqlite-clean-0.0.2.tar.gz

---
+++ packages/sqlite-clean/sqlite-clean-0.0.2.tar.gz/sqlite-clean-0.0.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sqlite_clean']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['SQLAlchemy>=1.4.37,<2.0.0']
+
+setup_kwargs = {
+    'name': 'sqlite-clean',
+    'version': '0.0.2',
+    'description': 'A SQLite data validation and cleanup tool.',
+    'long_description': None,
+    'author': 'd33bs',
+    'author_email': 'dave.bunten@cuanschutz.edu',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.2,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
