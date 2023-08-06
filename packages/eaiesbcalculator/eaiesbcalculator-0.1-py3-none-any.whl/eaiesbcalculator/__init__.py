b153abb8241123d600833562e19a87b0f69b265d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:53 +0000

    Add package eaiesbcalculator/0.1/eaiesbcalculator-0.1-py3-none-any.whl

---
+++ packages/eaiesbcalculator/eaiesbcalculator-0.1-py3-none-any.whl/eaiesbcalculator/__init__.py
@@ -0,0 +1,20 @@
+from matplotlib import mathtext
+
+
+def add_num(a,b):
+    return a+b
+
+def asubtract_num(a,b):
+    return a+b
+
+def multiply_num(a,b):
+    return a+b
+
+def divide_num(a,b):
+    return a+b
+
+def simpleInterest(p,n,r):
+    return ((p*n*r)/100)
+
+def compoundInterest(p,n,r):
+    return (p * mathtext.pow(1.0+r/100.0,n) - p)
