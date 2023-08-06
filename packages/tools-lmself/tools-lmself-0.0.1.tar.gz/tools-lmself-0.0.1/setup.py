75f872d7f3417492854d05d117813a2827a25175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:50 +0000

    Add package tools-lmself/0.0.1/tools-lmself-0.0.1.tar.gz

---
+++ packages/tools-lmself/tools-lmself-0.0.1.tar.gz/tools-lmself-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name='tools-lmself',
+    version='0.0.1',
+    author='upcoder',
+    auther_email='3468598586@qq.com',
+    description='简单四则运算pypi上传例子',
+    long_description='一个简单的四则运算 PyPI 上传例子',
+    long_description_content_type='text/markdown',
+    url='https://github.com/me/myproject',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
