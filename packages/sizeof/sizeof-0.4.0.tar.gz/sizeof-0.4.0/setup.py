67288b5c38c597d65a85417ea444e0295e15c05b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:59:13 +0000

    Add package sizeof/0.4.0/sizeof-0.4.0.tar.gz

---
+++ packages/sizeof/sizeof-0.4.0.tar.gz/sizeof-0.4.0/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup
+
+with open('README.rst', 'r') as fh:
+    long_description = fh.read().replace('.. include:: toc.rst\n\n', '')
+
+# The lines below can be parsed by ``docs/conf.py``.
+name = 'sizeof'
+version = '0.4.0'
+
+setup(
+    name=name,
+    version=version,
+    packages=[name,],
+    install_requires=[],
+    extras_require={
+        'docs': [
+            'sphinx~=4.2.0',
+            'sphinx-rtd-theme~=1.0.0'
+        ],
+        'test': [
+            'pytest~=7.0',
+            'pytest-cov~=3.0'
+        ],
+        'lint': [
+            'pylint~=2.14.0'
+        ],
+        'coveralls': [
+            'coveralls~=3.3.1'
+        ],
+        'publish': [
+            'setuptools~=62.0',
+            'wheel~=0.37',
+            'twine~=4.0'
+        ]
+    },
+    license='MIT',
+    url='https://github.com/lapets/sizeof',
+    author='Andrei Lapets',
+    author_email='a@lapets.io',
+    description='Simple function for determining the memory usage ' + \
+                'of common Python values and objects.',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+)
