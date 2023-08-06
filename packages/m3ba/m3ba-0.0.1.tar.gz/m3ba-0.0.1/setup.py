e0b9291bd1a3b044a811a173f7d63c2d9c70309e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:15 +0000

    Add package m3ba/0.0.1/m3ba-0.0.1.tar.gz

---
+++ packages/m3ba/m3ba-0.0.1.tar.gz/m3ba-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='m3ba',
+      version='0.0.1',
+      description='Python Functionalities to Analyse MD data by the M3B lab',
+      author='M3B',
+      author_email='help@m3b.com',
+      packages=find_packages(),
+     )
