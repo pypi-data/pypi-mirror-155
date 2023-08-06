892d27991018393cd87fb4c6fcdaff743eb4e9b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:08:30 +0000

    Add package nodejs-bin/16.15.1a1/nodejs_bin-16.15.1a1-py3-none-manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/nodejs-bin/nodejs_bin-16.15.1a1-py3-none-manylinux_2_12_x86_64.manylinux2010_x86_64.whl/nodejs/npx.py
@@ -0,0 +1,11 @@
+import os, sys
+from .node import run as run_node
+def run(args):
+    return run_node([
+        os.path.join(os.path.dirname(__file__), "lib/node_modules/npm/bin/npx-cli.js"),
+        *args
+    ])
+def main():
+    sys.exit(run(sys.argv[1:]))
+if __name__ == '__main__':
+    main()
