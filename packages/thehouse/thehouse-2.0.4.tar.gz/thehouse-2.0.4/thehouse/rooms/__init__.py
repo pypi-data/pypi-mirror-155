11ad4b224889a698cddc19f7b0a6b03fa5561b50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:35:36 +0000

    Add package thehouse/2.0.4/thehouse-2.0.4.tar.gz

---
+++ packages/thehouse/thehouse-2.0.4.tar.gz/thehouse-2.0.4/thehouse/rooms/__init__.py
@@ -0,0 +1,19 @@
+from .bedroom import Bedroom
+from .diningroom import Diningroom
+from .hall import Hall
+from .hallway import Hallway
+from .kitchen import Kitchen
+from .livingroom import Livingroom
+from .room import Room
+from .studio import Studio
+
+__all__ = [
+    "Bedroom",
+    "Diningroom",
+    "Hall",
+    "Hallway",
+    "Kitchen",
+    "Livingroom",
+    "Room",
+    "Studio",
+]
