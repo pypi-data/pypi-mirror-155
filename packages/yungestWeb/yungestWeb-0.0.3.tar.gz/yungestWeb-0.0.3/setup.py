d39aadf994ad000c9065cf1365808bc5874fafd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:04 +0000

    Add package yungestWeb/0.0.3/yungestWeb-0.0.3.tar.gz

---
+++ packages/yungestWeb/yungestWeb-0.0.3.tar.gz/yungestWeb-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'yungestWeb',         
+  packages = ['yungestWeb'],   
+  version = '0.0.3',      
+  license='MIT',        
+  description = 'TYPE YOUR DESCRIPTION HERE',   
+  author = 'yungestDev',                   
+  author_email = 'yungestmonsterhahaha@gmail.com',      
+  #url = 'https://github.com/user/reponame',   
+  #download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    
+  keywords = ['WebFramework', 'Framework'],   
+  install_requires=[            
+          
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3.10',      
+
+  ],
+)
