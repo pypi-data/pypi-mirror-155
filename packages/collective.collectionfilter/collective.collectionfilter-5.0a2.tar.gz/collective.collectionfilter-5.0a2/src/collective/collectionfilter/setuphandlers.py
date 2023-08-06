d07cbc00f944f6a7b37d55c8f5dd6a636bd35711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:54:11 +0000

    Add package collective.collectionfilter/5.0a2/collective.collectionfilter-5.0a2.tar.gz

---
+++ packages/collective.collectionfilter/collective.collectionfilter-5.0a2.tar.gz/collective.collectionfilter-5.0a2/src/collective/collectionfilter/setuphandlers.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from Products.CMFPlone.interfaces import INonInstallable
+from zope.interface import implementer
+
+
+@implementer(INonInstallable)
+class HiddenProfiles:
+    def getNonInstallableProfiles(self):
+        """Hide uninstall profile from site-creation."""
+        return [
+            "collective.collectionfilter:uninstall",
+            "collective.collectionfilter.tiles:uninstall",
+            "collective.collectionfilter.portlets:uninstall",
+        ]
