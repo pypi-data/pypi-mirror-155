dd2f917ece6e44649049818b9a64d6b7919d37a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:47 +0000

    Add package lingrex/1.3.0/lingrex-1.3.0.tar.gz

---
+++ packages/lingrex/lingrex-1.3.0.tar.gz/lingrex-1.3.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+setup(
+        name='lingrex',
+        description="Linguistic reconstruction with LingPy",
+        long_description=open('README.md').read(),
+        author='Johann-Mattis List and Robert Forkel',
+        url='https://github.com/lingpy/lingrex',
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Science/Research',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
+        ],
+        version='1.3.0',
+        packages=find_packages(where='src'),
+        package_dir={'': 'src'},
+        install_requires=['lingpy>=2.6.8'],
+        extras_require={
+            "test": ["pytest", "coverage", "pytest-mock", "pytest-cov"],
+            "dev": ["wheel", "twine", "tox", "black", "flake8"],
+        },
+        keywords="historical linguistics, computational linguistics, computer-assisted language comparison"
+        )
