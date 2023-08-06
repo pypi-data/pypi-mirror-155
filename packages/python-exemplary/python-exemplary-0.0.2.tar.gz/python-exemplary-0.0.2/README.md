729422ce7c57fa32f8798bc543318a39a675a5ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:21:44 +0000

    Add package python-exemplary/0.0.2/python-exemplary-0.0.2.tar.gz

---
+++ packages/python-exemplary/python-exemplary-0.0.2.tar.gz/python-exemplary-0.0.2/README.md
@@ -0,0 +1,26 @@
+# Exemplary
+
+A tool which provides extremely simple way to generate markdown documentation
+from actual source code examples!
+
+## Requirements
+
+The requirements for using this package are:
+
+- Python (>= 3.8).
+
+## Installation Guide
+
+Install the package as you would any other, using `pip`:
+
+``` sh
+> pip install exemplary
+# or use `pipx` for virtual-env isolation
+> pipx install exemplary
+```
+
+And that's it, you're good to go.
+
+## Usage Guide
+
+
---
+++ packages/python-exemplary/python-exemplary-0.0.2.tar.gz/python-exemplary-0.0.2/exemplary/generator.py
@@ -0,0 +1,19 @@
+import pathlib
+
+from .processors._all_builtins import get_all_builtin_processors
+from .processors._processor_pool import ProcessorPool
+from .scanner import scan
+
+_processors = ProcessorPool(get_all_builtin_processors())
+
+
+def generate(filepath: pathlib.Path) -> str:
+    """
+    Read a file and generate a markdown from the content.
+    """
+    compiled_markdown_segments = []
+    for segment in scan(filepath.read_text("utf-8")):
+        compiled_markdown_segments.append(
+            _processors[segment.processor].process(segment)
+        )
+    return "\n\n".join(compiled_markdown_segments)
