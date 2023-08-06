6c2ece7b0d278a2d8b806965fda9372f4183782e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:31:39 +0000

    Add package KEGE22/1.0/KEGE22-1.0.tar.gz

---
+++ packages/KEGE22/KEGE22-1.0.tar.gz/KEGE22-1.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='KEGE22',
+	version='1.0',
+	author='ShakeDLL',
+	author_email='repetskys@yandex.ru',
+	description='ege22',
+	packages=['KEGE22'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
