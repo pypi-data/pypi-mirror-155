68d13827f7d1e739314ed5f196622972f3a4498e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:07 +0000

    Add package tools-lmself/0.0.2/tools-lmself-0.0.2.tar.gz

---
+++ packages/tools-lmself/tools-lmself-0.0.2.tar.gz/tools-lmself-0.0.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name='tools-lmself',
+    version='0.0.2',
+    author='upcoder',
+    auther_email='',
+    description='工具包',
+    long_description='一些常用的小工具',
+    long_description_content_type='text/markdown',
+    url='',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
