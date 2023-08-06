d71deffa68efb90a81f1fae7162a5848272febed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:04:55 +0000

    Add package integer-pairing/0.5.0/integer-pairing-0.5.0.tar.gz

---
+++ packages/integer-pairing/integer-pairing-0.5.0.tar.gz/integer-pairing-0.5.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['integer_pairing']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'integer-pairing',
+    'version': '0.5.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Nejc Ševerkar',
+    'author_email': 'nseverkar@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/kuco23/integer-pairing/',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
