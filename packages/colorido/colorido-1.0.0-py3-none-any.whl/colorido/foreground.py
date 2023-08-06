7ec767eb35e1bd244f26dcd751ea914030fc47da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:24 +0000

    Add package colorido/1.0.0/colorido-1.0.0-py3-none-any.whl

---
+++ packages/colorido/colorido-1.0.0-py3-none-any.whl/colorido/foreground.py
@@ -0,0 +1,17 @@
+'''
+Foreground Constant Module
+--------------------------
+
+This module stores the foreground color constants used throughout the lib.
+
+@author: @italocampos
+'''
+
+BLACK = '\x1b[30m'
+RED = '\x1b[31m'
+GREEN = '\x1b[32m'
+YELLOW = '\x1b[33m'
+BLUE = '\x1b[34m'
+MAGENTA = '\x1b[35m'
+CYAN = '\x1b[36m'
+WHITE = '\x1b[37m'
