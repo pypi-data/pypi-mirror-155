1e0c93ad6d58ef8770bc8467a0e80a59c07f891d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:27 +0000

    Add package RFglovesPackages/0.1/RFglovesPackages-0.1.tar.gz

---
+++ packages/RFglovesPackages/RFglovesPackages-0.1.tar.gz/RFglovesPackages-0.1/setup.py
@@ -0,0 +1,10 @@
+# setup.py
+from setuptools import setup
+setup(name='RFglovesPackages',
+version='0.1',
+description='Testing installation of Package',
+author='Zhu Yanjin',
+author_email='582820136@qq.com',
+license='MIT',
+packages=['RFglovesPackages'],
+zip_safe=False)
