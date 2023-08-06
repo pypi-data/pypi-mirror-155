d418b6a21789be3223946737e9c36272a7f7de07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:31:21 +0000

    Add package webvid/0.0.3/webvid-0.0.3.tar.gz

---
+++ packages/webvid/webvid-0.0.3.tar.gz/webvid-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='webvid',
+    version='0.0.3',
+    packages=['webvid.IPL', 'webvid.PHUB', 'webvid.get_m3u8'],
+    # packages=['webvid'],
+    url='',
+    license='',
+    author='subrata',
+    author_email='subratamondal11@gmail.com',
+    description='download videos from multiple websites',
+    entry_points={
+        'console_scripts': [
+            'webvid = webvid:DownloadLink',
+        ],
+    },
+    install_requires=["selenium==4.2.0",
+                      "colorama==0.4.4",
+                      "ffmpeg-python==0.2.0",
+                      "m3u8==2.0.0",
+                      "requests==2.25.1"
+                      ]
+)
