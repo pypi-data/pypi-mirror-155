1cd7e911329fb012b7a991ec7834caea41e6015a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:20 +0000

    Add package pydocstringformatter/0.6.1/pydocstringformatter-0.6.1.tar.gz

---
+++ packages/pydocstringformatter/pydocstringformatter-0.6.1.tar.gz/pydocstringformatter-0.6.1/pydocstringformatter/formatting/__init__.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+__all__ = ["FORMATTERS", "Formatter"]
+
+
+from pydocstringformatter.formatting.base import Formatter
+from pydocstringformatter.formatting.formatter import (
+    BeginningQuotesFormatter,
+    CapitalizeFirstLetterFormatter,
+    ClosingQuotesFormatter,
+    FinalPeriodFormatter,
+    LineWrapperFormatter,
+    QuotesTypeFormatter,
+    SplitSummaryAndDocstringFormatter,
+    StripWhitespacesFormatter,
+)
+
+# The order of these formatters is important as they are called in order.
+# The order is currently:
+#   String manipulation such as adding extra new lines
+#   Determine if multi-line or single line and position quotes accordingly
+#   String manipulation in which being multi-line or single line matters
+FORMATTERS: list[Formatter] = [
+    StripWhitespacesFormatter(),
+    SplitSummaryAndDocstringFormatter(),
+    LineWrapperFormatter(),
+    BeginningQuotesFormatter(),
+    ClosingQuotesFormatter(),
+    CapitalizeFirstLetterFormatter(),
+    FinalPeriodFormatter(),
+    QuotesTypeFormatter(),
+]
