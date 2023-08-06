a8bb3bc22592900d9d9fa572d70a9215c8e0f41a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:46 +0000

    Add package cleaner-id/0.0.3/cleaner_id-0.0.3.tar.gz

---
+++ packages/cleaner-id/cleaner_id-0.0.3.tar.gz/cleaner_id-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='cleaner_id',
+  version='0.0.3',
+  description='Text Preprocessing untuk bahasa Indonesia',
+  long_description='',
+  url='',  
+  author='Tarangga Arief',
+  author_email='tarangga.arief@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='text preprocessing', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
