098c8fbca1eda8ffbe1d6476cc975a25ee876089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:12 +0000

    Add package dushizhenMath2/1.0/dushizhenMath2-1.0.tar.gz

---
+++ packages/dushizhenMath2/dushizhenMath2-1.0.tar.gz/dushizhenMath2-1.0/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+
+name='dushizhenMath2',	# 对外我们模块的名字
+
+version='1.0', # 版本号
+
+description='这是第一个对外发布的模块，测试哦',	#描述
+
+author='luoluo', # 作者
+
+author_email='910817507@qq.com',
+
+py_modules=['dushizhenMath2.demo1','dushizhenMath2.demo2'] # 要发布的模块
+
+)
