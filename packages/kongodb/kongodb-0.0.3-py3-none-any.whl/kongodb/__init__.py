4d18214b9d6af61557ad4beffc26ba7162bb5392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:27 +0000

    Add package kongodb/0.0.3/kongodb-0.0.3-py3-none-any.whl

---
+++ packages/kongodb/kongodb-0.0.3-py3-none-any.whl/kongodb/__init__.py
@@ -0,0 +1,8 @@
+#-----------------------------
+# -- kongodb --
+#-----------------------------
+
+from . import adapters, jsonquery, lib, database, cursor, exceptions
+
+# alias to Database
+kongodb = database.Database
