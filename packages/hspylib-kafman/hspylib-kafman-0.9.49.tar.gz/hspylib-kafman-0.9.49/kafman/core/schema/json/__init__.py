6201512b9f16dbca64405eea96a64b06176ee27d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:52 +0000

    Add package hspylib-kafman/0.9.49/hspylib-kafman-0.9.49.tar.gz

---
+++ packages/hspylib-kafman/hspylib-kafman-0.9.49.tar.gz/hspylib-kafman-0.9.49/kafman/core/schema/json/__init__.py
@@ -0,0 +1,13 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-kafman v0.9.49
+#
+# Package: main.kafman.core.schema.json
+"""Package initialization."""
+
+__all__ = [
+  'json_schema', 
+  'json_type', 
+  'property'
+]
+__version__ = '0.9.49'
