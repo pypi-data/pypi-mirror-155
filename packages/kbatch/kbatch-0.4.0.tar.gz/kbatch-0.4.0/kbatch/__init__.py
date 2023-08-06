3ac2f9e716213338460ca33f85309ecdff12498b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:33:06 +0000

    Add package kbatch/0.4.0/kbatch-0.4.0.tar.gz

---
+++ packages/kbatch/kbatch-0.4.0.tar.gz/kbatch-0.4.0/kbatch/__init__.py
@@ -0,0 +1,29 @@
+"""
+kbatch
+"""
+from ._core import (
+    list_jobs,
+    submit_job,
+    configure,
+    show_job,
+    logs,
+    list_pods,
+    logs_streaming,
+)
+from ._types import Job, CronJob
+from ._backend import make_job, make_cronjob
+
+
+__version__ = "0.4.0"
+
+__all__ = [
+    "__version__",
+    "Job",
+    "list_jobs",
+    "submit_job",
+    "make_job",
+    "make_cronjob",
+    "configure",
+    "show_job",
+    "logs",
+]
