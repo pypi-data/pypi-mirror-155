d42c6e615987121f54593e880c69db581f2216cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:17:36 +0000

    Add package git-acquire/0.1.0/git-acquire-0.1.0.tar.gz

---
+++ packages/git-acquire/git-acquire-0.1.0.tar.gz/git-acquire-0.1.0/src/git_acquire/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2021-2022 SanCloud Ltd
+# SPDX-License-Identifier: Apache-2.0
+
+__progname__ = "git-acquire"
+__description__ = "Efficient acquisition of a git branch/tag/commit"
+__version__ = "0.1.0"
