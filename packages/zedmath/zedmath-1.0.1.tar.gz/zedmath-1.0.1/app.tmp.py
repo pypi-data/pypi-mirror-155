2b0045e37f4fc70b8cae464518a0011eb8372de8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:21 +0000

    Add package zedmath/1.0.1/zedmath-1.0.1.tar.gz

---
+++ packages/zedmath/zedmath-1.0.1.tar.gz/zedmath-1.0.1/app.tmp.py
@@ -0,0 +1,11 @@
+# Import zedmath library
+import zedmath as zm
+
+# Create variables called "result" and assign its the result of zd.max method
+
+numbers = [1, 3, 25, 32.5, [21.6, "66.6", ["4", "6"]]]
+
+result = zm.max(numbers)
+
+# Print the result variable
+print(result)
