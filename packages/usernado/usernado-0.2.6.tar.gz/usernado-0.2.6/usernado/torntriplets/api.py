1d33f3a303c6ea59019d6998dfd0abc026752a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:02 +0000

    Add package usernado/0.2.6/usernado-0.2.6.tar.gz

---
+++ packages/usernado/usernado-0.2.6.tar.gz/usernado-0.2.6/usernado/torntriplets/api.py
@@ -0,0 +1,55 @@
+from typing import Any, Dict, Optional
+
+import tornado.escape
+import tornado.web
+
+from usernado.torntriplets.base import BaseHandler
+
+
+class BaseValidationError(ValueError):
+    pass
+
+
+class DataMalformedOrNotProvidedError(BaseValidationError):
+    pass
+
+
+class APIHandler(BaseHandler):
+    def get_json_argument(
+        self,
+        name: str,
+        default: Optional[str] = None,
+    ) -> str:
+        """Get json argument from current request.
+
+        :param name: Name of the argument
+        :type name: str
+        :param default: Default value for argument if not presented,
+         defaults to None
+        :type default: str, optional
+        :raises DataMalformedOrNotProvidedError:
+        :return: Particular JSON argument that comes with current request
+        :rtype: str
+        """
+        try:
+            raw_data = self.request.body.decode().replace("'", '"')
+        except Exception:
+            raise DataMalformedOrNotProvidedError
+        else:
+            json_data = tornado.escape.json_decode(raw_data)
+            return json_data.get(name, default)
+
+    def get_json_arguments(self) -> Dict[Any, Any]:
+        """Get all json arguments from current request.
+
+        :raises DataMalformedOrNotProvidedError:
+        :return: All JSON argument that comes with current request
+        :rtype: Dict[Any, Any]
+        """
+        try:
+            raw_data = self.request.body.decode().replace("'", '"')
+        except Exception:
+            raise DataMalformedOrNotProvidedError
+        else:
+            json_data = tornado.escape.json_decode(raw_data)
+            return json_data
