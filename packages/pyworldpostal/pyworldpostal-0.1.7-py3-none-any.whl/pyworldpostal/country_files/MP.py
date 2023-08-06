9e65510593a90be37583a1c65b78497862ba01d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:15:48 +0000

    Add package pyworldpostal/0.1.7/pyworldpostal-0.1.7-py3-none-any.whl

---
+++ packages/pyworldpostal/pyworldpostal-0.1.7-py3-none-any.whl/pyworldpostal/country_files/MP.py
@@ -0,0 +1,8 @@
+
+import json
+
+class PostalData:
+    def data(self):
+            return json.loads(d)
+
+d=r'[{"postal_code": "96951", "country_code": "MP", "admin_code1": "100", "admin_name1": "Rota", "admin_name2": null, "admin_name3": null, "place_name": "Rota"}, {"postal_code": "96950", "country_code": "MP", "admin_code1": "110", "admin_name1": "Saipan", "admin_name2": null, "admin_name3": null, "place_name": "Saipan"}, {"postal_code": "96952", "country_code": "MP", "admin_code1": "120", "admin_name1": "Tinian", "admin_name2": null, "admin_name3": null, "place_name": "Tinian"}]'
