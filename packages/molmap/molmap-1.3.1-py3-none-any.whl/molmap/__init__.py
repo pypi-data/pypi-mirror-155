917bca4b917fb28994ddf3ab19ca28e76c8318bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:19:47 +0000

    Add package molmap/1.3.1/molmap-1.3.1-py3-none-any.whl

---
+++ packages/molmap/molmap-1.3.1-py3-none-any.whl/molmap/__init__.py
@@ -0,0 +1,20 @@
+__version__ = '1.3.1'
+
+## to custom map object
+from .agg import AggMolMap
+
+## cpds features
+from .map import MolMap
+
+## aa sequence features
+from .seq import LocalAASeqMolMap
+from .seq import GlobAASeqMolMap
+
+
+## pdb features
+from .pdb import PDB2Fmap, PDB2Img
+
+
+
+from joblib import load as loadmap
+
