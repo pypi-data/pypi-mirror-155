c80edd42fcf362cce3db22054cdb23ffaed61caa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:48 +0000

    Add package cbutil/1.2.3/cbutil-1.2.3.tar.gz

---
+++ packages/cbutil/cbutil-1.2.3.tar.gz/cbutil-1.2.3/inc/cbutil/util/__init__.py
@@ -0,0 +1,11 @@
+import re
+
+from .iterutil import *
+from .path import *
+from .adaptor import *
+# from .util import *
+# from .url import *
+# from .pbar import *
+# from .printutil import *
+
+__all__ = list(filter(lambda s: not(re.match('_+',s) or re.search('_+$',s)), globals() ))
---
+++ packages/cbutil/cbutil-1.2.3.tar.gz/cbutil-1.2.3/setup.py
@@ -0,0 +1,19 @@
+import logging
+from setuptools import *
+
+
+setup(
+    name='cbutil',
+    version='1.2.3',
+    packages=find_namespace_packages('inc'),
+    package_dir={'cbutil': 'inc/cbutil'},
+    install_requires=[
+        'chardet'],
+    python_requires='>=3.8',
+
+    url='https://github.com/happyxianyu/cbutil-python',
+    license='Apache License 2.0',
+    author='happyxianyu',
+    author_email=' happyxianyu623@outlook.com',
+    description='Utility Library'
+)
