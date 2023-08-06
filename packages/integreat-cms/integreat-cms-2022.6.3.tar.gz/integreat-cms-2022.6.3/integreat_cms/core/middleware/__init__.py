0d358c806565494063733bdf4cb3c883ace6d551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:56 +0000

    Add package integreat-cms/2022.6.3/integreat-cms-2022.6.3.tar.gz

---
+++ packages/integreat-cms/integreat-cms-2022.6.3.tar.gz/integreat-cms-2022.6.3/integreat_cms/core/middleware/__init__.py
@@ -0,0 +1,6 @@
+"""
+This package contains custom middlewares, see :doc:`topics/http/middleware` and :doc:`ref/middleware`.
+"""
+from .access_control_middleware import AccessControlMiddleware
+from .region_middleware import RegionMiddleware
+from .timezone_middleware import TimezoneMiddleware
