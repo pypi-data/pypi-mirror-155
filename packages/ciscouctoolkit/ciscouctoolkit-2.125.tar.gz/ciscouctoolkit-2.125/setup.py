88916b3d051e2f6f6a5bb32d5b9ed0e1d063402d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:20 +0000

    Add package ciscouctoolkit/2.125/ciscouctoolkit-2.125.tar.gz

---
+++ packages/ciscouctoolkit/ciscouctoolkit-2.125.tar.gz/ciscouctoolkit-2.125/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+setup(
+    name="ciscouctoolkit",
+    version="2.125",
+    author="Michael Ralston",
+    author_email="michaelaaralston2@gmail.com",
+    description="Cisco CUCM AXL Library, PAWS, DIME, IMP AXL, Log Collection.",
+    license="MIT",
+    url="https://github.com/MichaelRalston98/ciscouctoolkit",
+    keywords=["Cisco", "Call Manager", "CUCM", "AXL", "VoIP"],
+    packages=["UCToolkit"],
+    package_data={"UCToolkit": ["*.wsdl", "*.xsd", "CUCM/*/*/*", "IMP/*/*/*", "paws/*"],},
+    install_requires=[
+        'xmltodict',
+        'requests',
+        'zeep',
+        'lxml',
+        'urllib3',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
