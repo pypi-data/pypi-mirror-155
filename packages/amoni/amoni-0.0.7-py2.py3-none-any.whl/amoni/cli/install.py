f848bbf3e260e265640c540e73ec2d8b8250ed6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:48:34 +0000

    Add package amoni/0.0.7/amoni-0.0.7-py2.py3-none-any.whl

---
+++ packages/amoni/amoni-0.0.7-py2.py3-none-any.whl/amoni/cli/install.py
@@ -0,0 +1,45 @@
+# SPDX-License-Identifier: MIT
+#
+# Copyright (c) 2021 The Amoni project team members listed at
+# https://github.com/anvilistas/amoni/graphs/contributors
+#
+# This software is published at https://github.com/anvilistas/amoni
+from pathlib import Path
+
+import typer
+
+from .. import api
+from . import echo
+
+__version__ = "0.0.7"
+
+
+cmd = typer.Typer()
+
+
+@cmd.command()
+def app(
+    url: str = typer.Argument(..., help="Clone URL for the Anvil app"),
+    name: str = typer.Argument(..., help="Name of the Anvil app"),
+):
+    """Fetch an anvil app and set it as the app to run"""
+    api.add_submodule(url, Path("app", name), name)
+    echo.progress(f"Added {name} as a submodule in the app directory")
+    api.set_app(name)
+    echo.progress(f"Updated config to set {name} as the app")
+    api.generate_table_stubs(name)
+    echo.progress(f"Created table definitions in {api.TABLE_STUB_FILE}")
+    echo.done()
+
+
+@cmd.command()
+def dependency(
+    url: str = typer.Argument(..., help="Clone URL for the Anvil app"),
+    name: str = typer.Argument(..., help="Name of the dependency app"),
+    id: str = typer.Argument(..., help="App ID of the dependency"),
+):
+    """Fetch an anvil app and set it as a dependency"""
+    api.add_submodule(url, Path("app", name), name)
+    echo.progress(f"Added {name} as a submodule in the app directory")
+    api.set_dependency(id, name)
+    echo.done()
