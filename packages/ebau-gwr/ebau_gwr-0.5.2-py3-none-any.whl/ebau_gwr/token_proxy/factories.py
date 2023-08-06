5e2e141d03804cedcc970a917e70d40bc2f01a97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:37 +0000

    Add package ebau-gwr/0.5.2/ebau_gwr-0.5.2-py3-none-any.whl

---
+++ packages/ebau-gwr/ebau_gwr-0.5.2-py3-none-any.whl/ebau_gwr/token_proxy/factories.py
@@ -0,0 +1,14 @@
+from factory import Faker
+from factory.django import DjangoModelFactory
+
+from . import models
+
+
+class HousingStatCredsFactory(DjangoModelFactory):
+    owner = "admin"
+    username = Faker("first_name")
+    password = Faker("password")
+    municipality = Faker("random_int")
+
+    class Meta:
+        model = models.HousingStatCreds
