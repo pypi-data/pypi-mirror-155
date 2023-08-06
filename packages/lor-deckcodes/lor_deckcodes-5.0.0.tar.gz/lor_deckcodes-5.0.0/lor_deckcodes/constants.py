b411e92734391fb0d5036b2ac889c4f853b6cd1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:08:14 +0000

    Add package lor-deckcodes/5.0.0/lor_deckcodes-5.0.0.tar.gz

---
+++ packages/lor-deckcodes/lor_deckcodes-5.0.0.tar.gz/lor_deckcodes-5.0.0/lor_deckcodes/constants.py
@@ -0,0 +1,26 @@
+CURRENT_FORMAT_VERSION = 21
+SUPPORTED_VERSIONS = (17, 18, 19, 20, 21)
+faction_mapping = {
+    0: 'DE',
+    1: 'FR',
+    2: 'IO',
+    3: 'NX',
+    4: 'PZ',
+    5: 'SI',
+    6: 'BW',
+    7: 'SH',
+    9: 'MT',
+    10: 'BC',
+    12: 'RU',
+    'DE': 0,
+    'FR': 1,
+    'IO': 2,
+    'NX': 3,
+    'PZ': 4,
+    'SI': 5,
+    'BW': 6,
+    'SH': 7,
+    'MT': 9,
+    'BC': 10,
+    'RU': 12,
+}
