9d746d12d5236092080aa2a040517577e6016fe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:53:02 +0000

    Add package scupy/0.0.1/scupy-0.0.1.tar.gz

---
+++ packages/scupy/scupy-0.0.1.tar.gz/scupy-0.0.1/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 18 18:07:05 2022
+@author:
+Зайцева Дарья
+"""
+
+from setuptools import setup, find_packages
+long_description= '''Library for the 3 semester'''
+setup(name='scupy',
+      version='0.0.1',
+      url='https://github.com/dashkazaitseva',
+      license='MIT',
+      description='',
+      zip_safe=False)
