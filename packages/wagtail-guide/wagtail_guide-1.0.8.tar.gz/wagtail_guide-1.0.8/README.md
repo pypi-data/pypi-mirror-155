9f5b9cf58dcec5920fc70384d4183c95fe29adb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:01 +0000

    Add package wagtail-guide/1.0.8/wagtail_guide-1.0.8.tar.gz

---
+++ packages/wagtail-guide/wagtail_guide-1.0.8.tar.gz/wagtail_guide-1.0.8/README.md
@@ -0,0 +1,43 @@
+# Wagtail Guide
+![tests](https://github.com/kevinhowbrook/wagtailguide/workflows/Test/badge.svg) [![codecov](https://codecov.io/gh/kevinhowbrook/wagtailguide/branch/master/graph/badge.svg?token=K2XKBfubBh)](https://codecov.io/gh/kevinhowbrook/wagtailguide)
+
+## What is it?
+
+The Wagtail Guide app adds functionality for creating and editing a guide for editors and publishers to help them create good content.
+
+## Installation
+
+Wagtailguide has a pypi package and can be installed with:
+
+```
+pip install wagtail-guide
+```
+
+After installing, add it to your settings file along with `wagtail.contrib.settings`, the settings inclusion should be placed with your other wagtail.contrib libraries:
+
+```
+INSTALLED_APPS = [
+    ...
+    'wagtail_guide',
+    'wagtail.contrib.settings',
+]
+```
+
+## Dependencies
+Wagtail Guide requires `wagtail>=2.16`
+
+## Usage
+To edit the guide, the user needs to be an admin, or have the `Manage editor guide` 'change' value checked in their user group permissions.
+
+### Editing the guide
+To edit the guide, from the left hand menu open settings > then click 'Manage editor guide'. Edit your content and then save.
+
+![guide edit](https://i.imgur.com/ZGRlu3I.png)
+
+#### Guide navigation menu
+If more than one 'heading' blocks are added to the content, an automatic menu will be displayed at the top of the guide under the heading 'Contents'
+
+### Viewing the guide
+Once logged in, a new menu icon towards the bottom of the left hand menu will be visible labeled as 'Editor guide':
+
+![guide view](https://i.imgur.com/HbCVvXy.png)
---
+++ packages/wagtail-guide/wagtail_guide-1.0.8.tar.gz/wagtail_guide-1.0.8/wagtail_guide/models.py
@@ -0,0 +1,121 @@
+from django.db import models
+from modelcluster.models import ClusterableModel
+from wagtail import VERSION as WAGTAIL_VERSION
+from wagtail.contrib.settings.models import BaseSetting, register_setting
+from wagtail.embeds.blocks import EmbedBlock
+from wagtail.images.blocks import ImageChooserBlock
+
+if WAGTAIL_VERSION >= (3, 0):
+    from wagtail import blocks
+    from wagtail.admin.panels import FieldPanel
+    from wagtail.fields import StreamField
+else:
+    from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+    from wagtail.core import blocks
+    from wagtail.core.fields import StreamField
+
+
+class ImageBlock(blocks.StructBlock):
+    image = ImageChooserBlock()
+    caption = blocks.CharBlock(required=False)
+
+    class Meta:
+        icon = "image"
+        template = "wagtail_guide/streamfield/blocks/image_block.html"
+
+
+class QuoteBlock(blocks.StructBlock):
+    quote = blocks.CharBlock(classname="title")
+    attribution = blocks.CharBlock(required=False)
+
+    class Meta:
+        icon = "openquote"
+        template = "wagtail_guide/streamfield/blocks/quote_block.html"
+
+
+class PullQuoteBlock(blocks.StructBlock):
+    pull_quote = blocks.CharBlock(classname="title")
+
+    class Meta:
+        icon = "openquote"
+        template = "wagtail_guide/streamfield/blocks/pull_quote_block.html"
+
+
+class IframeBlock(blocks.StructBlock):
+    title = blocks.CharBlock(required=False)
+    source = blocks.URLBlock()
+
+    class Meta:
+        icon = "code"
+        template = "wagtail_guide/streamfield/blocks/iframe_block.html"
+
+
+class EmbedTitleBlock(blocks.StructBlock):
+    title = blocks.CharBlock(required=False)
+    embed = EmbedBlock()
+
+    class Meta:
+        icon = "media"
+        template = "wagtail_guide/streamfield/blocks/embed_block.html"
+
+
+class VideoBlock(blocks.StructBlock):
+    video = EmbedBlock(required=False, label="Video URL")
+
+    class Meta:
+        icon = "media"
+        template = "wagtail_guide/streamfield/blocks/video_block.html"
+        label = "Video"
+
+
+class GuideBlock(blocks.StreamBlock):
+    heading = blocks.CharBlock(
+        classname="full title",
+        icon="title",
+        template="wagtail_guide/streamfield/blocks/heading_block.html",
+    )
+
+    paragraph = blocks.RichTextBlock(
+        features=[
+            "h2",
+            "h3",
+            "h4",
+            "h5",
+            "h6",
+            "bold",
+            "italic",
+            "link",
+            "ul",
+            "ol",
+            "hr",
+        ]
+    )
+    image = ImageBlock()
+    quote = QuoteBlock()
+    pull_quote = PullQuoteBlock()
+    embed = EmbedTitleBlock()
+    video = VideoBlock()
+
+    class Meta:
+        template = "wagtail_guide/streamfield/stream_block.html"
+
+
+@register_setting(icon="help")
+class EditorGuide(BaseSetting, ClusterableModel):
+    information_text = models.TextField(
+        blank=True, help_text="Add a leading information paragraph explaining the guide"
+    )
+    sections = (
+        StreamField(GuideBlock(required=False), blank=True, use_json_field=True)
+        if WAGTAIL_VERSION >= (3, 0)
+        else StreamField(GuideBlock(required=False), blank=True)
+    )
+
+    panels = (
+        [FieldPanel("information_text"), FieldPanel("sections")]
+        if WAGTAIL_VERSION >= (3, 0)
+        else [FieldPanel("information_text"), StreamFieldPanel("sections")]
+    )
+
+    class Meta:
+        verbose_name = "Manage Editor Guide"
