c8499e7b728063caa9d3ba4e68acccbd91a2fa46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:33 +0000

    Add package scmrepo/0.0.25/scmrepo-0.0.25.tar.gz

---
+++ packages/scmrepo/scmrepo-0.0.25.tar.gz/scmrepo-0.0.25/noxfile.py
@@ -0,0 +1,38 @@
+"""Automation using nox.
+"""
+import glob
+
+import nox
+
+nox.options.reuse_existing_virtualenvs = True
+nox.options.sessions = "lint", "tests"
+locations = "scmrepo", "tests"
+
+
+@nox.session(python=["3.7", "3.8", "3.9", "3.10", "pypy3.8", "pypy3.9"])
+def tests(session: nox.Session) -> None:
+    session.install("-e", ".[dev]")
+    session.run("pytest", *session.posargs)
+
+
+@nox.session
+def lint(session: nox.Session) -> None:
+    session.install("pre-commit")
+    session.install("-e", ".[dev]")
+
+    if session.posargs:
+        args = session.posargs + ["--all-files"]
+    else:
+        args = ["--all-files", "--show-diff-on-failure"]
+
+    session.run("pre-commit", "run", *args)
+    session.run("python", "-m", "mypy")
+    session.run("python", "-m", "pylint", *locations)
+
+
+@nox.session
+def build(session: nox.Session) -> None:
+    session.install("build", "setuptools", "twine")
+    session.run("python", "-m", "build")
+    dists = glob.glob("dist/*")
+    session.run("twine", "check", *dists, silent=True)
