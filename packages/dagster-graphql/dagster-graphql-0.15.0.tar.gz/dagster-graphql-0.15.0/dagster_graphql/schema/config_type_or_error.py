8526e1821c6c8be1b68225f1d09dbed505907be1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:56 +0000

    Add package dagster-graphql/0.15.0/dagster-graphql-0.15.0.tar.gz

---
+++ packages/dagster-graphql/dagster-graphql-0.15.0.tar.gz/dagster-graphql-0.15.0/dagster_graphql/schema/config_type_or_error.py
@@ -0,0 +1,26 @@
+# pylint: disable=missing-graphene-docstring
+import graphene
+
+from .config_types import (
+    GrapheneCompositeConfigType,
+    GrapheneEnumConfigType,
+    GrapheneRegularConfigType,
+)
+from .errors import (
+    GrapheneConfigTypeNotFoundError,
+    GraphenePipelineNotFoundError,
+    GraphenePythonError,
+)
+
+
+class GrapheneConfigTypeOrError(graphene.Union):
+    class Meta:
+        types = (
+            GrapheneEnumConfigType,
+            GrapheneCompositeConfigType,
+            GrapheneRegularConfigType,
+            GraphenePipelineNotFoundError,
+            GrapheneConfigTypeNotFoundError,
+            GraphenePythonError,
+        )
+        name = "ConfigTypeOrError"
---
+++ packages/dagster-graphql/dagster-graphql-0.15.0.tar.gz/dagster-graphql-0.15.0/dagster_graphql/schema/roots/assets.py
@@ -0,0 +1,25 @@
+# pylint: disable=missing-graphene-docstring
+import graphene
+
+from ..errors import GrapheneAssetNotFoundError, GraphenePythonError
+from ..pipelines.pipeline import GrapheneAsset
+from ..util import non_null_list
+
+
+class GrapheneAssetConnection(graphene.ObjectType):
+    nodes = non_null_list(GrapheneAsset)
+
+    class Meta:
+        name = "AssetConnection"
+
+
+class GrapheneAssetsOrError(graphene.Union):
+    class Meta:
+        types = (GrapheneAssetConnection, GraphenePythonError)
+        name = "AssetsOrError"
+
+
+class GrapheneAssetOrError(graphene.Union):
+    class Meta:
+        types = (GrapheneAsset, GrapheneAssetNotFoundError)
+        name = "AssetOrError"
