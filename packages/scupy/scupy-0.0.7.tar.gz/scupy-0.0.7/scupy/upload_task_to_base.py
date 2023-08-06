a9cbf748d5874a6815df70f0cbb6486c61b5e788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:31:33 +0000

    Add package scupy/0.0.7/scupy-0.0.7.tar.gz

---
+++ packages/scupy/scupy-0.0.7.tar.gz/scupy-0.0.7/scupy/upload_task_to_base.py
@@ -0,0 +1,29 @@
+# Это программа для разработки, если вы случайно тут, то выйдите, пожалуйста, и ничего не ломайте
+# ID меняется вручную, молю всех не продолбить это (я перед экзом поревьюю, но не обещаю)
+# если вы не уверены в задаче, но хотите ее добавить в базу, пишите тем, кто знает тервер чтобы проверили
+# если вы считаете, что это говнокод и соберете лучше -- пишите свою либу и шерьте со всеми
+# если вы пришли с фичреквестом -- пишите dashkaz или rozyev23
+
+import json
+import pathlib
+
+my_own_task = {
+    "id" : 1,
+    "unit": "test",
+    "task_text": "сложите два числа",
+    "task_solution_code_analytics":
+        """
+        a = 1
+        b = 5
+        print(a + b)
+        """,
+    "task_solution_code":
+        """
+        a = 1
+        b = 5
+        print(sum(a, b))
+        """
+}
+new_task = json.dumps(my_own_task, ensure_ascii=False)
+with open(pathlib.Path(pathlib.Path(pathlib.Path.cwd(), "task_struct", "tasks_base.json")), "a", encoding="UTF8") as f:
+    f.write(new_task)
