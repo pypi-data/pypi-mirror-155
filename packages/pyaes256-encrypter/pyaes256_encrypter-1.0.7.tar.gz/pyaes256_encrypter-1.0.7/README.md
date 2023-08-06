bc8f9af67fea64b68e7ad78fcd72cc9ce9d1fd52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:48 +0000

    Add package pyaes256-encrypter/1.0.7/pyaes256_encrypter-1.0.7.tar.gz

---
+++ packages/pyaes256-encrypter/pyaes256_encrypter-1.0.7.tar.gz/pyaes256_encrypter-1.0.7/README.md
@@ -0,0 +1,23 @@
+# pyaes256_encrypter
+A package to simplify the use of AES-256 encryption with random initialization vector.
+
+## Install
+```
+pip install pyaes256-encrypter
+```
+
+## Usage
+~~~python
+from pyaes256_encrypter import encode_text, decode_text
+
+# text to be encrypted
+text = 'hello world'
+# encryption key
+key = 'key'
+
+
+# RANDOM ENCODED e.g. 'Dx3dCTUSXzzM8wn1L/+NHVbyaDxZFpdqe+SN2NVZgfE='
+encoded = encode_text(text, key)
+# 'hello world'
+decoded = decode_text(encoded, key)
+~~~
---
+++ packages/pyaes256-encrypter/pyaes256_encrypter-1.0.7.tar.gz/pyaes256_encrypter-1.0.7/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pyaes256_encrypter']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cryptography>=37.0.0']
+
+setup_kwargs = {
+    'name': 'pyaes256-encrypter',
+    'version': '1.0.7',
+    'description': 'A package to simplify the use of AES-256 encryption with random initialization vector.',
+    'long_description': "# pyaes256_encrypter\nA package to simplify the use of AES-256 encryption with random initialization vector.\n\n## Install\n```\npip install pyaes256-encrypter\n```\n\n## Usage\n~~~python\nfrom pyaes256_encrypter import encode_text, decode_text\n\n# text to be encrypted\ntext = 'hello world'\n# encryption key\nkey = 'key'\n\n\n# RANDOM ENCODED e.g. 'Dx3dCTUSXzzM8wn1L/+NHVbyaDxZFpdqe+SN2NVZgfE='\nencoded = encode_text(text, key)\n# 'hello world'\ndecoded = decode_text(encoded, key)\n~~~\n",
+    'author': 'Maicon Renildo',
+    'author_email': 'maicon.renildo1@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/MaiconRenildo/pyaes256_encrypter',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6.2,<4.0',
+}
+
+
+setup(**setup_kwargs)
