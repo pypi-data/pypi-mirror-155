1e7b729c7ea5c393ba2bf7ba4c9d99d2007397c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:25 +0000

    Add package nhapp/0.0.3/nhapp-0.0.3.tar.gz

---
+++ packages/nhapp/nhapp-0.0.3.tar.gz/nhapp-0.0.3/setup.py
@@ -0,0 +1,23 @@
+import os
+import pandas
+from setuptools import find_packages, setup
+import setuptools
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
+    README = readme.read()
+
+setuptools.setup(
+    name="nhapp",
+    version="0.0.3",
+    author="Nguyen Manh Dung ",
+    author_email="123456zvn2@gmail.com",
+    description="inspire your love ",
+    long_description="Make you feel the strong of love",
+    long_description_content_type="text/markdown",
+    url="https://github.com/mwpnava/Python-Code/tree/master/My_own_Python_package",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
