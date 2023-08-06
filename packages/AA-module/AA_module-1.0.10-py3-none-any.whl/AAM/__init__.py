b16d309b15b5a0e02c701beefdbba6cd3dd276cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:32:15 +0000

    Add package AA-module/1.0.10/AA_module-1.0.10-py3-none-any.whl

---
+++ packages/AA-module/AA_module-1.0.10-py3-none-any.whl/AAM/__init__.py
@@ -0,0 +1,11 @@
+from AAM import CAA_class
+from AAM import loading_bar_class
+from AAM import OAA_class
+from AAM import plots_class
+from AAM import RBOAA_class
+from AAM import synthetic_data_class
+from AAM import TSAA_class
+from AAM import AA_class
+from AAM import AA_result_class
+
+AA = CAA_class.AA()
