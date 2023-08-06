2724c79473fdf78f0179a9367e18ed87155505a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:27 +0000

    Add package OPML-Methods/0.1.5/OPML_Methods-0.1.5.tar.gz

---
+++ packages/OPML-Methods/OPML_Methods-0.1.5.tar.gz/OPML_Methods-0.1.5/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='OPML_Methods',
+      version='0.1.5',
+      description='Too much math methods',
+      packages=['OPML_Methods'],
+      author_email='alexekozikl@mail.ru',
+      zip_safe=False)
