e19c55c2e311f51e9ec9854bdedbd281ebc6a7c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:15:58 +0000

    Add package pyscs/1.1.1/pyscs-1.1.1-py3-none-any.whl

---
+++ packages/pyscs/pyscs-1.1.1-py3-none-any.whl/pyscs/atom.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+
+
+class AtomSignal(BaseModel):
+    timeout: int = 0          # 超时时间， 单位:s
+    restart: bool = False            # 如果超时了是否重启
+    notice: bool = False             # 如果超时了是否报警通知
+    continuityInterval:int = 0   # 下次报警时间
