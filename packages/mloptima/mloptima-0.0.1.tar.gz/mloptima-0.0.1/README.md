964d6018d19a5650182ddb165dd6085d85c87bc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:31:08 +0000

    Add package mloptima/0.0.1/mloptima-0.0.1.tar.gz

---
+++ packages/mloptima/mloptima-0.0.1.tar.gz/mloptima-0.0.1/README.md
@@ -0,0 +1,10 @@
+# ML_opt
+## Repository of optimization methods in machine learning.
+## Machine Learning models and valuable visualization.
+1. Two-variable function extrema finding and visualization implementation. Use User interface notebook twovarextremas Jupyter Notebook with downloaded modules.
+2. One dimensional optimization(minimization) methods implementation and visualization. Use User interface notebook onedimensionaloptimization Jupyter Notebook with downloaded modules.
+3. Gradients optimization(minimization) methods and visualization implementation. Use User interface notebook gradients Jupyter Notebook with downloaded modules.
+4. Regression(Linear, Polynomial, Exponential) models and visualization implementation with StochasticGradientDescent/NormalEquation solvers. Use User interface notebook regression colab Jupyter Notebook with downloaded modules.
+5. Optimization with equality/inequality constraints. Interior point methods and visualization implementation. Use User interface notebook interiorpoint Jupyter Notebook with downloaded modules.
+6. Classification. Implementation of LogisticRegression(Ridge, Lasso), LogisticRegression with RBF kernel function, SVMs classifiers. Use User Interface notebook classification notebook colab with downloaded modules.
+8. Stochastic optimization. Implementation of Stochastic gradient descent, Support vector classifier on 2 classes optimized by SGD, simulated annealing algorithm for function minimization, genetic algorithm for function minimization. Built in visualization implemented.
---
+++ packages/mloptima/mloptima-0.0.1.tar.gz/mloptima-0.0.1/coreml/twovarextremas/find_extrema.py
@@ -0,0 +1,89 @@
+import sympy
+from .plotting_3d import make_3d_plot, make_3d_plot_lagrange, make_level_lines_plot
+from .utils_twovarextremas import preproc_fun, check_constraints, create_xyz
+from .extrema_methods import find_extrema, find_extrema_lagrange
+
+
+def find_local_2var(vars_, fun_anl, constr=None, plot=False, plot_ll=False):
+    """Finds local extrema of a function of two variables.
+
+    Positional arguments:
+    vars_ -- list of variable names
+    fun_anl -- function analytic form
+
+    Keyword arguments:
+    constr -- list (list of tuples) of function constraints(default=None)
+    plot -- Draw plot with surface and points(default=False)
+    plot_ll -- Draw level lines(default=False)
+    """
+    var1, var2 = sympy.symbols(' '.join(vars_))
+    fun = preproc_fun(fun_anl)
+
+    if constr is None:
+        m_points = sympy.solve([fun.diff(var1), fun.diff(var2)], dict=True)
+    else:
+        m_points = check_constraints(sympy.solve([fun.diff(var1), fun.diff(var2)], dict=True), constr, vars_)
+
+    if m_points:
+        for m in m_points:
+            coord1, coord2, label = find_extrema(fun, var1, var2, m)
+            print(f'Point: ({float(coord1)}, {float(coord2)}) | Z: {float(fun.subs(m))} | Extrema: {label}')
+    else:
+        print('No extrema has been found!')
+        need_draw = input('Draw graphs? (yes/no): ').lower()
+        if not(need_draw and need_draw != 'no'):
+            return None
+
+    if plot or plot_ll:
+        X, Y, Z = create_xyz(-10, 10, 0.5, constr, fun, var1, var2)
+        if plot:
+            make_3d_plot(X, Y, Z, m_points, var1, var2, fun)
+        if plot_ll:
+            make_level_lines_plot(X, Y, Z, m_points, var1, var2)
+    return None
+
+
+def find_lagrange_2var(vars_, fun_anl, fun_constr, constr=None, plot=False, plot_ll=False):
+    """Finds extrema of a function of two variables by Lagrange method.
+
+    Positional arguments:
+    vars_ -- list of variable names
+    fun_anl -- function analytic form
+    fun_constr -- limiting function analytic form
+
+    Keyword arguments:
+    constr -- list (list of tuples) of function constraints(default=None)
+    plot -- Draw plot with surface and points(default=False)
+    plot_ll -- Draw level lines(default=False)
+    """
+    l, var1, var2 = sympy.symbols('lambda ' + ' '.join(vars_))
+    fun = preproc_fun(fun_anl)
+    fun_c = preproc_fun(fun_constr)
+    lagrange = fun + l * fun_c
+
+    if constr is None:
+        m_points = sympy.solve([lagrange.diff(var1), lagrange.diff(var2), fun_c], dict=True)
+    else:
+        m_points = check_constraints(sympy.solve([lagrange.diff(var1), lagrange.diff(var2), fun_c], dict=True),
+                                     # Искусственное ограничение на лямбду
+                                     [(float('-inf'), float('+inf'))] + constr,
+                                     vars_)
+
+    if m_points:
+        for m in m_points:
+            coord1, coord2, label = find_extrema_lagrange(fun_c, lagrange, m, var1, var2)
+            print(f'Point: ({float(coord1)}, {float(coord2)}) | Z: {float(fun.subs(m))} | Extrema: {label}')
+    else:
+        print('No extrema has been found! ')
+        need_draw = input('Draw graphs? (yes/no): ').lower()
+        if not(need_draw and need_draw != 'no'):
+            return None
+
+    if plot or plot_ll:
+        X, Y, Z = create_xyz(-10, 10, 0.5, constr, fun, var1, var2)
+        _, _, Z_c = create_xyz(-10, 10, 0.5, constr, fun_c, var1, var2)
+        if plot:
+            make_3d_plot_lagrange(X, Y, Z, Z_c, m_points, var1, var2, fun)
+        if plot_ll:
+            make_level_lines_plot(X, Y, Z, m_points, var1, var2, Z_c)
+    return None
