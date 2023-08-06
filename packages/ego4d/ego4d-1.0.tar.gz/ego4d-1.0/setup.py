d7b61815d820e8628b90d94776b8fa51cf7c0431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:02:46 +0000

    Add package ego4d/1.0/ego4d-1.0.tar.gz

---
+++ packages/ego4d/ego4d-1.0.tar.gz/ego4d-1.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.
+
+from setuptools import find_packages, setup
+
+setup(
+    name="ego4d",
+    version="1.0",
+    author="FAIR",
+    author_email='info@ego4d-data.org',
+    url="https://github.com/facebookresearch/Ego4d",
+    install_requires=[
+        "boto3",
+        "tqdm",
+        # "av",
+        # "torch",
+        # "torchvision",
+        # "pytorch_lightning",
+        # "matplotlib",
+        # "simplejson",
+        # "matplotlib",
+        # "pandas",
+    ],
+    tests_require=[
+        "pytest",
+        "moto",
+    ],
+    packages=find_packages("ego4d/cli", exclude=["features", "tests"]),
+    # packages=find_packages(exclude=("configs", "tests")),
+)
