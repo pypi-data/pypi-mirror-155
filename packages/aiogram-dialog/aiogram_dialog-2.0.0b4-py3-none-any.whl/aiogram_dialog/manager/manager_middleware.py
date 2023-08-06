6a0a3885bc4004f4d498f1b68fffe89257b9b0d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:21 +0000

    Add package aiogram-dialog/2.0.0b4/aiogram_dialog-2.0.0b4-py3-none-any.whl

---
+++ packages/aiogram-dialog/aiogram_dialog-2.0.0b4-py3-none-any.whl/aiogram_dialog/manager/manager_middleware.py
@@ -0,0 +1,35 @@
+from typing import Any, Callable, Dict, Awaitable, Union
+
+from aiogram.dispatcher.middlewares.base import BaseMiddleware
+from aiogram.types import Update
+
+from .manager import ManagerImpl
+from .protocols import DialogManager, DialogRegistryProto
+from ..context.events import DialogUpdateEvent, ChatEvent
+
+MANAGER_KEY = "dialog_manager"
+
+
+class ManagerMiddleware(BaseMiddleware):
+    def __init__(self, registry: DialogRegistryProto):
+        super().__init__()
+        self.registry = registry
+
+    async def __call__(
+            self,
+            handler: Callable[[Union[Update, DialogUpdateEvent], Dict[str, Any]], Awaitable[Any]],
+            event: ChatEvent,
+            data: Dict[str, Any],
+    ) -> Any:
+        data[MANAGER_KEY] = ManagerImpl(
+            event=event,
+            registry=self.registry,
+            data=data,
+        )
+
+        try:
+            return await handler(event, data)
+        finally:
+            manager: DialogManager = data.pop(MANAGER_KEY, None)
+            if manager:
+                await manager.close_manager()
