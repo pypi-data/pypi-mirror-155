d2476f830325e6bdefcc6f8fd00f368291cbd8a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:55 +0000

    Add package redditflow/0.1.2/redditflow-0.1.2.tar.gz

---
+++ packages/redditflow/redditflow-0.1.2.tar.gz/redditflow-0.1.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+install_requires = ["sentence_transformers", "praw"]
+setuptools.setup(
+    name="redditflow",
+    version="0.1.2",
+    author="Abhijith Neil Abraham",
+    author_email="abhijithneilabrahampk@gmail.com",
+    description="Data Curation over Time",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT License',
+    url="https://github.com/nfflow/redditflow",
+    install_requires=install_requires,
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    include_package_data=True
+)
