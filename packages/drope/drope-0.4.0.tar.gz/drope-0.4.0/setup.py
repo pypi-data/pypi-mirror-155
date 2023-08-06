9e9de55601d588fe65a800ba302666fb74ccc123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:05 +0000

    Add package drope/0.4.0/drope-0.4.0.tar.gz

---
+++ packages/drope/drope-0.4.0.tar.gz/drope-0.4.0/setup.py
@@ -0,0 +1,47 @@
+import setuptools
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="drope",
+    version="0.4.0",
+    description="A utility for receiving file uploads",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jvstme/drope",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Utilities",
+        "Topic :: Communications :: File Sharing",
+    ],
+
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+
+    entry_points={
+        "console_scripts": [
+            "drope=drope.cmdline:main",
+        ],
+    },
+
+    install_requires=[
+        "aiofiles ~= 0.8",
+        "aiohttp ~= 3.8",
+    ],
+
+    extras_require={
+        "dev": [
+            "pytest",
+            "pytest-aiohttp",
+        ]
+    },
+
+    package_data={
+        "": ["static/*"],
+    },
+    zip_safe=False,
+)
