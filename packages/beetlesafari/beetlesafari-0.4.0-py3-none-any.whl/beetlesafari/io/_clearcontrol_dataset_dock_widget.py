882e28643dd7a2c5e2763a89555ba726e0588ddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:46 +0000

    Add package beetlesafari/0.4.0/beetlesafari-0.4.0-py3-none-any.whl

---
+++ packages/beetlesafari/beetlesafari-0.4.0-py3-none-any.whl/beetlesafari/io/_clearcontrol_dataset_dock_widget.py
@@ -0,0 +1,38 @@
+import datetime
+
+from magicgui import magicgui
+from ._clearcontrol_dataset import ClearControlDataset
+from napari_tools_menu import register_function
+import napari
+
+@register_function(menu="File Import/Export > Load ClearControl dataset (beetlesafari)")
+def _clearcontrol_loader(directory : str = "C:/structure/data/2019-12-17-16-54-37-81-Lund_Tribolium_nGFP_TMR", dataset_name : str = 'C0opticsprefused', hours : int = 0, minutes : int = 0, seconds : int = 0) -> napari.types.ImageData:
+    import time
+    start_time = time.time()
+
+    if directory is None:
+        # we have to do it like this because the assistant hands over None at the first call
+        directory = "C:/structure/data/2019-12-17-16-54-37-81-Lund_Tribolium_nGFP_TMR"
+
+    cc_dataset : ClearControlDataset = ClearControlDataset(directory)
+
+    #hours = timepoint.hour
+    #minutes = timepoint.minute
+    #seconds = timepoint.second
+
+    index = cc_dataset.get_index_after_time(hours * 60 * 60 + minutes * 60 + seconds)
+    filename = cc_dataset.get_image_filename(index)
+    print('Load index', index)
+    output = cc_dataset.get_resampled_image(index)
+
+    import pyclesperanto_prototype as cle
+
+    min_intensity = 0
+    max_intensity = cle.maximum_of_all_pixels(output)
+
+    output = cle.pull(output)
+    print("Loading took " + str(time.time() - start_time) + " s")
+
+    # show result in napari
+    return output
+
---
+++ packages/beetlesafari/beetlesafari-0.4.0-py3-none-any.whl/beetlesafari/utils/__init__.py
@@ -0,0 +1,9 @@
+from ._time import days_to_seconds
+from ._time import hours_to_seconds
+from ._time import minutes_to_seconds
+from ._time import seconds_to_hours
+from ._time import seconds_to_minutes
+
+from ._filenames import index_to_clearcontrol_filename
+
+from ._stopwatch import stopwatch
