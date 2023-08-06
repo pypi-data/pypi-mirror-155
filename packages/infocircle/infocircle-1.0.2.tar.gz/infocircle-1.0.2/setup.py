5e581fa18bc38453866096e15b16ae8aeb95a537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:54 +0000

    Add package infocircle/1.0.2/infocircle-1.0.2.tar.gz

---
+++ packages/infocircle/infocircle-1.0.2.tar.gz/infocircle-1.0.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='infocircle',
+    packages=find_packages(),
+    include_package_data=True,
+    version="1.0.2",
+    description='RPBI CIRCLE ID TO INFORMATION',
+    author='AKXVAU',
+    author_email='akxvau@gmail.com',
+    long_description=(open("README.md","r")).read(),
+    long_description_content_type="text/markdown",
+   install_requires=['requests', 'lolcat'],
+ 
+    keywords=['hacker','blsms', 'spam', 'tool', 'sms', 'pyenc', 'call', 'prank', 'termux', 'hack','Py Encrypt','Encryptor', 'AKXVAU','ROBI','CORCLE','ROBI CIRCLE', 'INFOCIRCLE'],
+    classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3',
+            'Operating System :: OS Independent',
+            'Environment :: Console',
+    ],
+    
+    license='MIT',
+    entry_points={
+            'console_scripts': [
+                'infocircle = infocircle.infocircle:menu',
+                
+            ],
+    },
+    python_requires='>=3.9'
+)
