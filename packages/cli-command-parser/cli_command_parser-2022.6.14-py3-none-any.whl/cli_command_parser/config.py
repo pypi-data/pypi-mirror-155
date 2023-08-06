aee25ec9548aee921ce56b0ccac66c51fb5cce2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:10 +0000

    Add package cli-command-parser/2022.6.14/cli_command_parser-2022.6.14-py3-none-any.whl

---
+++ packages/cli-command-parser/cli_command_parser-2022.6.14-py3-none-any.whl/cli_command_parser/config.py
@@ -0,0 +1,214 @@
+"""
+Configuration options for Command behavior.
+
+:author: Doug Skrypa
+"""
+
+from dataclasses import dataclass, fields
+from enum import Enum
+from typing import TYPE_CHECKING, Optional, Any, Union, Callable, Type, Dict, FrozenSet
+
+from .utils import Bool, FixedFlag, _NotSet, cached_class_property
+
+if TYPE_CHECKING:
+    from .command_parameters import CommandParameters
+    from .core import CommandType
+    from .error_handling import ErrorHandler
+    from .formatting.commands import CommandHelpFormatter
+    from .formatting.params import ParamHelpFormatter
+    from .parameters import ParamOrGroup
+
+__all__ = ['CommandConfig', 'ShowDefaults', 'OptionNameMode']
+
+# TODO: Config / handling for subcommand option conflict, especially for short form where name/long don't match previous
+
+
+# region Config Option Enums
+
+
+class ShowDefaults(FixedFlag):
+    """
+    Options for showing ``(default: <default>)`` in ``--help`` text.  Options can be combined, but
+    :attr:`ShowDefaults.NEVER` will override all other options.
+
+    If ``TRUTHY`` / ``NON_EMPTY`` / ``ANY`` are combined, then the most permissive (rightmost / highest value)
+    option will be used.
+
+    The ``MISSING`` option must be combined with one of ``TRUTHY`` / ``NON_EMPTY`` / ``ANY`` - using ``MISSING`` alone
+    is equivalent to ``ShowDefaults.MISSING | ShowDefaults.NEVER``, which will result in no default values being shown.
+    """
+
+    # fmt: off
+    NEVER = 1       #: Never include the default value in help text
+    MISSING = 2     #: Only include the default value if ``default:`` is not already present
+    TRUTHY = 4      #: Only include the default value if it is treated as True in a boolean context
+    NON_EMPTY = 8   #: Only include the default value if it is not ``None`` or an empty container
+    ANY = 16        #: Any default value, regardless of truthiness, will be included
+    # fmt: on
+
+    @classmethod
+    def _missing_(cls, value: Union[str, int]) -> 'ShowDefaults':
+        if isinstance(value, str):
+            try:
+                return cls._member_map_[value.upper().replace('-', '_')]  # noqa
+            except KeyError:
+                expected = ', '.join(cls._member_map_)
+                raise ValueError(f'Invalid {cls.__name__} value={value!r} - expected one of {expected}') from None
+        return super()._missing_(value)
+
+    def __or__(self, other: 'ShowDefaults') -> 'ShowDefaults':
+        if ShowDefaults.NEVER in (self, other):
+            return ShowDefaults.NEVER
+        return super().__or__(other)  # noqa
+
+
+class OptionNameMode(FixedFlag):
+    """
+    How the default long form that is added for Option/Flag/Counter/etc. Parameters should handle underscores/dashes.
+
+    Given a Parameter defined as ``foo_bar = Option(...)``, the default long form handling based on this setting would
+    be:
+
+    :UNDERSCORE: ``--foo_bar``
+    :DASH: ``--foo-bar``
+    :BOTH: Both ``--foo-bar`` and ``--foo_bar`` will be accepted
+
+    If a long form is provided explicitly for a given optional Parameter, then this setting will be ignored.
+    """
+
+    # fmt: off
+    UNDERSCORE = 1
+    DASH = 2
+    BOTH = 3
+    # fmt: on
+
+
+# endregion
+
+
+class EnumConfigOption:
+    """
+    Descriptor that simplifies config option validation using Enums and works around dataclass property idiosyncrasies.
+    """
+
+    def __init__(self, enum_cls: Type[Enum], default: Enum):
+        self.enum_cls = enum_cls
+        self.default = default
+
+    def __set_name__(self, owner: Type['CommandConfig'], name: str):
+        self.name = name
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        return instance.__dict__.get(self.name, self.default)
+
+    def __set__(self, instance, value):
+        if isinstance(value, EnumConfigOption):  # Workaround for initial value setting
+            instance.__dict__[self.name] = self.default
+        else:
+            instance.__dict__[self.name] = self.enum_cls(value)
+
+
+@dataclass
+class CommandConfig:
+    # region Error Handling Options
+
+    #: The :class:`.ErrorHandler` to be used by :meth:`.Command.__call__`
+    error_handler: Optional['ErrorHandler'] = _NotSet
+
+    #: Whether :meth:`.Command._after_main_` should always be called, even if an exception was raised in
+    #: :meth:`.Command.main` (similar to a ``finally`` block)
+    always_run_after_main: Bool = False
+
+    # endregion
+
+    # region ActionFlag Options
+
+    #: Whether multiple action_flag methods are allowed to run if they are all specified
+    multiple_action_flags: Bool = True
+
+    #: Whether action_flag methods are allowed to be combined with a positional Action method in a given CLI invocation
+    action_after_action_flags: Bool = True
+
+    # endregion
+
+    # region Parsing Options
+
+    #: Whether unknown arguments should be ignored (default: raise an exception when unknown arguments are encountered)
+    ignore_unknown: Bool = False
+
+    #: Whether missing required arguments should be allowed (default: raise an exception when they are missing)
+    allow_missing: Bool = False
+
+    #: Whether backtracking is enabled for positionals following params with variable nargs
+    allow_backtrack: Bool = True
+
+    #: How the default long form that is added for Option/Flag/Counter/etc. Parameters should handle underscores/dashes
+    option_name_mode: OptionNameMode = EnumConfigOption(OptionNameMode, OptionNameMode.UNDERSCORE)
+
+    # endregion
+
+    # region Usage & Help Text Options
+
+    #: Whether the ``--help`` / ``-h`` action_flag should be added
+    add_help: Bool = True
+
+    #: Whether the metavar for Parameters that accept values should default to the name of the specified type
+    #: (default: the name of the parameter)
+    use_type_metavar: Bool = False
+
+    #: Whether the default value for Parameters should be shown in help text, and related behavior
+    show_defaults: ShowDefaults = EnumConfigOption(ShowDefaults, ShowDefaults.MISSING | ShowDefaults.NON_EMPTY)
+
+    #: Whether there should be a visual indicator in help text for the parameters that are members of a given group
+    show_group_tree: Bool = False
+
+    #: Whether mutually exclusive / dependent groups should include that fact in their descriptions
+    show_group_type: Bool = True
+
+    #: A callable that accepts 2 arguments, a :class:`.Command` class (not object) and a :class:`.CommandParameters`
+    #: object, and returns a :class:`.CommandHelpFormatter`
+    command_formatter: Callable[['CommandType', 'CommandParameters'], 'CommandHelpFormatter'] = None
+
+    #: A callable that accepts a :class:`.Parameter` or :class:`.ParamGroup` and returns a :class:`.ParamHelpFormatter`
+    param_formatter: Callable[['ParamOrGroup'], 'ParamHelpFormatter'] = None
+
+    #: Whether the program version, author email, and documentation URL should be included in the help text epilog, if
+    #: they were successfully detected
+    extended_epilog: Bool = True
+
+    #: Whether the top level script's docstring should be included in generated documentation
+    show_docstring: Bool = True
+
+    # endregion
+
+    # region Planned Options
+
+    # #: Whether handling of spaces (`` ``), dashes (``-``), and underscores (``_``) in the middle of positional action
+    # #: names should be strict (``True``) when processing user input, or if they should be allowed to be interchanged
+    # #: (``False``)
+    # strict_action_punctuation: Bool = False
+    #
+    # #: Whether handling of spaces (`` ``), dashes (``-``), and underscores (``_``) in the middle of positional sub
+    # #: command names should be strict (``True``) when processing user input, or if they should be allowed to be
+    # #: interchanged (``False``)
+    # strict_sub_command_punctuation: Bool = False
+
+    # endregion
+
+    @cached_class_property
+    def _field_names(cls) -> FrozenSet[str]:  # noqa
+        """Cache the names of the config options for use in :meth:`.as_dict`"""
+        names = {f.name for f in fields(cls)}
+        return frozenset(names)
+
+    def as_dict(self) -> Dict[str, Any]:
+        """
+        Return a dict representing the configured options.
+
+        This was necessary because :func:`dataclasses.asdict` copies values, which breaks the use of _NotSet as a
+        non-None sentinel value.
+        """
+        d = self.__dict__
+        return {f: d[f] for f in self._field_names}  # noqa
---
+++ packages/cli-command-parser/cli_command_parser-2022.6.14-py3-none-any.whl/cli_command_parser/utils.py
@@ -0,0 +1,395 @@
+"""
+Utilities for extracting types from annotations, finding / storing program metadata, and other misc utilities.
+
+:author: Doug Skrypa
+"""
+
+import re
+import sys
+from collections.abc import Collection, Iterable, Callable
+from contextlib import contextmanager
+from enum import Flag
+from inspect import stack, isclass, FrameInfo
+from pathlib import Path
+from textwrap import dedent
+from typing import Any, Union, Optional, Type, TypeVar, Sequence, get_type_hints, Tuple, Dict, List
+from string import whitespace, printable
+from urllib.parse import urlparse
+
+try:
+    from typing import get_origin, get_args as _get_args
+except ImportError:  # Added in 3.8; the versions from 3.8 are copied here
+    from .compat import get_origin, _get_args
+
+try:
+    from types import NoneType
+except ImportError:  # Added in 3.10
+    NoneType = type(None)
+
+from .compat import create_pseudo_member, decompose_flag, missing_flag
+from .exceptions import ParameterDefinitionError
+
+Bool = Union[bool, Any]
+FlagEnum = TypeVar('FlagEnum', bound='FixedFlag')
+_NotSet = object()
+
+
+class cached_class_property(classmethod):
+    def __init__(self, func: Callable):
+        super().__init__(property(func))  # noqa  # makes Sphinx handle it better than if this was not done
+        self.__doc__ = func.__doc__
+        self.func = func
+        self.values = {}
+
+    def __get__(self, obj: None, cls):  # noqa
+        try:
+            return self.values[cls]
+        except KeyError:
+            self.values[cls] = value = self.func(cls)
+            return value
+
+
+def validate_positional(
+    param_cls: str, value: str, prefix: str = 'choice', exc: Type[Exception] = ParameterDefinitionError
+):
+    if not value or value.startswith('-'):
+        raise exc(f"Invalid {param_cls} {prefix}={value!r} - may not be empty or start with '-'")
+
+    bad = {c for c in value if (c in whitespace and c != ' ') or c not in printable}
+    if bad:
+        raise exc(f'Invalid {param_cls} {prefix}={value!r} - invalid characters: {bad}')
+
+
+class ProgramMetadata:
+    description: Optional[str] = None
+
+    def __init__(
+        self,
+        prog: str = None,
+        *,
+        url: str = None,
+        docs_url: str = None,
+        email: str = None,
+        version: str = None,
+        usage: str = None,
+        description: str = None,
+        epilog: str = None,
+        doc_name: str = None,
+        doc: str = None,
+    ):
+        self._cmd_args = {
+            'prog': prog,
+            'url': url,
+            'docs_url': docs_url,
+            'email': email,
+            'version': version,
+            'doc_name': doc_name,
+        }
+        self._init(ProgInfo())
+        self.doc_name = doc_name
+        self.usage = usage
+        self.epilog = epilog
+        if description:
+            self.description = description
+        elif doc:
+            doc = dedent(doc).lstrip()
+            if doc.strip():  # avoid space-only doc, but let possibly intentional trailing spaces / newlines to persist
+                self.description = doc
+
+    def _init(self, info: 'ProgInfo'):
+        a = self._cmd_args
+        self.path = info.path
+        self.prog = a['prog'] or info.path.name
+        docs_url_from_repo_url = self._docs_url_from_repo_url
+        self.docs_url = a['docs_url'] or docs_url_from_repo_url(a['url']) or docs_url_from_repo_url(info.repo_url)
+        self.url = a['url'] or info.repo_url
+        self.email = a['email'] or info.email
+        self.version = a['version'] or info.version or ''
+        self.doc_str = info.doc_str
+        self.doc_name = a['doc_name']
+
+    @property
+    def doc_name(self) -> str:
+        return self._doc_name
+
+    @doc_name.setter
+    def doc_name(self, value: Optional[str]):
+        if value:
+            self._doc_name = value
+        elif self.path.name != ProgInfo.default_file_name:
+            self._doc_name = self.path.stem
+        else:
+            self._doc_name = self.prog
+
+    def _docs_url_from_repo_url(self, repo_url: Optional[str]):  # noqa
+        try:  # Note: This is only done this way to address a false positive on a GitHub security scan
+            parsed = urlparse(repo_url)
+            if parsed.scheme == 'https' and parsed.hostname == 'github.com':
+                user, repo = parsed.path[1:].split('/')
+                return f'https://{user}.github.io/{repo}/'
+        except Exception:  # noqa
+            pass
+        return None
+
+    def format_epilog(self, extended: Bool = True) -> str:
+        parts = [self.epilog] if self.epilog else []
+        if parts and not extended:
+            return parts[0]
+
+        version = self.version
+        if version:
+            version = f' [ver. {version}]'
+        if self.email:
+            parts.append(f'Report {self.prog}{version} bugs to {self.email}')
+        url = self.docs_url or self.url
+        if url:
+            parts.append(f'Online documentation: {url}')
+        return '\n\n'.join(parts)
+
+
+class ProgInfo:
+    __dynamic_import: Optional[Tuple[Path, Dict[str, Any]]] = None
+    default_file_name: str = 'UNKNOWN'  #: Default name used when it cannot be determined from the stack or sys.argv
+    installed_via_setup: bool = False
+    email: Optional[str] = None
+    version: Optional[str] = None
+    repo_url: Optional[str] = None
+    path: Optional[Path] = None
+    doc_str: Optional[str] = None
+
+    def __init__(self):
+        self.path, g = self._path_and_globals()
+        self.email = g.get('__author_email__')
+        self.version = g.get('__version__')
+        self.repo_url = g.get('__url__')
+        self.doc_str = g.get('__doc__')
+
+    def __repr__(self) -> str:
+        return (
+            f'<ProgInfo[path={self.path.as_posix()}, email={self.email!r}, version={self.version},'
+            f' url={self.repo_url!r}, doc_str={self.doc_str!r}]>'
+        )
+
+    @classmethod
+    def _print_stack_info(cls):
+        for i, level in reversed(tuple(enumerate(stack()))):
+            g = level.frame.f_globals
+            print(
+                f'\n[{i:02d}] {level.filename}:{level.lineno} fn={level.function}:'
+                f'\n    __package__={g["__package__"]!r}'
+                f'\n    {", ".join(sorted(g))}'
+            )
+
+    @classmethod
+    @contextmanager
+    def _dynamic_import(cls, path: Path, module_globals: Dict[str, Any]):
+        cls.__dynamic_import = path, module_globals
+        try:
+            yield
+        finally:
+            cls.__dynamic_import = None
+
+    def _path_and_globals(self) -> Tuple[Path, Dict[str, Any]]:
+        if self.__dynamic_import:
+            return self.__dynamic_import
+        try:
+            top_level, g = self._find_top_frame_and_globals()
+            return self._resolve_path(top_level.filename), g
+        except Exception as e:  # noqa
+            return self._resolve_path(), {}
+
+    def _resolve_path(self, path: str = None) -> Path:
+        from_setup = path and self.installed_via_setup and path.endswith('-script.py')
+        if path and not from_setup:
+            return Path(path)
+
+        try:
+            name = sys.argv[0]
+        except IndexError:
+            if from_setup:
+                path = Path(path)
+                return path.with_name(path.stem[:-7] + '.py')
+            else:
+                return Path.cwd().joinpath(self.default_file_name)
+
+        argv_path = Path(name)
+        try:
+            if argv_path.is_file():
+                return argv_path
+        except OSError:
+            pass
+
+        return Path.cwd().joinpath(self.default_file_name)
+
+    def _find_cmd_frame_info(self, fi_stack: Sequence[FrameInfo]) -> FrameInfo:
+        if not self.installed_via_setup:
+            return fi_stack[-1]
+
+        this_pkg = __package__.split('.', 1)[0]
+        # ignore_pkgs = {this_pkg, '', 'IPython', 'IPython.core', 'IPython.terminal', 'traitlets.config'}
+        this_pkg_dot = this_pkg + '.'
+        for level in reversed(fi_stack[:-1]):
+            pkg = level.frame.f_globals.get('__package__') or ''
+            if pkg != this_pkg and not pkg.startswith(this_pkg_dot):  # Exclude intermediate frames in this package
+                return level
+
+        return fi_stack[-1]
+
+    def _detect_install_type(self, fi_stack: Sequence[FrameInfo]):
+        top_level = fi_stack[-1]
+        g = top_level.frame.f_globals
+        self.installed_via_setup = 'load_entry_point' in g and 'main' not in g
+
+    def _find_top_frame_and_globals(self) -> Tuple[FrameInfo, Dict[str, Any]]:
+        fi_stack = stack()
+        # TODO: Find globals for the module the Command is in instead
+        self._detect_install_type(fi_stack)
+        cmd_frame_info = self._find_cmd_frame_info(fi_stack)
+        return fi_stack[-1], cmd_frame_info.frame.f_globals
+
+
+def camel_to_snake_case(text: str, delim: str = '_') -> str:
+    return ''.join(f'{delim}{c}' if i and c.isupper() else c for i, c in enumerate(text)).lower()
+
+
+# region Annotation Inspection
+
+
+def get_descriptor_value_type(command_cls: type, attr: str) -> Optional[type]:
+    try:
+        annotation = get_type_hints(command_cls)[attr]
+    except KeyError:
+        return None
+
+    return get_annotation_value_type(annotation)
+
+
+def get_annotation_value_type(annotation) -> Optional[type]:
+    origin = get_origin(annotation)
+    """
+    Note on get_origin return values:
+    get_origin(List[str]) -> list
+    get_origin(List) -> list
+    get_origin(list[str]) -> list
+    get_origin(list) -> None
+    """
+    if origin is None and isinstance(annotation, type):
+        return annotation
+    elif isclass(origin) and issubclass(origin, (Collection, Iterable)):
+        return _type_from_collection(origin, annotation)
+    elif origin is Union:
+        return _type_from_union(annotation)
+    return None
+
+
+def get_args(annotation) -> tuple:
+    """
+    Wrapper around :func:`python:typing.get_args` for 3.7~8 compatibility, to make it behave more like it does in 3.9+
+    """
+    if getattr(annotation, '_special', False):  # 3.7-3.8 generic collection alias with no content types
+        return ()
+    return _get_args(annotation)
+
+
+def _type_from_union(annotation) -> Optional[type]:
+    args = get_args(annotation)
+    # Note: Unions of a single argument return the argument; i.e., Union[T] returns T, so the len can never be 1
+    if len(args) == 2 and NoneType in args:
+        arg = args[0] if args[1] is NoneType else args[1]
+    else:
+        return None
+
+    origin = get_origin(arg)
+    if origin is None and isinstance(arg, type):
+        return arg
+    elif isclass(origin) and issubclass(origin, (Collection, Iterable)):
+        return _type_from_collection(origin, arg)
+    else:
+        return None
+
+
+def _type_from_collection(origin, annotation) -> Optional[type]:
+    args = get_args(annotation)
+    try:
+        annotation = args[0]
+    except IndexError:  # The annotation was a collection with no content types specified
+        return origin
+
+    n_args = len(args)
+    if n_args > 2 or (n_args > 1 and (origin is not tuple or args[1] is not Ellipsis)):
+        return None
+
+    origin = get_origin(annotation)
+    if origin is None and isinstance(annotation, type):
+        return annotation
+    elif origin is Union:
+        return _type_from_union(annotation)
+    else:
+        return None
+
+
+# endregion
+
+
+def is_numeric(text: str) -> Bool:
+    try:
+        num_match = is_numeric._num_match
+    except AttributeError:
+        is_numeric._num_match = num_match = re.compile(r'^-\d+$|^-\d*\.\d+?$').match
+    return num_match(text)
+
+
+class FixedFlag(Flag):
+    """Extends Flag to work around breaking changes in 3.11 for repr, missing, and pseudo-members."""
+
+    def __repr__(self) -> str:
+        # In 3.11, this needs to be declared in the parent of a Flag that actually has members - it breaks if it is
+        # defined in a mixin or the class with members.
+        names = '|'.join(part._name_ for part in self._decompose())
+        return f'<{self.__class__.__name__}:{names}>'
+
+    @classmethod
+    def _missing_(cls, value) -> FlagEnum:
+        if isinstance(value, str):
+            if value.startswith(('!', '~')):
+                invert = True
+                value = value[1:]
+            else:
+                invert = False
+
+            try:
+                member = cls._missing_str(value)
+            except KeyError:
+                expected = ', '.join(cls._member_map_)
+                raise ValueError(f'Invalid {cls.__name__} value={value!r} - expected one of {expected}') from None
+            else:
+                return ~member if invert else member
+
+        return missing_flag(cls, value)
+
+    @classmethod
+    def _missing_str(cls, value: str) -> FlagEnum:
+        try:
+            return cls._member_map_[value.upper()]  # noqa
+        except KeyError:
+            pass
+        if '|' in value:
+            tmp = cls(0)
+            for part in map(str.strip, value.split('|')):
+                if not part:
+                    continue
+                try:
+                    tmp |= cls._member_map_[part.upper()]
+                except KeyError:
+                    break
+            else:
+                if tmp._value_ != 0:
+                    return tmp
+
+        raise KeyError
+
+    def _decompose(self) -> List[FlagEnum]:
+        return decompose_flag(self.__class__, self._value_, self._name_)[0]
+
+    def __lt__(self, other: FlagEnum) -> bool:
+        return self._value_ < other._value_
