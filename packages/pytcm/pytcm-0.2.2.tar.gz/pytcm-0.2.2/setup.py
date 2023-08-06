5ba1b1b51b56f02610e7e75cb4759385e93e5fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:24 +0000

    Add package pytcm/0.2.2/pytcm-0.2.2.tar.gz

---
+++ packages/pytcm/pytcm-0.2.2.tar.gz/pytcm-0.2.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pytcm']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pytcm',
+    'version': '0.2.2',
+    'description': 'pytcm - Python Terminal Commands Manager',
+    'long_description': '# pytcm\n\nA Python Terminal Commands Manager',
+    'author': 'Alexis Beaulieu',
+    'author_email': 'alexisbeaulieu97@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/alexisbeaulieu97/pytcm',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
