76376ae0703151125950d4a23ba05f12437fb6af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:50 +0000

    Add package python-structures/0.1.1/python-structures-0.1.1.tar.gz

---
+++ packages/python-structures/python-structures-0.1.1.tar.gz/python-structures-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="python-structures",
+    version="0.1.1",
+    author="Piotr Żarczyński",
+    description="A small package for creating and manipulating structs similar to those in C/C++",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tpxHorus/python-structures",
+    project_urls={
+        "Bug Tracker": "https://github.com/tpxHorus/python-structures/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.10",
+)
