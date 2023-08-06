ac2c3f5310d5d77544457b8f17ae3fb52f9ad481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:10 +0000

    Add package marinvaders/0.3.5/marinvaders-0.3.5.tar.gz

---
+++ packages/marinvaders/marinvaders-0.3.5.tar.gz/marinvaders-0.3.5/marinvaders/__init__.py
@@ -0,0 +1,12 @@
+"""
+Marinvader - A python module for analysing alien and native marine species
+===========================================================================
+
+"""
+
+from marinvaders.main import MarineLife  # noqa
+from marinvaders.main import Species  # noqa
+from marinvaders.main import plot  # noqa
+from marinvaders.main import marine_ecoregions  # noqa
+
+# from marinvaders.util.gisd_scraper import get_gisd
