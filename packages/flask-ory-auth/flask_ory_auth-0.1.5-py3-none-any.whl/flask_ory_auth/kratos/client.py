1742a83c8ee528dbec943036b0432c86490c44b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:21:19 +0000

    Add package flask-ory-auth/0.1.5/flask_ory_auth-0.1.5-py3-none-any.whl

---
+++ packages/flask-ory-auth/flask_ory_auth-0.1.5-py3-none-any.whl/flask_ory_auth/kratos/client.py
@@ -0,0 +1,36 @@
+import json
+
+import requests
+from flask import abort
+from flask import request
+
+
+class Authentication:
+    def __init__(self, api_url):
+        self.api_url = api_url
+
+    def set_email_to_session(self, session) -> None:
+        if session.get('email', None):
+            return
+
+        resp = requests.get(
+            f"{self.api_url}/sessions/whoami",
+            cookies=request.cookies,
+            headers={"Authorization": request.headers.get("Authorization", "")},
+        )
+        if resp.status_code == 200:
+            data = resp.json()
+            identity = data.get("identity", {})
+            session["email"] = identity.get("traits", {})..get("email")
+            session["kratos_id"] = identity.get("id")
+            session["traits"] = json.dumps(traits)
+
+    def set_user_to_session(self, session) -> None:
+        self.set_email_to_session(session)
+
+    def get_current_user(self, session):
+        user_id = session.get("user_id", None)
+        if not user_id:
+            abort(403)
+
+        return user_id
