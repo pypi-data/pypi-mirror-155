92fe37203540b926f0228d259ab74369026554f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:32:21 +0000

    Add package mtutils/2.0.8/mtutils-2.0.8.tar.gz

---
+++ packages/mtutils/mtutils-2.0.8.tar.gz/mtutils-2.0.8/mtutils/__init__.py
@@ -0,0 +1,6 @@
+from .lib.data import DataManager
+from .lib.evaluator import ClassificationEvaluator, ClassificationMultiLabelEvaluator, MultiLabelEvaluator
+from .lib.evaluator import ClassificationMultiClassEvaluator, MultiClassEvaluator
+from .lib.evaluator import DetectionEvaluator
+from .lib.utils import *
+from .lib.labelme import *
