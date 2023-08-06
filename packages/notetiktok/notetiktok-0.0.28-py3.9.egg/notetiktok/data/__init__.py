2f782c76db569ad90369ff52fe7f17c111a568fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:13 +0000

    Add package notetiktok/0.0.28/notetiktok-0.0.28-py3.9.egg

---
+++ packages/notetiktok/notetiktok-0.0.28-py3.9.egg/notetiktok/data/__init__.py
@@ -0,0 +1,3 @@
+from .base import AuthorDetail, ResourceDetail, ResourceType, SourceDetail
+from .core import (add_author, add_image, add_resource, add_source, add_video,
+                   author_db, resource_db, source_db)
