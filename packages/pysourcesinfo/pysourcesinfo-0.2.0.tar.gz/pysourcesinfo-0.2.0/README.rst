0dd83dc9fc92d047f86607fddd2120c0d5e11b7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:40 +0000

    Add package pysourcesinfo/0.2.0/pysourcesinfo-0.2.0.tar.gz

---
+++ packages/pysourcesinfo/pysourcesinfo-0.2.0.tar.gz/pysourcesinfo-0.2.0/README.rst
@@ -0,0 +1,37 @@
+=============
+pysourcesinfo
+=============
+
+
+.. image:: https://img.shields.io/pypi/v/pysourcesinfo.svg
+        :target: https://pypi.python.org/pypi/pysourcesinfo
+
+.. image:: https://img.shields.io/travis/weallfloatdownhere/pysourcesinfo.svg
+        :target: https://travis-ci.com/weallfloatdownhere/pysourcesinfo
+
+.. image:: https://readthedocs.org/projects/pysourcesinfo/badge/?version=latest
+        :target: https://pysourcesinfo.readthedocs.io/en/latest/?version=latest
+        :alt: Documentation Status
+
+
+
+
+Python3 module and cli retrieving userful info from projects sources code, that can be later used in further steps in the context of a CI/CD pipeline for instance.
+
+
+* Free software: MIT license
+* Documentation: https://pysourcesinfo.readthedocs.io.
+
+
+Features
+--------
+
+* TODO
+
+Credits
+-------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
---
+++ packages/pysourcesinfo/pysourcesinfo-0.2.0.tar.gz/pysourcesinfo-0.2.0/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [ 
+    'fire==0.4.0',
+    'omegaconf==2.2.2',
+    'ripgrepy==1.1.0'
+]
+
+test_requirements = [ ]
+
+setup(
+    author="redacted",
+    author_email='noreply@local.io',
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Python3 module and cli retrieving userful info from projects sources code, that can be later used in further steps in the context of a CI/CD pipeline for instance.",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='pysourcesinfo',
+    name='pysourcesinfo',
+    packages=find_packages(include=['pysourcesinfo', 'pysourcesinfo.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/weallfloatdownhere/pysourcesinfo',
+    version='0.2.0',
+    zip_safe=False,
+)
