887029dfef4f4a49f2f33a1c0e53cee763b51d14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:37 +0000

    Add package pipetorch/0.1b1/pipetorch-0.1b1.tar.gz

---
+++ packages/pipetorch/pipetorch-0.1b1.tar.gz/pipetorch-0.1b1/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+from pipetorch import __version__
+
+alpha = [
+         "Development Status :: 3 - Alpha",
+         "Intended Audience :: Education",
+         "Intended Audience :: Science/Research",
+         "Topic :: Scientific/Engineering :: Artificial Intelligence",
+         "Topic :: Software Development :: Libraries :: Python Modules",
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ]
+beta = [
+         "Development Status :: 4 - Beta",
+         "Intended Audience :: Education",
+         "Intended Audience :: Science/Research",
+         "Topic :: Scientific/Engineering :: Artificial Intelligence",
+         "Topic :: Software Development :: Libraries :: Python Modules",
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ]
+
+setup(
+     name='pipetorch',
+     packages=find_packages(),
+     #packages=['pipetorch', 'pipetorch.data'],
+     package_data={'pipetorch': ['data/datasets/*']},
+     include_package_data=True,
+     version=__version__,
+     author="Jeroen Vuurens",
+     author_email="jbpvuurens@gmail.com",
+     description="A data pipeline library for PyTorch and Machine Learning projects",
+     url="https://github.com/jeroenvuurens/pipetorch",
+     download_url="https://github.com/jeroenvuurens/pipetorch/archive/refs/tags/v0.1a.tar.gz",
+     keywords=['PyTorch', 'SKLearn', 'Machine Learning', 'Neural Network', 'Predictive Analytics'],
+     install_requires=['torch', 'torchvision', 'numpy', 'sklearn', 'matplotlib', 'pandas', 'pathlib', 'tqdm', 'statistics', 'iterative-stratification', 'optuna'],
+     classifiers=beta
+ )
+
