279eac31440cc6a0759574a6706a7ac91f0a547b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:31:44 +0000

    Add package geniusbot/2.1.1/geniusbot-2.1.1-py2.py3-none-any.whl

---
+++ packages/geniusbot/geniusbot-2.1.1-py2.py3-none-any.whl/geniusbot/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+from geniusbot.version import __version__, __author__, __credits__
+from geniusbot.videodownloader import VideoDownloader
+from geniusbot.geniusbot import geniusbot
+"""
+webarchiver
+
+A web page full page screenshooting tool.
+"""
+
+__version__ = __version__
+__author__ = __author__
+__credits__ = __credits__
