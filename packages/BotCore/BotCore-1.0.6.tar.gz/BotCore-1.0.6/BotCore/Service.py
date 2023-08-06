17cbd074882745fe99baf0a9fd242b694f1f39a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:35 +0000

    Add package BotCore/1.0.6/BotCore-1.0.6.tar.gz

---
+++ packages/BotCore/BotCore-1.0.6.tar.gz/BotCore-1.0.6/BotCore/Service.py
@@ -0,0 +1,19 @@
+
+from typing import TypeVar, Generic, Type
+
+from BotCore.ModelInfo import ModelInfo
+
+T = TypeVar("T", bound=ModelInfo)
+
+
+class Service(Generic[T]):
+
+	def __init__(self, model: Type[T], fields: list[str]):
+		self.model = model
+		self.fields = fields
+
+	def to_objects(self, rows: list[tuple]) -> list[T]:
+		return [self.to_object(row) for row in rows]
+
+	def to_object(self, row: tuple) -> T:
+		return self.model(dict(zip(self.fields, row)))
