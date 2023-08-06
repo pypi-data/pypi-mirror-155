e4a4d1b5e7f4294dbca88f2315667af107f752a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:18 +0000

    Add package mlops-sdk/2022.6.20/mlops-sdk-2022.6.20.tar.gz

---
+++ packages/mlops-sdk/mlops-sdk-2022.6.20.tar.gz/mlops-sdk-2022.6.20/mlops_sdk/ml/__init__.py
@@ -0,0 +1,6 @@
+from .model import OdeNet
+from .model import SimpleVF
+from .model import CustomLoader
+from .eo_model import OdeNetEo
+
+__all__ = ["OdeNet", "OdeNetEo", "SimpleVF", "CustomLoader", "ModelRegistry"]
