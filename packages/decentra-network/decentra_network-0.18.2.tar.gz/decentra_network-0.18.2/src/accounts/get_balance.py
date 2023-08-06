50cca88b251fe963d3ad45e6ee4db18bd71dd7cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:33:17 +0000

    Add package decentra-network/0.18.2/decentra_network-0.18.2.tar.gz

---
+++ packages/decentra-network/decentra_network-0.18.2.tar.gz/decentra_network-0.18.2/src/accounts/get_balance.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+from accounts.get_accounts import GetAccounts
+from blockchain.block.get_block import GetBlock
+from wallet.wallet import Address
+
+
+def GetBalance(block, user):
+    """
+    Returns the users balance.
+    """
+
+    balance = -GetBlock().minumum_transfer_amount
+    user = "".join([
+        l.strip() for l in user.splitlines() if l and not l.startswith("-----")
+    ])
+    user = Address(user)
+    for Accounts in GetAccounts():
+
+        if Accounts.Address == user:
+            balance += Accounts.balance
+            break
+
+    for trans in block.pendingTransaction + block.validating_list:
+        if user == trans.fromUser:
+            balance -= trans.amount + trans.transaction_fee
+    return balance
