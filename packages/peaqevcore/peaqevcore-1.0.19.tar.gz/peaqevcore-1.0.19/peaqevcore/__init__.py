153da5ae14d752bbbe18b501a5f19d796f63898a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:48:34 +0000

    Add package peaqevcore/1.0.19/peaqevcore-1.0.19.tar.gz

---
+++ packages/peaqevcore/peaqevcore-1.0.19.tar.gz/peaqevcore-1.0.19/peaqevcore/__init__.py
@@ -0,0 +1,10 @@
+# from .Threshold import ThresholdBase as Threshold
+# from .Chargecontroller import ChargeControllerBase as ChargeController
+# from .Prediction import PredictionBase as Prediction
+# from .hoursselection import Hoursselectionbase as Hoursselection
+# from .session import SessionPrice as Session
+from .locale.querytypes import querytypes
+from .locale import (Locale, locale_model)
+from .country import (belgium, sweden, norway, default)
+
+
