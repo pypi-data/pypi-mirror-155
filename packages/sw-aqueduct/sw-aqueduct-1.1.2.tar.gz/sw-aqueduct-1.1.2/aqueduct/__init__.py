f493397ad69822405b76380d6de6c08052e35f83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:12 +0000

    Add package sw-aqueduct/1.1.2/sw-aqueduct-1.1.2.tar.gz

---
+++ packages/sw-aqueduct/sw-aqueduct-1.1.2.tar.gz/sw-aqueduct-1.1.2/aqueduct/__init__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from .aqueduct import Aqueduct
+from .instance import SwimlaneInstance
+
+__title__ = "sw-aqueduct"
+__name__ = "aqueduct"
+__description__ = "A Swimlane content delivery system written in Python"
+__url__ = "https://github.com/swimlane/aqueduct"
+__version__ = "1.1.2"
+__author__ = "Swimlane"
+__author_email__ = "info@swimlane.com"
+__maintainer__ = "MSAdministrator"
+__maintainer_email__ = "rickardja@live.com"
+__license__ = "MIT"
+__copyright__ = "Copyright 2022 Swimlane"
+
+
+__all__ = [
+    "Aqueduct",
+    "SwimlaneInstance",
+    "__title__",
+    "__name__",
+    "__description__",
+    "__url__",
+    "__version__",
+    "__author__",
+    "__author_email__",
+    "__maintainer__",
+    "__maintainer_email__",
+    "__license__",
+    "__copyright__",
+    "__logo__",
+]
