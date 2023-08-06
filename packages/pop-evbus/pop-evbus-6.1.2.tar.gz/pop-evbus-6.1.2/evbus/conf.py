d97855ab6756fa68702d68f86681995967683f8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:35 +0000

    Add package pop-evbus/6.1.2/pop-evbus-6.1.2.tar.gz

---
+++ packages/pop-evbus/pop-evbus-6.1.2.tar.gz/pop-evbus-6.1.2/evbus/conf.py
@@ -0,0 +1,15 @@
+CLI_CONFIG = {
+    "serial_plugin": {},
+}
+CONFIG = {
+    "serial_plugin": {
+        "help": "The plugin used to serialize data. Defaults to json",
+        "default": "json",
+    },
+}
+DYNE = {
+    "log": ["log"],
+    "evbus": ["evbus"],
+    "ingress": ["ingress"],
+    "match": ["match"],
+}
