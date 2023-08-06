9dc7a415a6bec6785e3d2f242be867794cbb9da2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:11 +0000

    Add package Orange3-Text/1.8.2/Orange3_Text-1.8.2-py3-none-any.whl

---
+++ packages/Orange3-Text/Orange3_Text-1.8.2-py3-none-any.whl/orangecontrib/text/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM ORANGE3-TEXT SETUP.PY
+short_version = '1.8.2'
+version = '1.8.2'
+full_version = '1.8.2'
+git_revision = '660a0da7edd0d40936800d9832ef017295f14bf4'
+release = True
+if not release:
+    version = full_version
+    short_version += ".dev"
