9ee2eed0b7087550e4b8bcd2433316906502b564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:05 +0000

    Add package Anar/0.0/Anar-0.0.tar.gz

---
+++ packages/Anar/Anar-0.0.tar.gz/Anar-0.0/Anar/__init__.py
@@ -0,0 +1,19 @@
+from .pythonProject import Lagrange
+from .pythonProject import brent
+from .pythonProject import golden_ratio
+from .pythonProject import parse_variables_tz2
+from .pythonProject import parabolic_interpolation
+from .pythonProject import plot
+from .pythonProject import gradient_descent_fixed
+from .pythonProject import local_extrema
+from .pythonProject import gradient_descent_fixed
+from .pythonProject import parse__restr_function
+from .pythonProject import parse_restriction
+from .pythonProject import parse_variables
+from .pythonProject import parser_task1
+from .pythonProject import tz3
+from .pythonProject import tz4
+from .pythonProject import plot
+from .pythonProject import plot_tz2
+
+
---
+++ packages/Anar/Anar-0.0.tar.gz/Anar-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Anar',
+      version='0.0',
+      description='abc',
+      packages=['Anar'],
+      author_email='anariem@mail.ru',
+      zip_safe=False)
