a57920e97f3e6de9db34b196ee9f7716ebd85cb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:48:58 +0000

    Add package ddataflow/0.1.2/DDataFlow-0.1.2.tar.gz

---
+++ packages/ddataflow/DDataFlow-0.1.2.tar.gz/DDataFlow-0.1.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ddataflow', 'ddataflow.samples']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['databricks-cli>=0.16.6,<0.17.0', 'pyspark>=3.2.1,<4.0.0']
+
+entry_points = \
+{'console_scripts': ['ddataflow = ddataflow_cli:main']}
+
+setup_kwargs = {
+    'name': 'ddataflow',
+    'version': '0.1.2',
+    'description': 'A tool for e2et tests',
+    'long_description': None,
+    'author': 'Data products GYG',
+    'author_email': 'engineering.data-products@getyourguide.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
