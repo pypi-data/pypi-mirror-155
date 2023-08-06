4cf4c2d845e66ea1f60164857424dc39ac4e665d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:54 +0000

    Add package pyspeckit/1.0.1/pyspeckit-1.0.1.tar.gz

---
+++ packages/pyspeckit/pyspeckit-1.0.1.tar.gz/pyspeckit-1.0.1/pyspeckit/tests/ipython_log_2022-05-06.py
@@ -0,0 +1,76 @@
+########################################################
+# Started Logging At: 2022-05-06 17:52:27
+########################################################
+
+########################################################
+# # Started Logging At: 2022-05-06 17:52:27
+########################################################
+sp.plotter()
+import pylab as pl; pl.ion();
+pl.draw(); pl.show()
+sp.baseline.baselinepars
+sp.baseline(subtract=False, save=False, highlight_fitregion=True,
+            exclude=[0.05,0.10], order=1)
+get_ipython().run_line_magic('pinfo', 'np.polyval')
+get_ipython().run_line_magic('run', '/home/adam/repos/pyspeckit/pyspeckit/tests/simple_fit_interactive.py')
+sp.plotter()
+sp.baseline(subtract=False, save=False, highlight_fitregion=True,
+            exclude=[0.05,0.10], order=1)
+
+pl.draw(); pl.show()
+get_ipython().run_line_magic('run', '/home/adam/repos/pyspeckit/pyspeckit/tests/baseline_test_synth.py')
+pl.close('all')
+get_ipython().run_line_magic('run', '/home/adam/repos/pyspeckit/pyspeckit/tests/baseline_test_synth.py')
+np.polyval([0.005, 1], np.arange(100))
+get_ipython().run_line_magic('run', '/home/adam/repos/pyspeckit/pyspeckit/tests/baseline_test_synth.py')
+np.polyval([0.005, 1], np.arange(100))
+baseline
+get_ipython().run_line_magic('run', '/home/adam/repos/pyspeckit/pyspeckit/tests/baseline_test_synth.py')
+sp.baseline.spectofit
+sp.baseline.errspec
+get_ipython().run_line_magic('run', '/home/adam/repos/pyspeckit/pyspeckit/tests/baseline_test_synth.py')
+pl.plot(sp.xarr, np.polyval([0.005,1], np.arange(len(sp.xarr))))
+pl.plot(sp.xarr, np.polyval([0.005,0.995], np.arange(len(sp.xarr))))
+sp.xarr
+get_ipython().run_line_magic('run', '/home/adam/repos/pyspeckit/pyspeckit/tests/baseline_test_synth.py')
+log.setLevel('DEBUG')
+from astropy import log
+log.setLevel('DEBUG')
+get_ipython().run_line_magic('run', '/home/adam/repos/pyspeckit/pyspeckit/tests/baseline_test_synth.py')
+########################################################
+# Started Logging At: 2022-05-06 18:03:46
+########################################################
+
+########################################################
+# # Started Logging At: 2022-05-06 18:03:46
+########################################################
+get_ipython().run_line_magic('debug', '')
+########################################################
+# Started Logging At: 2022-05-06 18:05:30
+########################################################
+
+########################################################
+# # Started Logging At: 2022-05-06 18:05:30
+########################################################
+########################################################
+# Started Logging At: 2022-05-06 18:16:11
+########################################################
+
+########################################################
+# # Started Logging At: 2022-05-06 18:16:12
+########################################################
+########################################################
+# Started Logging At: 2022-05-06 18:16:15
+########################################################
+
+########################################################
+# # Started Logging At: 2022-05-06 18:16:16
+########################################################
+sp.plotter.xlabel
+########################################################
+# Started Logging At: 2022-05-06 18:16:45
+########################################################
+
+########################################################
+# # Started Logging At: 2022-05-06 18:16:46
+########################################################
