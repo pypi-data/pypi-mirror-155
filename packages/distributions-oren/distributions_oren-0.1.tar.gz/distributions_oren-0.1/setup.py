f714c2385d40e72ec7dacc28104bd398deebcf0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:05:05 +0000

    Add package distributions-oren/0.1/distributions_oren-0.1.tar.gz

---
+++ packages/distributions-oren/distributions_oren-0.1.tar.gz/distributions_oren-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_oren',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_oren'],
+      zip_safe=False)
