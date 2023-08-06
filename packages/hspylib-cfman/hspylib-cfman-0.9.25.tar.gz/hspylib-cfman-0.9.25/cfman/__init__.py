1ff8e279ded08a1ae4b3543d5ab3fee2706f31dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:00 +0000

    Add package hspylib-cfman/0.9.25/hspylib-cfman-0.9.25.tar.gz

---
+++ packages/hspylib-cfman/hspylib-cfman-0.9.25.tar.gz/hspylib-cfman-0.9.25/cfman/__init__.py
@@ -0,0 +1,13 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-cfman v0.9.20
+#
+# Package: main.cfman
+"""Package initialization."""
+
+__all__ = [
+  '__main__', 
+  'core', 
+  'exception'
+]
+__version__ = '0.9.20'
