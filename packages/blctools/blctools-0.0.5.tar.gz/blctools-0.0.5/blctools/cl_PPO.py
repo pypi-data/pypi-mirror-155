f56cab6e26663fac8a7fd8b4b59cc518ea5c7ed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:23 +0000

    Add package blctools/0.0.5/blctools-0.0.5.tar.gz

---
+++ packages/blctools/blctools-0.0.5.tar.gz/blctools-0.0.5/blctools/cl_PPO.py
@@ -0,0 +1,58 @@
+
+from . import dirs
+from . import fechas
+
+from .cl_ReporteBase import *
+
+__all__ = ['PPO',]
+
+class PPO(ReporteBase):
+    
+    def __init__(
+        self,
+        fecha_i = fechas.hoy(),
+        fecha_f = fechas.hoy(),
+        nemo_rpt = 'PARTE_POST_OPERATIVO_UNIF',
+        nombre = 'PPO',
+        formato_nombre_archivo = 'PO%y%m%d',
+        parques = [],
+        extension = 'mdb',
+        tabla_datos = 'VALORES_GENERADORES',
+        tabla_fecha = 'FECHA',
+        col_filtro = 'GRUPO',
+        dir_salida = dirs.get_raiz(),
+        dir_descarga = None,
+        dir_extraccion = None,
+        funcion_archivos_necesarios = fechas.iterar_entre_timestamps_diario,
+        ):
+
+        if dir_descarga is None:
+            try:
+                dir_descarga=dirs.get_dc_ppod() + '\\00 ZIP'
+            except:
+                dir_descarga = dirs.get_raiz() + '\\00 ZIP'
+
+        if dir_extraccion is None:
+            try:
+                dir_extraccion=dirs.get_dc_ppod() + '\\01 MDB'
+            except:
+                dir_extraccion = dirs.get_raiz() + '\\01 MDB'
+        
+        super().__init__(
+            fecha_i = fecha_i,
+            fecha_f = fecha_f,
+            nemo_rpt = nemo_rpt,
+            nombre = nombre,
+            formato_nombre_archivo = formato_nombre_archivo,
+            parques = parques,
+            extension = extension,
+            tabla_datos = tabla_datos,
+            tabla_fecha = tabla_fecha,
+            col_filtro = col_filtro,
+            dir_salida = dir_salida,
+            dir_descarga = dir_descarga,
+            dir_extraccion = dir_extraccion,
+            funcion_archivos_necesarios = funcion_archivos_necesarios
+            )
+    #Fin de la función __init__
+    
