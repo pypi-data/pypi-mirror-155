6c0acd41265d2e413656c14a74bbe766e9ae171c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:46 +0000

    Add package mmmao111/1.0.0/mmmao111-1.0.0.tar.gz

---
+++ packages/mmmao111/mmmao111-1.0.0.tar.gz/mmmao111-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='mmmao111', version='1.0.0',  # 版本号
+
+      description='这是第一个对外发布的模块，测试哦',  # 描述
+
+      author='mrr0224', author_email='11695847@qq.com',
+
+      py_modules=['mmmao111.demo1', 'mmmao111.demo2']  # 要发布的模块
+      )
