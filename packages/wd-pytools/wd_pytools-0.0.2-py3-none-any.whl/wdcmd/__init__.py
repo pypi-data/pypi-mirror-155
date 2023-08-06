147fc6795869335ec0d00b082238150f666cfc1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:01 +0000

    Add package wd-pytools/0.0.2/wd_pytools-0.0.2-py3-none-any.whl

---
+++ packages/wd-pytools/wd_pytools-0.0.2-py3-none-any.whl/wdcmd/__init__.py
@@ -0,0 +1,6 @@
+from wdcmd.wdcmd_core import Entity as Entity
+from wdcmd.wdcmd_cmd_config import CmdInfo as Info
+from wdcmd.wdcmd_args_parse import Argv as Argv
+from wdcmd.wdcmd_cmd_config import CmdConfig as Option
+
+entity = Entity()
