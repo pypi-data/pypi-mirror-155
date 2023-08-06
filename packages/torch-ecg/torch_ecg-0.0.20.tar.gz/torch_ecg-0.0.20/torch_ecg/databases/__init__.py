5d59010b38f56b4e7b6370ac66f312ef350f22ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:37 +0000

    Add package torch-ecg/0.0.20/torch_ecg-0.0.20.tar.gz

---
+++ packages/torch-ecg/torch_ecg-0.0.20.tar.gz/torch_ecg-0.0.20/torch_ecg/databases/__init__.py
@@ -0,0 +1,50 @@
+"""
+"""
+
+from . import aux_data
+from .base import (
+    DataBaseInfo,
+    BeatAnn,
+    WFDB_Beat_Annotations,
+    WFDB_Non_Beat_Annotations,
+    WFDB_Rhythm_Annotations,
+)
+from .cpsc_databases import CPSC2018, CPSC2019, CPSC2020, CPSC2021
+from .nsrr_databases import SHHS
+from .physionet_databases import (
+    AFDB,
+    CINC2017,
+    CINC2018,
+    CINC2020,
+    CINC2021,
+    LTAFDB,
+    LUDB,
+    MITDB,
+    ApneaECG,
+)
+
+__all__ = [
+    # from physionet
+    "AFDB",
+    "ApneaECG",
+    "CINC2017",
+    "CINC2018",
+    "CINC2020",
+    "CINC2021",
+    "LTAFDB",
+    "LUDB",
+    "MITDB",
+    # from CPSC
+    "CPSC2018",
+    "CPSC2019",
+    "CPSC2020",
+    "CPSC2021",
+    # from NSRR
+    "SHHS",
+    "aux_data",
+    "WFDB_Beat_Annotations",
+    "WFDB_Non_Beat_Annotations",
+    "WFDB_Rhythm_Annotations",
+    "BeatAnn",
+    "DataBaseInfo",
+]
