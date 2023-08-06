96331f49d4f1a6090bf8e6e441c4541d29c83431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:32 +0000

    Add package pyngen/0.1.47/pyngen-0.1.47.tar.gz

---
+++ packages/pyngen/pyngen-0.1.47.tar.gz/pyngen-0.1.47/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+# DEPENDENCIES = open('requirements.txt', 'r').read().split('\n')
+
+setuptools.setup(
+    name='pyngen',
+    version='0.1.47',
+    scripts=['pyngen/ngen'],
+    author="CERTUNLP",
+    author_email="soporte@cert.unlp.edu.ar",
+    description="Ngen REST. A python library for using Ngen",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CERTUNLP/pyngen",
+    packages=['pyngen'],
+    install_requires=required,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+)
