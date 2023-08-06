d00cc85054f36c18c7fc0b033d75adbee07b7f52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:36 +0000

    Add package yeshan/1.0/yeshan-1.0.tar.gz

---
+++ packages/yeshan/yeshan-1.0.tar.gz/yeshan-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name = 'yeshan',
+    version = '1.0',
+    descriptin = '这是一个对外发布的模块',
+    author = 'yeshan',
+    author_email = '2871088500@qq.com',
+    py_modules = ['yeshan.demo1','yeshan.demo2']
+)
