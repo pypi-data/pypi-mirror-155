8be4375892f304aa5d2c8941175aec08f2e1f0b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:25 +0000

    Add package local-fake-useragent/1.1.0/local_fake_useragent-1.1.0.tar.gz

---
+++ packages/local-fake-useragent/local_fake_useragent-1.1.0.tar.gz/local_fake_useragent-1.1.0/user_agent/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Author: Mehaei
+# @Date:   2019-09-28 12:14:56
+# @Last Modified by:   shuaiwei
+# @Last Modified time: 2022-06-16 18:37:47
+from .main import LocalUserAgent as UserAgent
