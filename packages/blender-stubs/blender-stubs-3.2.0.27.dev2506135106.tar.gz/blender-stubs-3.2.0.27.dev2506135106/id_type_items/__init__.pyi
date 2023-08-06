6276995e862c8d548b0c578f0de7f6301aa19f4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:37 +0000

    Add package blender-stubs/3.2.0.27.dev2506135106/blender-stubs-3.2.0.27.dev2506135106.tar.gz

---
+++ packages/blender-stubs/blender-stubs-3.2.0.27.dev2506135106.tar.gz/blender-stubs-3.2.0.27.dev2506135106/id_type_items/__init__.pyi
@@ -0,0 +1,125 @@
+"""
+
+
+Id Type Items
+^^^^^^^^^^^^^
+
+:ACTION:          
+  Action.
+
+:ARMATURE:        
+  Armature.
+
+:BRUSH:           
+  Brush.
+
+:CAMERA:          
+  Camera.
+
+:CACHEFILE:       
+  Cache File.
+
+:CURVE:           
+  Curve.
+
+:FONT:            
+  Font.
+
+:GREASEPENCIL:    
+  Grease Pencil.
+
+:COLLECTION:      
+  Collection.
+
+:IMAGE:           
+  Image.
+
+:KEY:             
+  Key.
+
+:LIGHT:           
+  Light.
+
+:LIBRARY:         
+  Library.
+
+:LINESTYLE:       
+  Line Style.
+
+:LATTICE:         
+  Lattice.
+
+:MASK:            
+  Mask.
+
+:MATERIAL:        
+  Material.
+
+:META:            
+  Metaball.
+
+:MESH:            
+  Mesh.
+
+:MOVIECLIP:       
+  Movie Clip.
+
+:NODETREE:        
+  Node Tree.
+
+:OBJECT:          
+  Object.
+
+:PAINTCURVE:      
+  Paint Curve.
+
+:PALETTE:         
+  Palette.
+
+:PARTICLE:        
+  Particle.
+
+:LIGHT_PROBE:     
+  Light Probe.
+
+:SCENE:           
+  Scene.
+
+:SIMULATION:      
+  Simulation.
+
+:SOUND:           
+  Sound.
+
+:SPEAKER:         
+  Speaker.
+
+:TEXT:            
+  Text.
+
+:TEXTURE:         
+  Texture.
+
+:CURVES:          
+  Hair Curves.
+
+:POINTCLOUD:      
+  Point Cloud.
+
+:VOLUME:          
+  Volume.
+
+:WINDOWMANAGER:   
+  Window Manager.
+
+:WORLD:           
+  World.
+
+:WORKSPACE:       
+  Workspace.
+
+.. _rna-enum-id-type-items:
+
+"""
+
+import typing
