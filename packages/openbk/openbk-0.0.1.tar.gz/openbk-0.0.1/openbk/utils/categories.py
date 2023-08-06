4eb38564bf51eb94ad79029d26e4d5a5b6a82cd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:50:10 +0000

    Add package openbk/0.0.1/openbk-0.0.1.tar.gz

---
+++ packages/openbk/openbk-0.0.1.tar.gz/openbk-0.0.1/openbk/utils/categories.py
@@ -0,0 +1,14 @@
+transaction_categories_cih = {
+    "ecom_inter": "PAIEMENT INTERNET INTERNATIONAL",
+    "ecom_national": "PAIEMENT INTERNET NATIONAL",
+    "wire_in_cih": "VIREMENT RECU DE",
+    "wire_in_conf": "VIRT RECU",
+    "wire_out": "VIREMENT EMIS EN FAVEUR DE",
+    "withdrawal_gab": "RETRAIT CARTE",
+    "invoice_pay": " PAIEMENT FACTURE",
+    "card_pay": "PAIEMENT PAR CARTE",
+    "wire_fee": "COMMISSION VIREMENT EMIS",
+    "refund_card": "REGLEMENT CREDIT CARTE",
+    "ext_gab_fee": "FRAIS RETRAIT GAB CONFRERE",
+    "allow_ecom_fee": "FRAIS ANNUELS DOTATIONS"
+}
