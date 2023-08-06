d3f2ca61729e5a80250aa5997644f12ff03a9528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:16 +0000

    Add package sematic/0.0.3a2/sematic-0.0.3_alpha.2-py3-none-any.whl

---
+++ packages/sematic/sematic-0.0.3_alpha.2-py3-none-any.whl/sematic/db/models/json_encodable_mixin.py
@@ -0,0 +1,55 @@
+# Standard library
+import base64
+import datetime
+import enum
+import json
+
+
+class JSONEncodableMixin:
+    """
+    Base class for all SQLAlchemy models.
+
+    Defines JSON-encodable behavior.
+    """
+
+    def to_json_encodable(self):
+        return {
+            column.key: _to_json_encodable(getattr(self, column.key), column)
+            for column in self.__table__.columns
+        }
+
+
+JSON_KEY = "json"
+
+
+def _to_json_encodable(value, column):
+    HEX_ENCODE = "hex_encode"
+
+    info = column.info
+    if isinstance(value, bytes):
+        if info.get(HEX_ENCODE, False):
+            return value.hex()
+        else:
+            return base64.b64encode(value).decode("ascii")
+
+    if isinstance(value, datetime.datetime):
+        # SQLite does not store timezone
+        utc_value = datetime.datetime(
+            value.year,
+            value.month,
+            value.day,
+            value.hour,
+            value.minute,
+            value.second,
+            value.microsecond,
+            tzinfo=datetime.timezone.utc,
+        )
+        return utc_value.isoformat()
+
+    if isinstance(value, enum.Enum):
+        return value.value
+
+    if info.get(JSON_KEY, False) and value is not None:
+        return json.loads(value)
+
+    return value
