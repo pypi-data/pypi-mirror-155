b536ce496df7a861b601cc0095f07ee7cfb33e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:33:03 +0000

    Add package nerdler-tc/1.0.0/nerdler_tc-1.0.0.tar.gz

---
+++ packages/nerdler-tc/nerdler_tc-1.0.0.tar.gz/nerdler_tc-1.0.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="nerdler_tc",
+    version="1.0.0",
+    description="Clean text from special characters and blanks",
+    author="nerdler_pt",
+    author_email="no-reply@nerdler.pt",
+    packages=["nerdler_tc"],
+    url="https://www.nerdler.pt",
+    license="MIT",
+    platforms="any"
+)
