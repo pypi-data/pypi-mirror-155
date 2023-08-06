8a021a88f3389ea0c628e52b29dc39f2907c7ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:16 +0000

    Add package cardo-sme-sdk/0.1.2/cardo_sme_sdk-0.1.2-py3-none-any.whl

---
+++ packages/cardo-sme-sdk/cardo_sme_sdk-0.1.2-py3-none-any.whl/sme_sdk/config.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from typing import Callable, Optional
+
+
+@dataclass
+class APIConfig:
+    host: str
+    username: str
+    password: str
+
+
+@dataclass
+class S3Config:
+    access_key_id: str
+    secret_access_key: str
+    bucket_name: str
+    region_name: str
+    presigned_url_expiration_time: int = 3600
+    file_name_generator: Optional[Callable] = None
