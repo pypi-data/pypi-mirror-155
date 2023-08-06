8f17f218114c2e7ae4d0115d08dc65213c194217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:00:16 +0000

    Add package pc4g/1.0.0/pc4g-1.0.0.tar.gz

---
+++ packages/pc4g/pc4g-1.0.0.tar.gz/pc4g-1.0.0/pc4g/api/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from pc4g.api.admin_api import AdminApi
+from pc4g.api.alpha_api import AlphaApi
+from pc4g.api.customers_api import CustomersApi
+from pc4g.api.locations_api import LocationsApi
+from pc4g.api.logs_api import LogsApi
+from pc4g.api.me_api import MeApi
+from pc4g.api.utilities_api import UtilitiesApi
