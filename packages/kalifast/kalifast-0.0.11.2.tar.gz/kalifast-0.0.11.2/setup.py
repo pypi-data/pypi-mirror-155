d4a5218d3f5b38631e854c4e727cc31c0e758920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:04 +0000

    Add package kalifast/0.0.11.2/kalifast-0.0.11.2.tar.gz

---
+++ packages/kalifast/kalifast-0.0.11.2.tar.gz/kalifast-0.0.11.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='kalifast',
+    version='0.0.11.2',
+    license='MIT',
+    author="EISGE",
+    author_email='e@eisge.com',
+    packages=['kalifast'],
+    description="Python package for kalifast record npp plugin",
+    install_requires=
+    [
+        'mpi4py>=2.0',
+        'numpy',     
+        'wheel',   
+        'shutil'         
+    ],
+    # install_requires=[
+    #      'pynput','shutil','tempfile','datetime','tkinter','turtle'
+    #   ],
+
+)
