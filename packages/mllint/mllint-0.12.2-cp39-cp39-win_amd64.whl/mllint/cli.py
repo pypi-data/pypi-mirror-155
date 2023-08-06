4f1d59ee398c26e036886b100804b36347acfcc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:04 +0000

    Add package mllint/0.12.2/mllint-0.12.2-cp39-cp39-win_amd64.whl

---
+++ packages/mllint/mllint-0.12.2-cp39-cp39-win_amd64.whl/mllint/cli.py
@@ -0,0 +1,18 @@
+import subprocess
+import sys
+import os
+
+def mllint() -> int:
+  """Runs the mllint executable and returns its exit code"""
+  dirname, _ = os.path.split(__file__)
+  mllint_exe = os.path.join(dirname, 'mllint-exe')
+  os.chmod(mllint_exe, os.stat(mllint_exe).st_mode | 0o111) # Ensures mllint-exe is executable, equivalent to `chmod +x`
+
+  try:
+    return subprocess.run([mllint_exe] + sys.argv[1:], check=False, stdin=subprocess.PIPE).returncode
+  except KeyboardInterrupt:
+    return 2 # i.e. SIGINT
+
+def main():
+  """Runs the mllint executable and exits with the exit code it returned"""
+  sys.exit(mllint())
