aec6c710e75db04680fd95a31ecc3ed3d34e60b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:33:55 +0000

    Add package kgc/0.0.1/kgc-0.0.1.tar.gz

---
+++ packages/kgc/kgc-0.0.1.tar.gz/kgc-0.0.1/kgc/__main__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os, sys, json
+
+
+def main(args):
+    if args.version:
+        from .__version__ import __version__
+        print(__version__)
+
+
+def get_args():
+    import argparse
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--version", action='store_true')
+
+    args, unknow_args = parser.parse_known_args()
+
+    return args, unknow_args
+
+
+if __name__ == '__main__':
+    args, _ = get_args()
+    main(args)
