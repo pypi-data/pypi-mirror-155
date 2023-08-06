5d5533eb040d40f74cfe8a6a2a0d44f9658c903c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:35 +0000

    Add package xww/1.0/xww-1.0.tar.gz

---
+++ packages/xww/xww-1.0.tar.gz/xww-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup (
+    name = 'xww',
+    version = '1.0',
+    description = '这是第一个对外发布的模板，测试',
+    author = 'xww',
+    author_email= '2541228551@qq.com',
+    py_modules=['xww.demo1','xww.demo2']
+)
