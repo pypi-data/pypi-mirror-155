abaaeb80f16818447301df34a331151d1c1296b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:49 +0000

    Add package trueway-geocoding-rapidapi/0.1/trueway_geocoding_rapidapi-0.1.tar.gz

---
+++ packages/trueway-geocoding-rapidapi/trueway_geocoding_rapidapi-0.1.tar.gz/trueway_geocoding_rapidapi-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='trueway_geocoding_rapidapi',
+      version='0.1',
+      description='TrueWay Geocoding API on RapidAPI',
+      packages=['trueway_geocoding_rapidapi'],
+      author_email='dshvedov@kotspin.ru',
+      zip_safe=False)
