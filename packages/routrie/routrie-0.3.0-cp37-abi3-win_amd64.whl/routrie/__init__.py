662e8893fe391ec69dba301e8e27be7d6a0b9348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:20:52 +0000

    Add package routrie/0.3.0/routrie-0.3.0-cp37-abi3-win_amd64.whl

---
+++ packages/routrie/routrie-0.3.0-cp37-abi3-win_amd64.whl/routrie/__init__.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+import typing
+
+from routrie._routrie import Router as _Router
+
+T = typing.TypeVar("T")
+
+Params = typing.List[typing.Tuple[str, str]]
+
+
+class Router(typing.Generic[T]):
+    __slots__ = ("_router",)
+
+    _router: "_Router[T]"
+
+    def __init__(self) -> None:
+        self._router = _Router()
+
+    def insert(self, path: str, value: T) -> None:
+        self._router.insert(path, value)
+
+    def find(self, path: str) -> typing.Tuple[T, Params]:
+        return self._router.find(path)
