24e461d3def6a74b96ed5290b6c42b789cda6a11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:50 +0000

    Add package nwon-django-toolbox/0.3.5/nwon_django_toolbox-0.3.5-py3-none-any.whl

---
+++ packages/nwon-django-toolbox/nwon_django_toolbox-0.3.5-py3-none-any.whl/nwon_django_toolbox/fields/__init__.py
@@ -0,0 +1,15 @@
+from nwon_django_toolbox.fields.pydantic_json_field import (
+    ModelSerializerWithPydantic,
+    PydanticJsonField,
+    PydanticJsonFieldSerializer,
+)
+from nwon_django_toolbox.fields.serializer_choice_field_for_polymorphic_ctype_id import (
+    serializer_choice_field_for_polymorphic_ctype_id,
+)
+
+__all__ = [
+    "serializer_choice_field_for_polymorphic_ctype_id",
+    "PydanticJsonFieldSerializer",
+    "PydanticJsonField",
+    "ModelSerializerWithPydantic",
+]
