8d5df768bc415a5fbe0422004e853338e512e381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:03 +0000

    Add package async-stripe/2.76.0/async-stripe-2.76.0.tar.gz

---
+++ packages/async-stripe/async-stripe-2.76.0.tar.gz/async-stripe-2.76.0/async_stripe/api_resources/customer.py
@@ -0,0 +1,49 @@
+import stripe
+from async_stripe.api_resources.abstract import (
+    patch_nested_resources, patch_custom_methods
+)
+from stripe import api_requestor
+from stripe import util
+
+
+async def delete_discount_patch(self, **params):
+    requestor = api_requestor.APIRequestor(
+        self.api_key,
+        api_version=self.stripe_version,
+        account=self.stripe_account,
+    )
+    url = self.instance_url() + "/discount"
+    _, api_key = await requestor.request("delete", url, params)
+    self.refresh_from({"discount": None}, api_key, True)
+
+async def list_payment_methods_patch(self, idempotency_key=None, **params):
+    url = self.instance_url() + "/payment_methods"
+    headers = util.populate_headers(idempotency_key)
+    resp = await self.request("get", url, params, headers)
+    stripe_object = util.convert_to_stripe_object(resp)
+    stripe_object._retrieve_params = params
+    return stripe_object
+
+async def create_funding_instructions_patch(self, idempotency_key=None, **params):
+    url = self.instance_url() + "/funding_instructions"
+    headers = util.populate_headers(idempotency_key)
+    resp = await self.request("post", url, params, headers)
+    stripe_object = util.convert_to_stripe_object(resp)
+    return stripe_object
+
+
+stripe.Customer.delete_discount = delete_discount_patch
+stripe.Customer.list_payment_methods = list_payment_methods_patch
+
+
+netsted_resources = ['balance_transaction', 'source', 'tax_id', 'cash_balance']
+patch_nested_resources(stripe.Customer, netsted_resources)
+
+
+# always patch custom methods after patching other methods
+custom_resources = [
+    {"name": "delete_discount", "http_verb": "delete", "http_path": "discount"},
+    {"name": "list_payment_methods", "http_verb": "get", "http_path": "payment_methods"},
+    {"name": "create_funding_instructions", "http_verb": "post", "http_path": "funding_instructions"},
+]
+patch_custom_methods(stripe.Customer, custom_resources)
