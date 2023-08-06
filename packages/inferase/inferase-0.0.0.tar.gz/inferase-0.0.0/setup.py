b78ab82783f37cb640f19796782834bd1ef30e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:07 +0000

    Add package inferase/0.0.0/inferase-0.0.0.tar.gz

---
+++ packages/inferase/inferase-0.0.0.tar.gz/inferase-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='inferase',
+    version='0.0.0',
+    description='automated inference of integration and edits',
+    long_description='',
+    url='',
+    author='Ayaan Hossain',
+    author_email='',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
