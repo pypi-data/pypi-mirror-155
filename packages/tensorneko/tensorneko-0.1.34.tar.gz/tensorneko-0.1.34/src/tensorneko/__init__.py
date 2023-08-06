ba499f31043d69c5f7eea5fffe65d55549602e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:56 +0000

    Add package tensorneko/0.1.34/tensorneko-0.1.34.tar.gz

---
+++ packages/tensorneko/tensorneko-0.1.34.tar.gz/tensorneko-0.1.34/src/tensorneko/__init__.py
@@ -0,0 +1,33 @@
+import os.path
+
+from . import callback
+from . import evaluation
+from . import io
+from . import layer
+from . import module
+from . import notebook
+from . import optim
+from . import preprocess
+from . import util
+from . import visualization
+from .neko_module import NekoModule
+from .neko_model import NekoModel
+from .neko_trainer import NekoTrainer
+
+__all__ = [
+    "callback",
+    "evaluation",
+    "io",
+    "layer",
+    "module",
+    "notebook",
+    "optim",
+    "preprocess",
+    "util",
+    "visualization",
+    "NekoModel",
+    "NekoTrainer",
+    "NekoModule"
+]
+
+__version__ = io.read.text.of(os.path.join(util.get_tensorneko_path(), "version.txt"))
