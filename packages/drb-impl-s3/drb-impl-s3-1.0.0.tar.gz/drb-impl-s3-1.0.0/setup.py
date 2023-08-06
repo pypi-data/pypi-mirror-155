03af5c3abecba243f1a9d5b53a42fb16f2100b10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:40 +0000

    Add package drb-impl-s3/1.0.0/drb-impl-s3-1.0.0.tar.gz

---
+++ packages/drb-impl-s3/drb-impl-s3-1.0.0.tar.gz/drb-impl-s3-1.0.0/setup.py
@@ -0,0 +1,39 @@
+import versioneer
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    REQUIREMENTS = f.readlines()
+
+with open('README.md') as fh:
+    long_description = fh.read()
+
+setup(
+    name='drb-impl-s3',
+    packages=['drb_impl_s3'],
+    description='DRB aws3 implementation',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='GAEL Systems',
+    author_email='drb-python@gael.fr',
+    url='https://gitlab.com/drb-python/impl/aws3',
+    install_requires=REQUIREMENTS,
+    test_suite='tests',
+    data_files=[('.', ['requirements.txt'])],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.8',
+    entry_points={'drb.impl': 'aws3 = drb_impl_s3'},
+    package_data={
+        'drb_impl_s3': ['cortex.yml']
+    },
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    project_urls={
+        'Documentation': 'https://drb-python.gitlab.io/impl/aws3/',
+        'Source': 'https://gitlab.com/drb-python/impl/aws3',
+    }
+
+)
