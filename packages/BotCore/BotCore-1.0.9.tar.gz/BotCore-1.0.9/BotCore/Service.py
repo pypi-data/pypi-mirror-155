0366dada6f4f22cc89bc202b937c9a7a00b3f038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:19 +0000

    Add package BotCore/1.0.9/BotCore-1.0.9.tar.gz

---
+++ packages/BotCore/BotCore-1.0.9.tar.gz/BotCore-1.0.9/BotCore/Service.py
@@ -0,0 +1,30 @@
+
+from typing import TypeVar, Type
+
+from BotCore import DAO
+from BotCore.ModelInfo import ModelInfo
+
+T = TypeVar("T", bound=ModelInfo)
+D = TypeVar("D", bound=DAO)
+
+
+class Service:
+
+	def __init__(self, model: Type[T], dao: D):
+		self.model = model
+		self.dao = dao
+
+	def create(self, **kwargs):
+		return self.dao.create(**kwargs)
+
+	def delete(self, row_id):
+		return self.dao.delete(row_id)
+
+	def getall(self):
+		return self.dao.getall()
+
+	def to_objects(self, rows: list[tuple]) -> list[T]:
+		return [self.to_object(row) for row in rows]
+
+	def to_object(self, row: tuple) -> T:
+		return self.model(dict(zip(self.model.fields(), row)))
