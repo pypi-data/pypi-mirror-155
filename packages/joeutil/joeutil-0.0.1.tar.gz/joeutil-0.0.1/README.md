49fb5ee255dfd60c2c0efe31c8bf1d2476e0630c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:16 +0000

    Add package joeutil/0.0.1/joeutil-0.0.1.tar.gz

---
+++ packages/joeutil/joeutil-0.0.1.tar.gz/joeutil-0.0.1/README.md
@@ -0,0 +1,5 @@
+# Example Package
+
+This is a simple example package. You can use
+[Github-flavored Markdown](https://https://https://github.com/ksky1313/ADP/mastering-markdown/)
+to write your content.
---
+++ packages/joeutil/joeutil-0.0.1.tar.gz/joeutil-0.0.1/src/joeutil/__init__.py
@@ -0,0 +1,7 @@
+"""
+Description for Package
+"""
+from joeutil import adptools
+
+__all__ = ['adptools'] # 이 배열에 포함되는 모듈의 이름은 from algorithms import * 를 통해서도 참조될 수 있다.
+__version__ = '0.0.1' # 패키지의 버전을 정의한다.
