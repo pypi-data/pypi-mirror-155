1980daec8f3580f0766930d6ea1c0975f236b30b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:21:48 +0000

    Add package tabular-data-explorer/0.1.0/tabular_data_explorer-0.1.0-py3-none-any.whl

---
+++ packages/tabular-data-explorer/tabular_data_explorer-0.1.0-py3-none-any.whl/tabular_data_explorer/__init__.py
@@ -0,0 +1,14 @@
+# Author: Simon Blanke
+# Email: simon.blanke@yahoo.com
+# License: MIT License
+
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+from .tabular_data_explorer import TabularDataExplorer
+
+
+__all__ = [
+    "OptimizationPlotter",
+]
