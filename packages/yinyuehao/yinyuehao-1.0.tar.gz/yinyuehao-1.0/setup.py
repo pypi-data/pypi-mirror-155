c2ffb1825776aff3c39b40d4da9fd5c375e082c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:59 +0000

    Add package yinyuehao/1.0/yinyuehao-1.0.tar.gz

---
+++ packages/yinyuehao/yinyuehao-1.0.tar.gz/yinyuehao-1.0/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+
+    name='yinyuehao',  # 对外我们模块的名字
+
+    version='1.0',  # 版本号
+
+    description='这是第一个对外发布的模块，测试哦',  # 描述
+
+    author='gaoqi',  # 作者
+
+    author_email='gaoqi110@163.com',
+
+    py_modules=['yinyuehao.demo1', 'yinyuehao.demo2']  # 要发布的模块
+
+)
+
+
