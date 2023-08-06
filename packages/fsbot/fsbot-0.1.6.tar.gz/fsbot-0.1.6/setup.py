4f09d509ccb4a1f959042a3555d06e235f6b61b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:46 +0000

    Add package fsbot/0.1.6/fsbot-0.1.6.tar.gz

---
+++ packages/fsbot/fsbot-0.1.6.tar.gz/fsbot-0.1.6/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: zhaojunhang
+# Mail: 454706391@qq.com
+# Created Time:  2022-05-07
+#############################################
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "fsbot",
+    version = "0.1.6",
+    keywords = ("feishu"),
+    description = "fei shu notification",
+    long_description = "fei shu notification",
+    license = "MIT Licence",
+
+    url = "https://github.com/zhaojunhang/feishu_notification.git",
+    author = "zhaojunhang",
+    author_email = "454706391@qq.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ['requests','urllib3']
+)
