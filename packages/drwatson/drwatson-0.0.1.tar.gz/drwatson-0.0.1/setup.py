c7547a301a447591e86e9dab912bd1a48bdc3d28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:12 +0000

    Add package drwatson/0.0.1/drwatson-0.0.1.tar.gz

---
+++ packages/drwatson/drwatson-0.0.1.tar.gz/drwatson-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="drwatson",
+    version="0.0.1",
+    author="Marco Vela",
+    description="A scientific assistant inspired by the homonimous Julia package",
+    packages=["drwatson"],
+)
