d4579b1d107208227e18aad37322a7df06c78866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:44 +0000

    Add package matcouply/0.1.1/matcouply-0.1.1-py3-none-any.whl

---
+++ packages/matcouply/matcouply-0.1.1-py3-none-any.whl/matcouply/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__author__ = """Marie Roald"""
+__email__ = "roald.marie@gmail.com"
+__version__ = "0.1.1"
+
+
+from . import coupled_matrices, data, decomposition, penalties, random
