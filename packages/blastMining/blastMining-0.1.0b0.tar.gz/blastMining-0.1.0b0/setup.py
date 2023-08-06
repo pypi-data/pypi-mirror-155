29284711c1a8838d14a38462d307cbc3ecdbe2c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:19 +0000

    Add package blastMining/0.1.0b0/blastMining-0.1.0b0.tar.gz

---
+++ packages/blastMining/blastMining-0.1.0b0.tar.gz/blastMining-0.1.0b0/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+requirements = ["numpy >= 1.22.3",
+	"pandas>=1.4.2",
+	"fastnumbers>=3.2.1"]
+
+setup(
+    name='blastMining',
+    version = '0.1.0-beta',
+    author = 'Ahmad Nuruddin Khoiri',
+    author_email = 'nuruddinkhoiri34@gmail.com',
+    long_description = 'Mining NCBI BLAST output with vote and lca methods',
+    license = 'GPLv3',
+    packages=find_packages(),
+    entry_points = {'console_scripts': ['blastMining = blastMining.blastMining:main']},
+    scripts=['blastMining/script/blastMining_lca.sh', 'blastMining/script/blastMining_lca2.sh'],
+    url='https://github.com/NuruddinKhoiry/blastMining.git',
+    python_requires='>=3',
+    install_requires=requirements,
+
+)
+
+
