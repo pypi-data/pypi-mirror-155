58e3dba0b2d909dc1025b42ca862d345f357e057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:26 +0000

    Add package estnltk/1.7.0/estnltk-1.7.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/estnltk/estnltk-1.7.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/estnltk/web_taggers/v01/stanza_syntax_ensemble_web_tagger.py
@@ -0,0 +1,23 @@
+from typing import MutableMapping
+
+from estnltk import Text
+from estnltk import Layer
+from estnltk.web_taggers import BatchProcessingWebTagger
+
+class StanzaSyntaxEnsembleWebTagger( BatchProcessingWebTagger ):
+    """Tags dependency syntactic analysis using EstNLTK StanzaSyntaxEnsembleTagger's webservice.
+
+    See also StanzaSyntaxEnsembleTagger's documentation:
+    https://nbviewer.jupyter.org/github/estnltk/estnltk/blob/devel_1.6/tutorials/syntax/syntax.ipynb
+    """
+
+    def __init__(self, url, output_layer='stanza_ensemble_syntax'):
+        self.url = url
+        self.output_layer = output_layer
+        self.output_attributes = ('id', 'lemma', 'upostag', 'xpostag', 'feats', 'head', 'deprel', 'deps', 'misc')
+        self.input_layers = ['words', 'sentences', 'morph_extended']
+        self.batch_layer            = self.input_layers[0]
+        self.batch_layer_max_size   = 125
+        self.batch_enveloping_layer = self.input_layers[1]
+
+
