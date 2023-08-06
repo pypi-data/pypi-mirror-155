8bbcd00374da3bea35f2fd0dfb2dc4e83bc1e89d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:25 +0000

    Add package edsnlp/0.6.0/edsnlp-0.6.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/edsnlp/edsnlp-0.6.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/edsnlp/pipelines/ner/scores/charlson/factory.py
@@ -0,0 +1,43 @@
+from typing import Any, Callable, List, Union
+
+from spacy.language import Language
+
+from edsnlp.pipelines.ner.scores import Score
+from edsnlp.pipelines.ner.scores.charlson import patterns
+from edsnlp.utils.deprecation import deprecated_factory
+
+DEFAULT_CONFIG = dict(
+    regex=patterns.regex,
+    after_extract=patterns.after_extract,
+    score_normalization=patterns.score_normalization_str,
+    attr="NORM",
+    window=7,
+    verbose=0,
+    ignore_excluded=False,
+)
+
+
+@deprecated_factory("charlson", "eds.charlson", default_config=DEFAULT_CONFIG)
+@Language.factory("eds.charlson", default_config=DEFAULT_CONFIG)
+def create_component(
+    nlp: Language,
+    name: str,
+    regex: List[str],
+    after_extract: str,
+    score_normalization: Union[str, Callable[[Union[str, None]], Any]],
+    attr: str,
+    window: int,
+    verbose: int,
+    ignore_excluded: bool,
+):
+    return Score(
+        nlp,
+        score_name=name,
+        regex=regex,
+        after_extract=after_extract,
+        score_normalization=score_normalization,
+        attr=attr,
+        window=window,
+        verbose=verbose,
+        ignore_excluded=ignore_excluded,
+    )
