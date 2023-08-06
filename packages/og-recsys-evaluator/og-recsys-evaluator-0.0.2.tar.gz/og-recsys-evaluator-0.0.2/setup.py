355cb071cf14f0621a020cc69374b3b87f117eed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:46 +0000

    Add package og-recsys-evaluator/0.0.2/og-recsys-evaluator-0.0.2.tar.gz

---
+++ packages/og-recsys-evaluator/og-recsys-evaluator-0.0.2.tar.gz/og-recsys-evaluator-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2' 
+DESCRIPTION = 'Recsys Evaluator package'
+LONG_DESCRIPTION = 'Recsys Evaluator package'
+
+# Setting up
+setup(
+        name="og-recsys-evaluator", 
+        version=VERSION,
+        author="OG",
+        author_email="blogphp.biz@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['surprise'], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'evaluator'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
