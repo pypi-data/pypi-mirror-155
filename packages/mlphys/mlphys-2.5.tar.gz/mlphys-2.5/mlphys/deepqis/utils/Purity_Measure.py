b02aa0f83b961ae6b2f37e679894fc6a38765112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:56:45 +0000

    Add package mlphys/2.5/mlphys-2.5.tar.gz

---
+++ packages/mlphys/mlphys-2.5.tar.gz/mlphys-2.5/mlphys/deepqis/utils/Purity_Measure.py
@@ -0,0 +1,11 @@
+"""
+author: Sanjaya Lohani
+email: slohani@mlphys_nightly.com
+Licence: Apache-2.0
+"""
+import tensorflow as tf
+
+
+def purity(dm):
+    mul = tf.math.real(tf.linalg.trace(tf.linalg.matmul(dm, dm, adjoint_b=True)))
+    return mul.numpy()
