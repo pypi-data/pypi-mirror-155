db437ecc96cefcfc34889e55318593f6d60694e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:35 +0000

    Add package jas-nester/1.3.0/jas_nester-1.3.0.tar.gz

---
+++ packages/jas-nester/jas_nester-1.3.0.tar.gz/jas_nester-1.3.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name         = 'jas_nester',
+    author       = 'Jjhar Singh',
+    author_email = 'sage_r3vok@icloud.com',
+    version      = '1.3.0',
+    description  = """This function takes a positional argument called “the_list",
+    which is any Python list (of, possibly, nested lists).
+    Each data item in the provided list is (recursively)
+    printed to the screen on its own line."""
+    
+    )
