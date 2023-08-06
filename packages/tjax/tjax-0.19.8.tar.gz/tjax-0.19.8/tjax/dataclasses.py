435121e264c3ba6842dd35c247195bd97f8f6493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:17:29 +0000

    Add package tjax/0.19.8/tjax-0.19.8.tar.gz

---
+++ packages/tjax/tjax-0.19.8.tar.gz/tjax-0.19.8/tjax/dataclasses.py
@@ -0,0 +1,5 @@
+from ._src.dataclasses.dataclass import dataclass
+from ._src.dataclasses.dataclass_transform import dataclass_transform
+from ._src.dataclasses.helpers import as_shallow_dict, field
+
+__all__ = ['dataclass', 'dataclass_transform', 'as_shallow_dict', 'field']
