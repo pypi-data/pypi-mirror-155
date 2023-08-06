6a7a9e0dff91a04cc6039ed096e735414d6b0ffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:47 +0000

    Add package xuchangxin/1.0/xuchangxin-1.0.tar.gz

---
+++ packages/xuchangxin/xuchangxin-1.0.tar.gz/xuchangxin-1.0/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+
+name='xuchangxin',	# 对外我们模块的名字
+
+version='1.0', # 版本号
+
+description='这是第一个对外发布的模块，测试哦',	#描述
+
+author='luoluo', # 作者
+
+author_email='910817507@qq.com',
+
+py_modules=['xuchangxin.demo1','xuchangxin.demo2'] # 要发布的模块
+
+)
