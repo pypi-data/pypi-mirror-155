b208cfb8a92339af41ad08e56eb994b250347301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:26 +0000

    Add package anvil-stubs/0.0.1/anvil_stubs-0.0.1-py2.py3-none-any.whl

---
+++ packages/anvil-stubs/anvil_stubs-0.0.1-py2.py3-none-any.whl/anvil-stubs/__init__.py
@@ -0,0 +1,8 @@
+"""Stub files for anvil"""
+# SPDX-License-Identifier: MIT
+#
+# Copyright (c) 2021 The Anvil Stubs project team members listed at
+# https://github.com/anvilistas/anvil-stubs/graphs/contributors
+#
+# This software is published at https://github.com/anvilistas/anvil-stubs
+__version__ = "0.0.1"
