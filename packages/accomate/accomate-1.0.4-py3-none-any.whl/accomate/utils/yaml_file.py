c9e4d191115bd6303d0a37a281cc88dd0bd7a5e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:12 +0000

    Add package accomate/1.0.4/accomate-1.0.4-py3-none-any.whl

---
+++ packages/accomate/accomate-1.0.4-py3-none-any.whl/accomate/utils/yaml_file.py
@@ -0,0 +1,18 @@
+import yaml
+
+def read_yaml_file(path: str) -> dict:
+    """
+    Reads a file and returns its contents.
+    """
+    with open(path, "r") as file:
+        parsed_yaml = yaml.safe_load(file)
+        return parsed_yaml
+
+
+def write_yaml_file(path: str, data: dict) -> bool:
+    try:
+        with open(path, "w") as file:
+            yaml.dump(data, file, sort_keys=False, default_flow_style=False)
+            return True
+    except Exception as e:
+        return False
