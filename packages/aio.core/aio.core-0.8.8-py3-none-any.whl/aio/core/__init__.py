23a5bc955dc3b1bc719d38cf8206503ba29c02c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:23 +0000

    Add package aio.core/0.8.8/aio.core-0.8.8-py3-none-any.whl

---
+++ packages/aio.core/aio.core-0.8.8-py3-none-any.whl/aio/core/__init__.py
@@ -0,0 +1,23 @@
+
+from . import (
+    dev,
+    directory,
+    event,
+    functional,
+    pipe,
+    stream,
+    subprocess,
+    tasks,
+    utils)
+
+
+__all__ = (
+    "dev",
+    "directory",
+    "event",
+    "functional",
+    "pipe",
+    "stream",
+    "subprocess",
+    "tasks",
+    "utils")
---
+++ packages/aio.core/aio.core-0.8.8-py3-none-any.whl/aio/core/pipe/interface.py
@@ -0,0 +1,38 @@
+
+import argparse
+
+from typing import Any
+
+import abstracts
+
+
+class IProcessor(metaclass=abstracts.Interface):
+
+    @abstracts.interfacemethod
+    def __call__(self, *args):
+        raise NotImplementedError
+
+
+class IStdinStdoutProcessor(IProcessor, metaclass=abstracts.Interface):
+
+    @abstracts.interfacemethod
+    def __init__(self, processor, stdin=None, stdout=None, log=None):
+        raise NotImplementedError
+
+
+class IProcessProtocol(metaclass=abstracts.Interface):
+
+    @abstracts.interfacemethod
+    def __init__(
+            self,
+            processor: IProcessor,
+            args: argparse.Namespace) -> None:
+        raise NotImplementedError
+
+    @abstracts.interfacemethod
+    async def __call__(self, request: Any) -> Any:
+        raise NotImplementedError
+
+    @abstracts.interfacemethod
+    async def process(self, request: Any) -> Any:
+        raise NotImplementedError
