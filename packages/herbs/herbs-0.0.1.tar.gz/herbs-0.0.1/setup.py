eb851c89070b10453bddd0c07014af70f1780693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:58 +0000

    Add package herbs/0.0.1/herbs-0.0.1.tar.gz

---
+++ packages/herbs/herbs-0.0.1.tar.gz/herbs-0.0.1/setup.py
@@ -0,0 +1,33 @@
+"""
+@author: Jingyi GF{jingyi.g.fuglstad@gmail.com}
+"""
+import os
+from setuptools import setup, find_packages
+exec(open('herbs/version.py').read())
+
+
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="herbs",
+    version=__version__,
+    author="Jingyi GF",
+    author_email="jingyi.g.fuglstad@gmail.com",
+    description="A toolkit for Histological E-data Registration in Brain Space",
+    keywords="brain atlas, histological image registration, probe coordinates",
+    url="https://github.com/JingyiGF/HERBS",
+    packages=find_packages(exclude=[]),
+    long_description=read('README.md'),
+    long_description_content_type="text/markdown",
+    project_urls={
+        "Bug Tracker": "https://github.com/JingyiGF/HERBS/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
