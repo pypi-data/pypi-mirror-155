55e087f3876f0480691cf2d396b09697e714ac25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:11 +0000

    Add package python-fundb/0.0.15/python_fundb-0.0.15-py3-none-any.whl

---
+++ packages/python-fundb/python_fundb-0.0.15-py3-none-any.whl/fundb/__init__.py
@@ -0,0 +1,8 @@
+#-----------------------------
+# -- fundb --
+#-----------------------------
+
+from . import adapters, jsonquery, lib, database, cursor
+
+# alias to Database
+fundb = database.Database
