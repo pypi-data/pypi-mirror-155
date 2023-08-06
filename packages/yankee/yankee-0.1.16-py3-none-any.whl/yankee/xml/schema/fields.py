00daeb153b6ee7f645e5710f02cd93f72c1300fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:42 +0000

    Add package yankee/0.1.16/yankee-0.1.16-py3-none-any.whl

---
+++ packages/yankee/yankee-0.1.16-py3-none-any.whl/yankee/xml/schema/fields.py
@@ -0,0 +1,66 @@
+from yankee.base import fields
+
+from .key import XmlMixin
+
+
+class Field(XmlMixin, fields.Field):
+    pass
+
+
+class String(XmlMixin, fields.String):
+    pass
+
+
+class DateTime(XmlMixin, fields.DateTime):
+    pass
+
+
+class Date(XmlMixin, fields.Date):
+    pass
+
+
+class Boolean(XmlMixin, fields.Boolean):
+    pass
+
+
+class Float(XmlMixin, fields.Float):
+    pass
+
+
+class Integer(XmlMixin, fields.Integer):
+    pass
+
+
+class Exists(XmlMixin, fields.Exists):
+    pass
+
+
+class Const(XmlMixin, fields.Const):
+    pass
+
+
+class List(XmlMixin, fields.List):
+    pass
+
+
+class Combine(XmlMixin, fields.Combine):
+    pass
+
+
+class Alternative(XmlMixin, fields.Alternative):
+    pass
+
+
+class Zip(XmlMixin, fields.Zip):
+    _list_field = List
+
+class DelimitedString(XmlMixin, fields.DelimitedString):
+    pass
+
+
+# Aliases
+Str = String
+DT = DateTime
+Bool = Boolean
+Int = Integer
+Alt = Alternative
