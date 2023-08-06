a79197cc4e143dd8fcd7d9b1629bf47003aa4b8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:30 +0000

    Add package python-qa/0.0.4/python-qa-0.0.4.tar.gz

---
+++ packages/python-qa/python-qa-0.0.4.tar.gz/python-qa-0.0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-qa",
+    version="0.0.4",
+    author="Starkov E.G.",
+    author_email="Starkov.Ev.Ge@gmail.com",
+    license="Apache 2.0",
+    description="Simplify the QA of your product",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Starkov-EG/python-qa",
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Software Development :: Testing",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+)
