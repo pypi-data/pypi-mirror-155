cf7232e565648327860d316c64ee3400f930fd94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:29 +0000

    Add package notetiktok/0.0.21/notetiktok-0.0.21.tar.gz

---
+++ packages/notetiktok/notetiktok-0.0.21.tar.gz/notetiktok-0.0.21/notetiktok/server/data_service.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+from notebuild.tool.fastapi import add_api_routes, api_route
+from notetiktok.data import ResourceType, resource_db
+
+
+class ResourceService(APIRouter):
+    def __init__(self, prefix='/resource', *args, **kwargs):
+        super(ResourceService, self).__init__(prefix=prefix, *args, **kwargs)
+        add_api_routes(self)
+
+    @api_route('/get', description="get value")
+    def get_resource(self, page_no=1, page_size=10):
+        return resource_db.get_resource(start=page_size * (page_no - 1),
+                                        stop=page_size * page_no,
+                                        resource_type=ResourceType.VIDEO)
