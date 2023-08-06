b8272097e26edddbbd8199d6e3467c1e46b8e889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:50 +0000

    Add package meme-python/1.0.3/meme-python-1.0.3.tar.gz

---
+++ packages/meme-python/meme-python-1.0.3.tar.gz/meme-python-1.0.3/README.md
@@ -0,0 +1,24 @@
+# meme-python
+ 
+Lets you easily generate text-based memes on images or gifs.
+```py
+import meme
+
+output = meme.generate(
+    image='nerd.png',
+    text='POV: You don\'t use meme-python'
+)
+
+output.show()
+```
+![example](examples/output/meme.png)
+
+
+## Setup
+1. Clone this repository (or download it as a .zip package)
+2. `pip install -r requirements.txt`
+    - *Attention Windows users:* Most python installations use the `py` version manager, so pip is accessed like `py -m pip install -r requirements.txt`
+    - *Attention Linux/MacOS users:* Most computers default to using Python version 2.x, you will need to speficially use Python 3.x like `pip3 -m pip install -r requirements.txt`
+
+## Docs
+TODO
---
+++ packages/meme-python/meme-python-1.0.3.tar.gz/meme-python-1.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='meme-python',
+    version='1.0.3',
+    license='MIT',
+    author='Gabe Millikan',
+    url='https://github.com/GabeMillikan/meme-python',
+    install_requires=['Pillow'],
+    python_requires='>=3',
+    description='Meme Generator',
+    long_description='Meme Generator. See [repository on Github](https://github.com/GabeMillikan/meme-python).',
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    packages=find_packages(include=['meme']),
+    package_data={
+        'meme': ['static/*'],
+    },
+)
