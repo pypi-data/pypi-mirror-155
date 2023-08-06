51a4701b7668f6282acc1923448add8c8b9ab470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:22 +0000

    Add package wlx123456789/1.0/wlx123456789-1.0.tar.gz

---
+++ packages/wlx123456789/wlx123456789-1.0.tar.gz/wlx123456789-1.0/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+
+name='wlx123456789',	# 对外我们模块的名字
+
+version='1.0', # 版本号
+
+description='这是第一个对外发布的模块，测试哦',	#描述
+
+author='gaoqi', # 作者
+
+author_email='gaoqi110@163.com',
+
+py_modules=['wlx123456789.demo1','wlx123456789.demo2'] # 要发布的模块
+
+)
