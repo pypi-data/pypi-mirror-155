bf0a58102f0c6bcab4d9c3f6146d8661e3b7c632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:25 +0000

    Add package x21/0.3.1/x21-0.3.1-cp39-cp39-musllinux_1_1_i686.whl

---
+++ packages/x21/x21-0.3.1-cp39-cp39-musllinux_1_1_i686.whl/x21/_cli.py
@@ -0,0 +1,48 @@
+import argparse
+from sys import version_info
+
+from .__about__ import __version__
+from ._encrypt_path import encrypt_path
+
+
+def cli(argv=None):
+    parser = argparse.ArgumentParser(
+        description=("x21 build tools."),
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+
+    parser.add_argument(
+        "--version",
+        "-v",
+        action="version",
+        version=get_version_text(),
+        help="display version information",
+    )
+
+    subparsers = parser.add_subparsers(title="subcommands", required=True)
+
+    parser_encrypt_path = subparsers.add_parser(
+        "encrypt-path",
+        help="Encrypt Python code in .py or .whl",
+        aliases=["e"],
+    )
+    parser_encrypt_path.add_argument(
+        "input_file",
+        type=str,
+        help="Python file/directory to encrypt",
+    )
+    parser_encrypt_path.set_defaults(func=lambda args: encrypt_path(args.input_file))
+
+    args = parser.parse_args(argv)
+    return args.func(args)
+
+
+def get_version_text():
+    python_version = f"{version_info.major}.{version_info.minor}.{version_info.micro}"
+    version_text = "\n".join(
+        [
+            f"x21 {__version__} [Python {python_version}]",
+            "Copyright (c) 2022 Nico Schlömer <nico.schloemer@gmail.com>",
+        ]
+    )
+    return version_text
