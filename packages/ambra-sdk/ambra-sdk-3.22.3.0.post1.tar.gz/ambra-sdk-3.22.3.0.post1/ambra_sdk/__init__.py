dd4041d2bac5db3ac789b2aa4135a6d7a17c199b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:31 +0000

    Add package ambra-sdk/3.22.3.0.post1/ambra-sdk-3.22.3.0.post1.tar.gz

---
+++ packages/ambra-sdk/ambra-sdk-3.22.3.0.post1.tar.gz/ambra-sdk-3.22.3.0.post1/ambra_sdk/__init__.py
@@ -0,0 +1,14 @@
+__version__ = '3.22.3.0-1'
+
+# from footer in api.html
+API_VERSION = 'LBL0022 v54.0 2022-05-25'
+
+# equal for API_VERSION based on dictionary.html
+MODELS_VERSION = API_VERSION
+
+# from storage_api.html
+STORAGE_VERSION = 'LBL0038 v22.0 2022-05-18'
+
+DOCS_URL = 'https://dicomgrid.github.io/sdk-python'
+ADDON_DOCS_URL = '{docs_url}/addon.html'.format(docs_url=DOCS_URL)
+
