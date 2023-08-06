e4afa809c78a3b8a8022379eae322607a22d9507
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:43 +0000

    Add package weilanbaizhanMath2/1.0/weilanbaizhanMath2-1.0.tar.gz

---
+++ packages/weilanbaizhanMath2/weilanbaizhanMath2-1.0.tar.gz/weilanbaizhanMath2-1.0/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+
+name='weilanbaizhanMath2',	# 对外我们模块的名字
+
+version='1.0', # 版本号
+
+description='这是第一个对外发布的模块，测试哦',	#描述
+
+author='lanwei', # 作者
+
+author_email='lanwei236@163.com',
+
+py_modules=['weilanbaizhanMath2.demo1','weilanbaizhanMath2.demo2'] # 要发布的模块
+
+)
