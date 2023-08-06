6aeb622d654022adfd4649644d8dfe60059ef088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:17:47 +0000

    Add package traveltask-google-artifact-registry/0.0.1/traveltask_google_artifact_registry-0.0.1.tar.gz

---
+++ packages/traveltask-google-artifact-registry/traveltask_google_artifact_registry-0.0.1.tar.gz/traveltask_google_artifact_registry-0.0.1/traveltask_google_artifact_registry/config.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class TaskConfig:
+
+    # Travel parameters
+    context: str
+    task: str
+
+    # Custom parameters
+    project: str
+    region: str
+    repository: str
+    action: str  # Can be upload or install
+    packages: str = None
