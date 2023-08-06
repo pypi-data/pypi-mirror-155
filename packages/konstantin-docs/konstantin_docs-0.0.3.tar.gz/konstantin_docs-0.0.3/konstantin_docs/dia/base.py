bd74832d23cdce04d5a52325e57fde1c51022b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:14 +0000

    Add package konstantin-docs/0.0.3/konstantin_docs-0.0.3.tar.gz

---
+++ packages/konstantin-docs/konstantin_docs-0.0.3.tar.gz/konstantin_docs-0.0.3/konstantin_docs/dia/base.py
@@ -0,0 +1,32 @@
+"""Базовая диаграмма."""
+
+from typing import NamedTuple
+
+
+class Image(NamedTuple):
+    """Изображение с имененем файла."""
+
+    filename: str
+    content: bytes
+
+
+class BaseDiagram:
+    """Базовая диаграмма.
+
+    Все диаграммы должны наследовать от этого класса
+    """
+
+    def __init__(
+        self: "BaseDiagram",
+        filename: str,
+    ) -> None:
+        """Создать объект базовой диаграммы."""
+        self.__filename = filename
+
+    @property
+    def filename(self: "BaseDiagram") -> str:
+        """Возвращает имя файла (без расширения)."""
+        return self.__filename
+
+    def get_images(self: "BaseDiagram") -> tuple[Image]:
+        """Возвращает изображение."""
---
+++ packages/konstantin-docs/konstantin_docs-0.0.3.tar.gz/konstantin_docs-0.0.3/konstantin_docs/dia/c4.py
@@ -0,0 +1,25 @@
+"""Модель C4
+
+Описание - https://c4model.com/
+
+
+Представление для https://github.com/blockdiag/nwdiag.
+
+Примеры диаграмм - http://blockdiag.com/en/nwdiag/nwdiag-examples.html
+"""
+
+from typing import NamedTuple
+
+
+from konstantin_docs.dia.base import BaseDiagram, Image
+from konstantin_docs.service.kroki import (
+    DiagramTypes,
+    OutputFormats,
+    get_image,
+)
+
+
+class C4(BaseDiagram):
+    """Диаграмма C4"""
+
+    pass
