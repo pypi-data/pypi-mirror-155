d2d744794a733a505bf28f211e39295aae2d5cd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:50 +0000

    Add package ignishpc/2.1.0/ignishpc-2.1.0.tar.gz

---
+++ packages/ignishpc/ignishpc-2.1.0.tar.gz/ignishpc-2.1.0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+import ignis.deploy.version as version
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='ignishpc',
+    version=version.__version__,
+    author="cesarpomar",
+    author_email="cesaralfredo.pineiro@usc.es",
+    description='IgnisHPC Deploy',
+    long_description=long_description,
+    url="https://github.com/ignishpc/deploy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux ",
+    ],
+    install_requires=[
+        'docker>=4.1.0',
+        'python-hosts>=1.0',
+        'GitPython'
+    ],
+    entry_points={
+        'console_scripts': ['ignis-deploy=ignis.deploy.deploy:main'],
+    }
+)
