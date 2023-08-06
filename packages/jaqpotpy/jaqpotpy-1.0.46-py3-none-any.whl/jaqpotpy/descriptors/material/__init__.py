b60c8e8faca63bb6a7dcd0fc7c516d1ce1f71caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:15:57 +0000

    Add package jaqpotpy/1.0.46/jaqpotpy-1.0.46-py3-none-any.whl

---
+++ packages/jaqpotpy/jaqpotpy-1.0.46-py3-none-any.whl/jaqpotpy/descriptors/material/__init__.py
@@ -0,0 +1,6 @@
+from jaqpotpy.descriptors.material.geometrical import GeomDescriptors
+from jaqpotpy.descriptors.material.element_property_fingerprint import ElementPropertyFingerprint
+# from jaqpotpy.descriptors.material.graph_data import GraphData
+from jaqpotpy.descriptors.material.cgcnn import CrystalGraphCNN
+from jaqpotpy.descriptors.material.elemnet import ElementNet
+from jaqpotpy.descriptors.material.sine_coulomb_matrix import SineCoulombMatrix
