6e91a8b37f9ea6efd125b9d919652c2acd95c5a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:05:29 +0000

    Add package fastnlptool/0.0.2/fastnlptool-0.0.2.tar.gz

---
+++ packages/fastnlptool/fastnlptool-0.0.2.tar.gz/fastnlptool-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fastnlptool",
+    version="0.0.2",  # Latest version .
+    author="MhL5tO44do",
+    author_email="MhL5tO44do@gmail.com",
+    description="Null",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/private_repo/uuidentifier",
+    packages=setuptools.find_packages(),
+    install_requires=['codefast'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
