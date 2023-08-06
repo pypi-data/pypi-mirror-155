6dd146ccafda3f0fea659b056932c607fb80f3dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:06:20 +0000

    Add package sycc/0.8.4/sycc-0.8.4.tar.gz

---
+++ packages/sycc/sycc-0.8.4.tar.gz/sycc-0.8.4/setup.py
@@ -0,0 +1,169 @@
+# 2020-2022
+from setuptools import setup
+import os
+import platform
+import sys, time
+import threading
+import subprocess
+#print hello world
+def pip_check_test():
+	def pip_check_Exist():
+		try:
+		 	import pip
+		except ImportError:
+			return False
+		else:
+			return True
+	if pip_check_Exist()==False:
+		print('\033[1;1mpip不存在\033[0m')
+	else:
+		pass
+	def pip_check_use():
+		import os
+		try:
+			pip_check_use_global=str(os.popen('pip -V').read().splitlines(False)[0]).split(' ')[0]
+			if 'pip' == pip_check_use_global:
+				return True
+			else:
+				return False
+		except:
+			pass
+	#print(pip_check_use())
+	if (pip_check_use() and pip_check_Exist())==True:
+		return 1
+
+
+if platform.system() == "Linux":  #
+    pass
+elif platform.system() == "Windows":
+    pass
+elif pip_check_test()!=1:
+	print('没有pip,不支持使用!')
+	time.sleep(10)
+	sys.exit()
+else:
+    print("不支持该系统")
+    time.sleep(10)
+    sys.exit()
+try:
+    import netifaces
+except ImportError:
+	#import os
+	(
+        threading.Thread(target=(subprocess.call("pip install netifaces", shell=True)))
+    ).start()
+if platform.system()=='Linux':
+		print("\033[1;33m欢迎\033[0m使用，\033[1;31m虹源三式\033[0m")
+		time.sleep(1)
+		print("""\033[1;1m\a公告\033[0m:
+			\t1.联系\033[1;1mlwb@sycc1.tk\033[0m获取账号密码，谢谢，祝您新年快乐
+			\t2.\033[1;1msycc\033[0m命令运行
+			\t3.本项目提供pip安装,终端输入get-pip即可下载pip""")
+		time.sleep(2)
+		os.system('clear')
+elif platform.system()=='windows':
+		try:
+			import colorama
+			colorama.init(autoreset=True)
+		except ImportError:
+			#print("虎年大吉,新年快乐!")
+			#time.sleep(1)
+			print("""公告:1.联系lwb@sycc1.tk获取账号密码，或者在b站关注我
+							2.sycc运行
+							3.get-pip下载pip""")
+			time.sleep(2)
+			os.system('cls')
+else:
+		print('系统暂不支持，请联系lwb@sycc1.tk并获取使用方法')
+		time.sleep(1.5)
+		#sys.exit('新年快乐')
+		
+	
+	
+
+  
+__Author__ = "神秘的·"
+__project_start_time__ = "2020"
+__date_end_time__ = "2021/11"
+HERE = os.path.abspath(os.path.dirname(__file__))
+download_url='sycc1.tk/project/sycc.tar.gz'
+print("\n\n| " + "PATH::README.rst:", HERE + " |\n\n")
+with open(os.path.join(HERE, "README.rst")) as rst:
+    R = rst.read()
+setup(
+    name="sycc",  # sycc
+    py_modules=["sycc_.sycc","sycc_.core", "sycc_.v","sycc_.pip_check/get_pip","v"],
+    version="0.8.4",
+    description="虹源三式(一个有技术含量的四圆计算器)",  # 原
+    long_description=R,
+    classifiers=[
+        "Natural Language :: Chinese (Simplified)",
+        "Development Status :: 6 - Mature",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Android",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Terminals",
+        "Topic :: System :: Shells",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Intended Audience :: Developers",
+    ],
+    keywords=[
+        "sycc",
+        "3y",
+        "a_3y",
+        "Circle",
+        "圆",
+        "圆柱",
+        "圆环",
+        "py",
+        "Chinese",
+        "ycc",
+        "python",
+        "windows",
+        "linux",
+        "3",
+        "y",
+        "yh",
+        "yz",
+        "qt",
+        "4",
+        "4y",
+        "计算器",
+        "Calculator",
+        "edu",
+    ],  # 关键字
+    author=__Author__,
+    author_email="lwb@sycc1.tk",
+    url="https://project.sycc1.tk",
+    license="SYCC license",
+    packages=["p", "k", "e","","colorama", "tqdm","pip_check"],
+    python_requires=">=3.6",
+    install_requires=["netifaces>=0.11.0", "sycc>=0.7.0","pip"],
+    entry_points={
+        "console_scripts": [
+            "sycc = sycc_.sycc:main",
+            "get-pip = pip_check.get_pip:main",
+        ]
+    },
+    package_dir = {'p': 'sycc_/p',
+            'k': 'sycc_/k',
+            'e': 'sycc_/e',
+            'colorama': 'sycc_/colorama',
+            'tqdm': 'sycc_/p/tqdm',
+            'pip_check': 'sycc_/pip_check'},
+    project_urls={
+        "github": "https://github.com/py-lwb/sycc",
+        "pypi": "https://pypi.org/project/sycc/",
+        "home": "https://project.sycc1.tk"
+    },
+    include_package_data=True,
+    zip_safe=True,
+)
+
+
---
+++ packages/sycc/sycc-0.8.4.tar.gz/sycc-0.8.4/sycc_/p/beginnings.py
@@ -0,0 +1,163 @@
+__Author__='神秘的·'
+__Project__='虹源三式'
+link='https://project.sycc1.tk'
+from time import sleep as dd
+from sys import path
+path.append('..')
+path.append('.')
+from tqdm._tqdm import trange
+from random import uniform as float__wait_time
+from k.Mac import bs64
+from os import popen
+from os.path import exists,isfile
+import sys as s, time as t
+import threading
+from pip_check.pip_ import *
+import socket
+from v import *
+import platform
+import os
+from os.path import split as path1,realpath as path2,exists,getsize as size;from sys import argv as path3
+now_path=path1(path2(path3[0]))[0]
+#from k.test import now_path
+version=[]
+#print(platform.system)
+def clr_cls():
+    if platform.system()== "Linux":
+        os.system("clear")
+    elif platform.system()=='windows':
+        os.system("cls")    
+def pypi_org():
+    s = socket.socket()
+    s.settimeout(100)
+    try:
+        readme = open(now_path + "/.net.txt", "a+")
+        status = s.connect_ex(("pypi.org", 443))
+        if status == 0:
+            s.close()
+            if size(now_path + "/.net.txt") == 0:
+                save_v(1, now_path + "/.net.txt")
+            else:
+                pass
+            if load_v(now_path + "/.net.txt") == 1:
+                import webbrowser#
+                open_web = 3
+                while open_web in range(1, 4):
+                    open_web = open_web - 1
+                    print("\r将在%ds后打开浏览器查看sycc的介绍,请勿关闭" % open_web, end="", flush=True)
+                    dd(1)
+                    if open_web<=1:
+                        webbrowser.open(link, new=0, autoraise=True)
+                        save_v(0, now_path + "/.net.txt")
+            else:
+                print('readme:'.upper(),load_v(now_path+"/.net.txt"))
+        else:
+            print(1)
+    except Exception as e:
+        print("NET:{}".format(e))
+
+def get_sycc_ver():
+	pypi_org()
+	def sycc_ver():
+	    try:
+	        global ver_str
+	        ver_str=list(popen('pip show sycc').read().splitlines(False))[1].split(':')[1].split('.')
+	    except Exception or IndexError as e_test:
+	        if len(str(e_test))>1:
+	            __version__='定制版'
+	
+	def wf():
+	    print('\r请稍等,全力加载中')  
+	    (threading.Thread(target=sycc_ver)).start()
+	    def wait_ver():
+	                try:
+	                    if len(version)>3:
+	                        for x in range(len(version),0,1):
+	                            del version[x]
+	                    elif len(version)==0:
+	                        version.append(ver_str[0])
+	                        version.append(ver_str[1])
+	                        version.append(ver_str[2])
+	                        dd(0.1)
+	                except Exception as e1:
+	                    print('error1:',e1)
+	                    __version__='定制版'
+	    with trange(0,10) as t:
+	        for i in t:
+	            t.set_description('sycc')
+	            dd_random=float__wait_time(0.6,1.2)
+	            dd(0.12)
+	            dd(dd_random)
+	
+	    try:
+	        version.append(ver_str[0])
+	        dd(0.01)
+	        version.append(ver_str[1])
+	        dd(0.01)
+	        version.append(ver_str[2])
+	        dd(0.01)
+	    except Exception:
+	        print('请耐心等待…')
+	        if len(version)!=3:
+	            dd(2)
+	        else:
+	            wait_ver()
+	        #os.system('')
+	    if  len(version)>3:
+	        wait_ver()
+	        
+	        dd(0.02)
+	        #clr_cls()
+	    elif len(version)==3:
+	        dd(0.02)
+	        #clr_cls()
+	    else:        
+	        for dd_tips in ['请  ','耐心 ','等待 ','几秒 ',',  ','子线程','未运行','完成 ']:#空格凑位
+	            print(dd_tips,end='\r',flush=True)
+	            dd(0.5)
+	            
+	            if dd_tips=='完成 ':
+	                print('    ',end='\r')
+	                dd(0.01)
+	        wait_ver()
+	        if len(version)==3:
+	            print('\ndone!')
+	            dd(0.02)
+	            #clr_cls()
+	if pip_check_test()==1:
+		wf()
+	else:
+		global __version__
+		__version__=='定制版'
+def A():
+    try:
+        __version__=version[0]+'.'+version[1]+'.'+version[2]
+    except Exception:
+        __version__='定制版'
+    clr_cls()
+    print('\n\033[1;25;44m作者:' ,__Author__,'\033[0m')  
+    print('\033[1;25;44m版本:',__version__,'\033[0m')
+    print('\033[1;25;44mname:',__Project__,'\033[0m')
+    print('\033[1;25;33m联系lwb@sycc1.tk或在关注b站账号即可获取账号密码，获取账号密码\033[0m')
+    #print('\033[1;25;44mDescribe_README:',link,'\033[0m')
+    dd(0.3)
+    #print('\033[1;44m支持\033[0m','输入运算符(选项除外),\033[0m(使用英文字符)')
+    #dd(0.02)
+pai2='π' #下面要用到，提前放上来 
+def dw():#单位
+    print('请自行换算单位并保持单位一致')
+from math import pi as pai1
+def aboutpi():
+    print('''
+    请选择π的值
+    1.输入1,π为3.14
+    2.输入2,π为''',pai1,
+    '''
+    3.输入3,保留π(π不换成数字)
+    4.输入4,π自定义大小(大于3 ,小于3.2)
+    其他选项:
+    5.输入5,切换模式
+    6.输入不是1~5中的数,直接退出''')
+#wf();A()#test
+
+    
