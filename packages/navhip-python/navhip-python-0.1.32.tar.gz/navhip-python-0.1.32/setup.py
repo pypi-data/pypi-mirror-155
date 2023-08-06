7588c58a2cd2292122ea02cc6e54398dc8e82820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:37 +0000

    Add package navhip-python/0.1.32/navhip-python-0.1.32.tar.gz

---
+++ packages/navhip-python/navhip-python-0.1.32.tar.gz/navhip-python-0.1.32/setup.py
@@ -0,0 +1,31 @@
+#from distribute_setup import use_setuptools
+#use_setuptools()
+
+from setuptools import setup, find_packages
+from os.path import dirname, join
+
+here = dirname(__file__)
+import navhip
+setup(
+    name='navhip-python',
+    version=navhip.__version__,
+    author='NAVhip',
+    author_email='alex@nav.community',
+    description='Python library to communicate with Ledger Nano dongle',
+    long_description=open(join(here, 'README.md')).read(),
+    url='https://github.com/aguycalled/navhip-python',
+    packages=find_packages(),
+    install_requires=['hidapi>=0.7.99', 'ecdsa>=0.9'],
+    extras_require = {
+	'smartcard': [ 'python-pyscard>=1.6.12-4build1' ]
+    },
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+	'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+	'Operating System :: MacOS :: MacOS X'
+    ]
+)
+
