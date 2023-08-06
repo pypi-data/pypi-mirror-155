5d288035b647fc72bbdb03070de76cdaa53c6949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:01 +0000

    Add package cvpr-2022-itt-pkg/0.0.16/cvpr_2022_itt_pkg-0.0.16.tar.gz

---
+++ packages/cvpr-2022-itt-pkg/cvpr_2022_itt_pkg-0.0.16.tar.gz/cvpr_2022_itt_pkg-0.0.16/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+VERSION = '0.0.16'
+
+setuptools.setup(
+    name="cvpr_2022_itt_pkg",
+    version=VERSION,
+    author="blank",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
