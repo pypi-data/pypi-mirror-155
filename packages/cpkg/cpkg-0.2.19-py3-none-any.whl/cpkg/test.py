8c23d83840c8d64c7f7ebbed7e971be27b32d26b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:44:15 +0000

    Add package cpkg/0.2.19/cpkg-0.2.19-py3-none-any.whl

---
+++ packages/cpkg/cpkg-0.2.19-py3-none-any.whl/cpkg/test.py
@@ -0,0 +1,29 @@
+import createPkg as c
+c.create(
+    path='./',
+    dirName='xrew/',
+    pkgName='xrew',
+    author='xiongtianshuo',
+    author_email='Mr_Xiongts@163.com',
+    description='.',
+    url='https://github.com/seoul2k/xrew',
+    project_urls={
+        "Bug Tracker": "https://github.com/seoul2k/xrew/issues",
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries'
+    ],
+    python_requires=2,
+    install_requires=[""]
+)
