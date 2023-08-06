fe479b6d9280f084b7af2a112953d1b68e4b7e9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:31 +0000

    Add package gradio/3.0.19b2/gradio-3.0.19b2-py3-none-any.whl

---
+++ packages/gradio/gradio-3.0.19b2-py3-none-any.whl/gradio/templates/cdn/assets/index.deaf7318.js
@@ -0,0 +1,13 @@
+import { ad as identity } from './index.40d901dd.js';
+
+function fade(node, { delay = 0, duration = 400, easing = identity } = {}) {
+    const o = +getComputedStyle(node).opacity;
+    return {
+        delay,
+        duration,
+        easing,
+        css: t => `opacity: ${t * o}`
+    };
+}
+
+export { fade as f };
---
+++ packages/gradio/gradio-3.0.19b2-py3-none-any.whl/gradio/templates/cdn/assets/index.f5d0bfb5.js
@@ -0,0 +1,5 @@
+export { M as Component } from './index.40d901dd.js';
+
+const modes = ["static"];
+
+export { modes };
