1d481a368c02e0fe3dc4510b6a9f26dc84c52a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:23 +0000

    Add package TxtStyle/1.1.2/TxtStyle-1.1.2.tar.gz

---
+++ packages/TxtStyle/TxtStyle-1.1.2.tar.gz/TxtStyle-1.1.2/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from setuptools import find_packages
+from txtstyle.version import VERSION
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="TxtStyle",
+    version=VERSION,
+    author="Arman Sharif",
+    author_email="armandino@gmail.com",
+    description="Command-line tool for colorizing output of log files and console programs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/armandino/TxtStyle",
+    packages = ["txtstyle"],
+    license="LICENSE.txt",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Utilities",
+    ],
+    python_requires=">=3.2, <4",
+    entry_points={
+        "console_scripts": ["txts = txtstyle.txts:main"],
+    },
+    keywords="color log console text processing",
+    test_suite="tests",
+    project_urls={
+        "Bug Reports": "https://github.com/armandino/TxtStyle/issues",
+        "Source": "https://github.com/armandino/TxtStyle",
+    },
+)
