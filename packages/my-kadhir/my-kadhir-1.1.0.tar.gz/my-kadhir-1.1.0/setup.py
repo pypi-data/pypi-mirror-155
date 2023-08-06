8b1fe426e9dc6fa88893ff3174decb500f674f7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:42 +0000

    Add package my-kadhir/1.1.0/my-kadhir-1.1.0.tar.gz

---
+++ packages/my-kadhir/my-kadhir-1.1.0.tar.gz/my-kadhir-1.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['my_kadhir']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'my-kadhir',
+    'version': '1.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'kadhiresh',
+    'author_email': 'kgajindren@project44.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
