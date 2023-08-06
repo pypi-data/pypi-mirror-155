9093de9b8b6f7a97f2bf37d1f7cd3c64c78c4f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:24 +0000

    Add package strawberry-django-auth/0.3.2.1/strawberry_django_auth-0.3.2.1-py2.py3-none-any.whl

---
+++ packages/strawberry-django-auth/strawberry_django_auth-0.3.2.1-py2.py3-none-any.whl/gqlauth/types.py
@@ -0,0 +1,15 @@
+from uuid import UUID
+import strawberry_django
+import strawberry
+
+from gqlauth import models
+from gqlauth.scalars import image
+
+
+@strawberry_django.type(model=models.Captcha)
+class CaptchaType:
+    uuid: UUID
+
+    @strawberry.field
+    def image(self) -> image:
+        return self.as_bytes()
---
+++ packages/strawberry-django-auth/strawberry_django_auth-0.3.2.1-py2.py3-none-any.whl/gqlauth/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from . import models
+
+
+def activate(request):
+    return render(request, "email/activation_email.html")
