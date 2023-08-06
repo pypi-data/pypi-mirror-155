96b6dd8ee0bc3d4684e7883ab383114cbea8b54c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:17 +0000

    Add package aiotapioca-wrapper/4.0.1/aiotapioca_wrapper-4.0.1-py3-none-any.whl

---
+++ packages/aiotapioca-wrapper/aiotapioca_wrapper-4.0.1-py3-none-any.whl/aiotapioca/adapters/__init__.py
@@ -0,0 +1,25 @@
+from .adapters import (
+    TapiocaAdapter,
+    TapiocaAdapterForm,
+    TapiocaAdapterJSON,
+    TapiocaAdapterPydantic,
+    TapiocaAdapterXML,
+)
+from .mixins import (
+    TapiocaAdapterFormMixin,
+    TapiocaAdapterJSONMixin,
+    TapiocaAdapterPydanticMixin,
+    TapiocaAdapterXMLMixin,
+)
+
+__all__ = (
+    "TapiocaAdapter",
+    "TapiocaAdapterForm",
+    "TapiocaAdapterJSON",
+    "TapiocaAdapterPydantic",
+    "TapiocaAdapterXML",
+    "TapiocaAdapterFormMixin",
+    "TapiocaAdapterJSONMixin",
+    "TapiocaAdapterPydanticMixin",
+    "TapiocaAdapterXMLMixin",
+)
