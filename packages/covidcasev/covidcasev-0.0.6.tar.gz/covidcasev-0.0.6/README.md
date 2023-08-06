7a8b601cf1af02401e8b7b287a4a17b1645c4030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:12 +0000

    Add package covidcasev/0.0.6/covidcasev-0.0.6.tar.gz

---
+++ packages/covidcasev/covidcasev-0.0.6.tar.gz/covidcasev-0.0.6/README.md
@@ -0,0 +1,20 @@
+# covidcasev
+
+<p>The goal of this project is to visualize the number of infected people by country due to Covid-19.</p>
+<p>Using a OWID dataset, covidcasev can visualize the number of infected people by country due to Covid-19.</p>
+The OWID dataset is downloadable from:</p>
+https://github.com/owid/covid-19-data/tree/master/public/data/vaccinations
+
+<img src="https://github.com/yugo-Ishihara/covidcasev/blob/main/result.png">
+
+
+<h1>
+  How to install covidcasev on Linux, MacOS, or WSL on Windows
+</h1>
+$ pip install covidcasev
+
+<h1>
+  How to run covidcasev 
+</h1>
+$ covidcasev country <br>
+ex. $ covidcasev Japan
---
+++ packages/covidcasev/covidcasev-0.0.6.tar.gz/covidcasev-0.0.6/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="covidcasev",
+    version="0.0.6",
+    author="Yugo Ishihara",
+    author_email="s2022041@stu.musashino-u.ac.jp",
+    description="A package for visualizing covid-19's vaccinations and infected people of each countries",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yugo-Ishihara/covidcasev",
+    project_urls={
+        "covidcasev": "https://github.com/yugo-Ishihara/covidcasev",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    py_modules=['covidcasev'],
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    entry_points = {
+        'console_scripts': [
+            'covidcasev = covidcasev:main'
+        ]
+    },
+)
