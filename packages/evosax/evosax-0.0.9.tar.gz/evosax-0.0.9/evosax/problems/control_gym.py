e990b97ba1e3b7703b0a7d854539cfb560d6ac6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:14 +0000

    Add package evosax/0.0.9/evosax-0.0.9.tar.gz

---
+++ packages/evosax/evosax-0.0.9.tar.gz/evosax-0.0.9/evosax/problems/control_gym.py
@@ -0,0 +1,154 @@
+import jax
+import jax.numpy as jnp
+from functools import partial
+from typing import Optional
+import chex
+
+
+class GymFitness(object):
+    def __init__(
+        self,
+        env_name: str = "CartPole-v1",
+        num_env_steps: Optional[int] = None,
+        num_rollouts: int = 16,
+        env_kwargs: dict = {},
+        env_params: dict = {},
+        test: bool = False,
+        n_devices: Optional[int] = None,
+    ):
+        self.env_name = env_name
+        self.num_rollouts = num_rollouts
+        self.test = test
+
+        try:
+            import gymnax
+        except ImportError:
+            raise ImportError(
+                "You need to install `gymnax` to use its fitness rollouts."
+            )
+
+        # Define the RL environment & replace default parameters if desired
+        self.env, self.env_params = gymnax.make(env_name, **env_kwargs)
+        self.env_params.replace(**env_params)
+
+        if num_env_steps is None:
+            self.num_env_steps = self.env_params.max_steps_in_episode
+        else:
+            self.num_env_steps = num_env_steps
+        self.steps_per_member = self.num_env_steps * num_rollouts
+
+        self.action_shape = self.env.num_actions
+        self.input_shape = self.env.observation_space(self.env_params).shape
+        if n_devices is None:
+            self.n_devices = jax.local_device_count()
+        else:
+            self.n_devices = n_devices
+
+    def set_apply_fn(self, map_dict, network_apply, carry_init=None):
+        """Set the network forward function."""
+        self.network = network_apply
+        # Set rollout function based on model architecture
+        if carry_init is not None:
+            self.single_rollout = self.rollout_rnn
+            self.carry_init = carry_init
+        else:
+            self.single_rollout = self.rollout_ffw
+        self.rollout_repeats = jax.vmap(self.single_rollout, in_axes=(0, None))
+        self.rollout_pop = jax.vmap(
+            self.rollout_repeats, in_axes=(None, map_dict)
+        )
+        # pmap over popmembers if > 1 device is available - otherwise pmap
+        if self.n_devices > 1:
+            self.rollout_map = self.rollout_pmap
+            print(
+                f"GymFitness: {self.n_devices} devices detected. Please make"
+                " sure that the ES population size divides evenly across the"
+                " number of devices to pmap/parallelize over."
+            )
+        else:
+            self.rollout_map = self.rollout_pop
+
+    def rollout_pmap(
+        self, rng_input: chex.PRNGKey, policy_params: chex.ArrayTree
+    ):
+        """Parallelize rollout across devices. Split keys/reshape correctly."""
+        keys_pmap = jnp.tile(rng_input, (self.n_devices, 1, 1))
+        rew_dev = jax.pmap(self.rollout_pop)(keys_pmap, policy_params)
+        rew_re = rew_dev.reshape(-1, self.num_rollouts)
+        return rew_re
+
+    @partial(jax.jit, static_argnums=(0,))
+    def rollout(self, rng_input: chex.PRNGKey, policy_params: chex.ArrayTree):
+        """Placeholder fn call for rolling out a population for multi-evals."""
+        rng_pop = jax.random.split(rng_input, self.num_rollouts)
+        return self.rollout_map(rng_pop, policy_params)
+
+    def rollout_ffw(
+        self, rng_input: chex.PRNGKey, policy_params: chex.ArrayTree
+    ):
+        """Rollout an episode with lax.scan."""
+        # Reset the environment
+        rng_reset, rng_episode = jax.random.split(rng_input)
+        obs, state = self.env.reset(rng_reset, self.env_params)
+
+        def policy_step(state_input, tmp):
+            """lax.scan compatible step transition in jax env."""
+            obs, state, policy_params, rng = state_input
+            rng, rng_step, rng_net = jax.random.split(rng, 3)
+            action = self.network(policy_params, obs, rng=rng_net)
+            # action = jnp.nan_to_num(action, nan=0.0)
+            next_o, next_s, reward, done, _ = self.env.step(
+                rng_step, state, action, self.env_params
+            )
+            carry, y = [next_o.squeeze(), next_s, policy_params, rng], [
+                reward,
+                done,
+            ]
+            return carry, y
+
+        # Scan over episode step loop
+        _, scan_out = jax.lax.scan(
+            policy_step,
+            [obs, state, policy_params, rng_episode],
+            [jnp.zeros((self.num_env_steps, 2))],
+        )
+        # Return the sum of rewards accumulated by agent in episode rollout
+        rewards, dones = scan_out[0], scan_out[1]
+        rewards = rewards.reshape(self.num_env_steps, 1)
+        ep_mask = (jnp.cumsum(dones) < 1).reshape(self.num_env_steps, 1)
+        return jnp.sum(rewards * ep_mask)
+
+    def rollout_rnn(
+        self, rng_input: chex.PRNGKey, policy_params: chex.ArrayTree
+    ):
+        """Rollout a jitted episode with lax.scan."""
+        # Reset the environment
+        rng, rng_reset = jax.random.split(rng_input)
+        obs, state = self.env.reset(rng_reset, self.env_params)
+        hidden = self.carry_init()
+
+        def policy_step(state_input, tmp):
+            """lax.scan compatible step transition in jax env."""
+            obs, state, policy_params, rng, hidden = state_input
+            rng, rng_step, rng_net = jax.random.split(rng, 3)
+            hidden, action = self.network(policy_params, obs, hidden, rng_net)
+            next_o, next_s, reward, done, _ = self.env.step(
+                rng_step, state, action, self.env_params
+            )
+            carry, y = [next_o.squeeze(), next_s, policy_params, rng, hidden], [
+                reward,
+                done,
+            ]
+            return carry, y
+
+        # Scan over episode step loop
+        _, scan_out = jax.lax.scan(
+            policy_step,
+            [obs, state, policy_params, rng, hidden],
+            [jnp.zeros((self.num_env_steps, 2))],
+        )
+        # Return masked sum of rewards accumulated by agent in episode
+        rewards, dones = scan_out[0], scan_out[1]
+        rewards = rewards.reshape(self.num_env_steps, 1)
+        ep_mask = (jnp.cumsum(dones) < 1).reshape(self.num_env_steps, 1)
+        return jnp.sum(rewards * ep_mask)
